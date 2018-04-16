ff729315a086733c238d8be0206bb475c6cb2685
Tom Wilkie <tom.wilkie@gmail.com>
2018-04-16 08:08:15 +0100

    Add scaffold and distributor implementation.

--- cmd/distributor/main.go
+++ cmd/distributor/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	log "github.com/sirupsen/logrus"
+	"github.com/weaveworks/common/middleware"
+	"github.com/weaveworks/common/server"
+	"github.com/weaveworks/cortex/pkg/ring"
+	"github.com/weaveworks/cortex/pkg/util"
+	"google.golang.org/grpc"
+
+	"github.com/grafana/logish/pkg/distributor"
+	"github.com/grafana/logish/pkg/logproto"
+)
+
+func main() {
+	var (
+		serverConfig = server.Config{
+			MetricsNamespace: "logish",
+			GRPCMiddleware: []grpc.UnaryServerInterceptor{
+				middleware.ServerUserHeaderInterceptor,
+			},
+		}
+		ringConfig        ring.Config
+		distributorConfig distributor.Config
+	)
+	util.RegisterFlags(&distributorConfig)
+	flag.Parse()
+
+	r, err := ring.New(ringConfig)
+	if err != nil {
+		log.Fatalf("Error initializing ring: %v", err)
+	}
+	defer r.Stop()
+
+	distributor, err := distributor.New(distributorConfig, r)
+	if err != nil {
+		log.Fatalf("Error initializing distributor: %v", err)
+	}
+
+	server, err := server.New(serverConfig)
+	if err != nil {
+		log.Fatalf("Error initializing server: %v", err)
+	}
+	defer server.Shutdown()
+
+	logproto.RegisterAggregatorServer(server.GRPC, distributor)
+	server.Run()
 }
