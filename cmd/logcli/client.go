a34ed1a7a49be5da80d217b42247198183308b12
Tom Wilkie <tom.wilkie@gmail.com>
2018-07-03 18:31:35 +0100

    Add tailing to CLI.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

---
+++ cmd/logcli/client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/grafana/logish/pkg/logproto"
+)
+
+const (
+	queryPath       = "/api/prom/query?query=%s&limit=%d&start=%d&end=%d&direction=%s&regexp=%s"
+	labelsPath      = "/api/prom/label"
+	labelValuesPath = "/api/prom/label/%s/values"
+)
+
+func query(from, through time.Time, direction logproto.Direction) (*logproto.QueryResponse, error) {
+	path := fmt.Sprintf(queryPath, url.QueryEscape(*queryStr), *limit, from.UnixNano(),
+		through.UnixNano(), direction.String(), url.QueryEscape(*regexpStr))
+
+	var resp logproto.QueryResponse
+	if err := doRequest(path, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func listLabelNames() (*logproto.LabelResponse, error) {
+	var labelResponse logproto.LabelResponse
+	if err := doRequest(labelsPath, &labelResponse); err != nil {
+		return nil, err
+	}
+	return &labelResponse, nil
+}
+
+func listLabelValues(name string) (*logproto.LabelResponse, error) {
+	path := fmt.Sprintf(labelValuesPath, url.PathEscape(name))
+	var labelResponse logproto.LabelResponse
+	if err := doRequest(path, &labelResponse); err != nil {
+		return nil, err
+	}
+	return &labelResponse, nil
+}
+
+func doRequest(path string, out interface{}) error {
+	url := *addr + path
+	fmt.Println(url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(*username, *password)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 != 2 {
+		buf, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("Error response from server: %s (%v)", string(buf), err)
+	}
+
+	return json.NewDecoder(resp.Body).Decode(out)
+}
---
+++ cmd/logcli/tail.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"time"
+
+	"github.com/grafana/logish/pkg/iter"
+	"github.com/grafana/logish/pkg/logproto"
+)
+
+const tailIteratorIncrement = 10 * time.Second
+
+func tailQuery() iter.EntryIterator {
+	return &tailIterator{
+		from: time.Now().Add(-tailIteratorIncrement),
+	}
+}
+
+type tailIterator struct {
+	from time.Time
+	err  error
+	iter.EntryIterator
+}
+
+func (t *tailIterator) Next() bool {
+	for t.EntryIterator == nil || !t.EntryIterator.Next() {
+		through, now := t.from.Add(tailIteratorIncrement), time.Now()
+		if through.After(now) {
+			time.Sleep(through.Sub(now))
+		}
+
+		resp, err := query(t.from, through, logproto.FORWARD)
+		if err != nil {
+			t.err = err
+			return false
+		}
+
+		t.from = through
+		t.EntryIterator = iter.NewQueryResponseIterator(resp, logproto.FORWARD)
+	}
+
+	return true
+}
+
+func (t *tailIterator) Entry() logproto.Entry {
+	entry := t.EntryIterator.Entry()
+	t.from = entry.Timestamp.Add(1 * time.Nanosecond)
+	return entry
+}
+
+func (t *tailIterator) Error() error {
+	return t.err
+}
