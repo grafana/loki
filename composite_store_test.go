f67d956b558b8a361d060b214210f9b724e3660a
Tom Wilkie <tom.wilkie@gmail.com>
2018-08-20 20:42:52 +0100

    Reduce some boilerplate in test.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- composite_store_test.go
+++ composite_store_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/weaveworks/common/test"
 )
@@ -171,157 +173,159 @@ func TestCompositeStore(t *testing.T) {
 	}
 }
 
-type dummyStoreAndClient struct {
-	client        StorageClient
-	schemaVersion int
+type dummy struct {
+	version int
+
+	// Include nil-implementations of these interfaces so I don't have to stub out
+	// the methods.
+	StorageClient
+	Store
 }
 
-func (dummyStoreAndClient) Put(ctx context.Context, chunks []Chunk) error { return nil }
-func (dummyStoreAndClient) PutOne(ctx context.Context, from, through model.Time, chunk Chunk) error {
-	return nil
-}
-func (dummyStoreAndClient) Get(tx context.Context, from, through model.Time, matchers ...*labels.Matcher) ([]Chunk, error) {
-	return nil, nil
-}
-func (dummyStoreAndClient) Stop() {}
-
-func (dummyStoreAndClient) NewWriteBatch() WriteBatch                    { return nil }
-func (dummyStoreAndClient) BatchWrite(context.Context, WriteBatch) error { return nil }
-func (dummyStoreAndClient) QueryPages(ctx context.Context, query IndexQuery, callback func(result ReadBatch) (shouldContinue bool)) error {
-	return nil
-}
-func (dummyStoreAndClient) PutChunks(ctx context.Context, chunks []Chunk) error { return nil }
-func (dummyStoreAndClient) GetChunks(ctx context.Context, chunks []Chunk) ([]Chunk, error) {
-	return nil, nil
+func dummySchema(from model.Time, version int) SchemaOpt {
+	return SchemaOpt{
+		From: from,
+		NewStore: func(s StorageClient) (Store, error) {
+			return dummy{
+				version:       version,
+				StorageClient: s,
+			}, nil
+		},
+	}
 }
 
 func TestNewStoreTimeConvergence(t *testing.T) {
-	oldClient, newClient := dummyStoreAndClient{schemaVersion: -1}, dummyStoreAndClient{schemaVersion: -2} // Just to make sure they are not equal.
+	oldClient := dummy{version: -1}
+	newClient := dummy{version: -2}
+	newerClient := dummy{version: -3}
 
 	type expectation struct {
-		time          int64
-		schemaVersion int
-		client        dummyStoreAndClient
+		time   model.Time
+		schema int
+		client StorageClient
 	}
 
-	testcases := []struct {
-		schemaTimes  []int64
-		storageTimes []int64
+	for i, testcase := range []struct {
+		schemaOpts  []SchemaOpt
+		storageOpts []StorageOpt
 
-		exp []expectation
+		expected []expectation
 	}{
 		{
-			schemaTimes:  []int64{0, 10, 20},
-			storageTimes: []int64{0},
-
-			exp: []expectation{
-				{
-					time:          0,
-					schemaVersion: 0,
-					client:        oldClient,
-				},
-				{
-					time:          10,
-					schemaVersion: 1,
-					client:        oldClient,
-				},
-				{
-					time:          20,
-					schemaVersion: 2,
-					client:        oldClient,
-				},
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
 			},
 		},
 		{
-			schemaTimes:  []int64{0, 10, 20, 40},
-			storageTimes: []int64{0, 30},
-
-			exp: []expectation{
-				{
-					time:          0,
-					schemaVersion: 0,
-					client:        oldClient,
-				},
-				{
-					time:          10,
-					schemaVersion: 1,
-					client:        oldClient,
-				},
-				{
-					time:          20,
-					schemaVersion: 2,
-					client:        oldClient,
-				},
-				{
-					time:          30,
-					schemaVersion: 2,
-					client:        newClient,
-				},
-				{
-					time:          40,
-					schemaVersion: 3,
-					client:        newClient,
-				},
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+				dummySchema(10, 2),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 2, oldClient},
 			},
 		},
 		{
-			schemaTimes:  []int64{0, 10, 20, 40},
-			storageTimes: []int64{0, 20},
-
-			exp: []expectation{
-				{
-					time:          0,
-					schemaVersion: 0,
-					client:        oldClient,
-				},
-				{
-					time:          10,
-					schemaVersion: 1,
-					client:        oldClient,
-				},
-				{
-					time:          20,
-					schemaVersion: 2,
-					client:        newClient,
-				},
-				{
-					time:          40,
-					schemaVersion: 3,
-					client:        newClient,
-				},
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+				{10, newClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 1, newClient},
 			},
 		},
+		{
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+				dummySchema(10, 2),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+				{10, newClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 2, newClient},
+			},
+		},
+		{
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+				dummySchema(20, 2),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+				{10, newClient},
+				{30, newerClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 1, newClient},
+				{20, 2, newClient},
+				{30, 2, newerClient},
+			},
+		},
+		{
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+				dummySchema(10, 2),
+				dummySchema(30, 3),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+				{20, newClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 2, oldClient},
+				{20, 2, newClient},
+				{30, 3, newClient},
+			},
+		},
+		{
+			schemaOpts: []SchemaOpt{
+				dummySchema(0, 1),
+				dummySchema(10, 2),
+				dummySchema(20, 3),
+				dummySchema(40, 4),
+			},
+			storageOpts: []StorageOpt{
+				{0, oldClient},
+				{20, newClient},
+			},
+			expected: []expectation{
+				{0, 1, oldClient},
+				{10, 2, oldClient},
+				{20, 3, newClient},
+				{40, 4, newClient},
+			},
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			store, err := NewStore(StoreConfig{}, SchemaConfig{}, testcase.schemaOpts, testcase.storageOpts)
+			require.NoError(t, err)
+			cs := store.(compositeStore)
+			require.Equal(t, len(testcase.expected), len(cs.stores))
+
+			for i, store := range cs.stores {
+				assert.Equal(t, testcase.expected[i].time, store.start, "%d", i)
+				assert.Equal(t, testcase.expected[i].schema, store.Store.(dummy).version, "%d", i)
+				assert.Equal(t, testcase.expected[i].client, store.Store.(dummy).StorageClient, "%d", i)
+			}
+		})
 	}
-
-	for _, testcase := range testcases {
-		storageOpts := []StorageOpt{{model.TimeFromUnix(testcase.storageTimes[0]), oldClient}}
-		if len(testcase.storageTimes) > 1 {
-			storageOpts = append(storageOpts, StorageOpt{model.TimeFromUnix(testcase.storageTimes[1]), newClient})
-		}
-
-		schemaOpts := make([]SchemaOpt, 0, len(testcase.schemaTimes))
-		for i, schemaTime := range testcase.schemaTimes {
-			store := dummyStoreAndClient{schemaVersion: i}
-
-			schemaOpts = append(schemaOpts, SchemaOpt{
-				model.TimeFromUnix(schemaTime),
-				func(storage StorageClient) (Store, error) {
-					store.client = storage
-					return store, nil
-				},
-			})
-		}
-
-		store, err := NewStore(StoreConfig{}, SchemaConfig{}, schemaOpts, storageOpts)
-		require.NoError(t, err)
-		cs := store.(compositeStore)
-		require.Equal(t, len(testcase.exp), len(cs.stores))
-
-		for i, store := range cs.stores {
-			require.Equal(t, model.TimeFromUnix(testcase.exp[i].time), store.start)
-			require.Equal(t, testcase.exp[i].schemaVersion, store.Store.(dummyStoreAndClient).schemaVersion)
-			require.Equal(t, testcase.exp[i].client, store.Store.(dummyStoreAndClient).client)
-		}
-	}
-
 }
