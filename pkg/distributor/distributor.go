grafana/loki

commit a422f394bb4660c98f7d692e16c3cc28747b7abd

Wait for the right number of streams.
Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

pkg/distributor/distributor.go
@@ -116,17 +116,19 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 	// First we flatten out the request into a list of samples.
 	// We use the heuristic of 1 sample per TS to size the array.
 	// We also work out the hash value at the same time.
-	streams := make([]streamTracker, len(req.Streams))
+	streams := make([]streamTracker, 0, len(req.Streams))
 	keys := make([]uint32, 0, len(req.Streams))
 	var validationErr error
-	for i, stream := range req.Streams {
+	for _, stream := range req.Streams {
 		if err := d.validateLabels(userID, stream.Labels); err != nil {
 			validationErr = err
 			continue
 		}
 
 		keys = append(keys, tokenFor(userID, stream.Labels))
-		streams[i].stream = stream
+		streams = append(streams, streamTracker{
+			stream: stream,
+		})
 	}
 
 	if len(streams) == 0 {
@@ -150,7 +152,7 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 	}
 
 	tracker := pushTracker{
-		samplesPending: int32(len(streams)),
+		samplesPending: int32(len(samplesByIngester)),
 		done:           make(chan struct{}),
 		err:            make(chan error),
 	}

commit e5d343fd5f895c749a478c964fa5a0b92ee0a2fb

Track the right number of streams, and cancel the push when the request is cancelled.
This was broken in a422f394bb4660c98f7d692e16c3cc28747b7abd.

Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

pkg/distributor/distributor.go
@@ -152,7 +152,7 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 	}
 
 	tracker := pushTracker{
-		samplesPending: int32(len(samplesByIngester)),
+		samplesPending: int32(len(streams)),
 		done:           make(chan struct{}),
 		err:            make(chan error),
 	}
@@ -173,6 +173,8 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 		return nil, err
 	case <-tracker.done:
 		return &logproto.PushResponse{}, validationErr
+	case <-ctx.Done():
+		return nil, ctx.Err()
 	}
 }
 