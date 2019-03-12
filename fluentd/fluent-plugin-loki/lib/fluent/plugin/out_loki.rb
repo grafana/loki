d0fcfd8e70fed70233d1caccd74f024abb519c5f
Brian Gann <briangann@users.noreply.github.com>
2019-03-12 08:44:03 -0500

    fixes rspec dependencies and rubocop errors (#391)

--- fluentd/fluent-plugin-loki/lib/fluent/plugin/out_loki.rb
+++ fluentd/fluent-plugin-loki/lib/fluent/plugin/out_loki.rb
@@ -142,7 +142,7 @@ module Fluent
           # 'labels' => '{worker="0"}',
           payload.push(
             'labels' => labels_to_protocol(k),
-	    'entries' => v.sort_by { |hsh| Time.parse(hsh["ts"])}
+            'entries' => v.sort_by { |hsh| Time.parse(hsh['ts']) }
           )
         end
         payload
