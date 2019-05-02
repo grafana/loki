b9e5bcad094b6ead993856c6a79c93eb9e2fac0d
Edward Welch <edward.welch@grafana.com>
2019-05-02 12:29:01 -0400

    don't delete elements from a map while iterating over it improve locking of positions

--- pkg/promtail/positions/positions.go
+++ pkg/promtail/positions/positions.go
@@ -84,8 +84,12 @@ func (p *Positions) Get(path string) int64 {
 // Remove removes the position tracking for a filepath
 func (p *Positions) Remove(path string) {
 	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	p.remove(path)
+}
+
+func (p *Positions) remove(path string) {
 	delete(p.positions, path)
-	p.mtx.Unlock()
 }
 
 // SyncPeriod returns how often the positions file gets resynced
@@ -126,11 +130,14 @@ func (p *Positions) save() {
 }
 
 func (p *Positions) cleanup() {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+	toRemove := []string{}
 	for k := range p.positions {
 		if _, err := os.Stat(k); err != nil {
 			if os.IsNotExist(err) {
 				// File no longer exists.
-				p.Remove(k)
+				toRemove = append(toRemove, k)
 				return
 			}
 			// Can't determine if file exists or not, some other error.
@@ -138,6 +145,9 @@ func (p *Positions) cleanup() {
 				"still exists while cleaning positions file", "error", err)
 
 		}
+	}
+	for _, tr := range toRemove {
+		p.remove(tr)
 	}
 }
 
