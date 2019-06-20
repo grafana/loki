e148ca04c206f52eea65e56d56cb831b6978554a
sh0rez <me@shorez.de>
2019-06-20 11:30:54 +0200

    feat(mage): lint, test
    Ports lint and test targets from Makefile

--- magefile.go
+++ magefile.go
@@ -5,4 +5,13 @@ package main
 import (
 	// mage:import
 	_ "github.com/grafana/loki/mage"
+	"github.com/magefile/mage/sh"
 )
+
+func Lint() error {
+	return sh.RunWith(map[string]string{"GOGC": "20"}, "golangci-lint", "run")
+}
+
+func Test() error {
+	return sh.Run("go", "test", "-p=8", "./...")
+}
