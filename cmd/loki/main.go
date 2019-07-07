e9bdc1f851eca552f1e68c221dde3dca697d61d2
sh0rez <me@shorez.de>
2019-07-07 11:32:58 +0200

    Revert "fix(loki): pass missing config error to user"
    This reverts commit b2744fcf427b2b386dce5e999c855ae5c899e4e2, because loki it
    assumed loki was incapable of running without config, which is not the case.

--- cmd/loki/main.go
+++ cmd/loki/main.go
@@ -43,9 +43,11 @@ func main() {
 
 	util.InitLogger(&cfg.Server)
 
-	if err := helpers.LoadConfig(configFile, &cfg); err != nil {
-		level.Error(util.Logger).Log("msg", "error loading config", "filename", configFile, "err", err)
-		os.Exit(1)
+	if configFile != "" {
+		if err := helpers.LoadConfig(configFile, &cfg); err != nil {
+			level.Error(util.Logger).Log("msg", "error loading config", "filename", configFile, "err", err)
+			os.Exit(1)
+		}
 	}
 
 	// Re-init the logger which will now honor a different log level set in cfg.Server
