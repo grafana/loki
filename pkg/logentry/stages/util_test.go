5436f95b5f8edf72c445d1fd8ac870d7e8c7195e
Edward Welch <edward.welch@grafana.com>
2019-07-09 21:25:45 -0400

    use strconf.FormatFloat instead of fmt.Sprintf for converting floats to strings, this way we can eliminate non significant trailing zeros such that the float value 1 would be "1" as a string instead of "1.000000"

--- pkg/logentry/stages/util_test.go
+++ pkg/logentry/stages/util_test.go
@@ -37,3 +37,31 @@ func assertLabels(t *testing.T, expect map[string]string, got model.LabelSet) {
 		assert.Equal(t, model.LabelValue(v), gotV, "mismatch label value")
 	}
 }
+
+// Verify the formatting of float conversion to make sure there are not any trailing zeros,
+// and also make sure unix timestamps are converted properly
+func TestGetString(t *testing.T) {
+	var f64, f64_1 float64
+	var f32 float32
+	f64 = 1
+	f64_1 = 1562723913000
+	f32 = 2.02
+	s64, err := getString(f64)
+	if err != nil {
+		t.Errorf("Failed to get string from float... this shouldn't have happened: %v", err)
+		return
+	}
+	s64_1, err := getString(f64_1)
+	if err != nil {
+		t.Errorf("Failed to get string from float... this shouldn't have happened: %v", err)
+		return
+	}
+	s32, err := getString(f32)
+	if err != nil {
+		t.Errorf("Failed to get string from float... this shouldn't have happened: %v", err)
+		return
+	}
+	assert.Equal(t, "1", s64)
+	assert.Equal(t, "2.02", s32)
+	assert.Equal(t, "1562723913000", s64_1)
+}
