7f27381335eb9ad8d3ceccf6ea19b4c10599e190
Joe Elliott <number101010@gmail.com>
2019-09-11 11:50:12 -0400

    Added a custom marshaller to map entries => values

--- pkg/logproto/marshal.go
+++ pkg/logproto/marshal.go
@@ -17,3 +17,17 @@ func (e *Entry) MarshalJSON() ([]byte, error) {
 	}
 	return []byte(fmt.Sprintf("[%s,%s]", t, l)), nil
 }
+
+// MarshalJSON converts a Stream object to be prom compatible for http queries
+func (s *Stream) MarshalJSON() ([]byte, error) {
+	l, err := json.Marshal(s.Labels)
+	if err != nil {
+		return nil, err
+	}
+	e, err := json.Marshal(s.Entries)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(fmt.Sprintf("{\"labels\":%s,\"values\":%s}", l, e)), nil
+}
