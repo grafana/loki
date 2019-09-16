0769c9bc7f73163684383cd9716c891684276403
Joe Elliott <number101010@gmail.com>
2019-09-16 13:16:41 -0400

    Removed unused testStream
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/loghttp/v1/marshal_test.go
+++ pkg/loghttp/v1/marshal_test.go
@@ -3,7 +3,6 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -296,34 +295,6 @@ func Test_MarshalTailResponse(t *testing.T) {
 		require.NoError(t, err)
 
 		testJSONBytesEqual(t, []byte(tailTest.expected), bytes, "Tail Test %d failed", i)
-	}
-}
-
-func testStream(t *testing.T, expectedValue *logproto.Stream, actualValue map[string]interface{}) {
-	expectedStream := expectedValue
-
-	expectedLabels, err := promql.ParseMetric(expectedStream.Labels)
-	require.NoErrorf(t, err, "Failed to convert string labels to map")
-
-	actualLabels, ok := actualValue["labels"].(map[string]interface{})
-	require.Truef(t, ok, "Failed to convert labels object")
-
-	require.Equalf(t, len(expectedLabels), len(actualLabels), "Labels have different lengths")
-	for _, l := range expectedLabels {
-		require.Equalf(t, l.Value, actualLabels[l.Name], "Label %s has different values", l.Name)
-	}
-
-	entries, ok := actualValue["entries"].([]interface{})
-	require.Truef(t, ok, "Failed to convert entries object on stream")
-	require.Equalf(t, len(expectedStream.Entries), len(entries), "Entries count different on stream")
-
-	for j, entry := range entries {
-		actualEntry, ok := entry.([]interface{})
-		require.Truef(t, ok, "Failed to convert entry object on entry %d", j)
-
-		expectedEntry := expectedStream.Entries[j]
-		require.Equalf(t, expectedEntry.Line, actualEntry[1], "Lines not equal on stream %d", j)
-		require.Equalf(t, fmt.Sprintf("%d", expectedEntry.Timestamp.UnixNano()), actualEntry[0], "Timestamps not equal on stream %d", j)
 	}
 }
 
