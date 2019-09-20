e555529821992584b4b81aeb5ee2c2d381ad6722
Joe Elliott <number101010@gmail.com>
2019-09-20 15:50:57 -0400

    Fixed tailing
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/logcli/query/tail.go
+++ pkg/logcli/query/tail.go
@@ -1,69 +1,74 @@
 package query
 
 import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/fatih/color"
 	"github.com/grafana/loki/pkg/logcli/client"
 	"github.com/grafana/loki/pkg/logcli/output"
+	"github.com/grafana/loki/pkg/loghttp"
 )
 
 // TailQuery connects to the Loki websocket endpoint and tails logs
 func (q *Query) TailQuery(delayFor int, c *client.Client, out output.LogOutput) {
-	/*	conn, err := c.LiveTailQueryConn(q.QueryString, delayFor, q.Limit, q.Start.UnixNano(), q.Quiet)
+	conn, err := c.LiveTailQueryConn(q.QueryString, delayFor, q.Limit, q.Start.UnixNano(), q.Quiet)
+	if err != nil {
+		log.Fatalf("Tailing logs failed: %+v", err)
+	}
+
+	tailReponse := new(loghttp.TailResponse)
+
+	if len(q.IgnoreLabelsKey) > 0 {
+		log.Println("Ignoring labels key:", color.RedString(strings.Join(q.IgnoreLabelsKey, ",")))
+	}
+
+	if len(q.ShowLabelsKey) > 0 {
+		log.Println("Print only labels key:", color.RedString(strings.Join(q.ShowLabelsKey, ",")))
+	}
+
+	for {
+		err := conn.ReadJSON(tailReponse)
 		if err != nil {
-			log.Fatalf("Tailing logs failed: %+v", err)
+			log.Println("Error reading stream:", err)
+			return
 		}
 
-		tailReponse := new(loghttp.TailResponse)
+		labels := loghttp.LabelSet{}
+		for _, stream := range tailReponse.Streams {
+			if !q.NoLabels {
 
-		if len(q.IgnoreLabelsKey) > 0 {
-			log.Println("Ignoring labels key:", color.RedString(strings.Join(q.IgnoreLabelsKey, ",")))
-		}
+				if len(q.IgnoreLabelsKey) > 0 || len(q.ShowLabelsKey) > 0 {
 
-		if len(q.ShowLabelsKey) > 0 {
-			log.Println("Print only labels key:", color.RedString(strings.Join(q.ShowLabelsKey, ",")))
-		}
+					ls := stream.Labels
 
-		for {
-			err := conn.ReadJSON(tailReponse)
-			if err != nil {
-				log.Println("Error reading stream:", err)
-				return
-			}
-
-			labels := ""
-			parsedLabels := promlabels.Labels{}
-			for _, stream := range tailReponse.Streams {
-				if !q.NoLabels {
-
-					if len(q.IgnoreLabelsKey) > 0 || len(q.ShowLabelsKey) > 0 {
-
-						ls := mustParseLabels(stream.GetLabels())
-
-						if len(q.ShowLabelsKey) > 0 {
-							ls = ls.MatchLabels(true, q.ShowLabelsKey...)
-						}
-
-						if len(q.IgnoreLabelsKey) > 0 {
-							ls = ls.MatchLabels(false, q.IgnoreLabelsKey...)
-						}
-
-						labels = ls.String()
-
-					} else {
-						labels = stream.Labels
+					if len(q.ShowLabelsKey) > 0 {
+						ls = matchLabels(true, ls, q.ShowLabelsKey)
 					}
-					parsedLabels = mustParseLabels(labels)
-				}
 
-				for _, entry := range stream.Entries {
-					fmt.Println(out.Format(entry.Timestamp, &parsedLabels, 0, entry.Line))
+					if len(q.IgnoreLabelsKey) > 0 {
+						ls = matchLabels(false, ls, q.ShowLabelsKey)
+					}
+
+					labels = ls
+
+				} else {
+					labels = stream.Labels
 				}
 
 			}
-			if len(tailReponse.DroppedEntries) != 0 {
-				log.Println("Server dropped following entries due to slow client")
-				for _, d := range tailReponse.DroppedEntries {
-					log.Println(d.Timestamp, d.Labels)
-				}
+
+			for _, entry := range stream.Entries {
+				fmt.Println(out.Format(entry.Timestamp, labels, 0, entry.Line))
 			}
-		}*/
+
+		}
+		if len(tailReponse.DroppedStreams) != 0 {
+			log.Println("Server dropped following entries due to slow client")
+			for _, d := range tailReponse.DroppedStreams {
+				log.Println(d.Timestamp, d.Labels)
+			}
+		}
+	}
 }
