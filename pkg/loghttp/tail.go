819874d72a38f51d7319f98f669dd06d22ba7406
Joe Elliott <number101010@gmail.com>
2019-09-20 10:30:53 -0400

    Fixed tail response marshal/unmarshal
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/loghttp/tail.go
+++ pkg/loghttp/tail.go
@@ -3,6 +3,7 @@ package loghttp
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -27,4 +28,28 @@ func (s *DroppedStream) MarshalJSON() ([]byte, error) {
 		Timestamp: fmt.Sprintf("%d", s.Timestamp.UnixNano()),
 		Labels:    s.Labels,
 	})
+}
+
+// UnmarshalJSON implements json.UnMarshaller
+func (s *DroppedStream) UnmarshalJSON(data []byte) error {
+	unmarshal := struct {
+		Timestamp string   `json:"timestamp"`
+		Labels    LabelSet `json:"labels,omitempty"`
+	}{}
+
+	err := json.Unmarshal(data, &unmarshal)
+
+	if err != nil {
+		return err
+	}
+
+	t, err := strconv.ParseInt(unmarshal.Timestamp, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	s.Timestamp = time.Unix(0, t)
+	s.Labels = unmarshal.Labels
+
+	return nil
 }
