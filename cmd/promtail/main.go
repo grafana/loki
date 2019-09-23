96805334edde0642aa558d8339985d6e1dd35b5b
sh0rez <me@shorez.de>
2019-09-23 22:19:51 +0200

    fix(cmd/promtail): remove debug code

--- cmd/promtail/main.go
+++ cmd/promtail/main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"reflect"
 
+	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/version"
-	"github.com/sanity-io/litter"
+	"github.com/weaveworks/common/logging"
 
 	"github.com/grafana/loki/pkg/cfg"
+	"github.com/grafana/loki/pkg/logentry/stages"
+	"github.com/grafana/loki/pkg/promtail"
 	"github.com/grafana/loki/pkg/promtail/config"
 )
 
@@ -29,33 +35,31 @@ func main() {
 		os.Exit(0)
 	}
 
-	litter.Dump(config.ServerConfig)
-
 	// Init the logger which will honor the log level set in cfg.Server
-	// if reflect.DeepEqual(&config.ServerConfig.Config.LogLevel, &logging.Level{}) {
-	// 	level.Error(util.Logger).Log("msg", "invalid log level")
-	// 	os.Exit(1)
-	// }
-	// util.InitLogger(&config.ServerConfig.Config)
+	if reflect.DeepEqual(&config.ServerConfig.Config.LogLevel, &logging.Level{}) {
+		level.Error(util.Logger).Log("msg", "invalid log level")
+		os.Exit(1)
+	}
+	util.InitLogger(&config.ServerConfig.Config)
 
-	// // Set the global debug variable in the stages package which is used to conditionally log
-	// // debug messages which otherwise cause huge allocations processing log lines for log messages never printed
-	// if config.ServerConfig.Config.LogLevel.String() == "debug" {
-	// 	stages.Debug = true
-	// }
+	// Set the global debug variable in the stages package which is used to conditionally log
+	// debug messages which otherwise cause huge allocations processing log lines for log messages never printed
+	if config.ServerConfig.Config.LogLevel.String() == "debug" {
+		stages.Debug = true
+	}
 
-	// p, err := promtail.New(config)
-	// if err != nil {
-	// 	level.Error(util.Logger).Log("msg", "error creating promtail", "error", err)
-	// 	os.Exit(1)
-	// }
+	p, err := promtail.New(config)
+	if err != nil {
+		level.Error(util.Logger).Log("msg", "error creating promtail", "error", err)
+		os.Exit(1)
+	}
 
-	// level.Info(util.Logger).Log("msg", "Starting Promtail", "version", version.Info())
+	level.Info(util.Logger).Log("msg", "Starting Promtail", "version", version.Info())
 
-	// if err := p.Run(); err != nil {
-	// 	level.Error(util.Logger).Log("msg", "error starting promtail", "error", err)
-	// 	os.Exit(1)
-	// }
+	if err := p.Run(); err != nil {
+		level.Error(util.Logger).Log("msg", "error starting promtail", "error", err)
+		os.Exit(1)
+	}
 
-	// p.Shutdown()
+	p.Shutdown()
 }
