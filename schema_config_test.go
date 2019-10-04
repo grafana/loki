edc7020196d3d91d242f60ceb2363b12434f5c51
Marco Pracucci <marco@pracucci.com>
2019-10-04 10:24:07 +0200

    Validate schema config to ensure table period is a multiple of buckets period
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- schema_config_test.go
+++ schema_config_test.go
@@ -288,6 +288,119 @@ func TestChunkTableFor(t *testing.T) {
 	}
 }
 
+func TestSchemaConfig_Validate(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		config   *SchemaConfig
+		expected error
+	}{
+		"should pass the default config (ie. used cortex runs with a target not requiring the schema config)": {
+			config:   &SchemaConfig{},
+			expected: nil,
+		},
+		"should fail on invalid schema version": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{Schema: "v0"},
+				},
+			},
+			expected: errInvalidSchemaVersion,
+		},
+		"should fail on index table period not multiple of 1h for schema v1": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v1",
+						IndexTables: PeriodicTableConfig{Period: 30 * time.Minute},
+					},
+				},
+			},
+			expected: errInvalidTablePeriod,
+		},
+		"should fail on chunk table period not multiple of 1h for schema v1": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v1",
+						IndexTables: PeriodicTableConfig{Period: 6 * time.Hour},
+						ChunkTables: PeriodicTableConfig{Period: 30 * time.Minute},
+					},
+				},
+			},
+			expected: errInvalidTablePeriod,
+		},
+		"should pass on index and chunk table period multiple of 1h for schema v1": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v1",
+						IndexTables: PeriodicTableConfig{Period: 6 * time.Hour},
+						ChunkTables: PeriodicTableConfig{Period: 6 * time.Hour},
+					},
+				},
+			},
+			expected: nil,
+		},
+		"should fail on index table period not multiple of 24h for schema v10": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v10",
+						IndexTables: PeriodicTableConfig{Period: 6 * time.Hour},
+					},
+				},
+			},
+			expected: errInvalidTablePeriod,
+		},
+		"should fail on chunk table period not multiple of 24h for schema v10": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v10",
+						IndexTables: PeriodicTableConfig{Period: 24 * time.Hour},
+						ChunkTables: PeriodicTableConfig{Period: 6 * time.Hour},
+					},
+				},
+			},
+			expected: errInvalidTablePeriod,
+		},
+		"should pass on index and chunk table period multiple of 24h for schema v10": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v10",
+						IndexTables: PeriodicTableConfig{Period: 24 * time.Hour},
+						ChunkTables: PeriodicTableConfig{Period: 24 * time.Hour},
+					},
+				},
+			},
+			expected: nil,
+		},
+		"should pass on index and chunk table period set to zero (no period tables)": {
+			config: &SchemaConfig{
+				Configs: []PeriodConfig{
+					{
+						Schema:      "v10",
+						IndexTables: PeriodicTableConfig{Period: 0},
+						ChunkTables: PeriodicTableConfig{Period: 0},
+					},
+				},
+			},
+			expected: nil,
+		},
+	}
+
+	for testName, testData := range tests {
+		testData := testData
+
+		t.Run(testName, func(t *testing.T) {
+			actual := testData.config.Validate()
+			assert.Equal(t, testData.expected, actual)
+		})
+	}
+}
+
 func MustParseDayTime(s string) DayTime {
 	t, err := time.Parse("2006-01-02", s)
 	if err != nil {
