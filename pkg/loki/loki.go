d3e1af45382f43add8f23866b63809f3a245c40d
Peter Štibraný <peter.stibrany@grafana.com>
2020-01-08 15:33:00 +0100

    Removed extra new line.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -31,21 +31,20 @@ type Config struct {
 	AuthEnabled bool       `yaml:"auth_enabled,omitempty"`
 	HTTPPrefix  string     `yaml:"http_prefix"`
 
-	Server           server.Config            `yaml:"server,omitempty"`
-	Distributor      distributor.Config       `yaml:"distributor,omitempty"`
-	Querier          querier.Config           `yaml:"querier,omitempty"`
-	IngesterClient   client.Config            `yaml:"ingester_client,omitempty"`
-	Ingester         ingester.Config          `yaml:"ingester,omitempty"`
-	StorageConfig    storage.Config           `yaml:"storage_config,omitempty"`
-	ChunkStoreConfig chunk.StoreConfig        `yaml:"chunk_store_config,omitempty"`
-	SchemaConfig     chunk.SchemaConfig       `yaml:"schema_config,omitempty"`
-	LimitsConfig     validation.Limits        `yaml:"limits_config,omitempty"`
-	TableManager     chunk.TableManagerConfig `yaml:"table_manager,omitempty"`
-	Worker           frontend.WorkerConfig    `yaml:"frontend_worker,omitempty"`
-	Frontend         frontend.Config          `yaml:"frontend,omitempty"`
-	QueryRange       queryrange.Config        `yaml:"query_range,omitempty"`
-
-	RuntimeConfig runtimeconfig.ManagerConfig `yaml:"runtime_config,omitempty"`
+	Server           server.Config               `yaml:"server,omitempty"`
+	Distributor      distributor.Config          `yaml:"distributor,omitempty"`
+	Querier          querier.Config              `yaml:"querier,omitempty"`
+	IngesterClient   client.Config               `yaml:"ingester_client,omitempty"`
+	Ingester         ingester.Config             `yaml:"ingester,omitempty"`
+	StorageConfig    storage.Config              `yaml:"storage_config,omitempty"`
+	ChunkStoreConfig chunk.StoreConfig           `yaml:"chunk_store_config,omitempty"`
+	SchemaConfig     chunk.SchemaConfig          `yaml:"schema_config,omitempty"`
+	LimitsConfig     validation.Limits           `yaml:"limits_config,omitempty"`
+	TableManager     chunk.TableManagerConfig    `yaml:"table_manager,omitempty"`
+	Worker           frontend.WorkerConfig       `yaml:"frontend_worker,omitempty"`
+	Frontend         frontend.Config             `yaml:"frontend,omitempty"`
+	QueryRange       queryrange.Config           `yaml:"query_range,omitempty"`
+	RuntimeConfig    runtimeconfig.ManagerConfig `yaml:"runtime_config,omitempty"`
 }
 
 // RegisterFlags registers flag.
