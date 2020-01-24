c7a3ec54f19cdce11fa516cc6f709ba3be551706
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2020-01-24 21:32:15 +0530

    limit for concurrent tail requests (#1562)
    * limit for concurrent tail requests
    
    When a new request comes in, querier would ask one of the active ingesters for count of active tail requests for the user
    If the count exceeds the configured limits for the user then tail request would be rejected right away.
    
    * goimports querier_test.go
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * query all ingesters for their max tail count
    
    Signed-off-by: Edward Welch <edward.welch@grafana.com>
    
    * changed to check only ACTIVE ingesters and tweaked error handling some
    
    Signed-off-by: Edward Welch <edward.welch@grafana.com>
    
    Co-authored-by: Cyril Tovena <cyril.tovena@gmail.com>
    Co-authored-by: Ed Welch <ed@oqqer.com>

--- pkg/ingester/instance.go
+++ pkg/ingester/instance.go
@@ -318,6 +318,23 @@ func (i *instance) addNewTailer(t *tailer) {
 }
 
 func (i *instance) addTailersToNewStream(stream *stream) {
+	i.tailerMtx.RLock()
+	defer i.tailerMtx.RUnlock()
+
+	for _, t := range i.tailers {
+		// we don't want to watch streams for closed tailers.
+		// When a new tail request comes in we will clean references to closed tailers
+		if t.isClosed() {
+			continue
+		}
+
+		if stream.matchesTailer(t) {
+			stream.addTailer(t)
+		}
+	}
+}
+
+func (i *instance) checkClosedTailers() {
 	closedTailers := []uint32{}
 
 	i.tailerMtx.RLock()
@@ -325,10 +342,6 @@ func (i *instance) addTailersToNewStream(stream *stream) {
 		if t.isClosed() {
 			closedTailers = append(closedTailers, t.getID())
 			continue
-		}
-
-		if stream.matchesTailer(t) {
-			stream.addTailer(t)
 		}
 	}
 	i.tailerMtx.RUnlock()
@@ -348,6 +361,15 @@ func (i *instance) closeTailers() {
 	for _, t := range i.tailers {
 		t.close()
 	}
+}
+
+func (i *instance) openTailersCount() uint32 {
+	i.checkClosedTailers()
+
+	i.tailerMtx.RLock()
+	defer i.tailerMtx.RUnlock()
+
+	return uint32(len(i.tailers))
 }
 
 func isDone(ctx context.Context) bool {
