985f2fe229c56ffbf7c6b908f2e8bf94062f2c0c
Cyril Tovena <cyril.tovena@gmail.com>
2020-02-06 17:02:43 -0500

    wip stats frontend
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/querier/queryrange/stats.go
+++ pkg/querier/queryrange/stats.go
@@ -7,10 +7,12 @@ import (
 	"github.com/cortexproject/cortex/pkg/querier/queryrange"
 )
 
-// StatsMiddleware creates a new Middleware that invalidates large queries based on Limits interface.
+// StatsMiddleware creates a new Middleware that recompute the stats summary based on the actual duration of the request.
+// The middleware also register Prometheus metrics.
 func StatsMiddleware() queryrange.Middleware {
 	return queryrange.MiddlewareFunc(func(next queryrange.Handler) queryrange.Handler {
 		return queryrange.HandlerFunc(func(ctx context.Context, req queryrange.Request) (queryrange.Response, error) {
+			//todo span logging.
 			start := time.Now()
 			resp, err := next.Do(ctx, req)
 			if resp != nil {
