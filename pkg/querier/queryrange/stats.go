dbb2b12b59ef2459cda9506e3660b74cdf942e01
Cyril Tovena <cyril.tovena@gmail.com>
2020-02-06 17:00:21 -0500

    wip stats frontend
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

---
+++ pkg/querier/queryrange/stats.go
@@ -0,0 +1,26 @@
+package queryrange
+
+import (
+	"context"
+	"time"
+
+	"github.com/cortexproject/cortex/pkg/querier/queryrange"
+)
+
+// StatsMiddleware creates a new Middleware that invalidates large queries based on Limits interface.
+func StatsMiddleware() queryrange.Middleware {
+	return queryrange.MiddlewareFunc(func(next queryrange.Handler) queryrange.Handler {
+		return queryrange.HandlerFunc(func(ctx context.Context, req queryrange.Request) (queryrange.Response, error) {
+			start := time.Now()
+			resp, err := next.Do(ctx, req)
+			if resp != nil {
+				switch r := resp.(type) {
+				case *LokiResponse:
+				case *LokiPromResponse:
+					r.Statistics.ComputeSummary(time.Since(start))
+				}
+			}
+			return resp, err
+		})
+	})
+}
