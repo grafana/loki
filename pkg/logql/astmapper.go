84fe72cb462e856897883a0c1d0478a6eb30b8a3
Owen Diehl <ow.diehl@gmail.com>
2020-03-22 22:23:47 -0400

    work on sharding mapper

--- pkg/logql/astmapper.go
+++ pkg/logql/astmapper.go
@@ -1,5 +1,12 @@
 package logql
 
+import (
+	"fmt"
+
+	"github.com/cortexproject/cortex/pkg/querier/astmapper"
+	"github.com/pkg/errors"
+)
+
 // ASTMapper is the exported interface for mapping between multiple AST representations
 type ASTMapper interface {
 	Map(Expr) (Expr, error)
@@ -14,31 +21,86 @@ type ShardMapper struct {
 	shards int
 }
 
-func (m ShardMapper) Map(expr Expr) (Expr, error) { return nil, nil }
+func (m ShardMapper) Map(expr Expr) (Expr, error) {
+	switch e := expr.(type) {
+	case *literalExpr:
+		return e, nil
+	case *matchersExpr, *filterExpr:
+		return m.mapLogSelectorExpr(e.(LogSelectorExpr)), nil
+	case *vectorAggregationExpr:
+		return m.mapVectorAggregationExpr(e)
+	case *rangeAggregationExpr:
+		return m.mapRangeAggregationExpr(e)
+	case *binOpExpr:
+		lhsMapped, err := m.Map(e.SampleExpr)
+		if err != nil {
+			return nil, err
+		}
+		rhsMapped, err := m.Map(e.SampleExpr)
+		if err != nil {
+			return nil, err
+		}
+		lhsSampleExpr, ok := lhsMapped.(SampleExpr)
+		if !ok {
+			return nil, badASTMapping("SampleExpr", lhsMapped)
+		}
+		rhsSampleExpr, ok := rhsMapped.(SampleExpr)
+		if !ok {
+			return nil, badASTMapping("SampleExpr", rhsMapped)
+		}
+		e.SampleExpr = lhsSampleExpr
+		e.RHS = rhsSampleExpr
+		return e, nil
+	default:
+		return nil, MapperUnsupportedType(expr, m)
+	}
+}
+
+func (m ShardMapper) mapLogSelectorExpr(expr LogSelectorExpr) LogSelectorExpr {
+	var head *ConcatLogSelectorExpr
+	for i := m.shards - 1; i >= 0; i-- {
+		head = &ConcatLogSelectorExpr{
+			LogSelectorExpr: DownstreamLogSelectorExpr{
+				LogSelectorExpr: expr,
+				shard:           &astmapper.ShardAnnotation{Shard: i, Of: m.shards},
+			},
+			next: head,
+		}
+	}
+
+	return head
+}
 
 // technically, std{dev,var} are also parallelizable if there is no cross-shard merging
 // in descendent nodes in the AST. This optimization is currently avoided for simplicity.
-var parallelOperations = map[string]bool{
-	OpTypeSum:           true,
-	OpTypeAvg:           true,
-	OpTypeMax:           true,
-	OpTypeMin:           true,
-	OpTypeCount:         true,
-	OpTypeBottomK:       true,
-	OpTypeTopK:          true,
-	OpTypeCountOverTime: true,
-	OpTypeRate:          true,
+func (m ShardMapper) mapVectorAggregationExpr(expr *vectorAggregationExpr) (Expr, error) {
+	switch expr.operation {
+	case OpTypeSum:
+	case OpTypeAvg:
+	case OpTypeMax:
+	case OpTypeMin:
+	case OpTypeCount:
+	case OpTypeBottomK:
+	case OpTypeTopK:
+	default:
+		return expr, nil
+	}
 }
 
-func CanParallel(e Expr) bool {
-	switch expr := e.(type) {
-	case *matchersExpr, *filterExpr:
-		return true
-	case *rangeAggregationExpr:
-		return parallelOperations[expr.operation]
-	case *vectorAggregationExpr:
-		return parallelOperations[expr.operation] && CanParallel(expr.left)
+func (m ShardMapper) mapRangeAggregationExpr(expr *rangeAggregationExpr) (Expr, error) {
+	switch expr.operation {
+	case OpTypeCountOverTime:
+	case OpTypeRate:
 	default:
-		return false
+		return expr, nil
 	}
+}
+
+func badASTMapping(expected string, got Expr) error {
+	return fmt.Errorf("Bad AST mapping: expected one type (%s), but got (%T)", expected, got)
+}
+
+// MapperUnsuportedType is a helper for signaling that an evaluator does not support an Expr type
+func MapperUnsupportedType(expr Expr, m ASTMapper) error {
+	return errors.Errorf("unexpected expr type (%T) for ASTMapper type (%T) ", expr, m)
 }
