8a9f4ff2a18ee0a37c9744171781f6b080e75367
Cyril Tovena <cyril.tovena@gmail.com>
2020-04-24 09:58:41 -0400

    Adds object storage client for Swift OpenStack (#2440)
    * Implement swift storage client.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * Pass nil registrerer.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Make lint happy.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    Co-authored-by: Peter Štibraný <peter.stibrany@grafana.com>

--- storage/factory.go
+++ storage/factory.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cortexproject/cortex/pkg/chunk/gcp"
 	"github.com/cortexproject/cortex/pkg/chunk/local"
 	"github.com/cortexproject/cortex/pkg/chunk/objectclient"
+	"github.com/cortexproject/cortex/pkg/chunk/openstack"
 	"github.com/cortexproject/cortex/pkg/chunk/purger"
 	"github.com/cortexproject/cortex/pkg/util"
 )
@@ -65,6 +66,7 @@ type Config struct {
 	CassandraStorageConfig cassandra.Config        `yaml:"cassandra"`
 	BoltDBConfig           local.BoltDBConfig      `yaml:"boltdb"`
 	FSConfig               local.FSConfig          `yaml:"filesystem"`
+	Swift                  openstack.SwiftConfig   `yaml:"swift"`
 
 	IndexCacheValidity time.Duration `yaml:"index_cache_validity"`
 
@@ -83,6 +85,7 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	cfg.BoltDBConfig.RegisterFlags(f)
 	cfg.FSConfig.RegisterFlags(f)
 	cfg.DeleteStoreConfig.RegisterFlags(f)
+	cfg.Swift.RegisterFlags(f)
 
 	f.StringVar(&cfg.Engine, "store.engine", "chunks", "The storage engine to use: chunks or tsdb. Be aware tsdb is experimental and shouldn't be used in production.")
 	cfg.IndexQueriesCacheConfig.RegisterFlagsWithPrefix("store.index-cache-read.", "Cache config for index entry reading. ", f)
@@ -96,6 +99,9 @@ func (cfg *Config) Validate() error {
 	}
 	if err := cfg.CassandraStorageConfig.Validate(); err != nil {
 		return errors.Wrap(err, "invalid Cassandra Storage config")
+	}
+	if err := cfg.Swift.Validate(); err != nil {
+		return errors.Wrap(err, "invalid Swift Storage config")
 	}
 	return nil
 }
@@ -221,6 +227,8 @@ func NewChunkClient(name string, cfg Config, schemaCfg chunk.SchemaConfig) (chun
 		return gcp.NewBigtableObjectClient(context.Background(), cfg.GCPStorageConfig, schemaCfg)
 	case "gcs":
 		return newChunkClientFromStore(gcp.NewGCSObjectClient(context.Background(), cfg.GCSConfig, chunk.DirDelim))
+	case "swift":
+		return newChunkClientFromStore(openstack.NewSwiftObjectClient(cfg.Swift, chunk.DirDelim))
 	case "cassandra":
 		return cassandra.NewStorageClient(cfg.CassandraStorageConfig, schemaCfg)
 	case "filesystem":
@@ -290,6 +298,8 @@ func NewObjectClient(name string, cfg Config) (chunk.ObjectClient, error) {
 		return gcp.NewGCSObjectClient(context.Background(), cfg.GCSConfig, chunk.DirDelim)
 	case "azure":
 		return azure.NewBlobStorage(&cfg.AzureStorageConfig, chunk.DirDelim)
+	case "swift":
+		return openstack.NewSwiftObjectClient(cfg.Swift, chunk.DirDelim)
 	case "inmemory":
 		return chunk.NewMockStorage(), nil
 	case "filesystem":
