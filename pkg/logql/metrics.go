c81be6603ebb89b3fab100b30a48780c959af09c
Cyril Tovena <cyril.tovena@gmail.com>
2020-05-19 10:43:28 -0400

    Fixes unit in stats request log. (#2093)
    * Fix unit conversion in stats log.
    
    🤦
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * Fix the tests because you know ! you know ?
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/logql/metrics.go
+++ pkg/logql/metrics.go
@@ -87,8 +87,8 @@ func RecordMetrics(ctx context.Context, p Params, status string, stats stats.Res
 		"step", p.Step(),
 		"duration", time.Duration(int64(stats.Summary.ExecTime*float64(time.Second))),
 		"status", status,
-		"throughput_mb", float64(stats.Summary.BytesProcessedPerSecond)/10e6,
-		"total_bytes_mb", float64(stats.Summary.TotalBytesProcessed)/10e6,
+		"throughput_mb", float64(stats.Summary.BytesProcessedPerSecond)/1e6,
+		"total_bytes_mb", float64(stats.Summary.TotalBytesProcessed)/1e6,
 	)
 
 	bytesPerSecond.WithLabelValues(status, queryType, rt, latencyType).
