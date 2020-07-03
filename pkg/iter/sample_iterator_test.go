9c0b656abc8827eaa20a2bedd474831161403d8a
Cyril Tovena <cyril.tovena@gmail.com>
2020-07-02 20:45:47 -0400

    Test heap sample iterator.
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/iter/sample_iterator_test.go
+++ pkg/iter/sample_iterator_test.go
@@ -1,12 +1,12 @@
 package iter
 
 import (
+	"context"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/grafana/loki/pkg/logproto"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewPeekingSampleIterator(t *testing.T) {
@@ -72,4 +72,64 @@ func TestNewPeekingSampleIterator(t *testing.T) {
 	}
 	require.NoError(t, iter.Close())
 	require.NoError(t, iter.Error())
+}
+
+func sample(i int) logproto.Sample {
+	return logproto.Sample{
+		Timestamp: int64(i),
+		Hash:      uint64(i),
+		Value:     float64(1),
+	}
+}
+
+func TestNewHeapSampleIterator(t *testing.T) {
+	it := NewHeapSampleIterator(context.Background(),
+		[]SampleIterator{
+			NewSeriesIterator(logproto.Series{
+				Labels: `{foo="var"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="car"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="car"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="var"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="car"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="var"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}), NewSeriesIterator(logproto.Series{
+				Labels: `{foo="car"}`,
+				Samples: []logproto.Sample{
+					sample(1), sample(2), sample(3),
+				},
+			}),
+		})
+
+	for i := 1; i < 4; i++ {
+		require.True(t, it.Next(), i)
+		require.Equal(t, `{foo="car"}`, it.Labels(), i)
+		require.Equal(t, sample(i), it.Sample(), i)
+		require.True(t, it.Next(), i)
+		require.Equal(t, `{foo="var"}`, it.Labels(), i)
+		require.Equal(t, sample(i), it.Sample(), i)
+	}
+
 }
