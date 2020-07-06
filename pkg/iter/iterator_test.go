c9298bcca5fbf87230727c9d1b5a547ceec00806
Cyril Tovena <cyril.tovena@gmail.com>
2020-07-06 14:36:06 -0400

    Improve entry deduplication.
    This PR removes mostcommon and sort insert function in the heap iterator. I discovered while working on #2293 that those are actually not helping since we're deduping those lines anyways. There were no tests checking if deduping was correctly working  so I did added those.
    
    Bonus point this means deduping will run faster and the code is less complex. The only side effect is that the order of entries that are at the same timestamp, before the most common entry would appear first, now we keep the same order as we stored them, which I think is better.
    
    I also change the label ordering, I think whether we are forward or backward we should keep the same aphabetical labels ordering not sure why direction was altering this before.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/iter/iterator_test.go
+++ pkg/iter/iterator_test.go
@@ -3,7 +3,6 @@ package iter
 import (
 	"context"
 	"fmt"
-	"sort"
 	"testing"
 	"time"
 
@@ -231,50 +230,67 @@ func inverse(g generator) generator {
 	}
 }
 
-func TestMostCommon(t *testing.T) {
-	// First is most common.
-	tuples := []tuple{
-		{Entry: logproto.Entry{Line: "a"}},
-		{Entry: logproto.Entry{Line: "b"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "a"}},
-		{Entry: logproto.Entry{Line: "b"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "a"}},
+func TestHeapIteratorDeduplication(t *testing.T) {
+	foo := logproto.Stream{
+		Labels: `{app="foo"}`,
+		Entries: []logproto.Entry{
+			{Timestamp: time.Unix(0, 1), Line: "1"},
+			{Timestamp: time.Unix(0, 2), Line: "2"},
+			{Timestamp: time.Unix(0, 3), Line: "3"},
+		},
 	}
-	require.Equal(t, "a", mostCommon(tuples).Entry.Line)
-
-	tuples = []tuple{
-		{Entry: logproto.Entry{Line: "a"}},
-		{Entry: logproto.Entry{Line: "b"}},
-		{Entry: logproto.Entry{Line: "b"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "d"}},
+	bar := logproto.Stream{
+		Labels: `{app="bar"}`,
+		Entries: []logproto.Entry{
+			{Timestamp: time.Unix(0, 1), Line: "1"},
+			{Timestamp: time.Unix(0, 2), Line: "2"},
+			{Timestamp: time.Unix(0, 3), Line: "3"},
+		},
 	}
-	require.Equal(t, "c", mostCommon(tuples).Entry.Line)
-}
+	assertIt := func(it EntryIterator) {
+		for i := 0; i < 3; i++ {
+			require.True(t, it.Next())
+			require.NoError(t, it.Error())
+			require.Equal(t, bar.Labels, it.Labels())
+			require.Equal(t, bar.Entries[i], it.Entry())
 
-func TestInsert(t *testing.T) {
-	toInsert := []tuple{
-		{Entry: logproto.Entry{Line: "a"}},
-		{Entry: logproto.Entry{Line: "e"}},
-		{Entry: logproto.Entry{Line: "c"}},
-		{Entry: logproto.Entry{Line: "b"}},
-		{Entry: logproto.Entry{Line: "d"}},
-		{Entry: logproto.Entry{Line: "a"}},
-		{Entry: logproto.Entry{Line: "c"}},
+			require.True(t, it.Next())
+			require.NoError(t, it.Error())
+			require.Equal(t, foo.Labels, it.Labels())
+			require.Equal(t, foo.Entries[i], it.Entry())
+
+		}
+		require.False(t, it.Next())
+		require.NoError(t, it.Error())
 	}
+	// forward iteration
+	it := NewHeapIterator(context.Background(), []EntryIterator{
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+	}, logproto.FORWARD)
+	assertIt(it)
 
-	var ts []tuple
-	for _, e := range toInsert {
-		ts = insert(ts, e)
+	// backward iteration
+	it = NewHeapIterator(context.Background(), []EntryIterator{
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+		NewStreamIterator(bar),
+		NewStreamIterator(foo),
+	}, logproto.BACKWARD)
+	// first reverse streams, they should already be correctly ordered for the heap iterator to work.
+	for i, j := 0, len(foo.Entries)-1; i < j; i, j = i+1, j-1 {
+		foo.Entries[i], foo.Entries[j] = foo.Entries[j], foo.Entries[i]
+		bar.Entries[i], bar.Entries[j] = bar.Entries[j], bar.Entries[i]
 	}
-
-	require.True(t, sort.SliceIsSorted(ts, func(i, j int) bool {
-		return ts[i].Line < ts[j].Line
-	}))
+	assertIt(it)
 }
 
 func TestReverseIterator(t *testing.T) {
@@ -288,10 +304,10 @@ func TestReverseIterator(t *testing.T) {
 	for i := int64((testSize / 2) + 1); i <= testSize; i++ {
 		assert.Equal(t, true, reversedIter.Next())
 		assert.Equal(t, identity(i), reversedIter.Entry(), fmt.Sprintln("iteration", i))
-		assert.Equal(t, reversedIter.Labels(), itr1.Labels())
+		assert.Equal(t, reversedIter.Labels(), itr2.Labels())
 		assert.Equal(t, true, reversedIter.Next())
 		assert.Equal(t, identity(i), reversedIter.Entry(), fmt.Sprintln("iteration", i))
-		assert.Equal(t, reversedIter.Labels(), itr2.Labels())
+		assert.Equal(t, reversedIter.Labels(), itr1.Labels())
 	}
 
 	assert.Equal(t, false, reversedIter.Next())
