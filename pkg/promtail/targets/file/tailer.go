62da8bb8584e582e3d9dd52deccc5bc30118f26c
Ed Welch <edward.welch@grafana.com>
2020-08-21 11:13:07 -0400

    restart the tailer if we fail to read and upate current position (#2532)
    (cherry picked from commit b6d9fd5a00d27a597a278c0be442462f9390e57a)

--- pkg/promtail/targets/file/tailer.go
+++ pkg/promtail/targets/file/tailer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-kit/kit/log/level"
 	"github.com/hpcloud/tail"
 	"github.com/prometheus/common/model"
+	"go.uber.org/atomic"
 
 	"github.com/grafana/loki/pkg/promtail/api"
 	"github.com/grafana/loki/pkg/promtail/positions"
@@ -25,8 +26,9 @@ type tailer struct {
 
 	posAndSizeMtx sync.Mutex
 
-	quit chan struct{}
-	done chan struct{}
+	running *atomic.Bool
+	quit    chan struct{}
+	done    chan struct{}
 }
 
 func newTailer(logger log.Logger, handler api.EntryHandler, positions positions.Positions, path string) (*tailer, error) {
@@ -46,9 +48,9 @@ func newTailer(logger log.Logger, handler api.EntryHandler, positions positions.
 	}
 
 	tail, err := tail.TailFile(path, tail.Config{
-		Follow: true,
-		Poll:   true,
-		ReOpen: true,
+		Follow:    true,
+		Poll:      true,
+		ReOpen:    true,
 		MustExist: true,
 		Location: &tail.SeekInfo{
 			Offset: pos,
@@ -64,11 +66,11 @@ func newTailer(logger log.Logger, handler api.EntryHandler, positions positions.
 		logger:    logger,
 		handler:   api.AddLabelsMiddleware(model.LabelSet{FilenameLabel: model.LabelValue(path)}).Wrap(handler),
 		positions: positions,
-
-		path: path,
-		tail: tail,
-		quit: make(chan struct{}),
-		done: make(chan struct{}),
+		path:      path,
+		tail:      tail,
+		running:   atomic.NewBool(false),
+		quit:      make(chan struct{}),
+		done:      make(chan struct{}),
 	}
 	tail.Logger = util.NewLogAdapter(logger)
 
@@ -81,9 +83,20 @@ func (t *tailer) run() {
 	level.Info(t.logger).Log("msg", "start tailing file", "path", t.path)
 	positionSyncPeriod := t.positions.SyncPeriod()
 	positionWait := time.NewTicker(positionSyncPeriod)
+	t.running.Store(true)
 
+	// This function runs in a goroutine, if it exits this tailer will never do any more tailing.
+	// Clean everything up.
 	defer func() {
+		err := t.tail.Stop()
+		if err != nil {
+			level.Error(t.logger).Log("msg", "error stopping tailer when exiting tail goroutine", "path", t.path, "error", err)
+		}
+
 		positionWait.Stop()
+		t.cleanupMetrics()
+		t.running.Store(false)
+
 		close(t.done)
 	}()
 
@@ -92,8 +105,8 @@ func (t *tailer) run() {
 		case <-positionWait.C:
 			err := t.markPositionAndSize()
 			if err != nil {
-				level.Error(t.logger).Log("msg", "error getting tail position and/or size", "path", t.path, "error", err)
-				continue
+				level.Error(t.logger).Log("msg", "error getting tail position and/or size, stopping tailer", "path", t.path, "error", err)
+				return
 			}
 
 		case line, ok := <-t.tail.Lines:
@@ -101,8 +114,10 @@ func (t *tailer) run() {
 				return
 			}
 
+			// Note currently the tail implementation hardcodes Err to nil, this should never hit.
 			if line.Err != nil {
 				level.Error(t.logger).Log("msg", "error reading line", "path", t.path, "error", line.Err)
+				continue
 			}
 
 			readLines.WithLabelValues(t.path).Inc()
@@ -137,25 +152,28 @@ func (t *tailer) markPositionAndSize() error {
 	return nil
 }
 
-func (t *tailer) stop() error {
+func (t *tailer) stop() {
 	// Save the current position before shutting down tailer
 	err := t.markPositionAndSize()
 	if err != nil {
-		level.Error(t.logger).Log("msg", "error getting tail position", "path", t.path, "error", err)
+		level.Error(t.logger).Log("msg", "error marking file position when stopping tailer", "path", t.path, "error", err)
 	}
-	err = t.tail.Stop()
 	close(t.quit)
 	<-t.done
-	filesActive.Add(-1.)
+	level.Info(t.logger).Log("msg", "stopped tailing file", "path", t.path)
+	return
+}
+
+func (t *tailer) isRunning() bool {
+	return t.running.Load()
+}
+
+// cleanupMetrics removes all metrics exported by this tailer
+func (t *tailer) cleanupMetrics() {
 	// When we stop tailing the file, also un-export metrics related to the file
+	filesActive.Add(-1.)
 	readLines.DeleteLabelValues(t.path)
 	readBytes.DeleteLabelValues(t.path)
 	totalBytes.DeleteLabelValues(t.path)
 	logLengthHistogram.DeleteLabelValues(t.path)
-	level.Info(t.logger).Log("msg", "stopped tailing file", "path", t.path)
-	return err
-}
-
-func (t *tailer) cleanup() {
-	t.positions.Remove(t.path)
 }
