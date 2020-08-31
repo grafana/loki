5866136e6c614f6c99885d38a5fe09c4bf4cb043
Champ-Goblem <cameron_mcdermott@yahoo.co.uk>
2020-08-31 11:01:10 +0100

    Add GRPC Middleware

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -181,6 +181,8 @@ func (t *Loki) setupAuthMiddleware() {
 		t.cfg.Server.GRPCStreamMiddleware = append(t.cfg.Server.GRPCStreamMiddleware, GRPCStreamAuthInterceptor)
 		t.httpAuthMiddleware = middleware.AuthenticateUser
 	} else if t.cfg.MultiTenancy.Enabled && t.cfg.MultiTenancy.Type == "label" {
+		t.cfg.Server.GRPCMiddleware = append(t.cfg.Server.GRPCMiddleware, multitenancy.GRPCAuthUnaryMiddleware(t.cfg.MultiTenancy.Label, t.cfg.MultiTenancy.Undefined))
+		t.cfg.Server.GRPCStreamMiddleware = append(t.cfg.Server.GRPCStreamMiddleware, multitenancy.GRPCAuthStreamMiddleware(t.cfg.MultiTenancy.Label, t.cfg.MultiTenancy.Undefined))
 		t.httpAuthMiddleware = multitenancy.HTTPAuthMiddleware(t.cfg.MultiTenancy.Label, t.cfg.MultiTenancy.Undefined)
 	} else {
 		t.cfg.Server.GRPCMiddleware = append(t.cfg.Server.GRPCMiddleware, fakeGRPCAuthUnaryMiddleware)
--- pkg/multitenancy/multitenancy_middleware.go
+++ pkg/multitenancy/multitenancy_middleware.go
@@ -2,10 +2,18 @@ package multitenancy
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"regexp"
 
+	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/weaveworks/common/middleware"
+	"github.com/weaveworks/common/user"
+	"google.golang.org/grpc"
 )
+
+var errUserIDNotPopulated = errors.New("context didnt posess value for either orgID or label and undefined")
 
 // Pretty similar to the fake_auth middleware, we basically just need a way to inform the handler that
 // we intend on using the a label from the data as the orgID rather than from the actual header, and pass
@@ -22,9 +30,61 @@ func GetLabelFromContext(ctx context.Context) (string, string) {
 	label, ok1 := ctx.Value("useLabelAsOrgID").(string)
 	undefined, ok2 := ctx.Value("useLabelAsOrgIDUndefined").(string)
 	if ok1 && ok2 {
-		return undefined, label
+		return label, undefined
 	}
 	return "", ""
+}
+
+// GetUserIDFromContextAndStringLabels returns either just userID aka orgID, or label value or finally undefined if neither exist
+// designed as a drop in upgrade to the original user.ExtractOrgID
+func GetUserIDFromContextAndStringLabels(ctx context.Context, labels string) (string, error) {
+	userID, err := user.ExtractOrgID(ctx)
+	label, undefined := GetLabelFromContext(ctx)
+	if err != nil && (label == "" || undefined == "") {
+		return "", err
+	} else if err == nil && userID == "" && (label == "" || undefined == "") {
+		return "", errUserIDNotPopulated
+	}
+	// Try get the associated label from the stream, otherwise we can use undefined
+	if label != "" {
+		re := regexp.MustCompile(label + "=\"([^ ]+)\"")
+		values := re.FindStringSubmatch(labels)
+		fmt.Println(values)
+		if len(values) == 0 {
+			return undefined, nil
+		} else if len(values) >= 1 {
+			return values[1], nil
+		}
+	} else {
+		return userID, nil
+	}
+	return "", errUserIDNotPopulated
+}
+
+// GetUserIDFromContextAndLabels returns userID from context and prometheus labels.Label array
+func GetUserIDFromContextAndLabels(ctx context.Context, labels []labels.Label) (string, error) {
+	userID, err := user.ExtractOrgID(ctx)
+	label, undefined := GetLabelFromContext(ctx)
+	if err != nil && (label == "" || undefined == "") {
+		return "", err
+	} else if err == nil && userID == "" && (label == "" || undefined == "") {
+		return "", errUserIDNotPopulated
+	}
+	// Try get the associated label from the stream, otherwise we can use undefined
+	if label != "" {
+		value := ""
+		for _, streamLabel := range labels {
+			if streamLabel.Name == label {
+				value = streamLabel.Value
+			}
+		}
+		fmt.Println(value)
+		if value == "" {
+			return value, nil
+		}
+		return value, nil
+	}
+	return userID, nil
 }
 
 // HTTPAuthMiddleware middleware use for injecting value specificying using labels
@@ -36,4 +96,34 @@ func HTTPAuthMiddleware(label string, undefined string) middleware.Func {
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	})
+}
+
+// GRPCAuthUnaryMiddleware auth middleware for GRPC
+func GRPCAuthUnaryMiddleware(label string, undefined string) func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		newCtx := InjectLabelForID(ctx, label, undefined)
+		return handler(newCtx, req)
+	}
+}
+
+// GRPCAuthStreamMiddleware auth middleware for GRPC stream
+func GRPCAuthStreamMiddleware(label string, undefined string) func(srv interface{}, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	return func(srv interface{}, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		ctx := InjectLabelForID(ss.Context(), label, undefined)
+		return handler(srv, serverStream{
+			ctx:          ctx,
+			ServerStream: ss,
+		})
+	}
+}
+
+// Taken from fake_auth
+type serverStream struct {
+	ctx context.Context
+	grpc.ServerStream
+}
+
+// Taken from fake_auth
+func (ss serverStream) Context() context.Context {
+	return ss.ctx
 }
