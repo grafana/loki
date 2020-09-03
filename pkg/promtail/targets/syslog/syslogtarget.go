82a8cf425fcc5d41583bf3d2ac647302cfc67503
Calle Pettersson <carlpett@users.noreply.github.com>
2020-09-03 16:52:41 +0200

    Add a counter for empty syslog messages (#2565)

--- pkg/promtail/targets/syslog/syslogtarget.go
+++ pkg/promtail/targets/syslog/syslogtarget.go
@@ -38,6 +38,11 @@ var (
 		Name:      "syslog_target_parsing_errors_total",
 		Help:      "Total number of parsing errors while receiving syslog messages",
 	})
+	syslogEmptyMessages = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "promtail",
+		Name:      "syslog_empty_messages_total",
+		Help:      "Total number of empty messages receiving from syslog",
+	})
 
 	defaultIdleTimeout = 120 * time.Second
 )
@@ -181,6 +186,7 @@ func (t *SyslogTarget) handleMessage(connLabels labels.Labels, msg syslog.Messag
 	rfc5424Msg := msg.(*rfc5424.SyslogMessage)
 
 	if rfc5424Msg.Message == nil {
+		syslogEmptyMessages.Inc()
 		return
 	}
 
