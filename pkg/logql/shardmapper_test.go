31c26c035930a959bb096e9234f508dcfbd135b0
Cyril Tovena <cyril.tovena@gmail.com>
2020-10-01 22:28:30 +0200

    Correctly setup sharding for logqlv2
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/logql/shardmapper_test.go
+++ pkg/logql/shardmapper_test.go
@@ -152,12 +152,28 @@ func TestMappingStrings(t *testing.T) {
 			out: `sum(max(downstream<rate({foo="bar"}[5m]), shard=0_of_2> ++ downstream<rate({foo="bar"}[5m]), shard=1_of_2>))`,
 		},
 		{
+			in:  `sum(max(rate({foo="bar"} | json | label_format foo=bar [5m])))`,
+			out: `sum(max(rate({foo="bar"} | json | label_format foo=bar [5m])))`,
+		},
+		{
+			in:  `rate({foo="bar"} | json | label_format foo=bar [5m])`,
+			out: `rate({foo="bar"} | json | label_format foo=bar [5m])`,
+		},
+		{
 			in:  `{foo="bar"} |= "id=123"`,
 			out: `downstream<{foo="bar"}|="id=123", shard=0_of_2> ++ downstream<{foo="bar"}|="id=123", shard=1_of_2>`,
 		},
 		{
 			in:  `sum by (cluster) (rate({foo="bar"} |= "id=123" [5m]))`,
 			out: `sum by(cluster)(downstream<sum by(cluster)(rate({foo="bar"}|="id=123"[5m])), shard=0_of_2> ++ downstream<sum by(cluster)(rate({foo="bar"}|="id=123"[5m])), shard=1_of_2>)`,
+		},
+		{
+			in:  `sum by (cluster) (sum_over_time({foo="bar"} |= "id=123" | logfmt | unwrap latency [5m]))`,
+			out: `sum by(cluster)(downstream<sum by(cluster)(sum_over_time({foo="bar"}|="id=123"| logfmt | unwrap latency[5m])), shard=0_of_2> ++ downstream<sum by(cluster)(sum_over_time({foo="bar"}|="id=123"| logfmt | unwrap latency[5m])), shard=1_of_2>)`,
+		},
+		{
+			in:  `sum by (cluster) (stddev_over_time({foo="bar"} |= "id=123" | logfmt | unwrap latency [5m]))`,
+			out: `sum by (cluster) (stddev_over_time({foo="bar"} |= "id=123" | logfmt | unwrap latency [5m]))`,
 		},
 	} {
 		t.Run(tc.in, func(t *testing.T) {
