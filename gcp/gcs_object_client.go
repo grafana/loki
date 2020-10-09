aa567270b778335fab0270c71077729bcd47787e
Marco Pracucci <marco@pracucci.com>
2020-10-09 17:34:46 +0200

    Clarify how to configure auth for BigTable and GCS (#3311)
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- gcp/gcs_object_client.go
+++ gcp/gcs_object_client.go
@@ -32,7 +32,7 @@ func (cfg *GCSConfig) RegisterFlags(f *flag.FlagSet) {
 
 // RegisterFlagsWithPrefix registers flags with prefix.
 func (cfg *GCSConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
-	f.StringVar(&cfg.BucketName, prefix+"gcs.bucketname", "", "Name of GCS bucket to put chunks in.")
+	f.StringVar(&cfg.BucketName, prefix+"gcs.bucketname", "", "Name of GCS bucket. Please refer to https://cloud.google.com/docs/authentication/production for more information about how to configure authentication.")
 	f.IntVar(&cfg.ChunkBufferSize, prefix+"gcs.chunk-buffer-size", 0, "The size of the buffer that GCS client for each PUT request. 0 to disable buffering.")
 	f.DurationVar(&cfg.RequestTimeout, prefix+"gcs.request-timeout", 0, "The duration after which the requests to GCS should be timed out.")
 }
