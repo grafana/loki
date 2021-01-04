a7f5321563ffe5cb570cd15d8c0db511b745cbd4
Owen Diehl <ow.diehl@gmail.com>
2021-01-04 18:47:53 -0500

    Update pkg/ingester/ingester.go
    Co-authored-by: Ed Welch <ed@oqqer.com>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -227,7 +227,7 @@ func (i *Ingester) starting(ctx context.Context) error {
 			i.metrics.walCorruptionsTotal.WithLabelValues(walTypeCheckpoint).Inc()
 			level.Error(util.Logger).Log(
 				"msg",
-				`Recovered from checkpoint with errors. Some streams were likely not recovered due to WAL checkpoint file corruptions. No administrator action is needed and data loss is only a possibility if more than (replication factor / 2 + 1) ingesters suffer from this.`,
+				`Recovered from checkpoint with errors. Some streams were likely not recovered due to WAL checkpoint file corruptions (or WAL file deletions while Loki is running). No administrator action is needed and data loss is only a possibility if more than (replication factor / 2 + 1) ingesters suffer from this.`,
 				"elapsed", time.Since(start).String(),
 			)
 		}
