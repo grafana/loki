825ce3e0d35350bb4f6624923b89c61b237df08f
Cyril Tovena <cyril.tovena@gmail.com>
2021-01-27 16:15:42 +0100

    add manager for non windows

--- pkg/promtail/targets/windows/targetmanager.go
+++ pkg/promtail/targets/windows/targetmanager.go
@@ -1,23 +1,18 @@
-//+build windows
+//+build !windows
 
 package windows
 
 import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/prometheus/client_golang/prometheus"
 
-	"github.com/grafana/loki/pkg/logentry/stages"
 	"github.com/grafana/loki/pkg/promtail/api"
 	"github.com/grafana/loki/pkg/promtail/scrapeconfig"
 	"github.com/grafana/loki/pkg/promtail/targets/target"
 )
 
 // TargetManager manages a series of windows event targets.
-type TargetManager struct {
-	logger  log.Logger
-	targets map[string]*Target
-}
+type TargetManager struct{}
 
 // NewTargetManager creates a new Windows managers.
 func NewTargetManager(
@@ -25,65 +20,18 @@ func NewTargetManager(
 	client api.EntryHandler,
 	scrapeConfigs []scrapeconfig.Config,
 ) (*TargetManager, error) {
-
-	tm := &TargetManager{
-		logger:  logger,
-		targets: make(map[string]*Target),
-	}
-
-	for _, cfg := range scrapeConfigs {
-		registerer := prometheus.DefaultRegisterer
-		pipeline, err := stages.NewPipeline(log.With(logger, "component", "windows_pipeline"), cfg.PipelineStages, &cfg.JobName, registerer)
-		if err != nil {
-			return nil, err
-		}
-
-		t, err := New(logger, pipeline.Wrap(client), cfg.RelabelConfigs, cfg.WindowsConfig)
-		if err != nil {
-			return nil, err
-		}
-
-		tm.targets[cfg.JobName] = t
-	}
-
-	return tm, nil
+	level.Warn(logger).Log("msg", "WARNING!!! Windows target was configured but support for reading the windows event is not compiled into this build of promtail!")
+	return &TargetManager{}, nil
 }
 
 // Ready returns true if at least one Windows target is also ready.
-func (tm *TargetManager) Ready() bool {
-	for _, t := range tm.targets {
-		if t.Ready() {
-			return true
-		}
-	}
-	return false
-}
+func (tm *TargetManager) Ready() bool { return false }
 
 // Stop stops the Windows target manager and all of its targets.
-func (tm *TargetManager) Stop() {
-	for _, t := range tm.targets {
-		if err := t.Stop(); err != nil {
-			level.Error(t.logger).Log("msg", "error stopping windows target", "err", err.Error())
-		}
-	}
-}
+func (tm *TargetManager) Stop() {}
 
 // ActiveTargets returns the list of actuve Windows targets.
-func (tm *TargetManager) ActiveTargets() map[string][]target.Target {
-	result := make(map[string][]target.Target, len(tm.targets))
-	for k, v := range tm.targets {
-		if v.Ready() {
-			result[k] = []target.Target{v}
-		}
-	}
-	return result
-}
+func (tm *TargetManager) ActiveTargets() map[string][]target.Target { return nil }
 
 // AllTargets returns the list of all targets.
-func (tm *TargetManager) AllTargets() map[string][]target.Target {
-	result := make(map[string][]target.Target, len(tm.targets))
-	for k, v := range tm.targets {
-		result[k] = []target.Target{v}
-	}
-	return result
-}
+func (tm *TargetManager) AllTargets() map[string][]target.Target { return nil }
