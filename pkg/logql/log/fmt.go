grafana/loki

commit b1ea8ac050e5c2f740d82b7528204f1cc6295740

support math functions in line_format and label_format (#3434)
* support math functions in line_format and label_format

* fix lint and tests

* added docs and more tests

* update doc

* remove toString and int64

* doc adjustement.

Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

Co-authored-by: Cyril Tovena <cyril.tovena@gmail.com>

pkg/logql/log/fmt.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"text/template"
 	"text/template/parse"
+
+	"github.com/Masterminds/sprig/v3"
 )
 
 var (
@@ -15,7 +17,7 @@ var (
 
 	// Available map of functions for the text template engine.
 	functionMap = template.FuncMap{
-		// olds function deprecated.
+		// olds functions deprecated.
 		"ToLower":    strings.ToLower,
 		"ToUpper":    strings.ToUpper,
 		"Replace":    strings.Replace,
@@ -25,28 +27,6 @@ var (
 		"TrimPrefix": strings.TrimPrefix,
 		"TrimSuffix": strings.TrimSuffix,
 		"TrimSpace":  strings.TrimSpace,
-
-		// New function ported from https://github.com/Masterminds/sprig/
-		// Those function takes the string as the last parameter, allowing pipe chaining.
-		// Example: .mylabel | lower | substring 0 5
-		"lower":      strings.ToLower,
-		"upper":      strings.ToUpper,
-		"title":      strings.Title,
-		"trunc":      trunc,
-		"substr":     substring,
-		"contains":   contains,
-		"hasPrefix":  hasPrefix,
-		"hasSuffix":  hasSuffix,
-		"indent":     indent,
-		"nindent":    nindent,
-		"replace":    replace,
-		"repeat":     repeat,
-		"trim":       strings.TrimSpace,
-		"trimAll":    trimAll,
-		"trimSuffix": trimSuffix,
-		"trimPrefix": trimPrefix,
-
-		// regex functions
 		"regexReplaceAll": func(regex string, s string, repl string) string {
 			r := regexp.MustCompile(regex)
 			return r.ReplaceAllString(s, repl)
@@ -55,6 +35,44 @@ var (
 			r := regexp.MustCompile(regex)
 			return r.ReplaceAllLiteralString(s, repl)
 		},
+	}
+
+	// sprig template functions
+	templateFunctions = []string{
+		"lower",
+		"upper",
+		"title",
+		"trunc",
+		"substr",
+		"contains",
+		"hasPrefix",
+		"hasSuffix",
+		"indent",
+		"nindent",
+		"replace",
+		"repeat",
+		"trim",
+		"trimAll",
+		"trimSuffix",
+		"trimPrefix",
+		"int",
+		"float64",
+		"add",
+		"sub",
+		"mul",
+		"div",
+		"mod",
+		"addf",
+		"subf",
+		"mulf",
+		"divf",
+		"max",
+		"min",
+		"maxf",
+		"minf",
+		"ceil",
+		"floor",
+		"round",
 	}
 )
 
@@ -65,7 +83,7 @@ type LineFormatter struct {
 
 // NewFormatter creates a new log line formatter from a given text template.
 func NewFormatter(tmpl string) (*LineFormatter, error) {
-	t, err := template.New("line").Option("missingkey=zero").Funcs(functionMap).Parse(tmpl)
+	t, err := template.New("line").Option("missingkey=zero").Funcs(templateFunctionMap()).Parse(tmpl)
 	if err != nil {
 		return nil, fmt.Errorf("invalid line template: %s", err)
 	}
@@ -189,7 +207,7 @@ func NewLabelsFormatter(fmts []LabelFmt) (*LabelsFormatter, error) {
 	for _, fm := range fmts {
 		toAdd := labelFormatter{LabelFmt: fm}
 		if !fm.Rename {
-			t, err := template.New("label").Option("missingkey=zero").Funcs(functionMap).Parse(fm.Value)
+			t, err := template.New("label").Option("missingkey=zero").Funcs(templateFunctionMap()).Parse(fm.Value)
 			if err != nil {
 				return nil, fmt.Errorf("invalid template for label '%s': %s", fm.Name, err)
 			}
@@ -298,17 +316,13 @@ func substring(start, end int, s string) string {
 	return string(runes[start:end])
 }
 
-func contains(substr string, str string) bool  { return strings.Contains(str, substr) }
-func hasPrefix(substr string, str string) bool { return strings.HasPrefix(str, substr) }
-func hasSuffix(substr string, str string) bool { return strings.HasSuffix(str, substr) }
-func repeat(count int, str string) string      { return strings.Repeat(str, count) }
-func replace(old, new, src string) string      { return strings.Replace(src, old, new, -1) }
-func trimAll(a, b string) string               { return strings.Trim(b, a) }
-func trimSuffix(a, b string) string            { return strings.TrimSuffix(b, a) }
-func trimPrefix(a, b string) string            { return strings.TrimPrefix(b, a) }
-func indent(spaces int, v string) string {
-	pad := strings.Repeat(" ", spaces)
-	return pad + strings.Replace(v, "\n", "\n"+pad, -1)
+// add sprig template functions maps
+func templateFunctionMap() map[string]interface{} {
+	sprigFuncMap := sprig.GenericFuncMap()
+	for _, v := range templateFunctions {
+		if function, ok := sprigFuncMap[v]; ok {
+			functionMap[v] = function
+		}
+	}
+	return functionMap
 }
-
-func nindent(spaces int, v string) string { return "\n" + indent(spaces, v) }

commit 60c127c54e64fec353a4d29cc5108cbd15c08251

Fixes a race introduced by #3434.
Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

pkg/logql/log/fmt.go
@@ -76,6 +76,15 @@ var (
 	}
 )
 
+func init() {
+	sprigFuncMap := sprig.GenericFuncMap()
+	for _, v := range templateFunctions {
+		if function, ok := sprigFuncMap[v]; ok {
+			functionMap[v] = function
+		}
+	}
+}
+
 type LineFormatter struct {
 	*template.Template
 	buf *bytes.Buffer
@@ -83,7 +92,7 @@ type LineFormatter struct {
 
 // NewFormatter creates a new log line formatter from a given text template.
 func NewFormatter(tmpl string) (*LineFormatter, error) {
-	t, err := template.New("line").Option("missingkey=zero").Funcs(templateFunctionMap()).Parse(tmpl)
+	t, err := template.New("line").Option("missingkey=zero").Funcs(functionMap).Parse(tmpl)
 	if err != nil {
 		return nil, fmt.Errorf("invalid line template: %s", err)
 	}
@@ -207,7 +216,7 @@ func NewLabelsFormatter(fmts []LabelFmt) (*LabelsFormatter, error) {
 	for _, fm := range fmts {
 		toAdd := labelFormatter{LabelFmt: fm}
 		if !fm.Rename {
-			t, err := template.New("label").Option("missingkey=zero").Funcs(templateFunctionMap()).Parse(fm.Value)
+			t, err := template.New("label").Option("missingkey=zero").Funcs(functionMap).Parse(fm.Value)
 			if err != nil {
 				return nil, fmt.Errorf("invalid template for label '%s': %s", fm.Name, err)
 			}
@@ -314,15 +323,4 @@ func substring(start, end int, s string) string {
 		return ""
 	}
 	return string(runes[start:end])
-}
-
-// add sprig template functions maps
-func templateFunctionMap() map[string]interface{} {
-	sprigFuncMap := sprig.GenericFuncMap()
-	for _, v := range templateFunctions {
-		if function, ok := sprigFuncMap[v]; ok {
-			functionMap[v] = function
-		}
-	}
-	return functionMap
 }