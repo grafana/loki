58000105aa65419140d753de90e86acdcee1ec3c
Danny Kopping <danny.kopping@grafana.com>
2021-05-25 22:21:23 +0200

    Reducing redundancy in metric names
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>

--- pkg/ruler/metrics.go
+++ pkg/ruler/metrics.go
@@ -7,24 +7,24 @@ import (
 
 var samplesEvicted = promauto.NewCounterVec(prometheus.CounterOpts{
 	Namespace: "loki",
-	Name:      "recording_rules_queue_samples_evicted_total",
+	Name:      "recording_rules_samples_evicted_total",
 	Help:      "Number of samples evicted from queue; queue is full!",
 }, []string{"user_id", "group_key"})
 
 var samplesQueuedTotal = promauto.NewCounterVec(prometheus.CounterOpts{
 	Namespace: "loki",
-	Name:      "recording_rules_queue_samples_queued_total",
+	Name:      "recording_rules_samples_queued_total",
 	Help:      "Number of samples queued in total.",
 }, []string{"user_id", "group_key"})
 
 var samplesQueued = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Namespace: "loki",
-	Name:      "recording_rules_queue_samples_queued_current",
+	Name:      "recording_rules_samples_queued_current",
 	Help:      "Number of samples queued to be remote-written.",
 }, []string{"user_id", "group_key"})
 
 var samplesQueueCapacity = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Namespace: "loki",
-	Name:      "recording_rules_queue_samples_queue_capacity",
+	Name:      "recording_rules_samples_queue_capacity",
 	Help:      "Number of samples that can be queued before eviction of oldest samples occurs.",
 }, []string{"user_id", "group_key"})
