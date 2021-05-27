128f86d99770eaf74aea39ea7fe5db37f2fe9c7f
kamijin_fanta <kamijin@live.jp>
2021-05-27 18:54:41 +0900

    Fixed Cache fetch error on Redis Cluster (#4056)
    * chunks/cache: redis cluster support
    
    Signed-off-by: kamijin_fanta <kamijin@live.jp>
    
    * add redis client test
    
    Signed-off-by: kamijin_fanta <kamijin@live.jp>
    
    * update miniredis
    
    Signed-off-by: kamijin_fanta <kamijin@live.jp>
    
    * redis_client: changed the test to simple.
    
    Signed-off-by: kamijin_fanta <kamijin@live.jp>
    
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- cache/redis_cache_test.go
+++ cache/redis_cache_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/alicebob/miniredis"
+	"github.com/alicebob/miniredis/v2"
 	"github.com/go-kit/kit/log"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/require"
