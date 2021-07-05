acf31411184598ac71fcea06f3daf3b8b1b3edb7
Cyril Tovena <cyril.tovena@gmail.com>
2021-07-05 11:54:34 +0200

    better bash comment
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- tools/dev/loki-boltdb-storage-s3/compose-up.sh
+++ tools/dev/loki-boltdb-storage-s3/compose-up.sh
@@ -4,8 +4,6 @@ set -e
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
-# -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
-# GOARCH is not changed.
 SRC_DEST=${SCRIPT_DIR}/.src/
 # sync all sources for dlv
 rm -Rf ${SRC_DEST}
@@ -15,12 +13,12 @@ do
     cp -Rf ${SCRIPT_DIR}/../../../${d}/ ${SRC_DEST}/${d}/
 done
 
-# ## build loki
+# build loki -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
 CGO_ENABLED=0 GOOS=linux go build -mod=vendor -gcflags "all=-N -l" -o ${SCRIPT_DIR}/loki ${SCRIPT_DIR}/../../../cmd/loki
 # ## install loki driver to send logs
 docker plugin install grafana/loki-docker-driver:latest --alias loki-compose --grant-all-permissions || true
-# ## build the compose image
+# build the compose image
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml build distributor
-# ## cleanup sources
+# cleanup sources
 rm -Rf ${SRC_DEST}
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up $@
