2d79dbafbf66fa6d07bbfb68de2501c69cad0c53
Cyril Tovena <cyril.tovena@gmail.com>
2021-07-12 12:06:20 +0200

    Reject labels with invalid runes when using implicit extraction parser.
    `json` and `logfmt` parser will attempt to extract all possible keys and values as labels.
    But if a values currently contains the rune error (replace when there's an invalid rune) PromQL parser will fail and the request
    will be fully rejected.
    
    This PR removes those labels values when using those two parsers.
    
    For other parser you can just decide to not extract thoses values.( At least that's my choice for now).
    
    Unfortunatelly this comes with a small cpu overhead prices:
    
    ```
    ~/go/src/github.com/grafana/loki utf8-parser*
    ❯ benchcmp  before.txt after.txt
    benchmark                                    old ns/op     new ns/op     delta
    Benchmark_Parser/json/no_labels_hints-16     3647          4210          +15.44%
    Benchmark_Parser/json/labels_hints-16        2210          2247          +1.67%
    
    benchmark                                    old allocs     new allocs     delta
    Benchmark_Parser/json/no_labels_hints-16     72             72             +0.00%
    Benchmark_Parser/json/labels_hints-16        35             35             +0.00%
    
    benchmark                                    old bytes     new bytes     delta
    Benchmark_Parser/json/no_labels_hints-16     1121          1121          +0.00%
    Benchmark_Parser/json/labels_hints-16        472           472           +0.00%
    ```
    
    Fixes #2859
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/logql/log/parser.go
+++ pkg/logql/log/parser.go
@@ -1,10 +1,13 @@
 package log
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"regexp"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/grafana/loki/pkg/logql/log/jsonexpr"
 	"github.com/grafana/loki/pkg/logql/log/logfmt"
@@ -150,7 +153,12 @@ func (j *JSONParser) RequiredLabelNames() []string { return []string{} }
 func readValue(iter *jsoniter.Iterator) string {
 	switch iter.WhatIsNext() {
 	case jsoniter.StringValue:
-		return iter.ReadString()
+		v := iter.ReadString()
+		// the rune error replacement is rejected by Prometheus, so we skip it.
+		if strings.ContainsRune(v, utf8.RuneError) {
+			return ""
+		}
+		return v
 	case jsoniter.NumberValue:
 		return iter.ReadNumber().String()
 	case jsoniter.BoolValue:
@@ -246,8 +254,12 @@ func (l *LogfmtParser) Process(line []byte, lbs *LabelsBuilder) ([]byte, bool) {
 			continue
 		}
 		key := string(l.dec.Key())
-		val := string(l.dec.Value())
-		addLabel(lbs, key, val)
+		val := l.dec.Value()
+		// the rune error replacement is rejected by Prometheus, so we skip it.
+		if bytes.ContainsRune(val, utf8.RuneError) {
+			val = nil
+		}
+		addLabel(lbs, key, string(val))
 	}
 	if l.dec.Err() != nil {
 		lbs.SetErr(errLogfmt)
