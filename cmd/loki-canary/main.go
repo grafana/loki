3ee8ed3b1d1d7b41563134589a4662b5f412d7ad
Callum Styan <callumstyan@gmail.com>
2021-08-20 05:14:09 -0700

    Allow for loki-canary to generate a percentage of out of order log lines (#4126)
    * Allow for loki-canary to generate a percentage of log lines with out of
    order timestamps, percentage and out of order time range is all
    configurable.
    
    Signed-off-by: Callum Styan <callumstyan@gmail.com>
    
    * Switch OoO min/max flags to time duration and properly calculate OoO
    percentage check.
    
    Signed-off-by: Callum Styan <callumstyan@gmail.com>
    
    * Change some variable names + add comments to make it more obvious what
    various parts of the canary code are actually doing.
    
    Signed-off-by: Callum Styan <callumstyan@gmail.com>
    
    * Update pkg/canary/writer/writer.go
    
    * Add notes about usage of the out-of-order-percentage flag and the requirement to use pipeline stages.
    
    Signed-off-by: Callum Styan <callumstyan@gmail.com>
    
    Co-authored-by: Owen Diehl <ow.diehl@gmail.com>

--- cmd/loki-canary/main.go
+++ cmd/loki-canary/main.go
@@ -43,6 +43,10 @@ func main() {
 	queryTimeout := flag.Duration("query-timeout", 10*time.Second, "How long to wait for a query response from Loki")
 
 	interval := flag.Duration("interval", 1000*time.Millisecond, "Duration between log entries")
+	outOfOrderPercentage := flag.Int("out-of-order-percentage", 0, "Percentage (0-100) of log entries that should be sent out of order.")
+	outOfOrderMin := flag.Duration("out-of-order-min", 30*time.Second, "Minimum amount of time to go back for out of order entries (in seconds).")
+	outOfOrderMax := flag.Duration("out-of-order-max", 60*time.Second, "Maximum amount of time to go back for out of order entries (in seconds).")
+
 	size := flag.Int("size", 100, "Size in bytes of each log line")
 	wait := flag.Duration("wait", 60*time.Second, "Duration to wait for log entries on websocket before querying loki for them")
 	maxWait := flag.Duration("max-wait", 5*time.Minute, "Duration to keep querying Loki for missing websocket entries before reporting them missing")
@@ -74,6 +78,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	if *outOfOrderPercentage < 0 || *outOfOrderPercentage > 100 {
+		_, _ = fmt.Fprintf(os.Stderr, "Out of order percentage must be between 0 and 100\n")
+		os.Exit(1)
+	}
+
 	sentChan := make(chan time.Time)
 	receivedChan := make(chan time.Time)
 
@@ -84,7 +93,7 @@ func main() {
 		c.lock.Lock()
 		defer c.lock.Unlock()
 
-		c.writer = writer.NewWriter(os.Stdout, sentChan, *interval, *size)
+		c.writer = writer.NewWriter(os.Stdout, sentChan, *interval, *outOfOrderMin, *outOfOrderMax, *outOfOrderPercentage, *size)
 		c.reader = reader.NewReader(os.Stderr, receivedChan, *tls, *addr, *user, *pass, *tenantID, *queryTimeout, *lName, *lVal, *sName, *sValue, *interval)
 		c.comparator = comparator.NewComparator(os.Stderr, *wait, *maxWait, *pruneInterval, *spotCheckInterval, *spotCheckMax, *spotCheckQueryRate, *spotCheckWait, *metricTestInterval, *metricTestQueryRange, *interval, *buckets, sentChan, receivedChan, c.reader, true)
 	}
