0094783a1b0958234c622ef644ca47c75527152e
Eran Raichstein <eran.raichstein@gmail.com>
2021-08-23 08:34:15 +0300

    fix issues in example secret + add LOKI_BUCKET_NAME as configurable environment variable (#67)

--- hack/deploy-example-secret.sh
+++ hack/deploy-example-secret.sh
@@ -8,25 +8,23 @@ REGION=""
 ENDPOINT=""
 ACCESS_KEY_ID=""
 SECRET_ACCESS_KEY=""
+LOKI_BUCKET_NAME="${LOKI_BUCKET_NAME:-loki}"
 
 set_credentials_from_aws() {
-  AWS_CONFIG_FILE_NAME=$HOME/.aws/config
-  AWS_CREDENTIALS_FILE_NAME=$HOME/.aws/credentials
-
-  REGION="$(grep -m 1 region < $AWS_CONFIG_FILE_NAME | awk '{print $3}')"
-  ENDPOINT="https://s3.${REGION}.amazonaws.com}"
-  ACCESS_KEY_ID="$(grep -m 1 aws_access_key_id < $AWS_CREDENTIALS_FILE_NAME | awk '{print $3}')"
-  SECRET_ACCESS_KEY="$(grep -m 1 aws_secret_access_key < $AWS_CREDENTIALS_FILE_NAME | awk '{print $3}')"
+  REGION="$(aws configure get region)"
+  ACCESS_KEY_ID="$(aws configure get aws_access_key_id)"
+  SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)"
+  ENDPOINT="https://s3.${REGION}.amazonaws.com"
 }
 
 create_secret() {
   kubectl -n $NAMESPACE delete secret test ||:
   kubectl -n $NAMESPACE create secret generic test \
-    --from-literal=endpoint=$(echo -n "$ENDPOINT" | base64) \
-    --from-literal=region=$(echo -n "$REGION" | base64) \
-    --from-literal=bucketnames=$(echo -n "loki" | base64) \
-    --from-literal=access_key_id=$(echo -n "$ACCESS_KEY_ID" | base64) \
-    --from-literal=access_key_secret=$(echo -n "$SECRET_ACCESS_KEY" | base64)
+    --from-literal=endpoint=$(echo -n "$ENDPOINT") \
+    --from-literal=region=$(echo -n "$REGION") \
+    --from-literal=bucketnames=$(echo -n "$LOKI_BUCKET_NAME") \
+    --from-literal=access_key_id=$(echo -n "$ACCESS_KEY_ID") \
+    --from-literal=access_key_secret=$(echo -n "$SECRET_ACCESS_KEY")
 }
 
 main() {
