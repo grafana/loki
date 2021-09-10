bfb4b00bcaa486e6538bc15fea400f9878e9aec6
Arve Knudsen <arve.knudsen@gmail.com>
2021-09-10 10:01:38 +0200

    Chore: Reformat Go files (#4304)
    * Chore: Reformat Go files
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * Undo change
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- clients/pkg/promtail/targets/windows/win_eventlog/win_eventlog_notwindows.go
+++ clients/pkg/promtail/targets/windows/win_eventlog/win_eventlog_notwindows.go
@@ -19,6 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+//go:build !windows
 // +build !windows
 
 //revive:disable-next-line:var-naming
