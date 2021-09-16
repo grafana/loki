0873d6b6de2a9dc2113bf09d5d83dbb9b951703f
Danny Kopping <danny.kopping@grafana.com>
2021-09-16 09:25:54 +0200

    Warn when an empty limit is skipped
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>

--- pkg/loki/runtime_config.go
+++ pkg/loki/runtime_config.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 
+	util_log "github.com/cortexproject/cortex/pkg/util/log"
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/kv"
 	"github.com/grafana/dskit/runtimeconfig"
 	"gopkg.in/yaml.v2"
@@ -25,6 +27,7 @@ type runtimeConfigValues struct {
 func (r runtimeConfigValues) validate() error {
 	for t, c := range r.TenantLimits {
 		if c == nil {
+			level.Warn(util_log.Logger).Log("msg", "skipping empty tenant limit definition", "tenant", t)
 			continue
 		}
 
