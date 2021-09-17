cfc53fe740324c2cb254cec566e711dedfd288a6
Callum Styan <callumstyan@gmail.com>
2021-09-16 17:10:31 -0700

    Fix small terraform comments.
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- tools/lambda-promtail/main.tf
+++ tools/lambda-promtail/main.tf
@@ -7,49 +7,46 @@ data "aws_region" "current" {}
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "logs" {
-  name   = "lambda-logs"
-  role   = aws_iam_role.iam_for_lambda.name
+  name = "lambda-logs"
+  role = aws_iam_role.iam_for_lambda.name
   policy = jsonencode({
-    "Statement": [
+    "Statement" : [
       {
-        "Action": [
+        "Action" : [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
         ],
-        "Effect": "Allow",
-        "Resource": "arn:aws:logs:*:*:*",
+        "Effect" : "Allow",
+        "Resource" : "arn:aws:logs:*:*:*",
       }
     ]
   })
 }
 
 resource "aws_lambda_function" "lambda_promtail" {
-  image_uri = "your-image-ecr:latest"
+  image_uri     = "your-image-ecr:latest"
   function_name = "lambda_promtail"
   role          = aws_iam_role.iam_for_lambda.arn
 
-  timeout=60
-  memory_size=128
-  package_type="Image"
+  timeout      = 60
+  memory_size  = 128
+  package_type = "Image"
 
   # vpc_config {
   #   # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
@@ -65,22 +62,22 @@ resource "aws_lambda_function" "lambda_promtail" {
 }
 
 resource "aws_lambda_function_event_invoke_config" "lambda_promtail_invoke_config" {
-  function_name                = aws_lambda_function.lambda_promtail.function_name
-  maximum_retry_attempts       = 2
+  function_name          = aws_lambda_function.lambda_promtail.function_name
+  maximum_retry_attempts = 2
 }
 
 resource "aws_lambda_permission" "lambda_promtail_allow_cloudwatch" {
   statement_id  = "lambda-promtail-allow-cloudwatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_promtail.function_name}"
+  function_name = aws_lambda_function.lambda_promtail.function_name
   principal     = "logs.${data.aws_region.current.name}.amazonaws.com"
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "test_lambdafunction_logfilter" {
   name            = "test_lambdafunction_logfilter"
-  log_group_name  = "/aws/lambda/some-lamda-log-group"
-  destination_arn = "${aws_lambda_function.lambda_promtail.arn}"
+  log_group_name  = "/aws/lambda/some-lambda-log-group"
+  destination_arn = aws_lambda_function.lambda_promtail.arn
   # required but can be empty string
   filter_pattern = ""
-  depends_on = [aws_iam_role_policy.logs]
+  depends_on     = [aws_iam_role_policy.logs]
 }
