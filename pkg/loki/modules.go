12253ecf1bfcdbfacef4c88ab0716c47696e08df
Trevor Whitney <trevorjwhitney@gmail.com>
2021-09-17 14:29:30 -0600

    allow ingester and distributor to run on same instance (#4348)
    * allow ingester and distributor to run on same instance
    
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>
    
    * add comment around conditional distributor Push registration
    
    Co-authored-by: Ed Welch <edward.welch@grafana.com>
    
    * lint
    
    Co-authored-by: Ed Welch <edward.welch@grafana.com>

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -173,7 +173,9 @@ func (t *Loki) initDistributor() (services.Service, error) {
 		return nil, err
 	}
 
-	if !t.Cfg.isModuleEnabled(All) {
+	// Register the distributor to receive Push requests over GRPC
+	// EXCEPT when running with `-target=all` or `-target=` contains `ingester`
+	if !t.Cfg.isModuleEnabled(All) && !t.Cfg.isModuleEnabled(Ingester) {
 		logproto.RegisterPusherServer(t.Server.GRPC, t.distributor)
 	}
 
