8f534ea5cd92a0fa593acf2ce3fee3fd0224d9f1
Callum Styan <callumstyan@gmail.com>
2021-09-24 10:59:11 -0700

    Update cloudformation template to also use parameters/env vars.
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- tools/lambda-promtail/variables.tf
+++ tools/lambda-promtail/variables.tf
@@ -13,7 +13,7 @@ variable "log_group_names" {
 variable "lambda_promtail_image" {
   type        = string
   description = "The ECR image URI to pull and use for lambda-promtail."
-  default     = "grafana-lambda-promtail-ecr:latest"
+  default     = ""
 }
 
 variable "username" {
