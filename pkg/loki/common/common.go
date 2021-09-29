81a8b1b3240e007e10f67e0fcb86429ee4ee82cc
Trevor Whitney <trevorjwhitney@gmail.com>
2021-09-29 10:01:57 -0600

    Missed a file, update common config with prefix property
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

--- pkg/loki/common/common.go
+++ pkg/loki/common/common.go
@@ -2,5 +2,5 @@ package common
 
 // Config holds common config that can be shared between multiple other config sections
 type Config struct {
-	Store string // This is just an example, but here we should define all the 'common' config values used to set other Loki values.
+	PathPrefix string `yaml:"path_prefix"`
 }
