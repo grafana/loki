4ed90c2015d4f3377a2ca523e9cfcb21b28d8397
Jordan Rushing <jordan.rushing@grafana.com>
2021-10-21 16:19:19 -0500

    Add more limits to export

--- pkg/validation/exporter.go
+++ pkg/validation/exporter.go
@@ -35,11 +35,27 @@ func (oe *OverridesExporter) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (oe *OverridesExporter) Collect(ch chan<- prometheus.Metric) {
-	// TODO: Add limits. . .
+	// TODO(jordanrushing): Add more limits. . .
 	for tenant, limits := range oe.TenantLimitMap() {
+		// Distributor enforced limits
 		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, limits.IngestionRateMB, "ingestion_rate_mb", tenant)
 		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, limits.IngestionBurstSizeMB, "ingestion_burst_size_mb", tenant)
-		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxQuerySeries), "max_query_series", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxLabelNameLength), "max_label_name_length", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxLabelValueLength), "max_label_value_length", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxLabelNamesPerSeries), "max_label_names_per_series", tenant)
+		// Ingester enforced limits
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxLocalStreamsPerUser), "max_local_streams_per_user", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxGlobalStreamsPerUser), "max_global_streams_per_user", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.PerStreamRateLimit), "per_stream_rate_limit", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.PerStreamRateLimitBurst), "per_stream_rate_limit_burst", tenant)
+		// Querier enforced limits
 		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxChunksPerQuery), "max_chunks_per_query", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxQuerySeries), "max_query_series", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxQueryParallelism), "max_query_parallelism", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.CardinalityLimit), "cardinality_limit", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxStreamsMatchersPerQuery), "max_streams_matchers_per_query", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxConcurrentTailRequests), "max_concurrent_tail_requests", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxEntriesLimitPerQuery), "max_entries_limit_per_query", tenant)
+		ch <- prometheus.MustNewConstMetric(oe.description, prometheus.GaugeValue, float64(limits.MaxQueriersPerTenant), "max_queriers_per_tenant", tenant)
 	}
 }
