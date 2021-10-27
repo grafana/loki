67b83e2f91eb71abf0c168541fbba6e92d06934f
Cyril Tovena <cyril.tovena@gmail.com>
2021-10-27 16:17:27 +0200

    Refactor client configs in Promtail.
    This refactor a big how we mutate the original config for backward compatibility.
    Also add tests on untested code path.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- clients/pkg/promtail/client/logger_test.go
+++ clients/pkg/promtail/client/logger_test.go
@@ -13,14 +13,13 @@ import (
 	"github.com/grafana/loki/clients/pkg/promtail/api"
 
 	"github.com/grafana/loki/pkg/logproto"
-	"github.com/grafana/loki/pkg/util/flagext"
 )
 
 func TestNewLogger(t *testing.T) {
-	_, err := NewLogger(nil, util_log.Logger, flagext.LabelSet{}, []Config{}...)
+	_, err := NewLogger(nil, util_log.Logger, []Config{}...)
 	require.Error(t, err)
 
-	l, err := NewLogger(nil, util_log.Logger, flagext.LabelSet{}, []Config{{URL: cortexflag.URLValue{URL: &url.URL{Host: "string"}}}}...)
+	l, err := NewLogger(nil, util_log.Logger, []Config{{URL: cortexflag.URLValue{URL: &url.URL{Host: "string"}}}}...)
 	require.NoError(t, err)
 	l.Chan() <- api.Entry{Labels: model.LabelSet{"foo": "bar"}, Entry: logproto.Entry{Timestamp: time.Now(), Line: "entry"}}
 	l.Stop()
