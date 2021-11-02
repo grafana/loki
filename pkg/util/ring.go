8c93239a6a6aeac51e96bcbe653275f6fbd007fa
Edward Welch <edward.welch@grafana.com>
2021-11-02 12:11:57 -0400

    make sure we send a shutdown message to frontends from the scheduler when the ReplicationSet changes so that inflight queries are canceled and retried in the frontends.

--- pkg/util/ring.go
+++ pkg/util/ring.go
@@ -1,6 +1,10 @@
 package util
 
-import "hash/fnv"
+import (
+	"hash/fnv"
+
+	"github.com/cortexproject/cortex/pkg/ring"
+)
 
 // TokenFor generates a token used for finding ingesters from ring
 func TokenFor(userID, labels string) uint32 {
@@ -8,4 +12,22 @@ func TokenFor(userID, labels string) uint32 {
 	_, _ = h.Write([]byte(userID))
 	_, _ = h.Write([]byte(labels))
 	return h.Sum32()
+}
+
+// IsInReplicationSet will query the provided ring for the provided key
+// and see if the provided address is in the resulting ReplicationSet
+func IsInReplicationSet(r *ring.Ring, ringKey uint32, address string) (bool, error) {
+	bufDescs, bufHosts, bufZones := ring.MakeBuffersForGet()
+	rs, err := r.Get(ringKey, ring.WriteNoExtend, bufDescs, bufHosts, bufZones)
+	if err != nil {
+		return false, err
+	}
+
+	addrs := rs.GetAddresses()
+	for _, a := range addrs {
+		if a == address {
+			return true, nil
+		}
+	}
+	return false, nil
 }
