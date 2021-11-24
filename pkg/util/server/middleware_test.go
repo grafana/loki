b0af928b4632a4b347d3ff885756ebc5766a7f93
Kaviraj <kavirajkanagaraj@gmail.com>
2021-11-24 20:08:14 +0100

    Add bit more test cases

--- pkg/util/server/middleware_test.go
+++ pkg/util/server/middleware_test.go
@@ -14,33 +14,35 @@ import (
 
 func TestQueryTags(t *testing.T) {
 	for _, tc := range []struct {
-		desc        string
-		method      string
-		headerValue string
-		body        io.Reader
-		error       bool
+		desc  string
+		in    string
+		exp   string
+		error bool
 	}{
 		{
-			desc:        "single-value",
-			method:      "GET",
-			headerValue: `Source=logvolhist`,
-			body:        nil,
+			desc: "single-value",
+			in:   `Source=logvolhist`,
+			exp:  `Source=logvolhist`,
 		},
 		{
-			desc:        "multiple-values",
-			method:      "POST",
-			headerValue: `Source=logvolhist,Statate=beta`,
-			body:        nil,
+			desc: "multiple-values",
+			in:   `Source=logvolhist,Statate=beta`,
+			exp:  `Source=logvolhist,Statate=beta`,
+		},
+		{
+			desc: "remove-invalid-chars",
+			in:   `Source=log+volhi\\st,Statate=be$ta`,
+			exp:  `Source=logvolhist,Statate=beta`,
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			req := httptest.NewRequest(tc.method, "http://testing.com", nil)
-			req.Header.Set(string(QueryTagsHTTPHeader), tc.headerValue)
+			req := httptest.NewRequest("GET", "http://testing.com", nil)
+			req.Header.Set(string(QueryTagsHTTPHeader), tc.in)
 
 			w := httptest.NewRecorder()
 			checked := false
 			mware := ExtractQueryTagsMiddleware().Wrap(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				require.Equal(t, tc.headerValue, req.Context().Value(QueryTagsHTTPHeader).(string))
+				require.Equal(t, tc.exp, req.Context().Value(QueryTagsHTTPHeader).(string))
 				checked = true
 			}))
 
