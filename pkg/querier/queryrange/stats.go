0e65c753d6be32ab6514dc1fcf503eb958539349
Dylan Guedes <djmgguedes@gmail.com>
2021-12-13 16:11:30 -0300

    Skip computing statistics for unsupported types.

--- pkg/querier/queryrange/stats.go
+++ pkg/querier/queryrange/stats.go
@@ -100,11 +100,15 @@ func StatsCollectorMiddleware() queryrange.Middleware {
 					level.Warn(logger).Log("msg", fmt.Sprintf("cannot compute stats, unexpected type: %T", resp))
 				}
 			}
-			if statistics != nil {
-				// Re-calculate the summary then log and record metrics for the current query
-				statistics.ComputeSummary(time.Since(start))
-				statistics.Log(level.Debug(logger))
+
+			// type doesn't support statistics, skipping stats collection.
+			if statistics == nil {
+				return resp, err
 			}
+
+			statistics.ComputeSummary(time.Since(start))
+			statistics.Log(level.Debug(logger))
+
 			ctxValue := ctx.Value(ctxKey)
 			if data, ok := ctxValue.(*queryData); ok {
 				data.recorded = true
