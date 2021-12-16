0b95a0e1ba4db51c3373622e2609b9e03136d08d
Cyril Tovena <cyril.tovena@gmail.com>
2021-12-16 13:47:58 +0100

    Update pkg/storage/chunk/gcp/gcs_object_client.go
    Co-authored-by: Danny Kopping <dannykopping@gmail.com>

--- pkg/storage/chunk/gcp/gcs_object_client.go
+++ pkg/storage/chunk/gcp/gcs_object_client.go
@@ -48,8 +48,8 @@ func (cfg *GCSConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
 	f.StringVar(&cfg.BucketName, prefix+"gcs.bucketname", "", "Name of GCS bucket. Please refer to https://cloud.google.com/docs/authentication/production for more information about how to configure authentication.")
 	f.IntVar(&cfg.ChunkBufferSize, prefix+"gcs.chunk-buffer-size", 0, "The size of the buffer that GCS client for each PUT request. 0 to disable buffering.")
 	f.DurationVar(&cfg.RequestTimeout, prefix+"gcs.request-timeout", 0, "The duration after which the requests to GCS should be timed out.")
-	f.BoolVar(&cfg.EnableOpenCensus, prefix+"gcs.enable-opencensus", true, "Enabled OpenCensus (OC) instrumentation for all requests.")
-	f.BoolVar(&cfg.EnableHTTP2, prefix+"gcs.enable-http2", true, "Enabled HTTP2 connections.")
+	f.BoolVar(&cfg.EnableOpenCensus, prefix+"gcs.enable-opencensus", true, "Enable OpenCensus (OC) instrumentation for all requests.")
+	f.BoolVar(&cfg.EnableHTTP2, prefix+"gcs.enable-http2", true, "Enable HTTP2 connections.")
 }
 
 func (cfg *GCSConfig) ToCortexGCSConfig() cortex_gcp.GCSConfig {
