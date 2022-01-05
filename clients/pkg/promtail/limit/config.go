73cd3be609bbb20704f1b07f65c8cbaf55dfc4cc
李国忠 <249032432@qq.com>
2022-01-06 00:01:46 +0800

    Update clients/pkg/promtail/limit/config.go
    Co-authored-by: Cyril Tovena <cyril.tovena@gmail.com>

--- clients/pkg/promtail/limit/config.go
+++ clients/pkg/promtail/limit/config.go
@@ -14,6 +14,6 @@ type Config struct {
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
 	f.Float64Var(&cfg.ReadlineRate, prefix+"limit.readline-rate", 10000, "promtail readline Rate.")
 	f.IntVar(&cfg.ReadlineBurst, prefix+"limit.readline-burst", 10000, "promtail readline Burst.")
-	f.BoolVar(&cfg.ReadlineRateEnabled, prefix+"limit.readline-rate-enabled", true, "Set to false to disable readline rate limit.")
+	f.BoolVar(&cfg.ReadlineRateEnabled, prefix+"limit.readline-rate-enabled", false, "Set to false to disable readline rate limit.")
 	f.BoolVar(&cfg.ReadlineRateDrop, prefix+"limit.readline-rate-drop", true, "Set to true to drop log when rate limit.")
 }
