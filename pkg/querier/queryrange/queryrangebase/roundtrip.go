fda12fd60c89c73fb2b4be56e32b7be6af45eb2c
Dylan Guedes <djmgguedes@gmail.com>
2022-01-10 13:30:02 -0300

    Use `_ = ...` syntax instead of comment to fix lint.

--- pkg/querier/queryrange/queryrangebase/roundtrip.go
+++ pkg/querier/queryrange/queryrangebase/roundtrip.go
@@ -297,7 +297,7 @@ func (q roundTripper) Do(ctx context.Context, r Request) (Response, error) {
 		return nil, err
 	}
 	defer func() {
-		io.Copy(ioutil.Discard, io.LimitReader(response.Body, 1024)) //nolint:errcheck
+		_, _ = io.Copy(ioutil.Discard, io.LimitReader(response.Body, 1024)) //nolint:errcheck
 		response.Body.Close()
 	}()
 
