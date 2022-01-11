e1e89baee0cf0febadbeeab37ed0fdb90bcd2f65
fuling <fuling.lgz@alibaba-inc.com>
2022-01-11 17:41:14 +0800

    [new] promtail pipeline: Promtail Rate Limit stage #5048 #5051

--- clients/pkg/logentry/stages/limit_test.go
+++ clients/pkg/logentry/stages/limit_test.go
@@ -6,12 +6,13 @@ import (
 
 	util_log "github.com/cortexproject/cortex/pkg/util/log"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 // Not all these are tested but are here to make sure the different types marshal without error
-var testLimitYaml = `
+var testLimitWaitYaml = `
 pipeline_stages:
 - json:
     expressions:
@@ -20,23 +21,60 @@ pipeline_stages:
 - limit:
     source: app
     value: loki
-    rate: 10
-    burst: 100
+    rate: 1
+    burst: 1
+    drop: false
+`
+
+// Not all these are tested but are here to make sure the different types marshal without error
+var testLimitDropYaml = `
+pipeline_stages:
+- json:
+    expressions:
+      app:
+      msg:
+- limit:
+    source: app
+    value: loki
+    rate: 1
+    burst: 1
     drop: true
 `
 
 // TestLimitPipeline is used to verify we properly parse the yaml config and create a working pipeline
-func TestLimitPipeline(t *testing.T) {
+func TestLimitWaitPipeline(t *testing.T) {
 	registry := prometheus.NewRegistry()
 	plName := "testPipeline"
-	pl, err := NewPipeline(util_log.Logger, loadConfig(testLimitYaml), &plName, registry)
+	pl, err := NewPipeline(util_log.Logger, loadConfig(testLimitWaitYaml), &plName, registry)
+	logs := make([]Entry, 0)
+	logCount := 5
+	for i := 0; i < logCount; i++ {
+		logs = append(logs, newEntry(nil, model.LabelSet{"app": "loki"}, testMatchLogLineApp1, time.Now()))
+	}
 	require.NoError(t, err)
 	out := processEntries(pl,
-		newEntry(nil, nil, testMatchLogLineApp1, time.Now()),
-		newEntry(nil, nil, testMatchLogLineApp2, time.Now()),
+		logs...,
 	)
-
 	// Only the second line will go through.
-	assert.Len(t, out, 2)
+	assert.Len(t, out, logCount)
+	assert.Equal(t, out[0].Line, testMatchLogLineApp1)
+}
+
+// TestLimitPipeline is used to verify we properly parse the yaml config and create a working pipeline
+func TestLimitDropPipeline(t *testing.T) {
+	registry := prometheus.NewRegistry()
+	plName := "testPipeline"
+	pl, err := NewPipeline(util_log.Logger, loadConfig(testLimitDropYaml), &plName, registry)
+	logs := make([]Entry, 0)
+	logCount := 10
+	for i := 0; i < logCount; i++ {
+		logs = append(logs, newEntry(nil, model.LabelSet{"app": "loki"}, testMatchLogLineApp1, time.Now()))
+	}
+	require.NoError(t, err)
+	out := processEntries(pl,
+		logs...,
+	)
+	// Only the second line will go through.
+	assert.Len(t, out, 1)
 	assert.Equal(t, out[0].Line, testMatchLogLineApp1)
 }
