grafana/loki

commit 52fa2114df4af9bf0c26b43d535c6c7c4b39438a

fluentd: Fix bug that caused lines to be dropped when containing non utf-8 characters (#5107)
* fluentd: Remove non utf-8 characters from log lines

When a log line in hash format contains non UTF-8 characters fluentd
would drop the complete line because it failed to convert the line in
key-value format.

By forcing UTF-8 encoding and replacing non UTF-8 characters with empty
strings the log line will not be dropped but only contain the valid
UTF-8 characters.

Fixes #5099

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

* Bump fluent-plugin-grafana-loki to version 1.2.17

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

* fixup! fluentd: Remove non utf-8 characters from log lines

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

* fixup! fluentd: Remove non utf-8 characters from log lines

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

clients/cmd/fluentd/lib/fluent/plugin/out_loki.rb
@@ -278,6 +278,8 @@ module Fluent
           when :key_value
             formatted_labels = []
             record.each do |k, v|
+              # Remove non UTF-8 characters by force-encoding the string
+              v = v.encode('utf-8', invalid: :replace)
               # Escape double quotes and backslashes by prefixing them with a backslash
               v = v.to_s.gsub(%r{(["\\])}, '\\\\\1')
               if v.include?(' ') || v.include?('=')
@@ -292,7 +294,6 @@ module Fluent
         line
       end
 
-      #
       # convert a line to loki line with labels
       def line_to_loki(record)
         chunk_labels = {}

clients/cmd/fluentd/spec/gems/fluent/plugin/loki_output_spec.rb
@@ -50,12 +50,12 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     driver = Fluent::Test::Driver::Output.new(described_class)
     driver.configure(config)
     content = File.readlines('spec/gems/fluent/plugin/data/syslog2')
-    single_chunk = [Time.at(1_546_270_458), content]
-    payload = driver.instance.generic_to_loki([single_chunk])
-    body = { 'streams': payload }
-    expect(body[:streams][0]['stream'].empty?).to eq true
-    expect(body[:streams][0]['values'].count).to eq 1
-    expect(body[:streams][0]['values'][0][0]).to eq '1546270458000000000'
+    chunk = [Time.at(1_546_270_458), content[0]]
+    payload = driver.instance.generic_to_loki([chunk])
+    expect(payload[0]['stream'].empty?).to eq true
+    expect(payload[0]['values'].count).to eq 1
+    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
+    expect(payload[0]['values'][0][1]).to eq content[0]
   end
 
   it 'converts syslog output with extra labels to loki output' do
@@ -66,12 +66,12 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     driver = Fluent::Test::Driver::Output.new(described_class)
     driver.configure(config)
     content = File.readlines('spec/gems/fluent/plugin/data/syslog2')
-    single_chunk = [Time.at(1_546_270_458), content]
-    payload = driver.instance.generic_to_loki([single_chunk])
-    body = { 'streams': payload }
-    expect(body[:streams][0]['stream']).to eq('env' => 'test')
-    expect(body[:streams][0]['values'].count).to eq 1
-    expect(body[:streams][0]['values'][0][0]).to eq '1546270458000000000'
+    chunk = [Time.at(1_546_270_458), content[0]]
+    payload = driver.instance.generic_to_loki([chunk])
+    expect(payload[0]['stream']).to eq('env' => 'test')
+    expect(payload[0]['values'].count).to eq 1
+    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
+    expect(payload[0]['values'][0][1]).to eq content[0]
   end
 
   it 'converts multiple syslog output lines to loki output' do
@@ -84,11 +84,12 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     line1 = [Time.at(1_546_270_458), content[0]]
     line2 = [Time.at(1_546_270_460), content[1]]
     payload = driver.instance.generic_to_loki([line1, line2])
-    body = { 'streams': payload }
-    expect(body[:streams][0]['stream'].empty?).to eq true
-    expect(body[:streams][0]['values'].count).to eq 2
-    expect(body[:streams][0]['values'][0][0]).to eq '1546270458000000000'
-    expect(body[:streams][0]['values'][1][0]).to eq '1546270460000000000'
+    expect(payload[0]['stream'].empty?).to eq true
+    expect(payload[0]['values'].count).to eq 2
+    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
+    expect(payload[0]['values'][0][1]).to eq content[0]
+    expect(payload[0]['values'][1][0]).to eq '1546270460000000000'
+    expect(payload[0]['values'][1][1]).to eq content[1]
   end
 
   it 'converts multiple syslog output lines with extra labels to loki output' do
@@ -102,11 +103,27 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     line1 = [Time.at(1_546_270_458), content[0]]
     line2 = [Time.at(1_546_270_460), content[1]]
     payload = driver.instance.generic_to_loki([line1, line2])
-    body = { 'streams': payload }
-    expect(body[:streams][0]['stream']).to eq('env' => 'test')
-    expect(body[:streams][0]['values'].count).to eq 2
-    expect(body[:streams][0]['values'][0][0]).to eq '1546270458000000000'
-    expect(body[:streams][0]['values'][1][0]).to eq '1546270460000000000'
+    expect(payload[0]['stream']).to eq('env' => 'test')
+    expect(payload[0]['values'].count).to eq 2
+    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
+    expect(payload[0]['values'][0][1]).to eq content[0]
+    expect(payload[0]['values'][1][0]).to eq '1546270460000000000'
+    expect(payload[0]['values'][1][1]).to eq content[1]
+  end
+
+  it 'removed non utf-8 characters from log lines' do
+    config = <<-CONF
+      url     https://logs-us-west1.grafana.net
+    CONF
+    driver = Fluent::Test::Driver::Output.new(described_class)
+    driver.configure(config)
+    content = File.readlines('spec/gems/fluent/plugin/data/non_utf8.log')
+    chunk = [Time.at(1_546_270_458), {'message'=>content[0], 'stream'=>'stdout'}]
+    payload = driver.instance.generic_to_loki([chunk])
+    expect(payload[0]['stream'].empty?).to eq true
+    expect(payload[0]['values'].count).to eq 1
+    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
+    expect(payload[0]['values'][0][1]).to eq 'message="� rest of line" stream=stdout'
   end
 
   it 'formats record hash as key_value' do
@@ -122,7 +139,7 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     expect(body[:streams][0]['stream'].empty?).to eq true
     expect(body[:streams][0]['values'].count).to eq 1
     expect(body[:streams][0]['values'][0][0]).to eq '1546270458000000000'
-    expect(body[:streams][0]['values'][0][1]).to eq 'message="' + content[0] + '" stream="stdout"'
+    expect(body[:streams][0]['values'][0][1]).to eq 'message="' + content[0] + '" stream=stdout'
   end
 
   it 'formats record hash as json' do

Makefile
@@ -425,9 +425,10 @@ fluent-bit-test:
 # fluentd plugin #
 ##################
 fluentd-plugin:
-	gem install bundler --version 1.16.2
+	gem install bundler --version 2.3.4
 	bundle config silence_root_warning true
-	bundle install --gemfile=clients/cmd/fluentd/Gemfile --path=clients/cmd/fluentd/vendor/bundle
+	bundle config set --local path clients/cmd/fluentd/vendor/bundle
+	bundle install --gemfile=clients/cmd/fluentd/Gemfile
 
 fluentd-image:
 	$(SUDO) docker build -t $(IMAGE_PREFIX)/fluent-plugin-loki:$(IMAGE_TAG) -f clients/cmd/fluentd/Dockerfile .
@@ -435,9 +436,9 @@ fluentd-image:
 fluentd-push:
 	$(SUDO) $(PUSH_OCI) $(IMAGE_PREFIX)/fluent-plugin-loki:$(IMAGE_TAG)
 
-fluentd-test: LOKI_URL ?= http://localhost:3100/loki/api/
+fluentd-test: LOKI_URL ?= http://loki:3100
 fluentd-test:
-	LOKI_URL="$(LOKI_URL)" docker-compose -f clients/cmd/fluentd/docker/docker-compose.yml up --build $(IMAGE_PREFIX)/fluent-plugin-loki:$(IMAGE_TAG)
+	LOKI_URL="$(LOKI_URL)" docker-compose -f clients/cmd/fluentd/docker/docker-compose.yml up --build
 
 ##################
 # logstash plugin #

clients/cmd/fluentd/.gitignore
@@ -1,3 +1,9 @@
-/coverage/
-/.rspec_status
-/Gemfile.lock
+Gemfile.lock
+.rspec_status
+# rbenv
+.ruby-version
+# bundler
+.bundle/
+vendor/
+# simplecov
+coverage/

clients/cmd/fluentd/.rubocop.yml
@@ -1,6 +1,7 @@
 require: rubocop-rspec
 
 AllCops:
+  NewCops: disable
   Exclude:
     - 'bin/**'
     - 'test/**/*.rb'

clients/cmd/fluentd/Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.6 as build
+FROM ruby:2.7.5 as build
 
 ENV DEBIAN_FRONTEND=noninteractive
 

clients/cmd/fluentd/bin/setup
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-set -vx
 
-gem install bundler
-bundle install
+set -euo pipefail
+
+ruby --version
+echo ""
+ruby -S gem install bundler --version 2.3.4
+ruby -S bundle config set --local path $(pwd)/vendor/bundle
+ruby -S bundle install

clients/cmd/fluentd/bin/test
@@ -1,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ruby -S bundle exec rspec

commit dc1fc31e449ce6fa182c41e4592bca865a05db63

Fix encoding error in fluentd client
This change fixes a bug that is caused by incorrectly trying to encode
any values other than string.
This bug was a regression introduced with #5107 and caused
`NoMethodError` errors.

Fixes #5099

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

clients/cmd/fluentd/lib/fluent/plugin/out_loki.rb
@@ -279,7 +279,9 @@ module Fluent
             formatted_labels = []
             record.each do |k, v|
               # Remove non UTF-8 characters by force-encoding the string
-              v = v.encode('utf-8', invalid: :replace)
+              if v.is_a?(String)
+                v = v.encode('utf-8', invalid: :replace)
+              end
               # Escape double quotes and backslashes by prefixing them with a backslash
               v = v.to_s.gsub(%r{(["\\])}, '\\\\\1')
               if v.include?(' ') || v.include?('=')

clients/cmd/fluentd/spec/gems/fluent/plugin/loki_output_spec.rb
@@ -117,13 +117,29 @@ RSpec.describe Fluent::Plugin::LokiOutput do
     CONF
     driver = Fluent::Test::Driver::Output.new(described_class)
     driver.configure(config)
-    content = File.readlines('spec/gems/fluent/plugin/data/non_utf8.log')
-    chunk = [Time.at(1_546_270_458), {'message'=>content[0], 'stream'=>'stdout'}]
+    content = File.readlines('spec/gems/fluent/plugin/data/non_utf8.log')[0]
+    chunk = [Time.at(1_546_270_458), {'message'=>content, 'number': 1.2345, 'stream'=>'stdout'}]
     payload = driver.instance.generic_to_loki([chunk])
     expect(payload[0]['stream'].empty?).to eq true
     expect(payload[0]['values'].count).to eq 1
-    expect(payload[0]['values'][0][0]).to eq '1546270458000000000'
-    expect(payload[0]['values'][0][1]).to eq 'message="� rest of line" stream=stdout'
+    expect(payload[0]['values'][0][0]).to eq "1546270458000000000"
+    expect(payload[0]['values'][0][1]).to eq "message=\"� rest of line\" number=1.2345 stream=stdout"
+  end
+
+  it 'handle non utf-8 characters from log lines in json format' do
+    config = <<-CONF
+      url         https://logs-us-west1.grafana.net
+      line_format json
+    CONF
+    driver = Fluent::Test::Driver::Output.new(described_class)
+    driver.configure(config)
+    content = File.readlines('spec/gems/fluent/plugin/data/non_utf8.log')[0]
+    chunk = [Time.at(1_546_270_458), {'message'=>content, 'number': 1.2345, 'stream'=>'stdout'}]
+    payload = driver.instance.generic_to_loki([chunk])
+    expect(payload[0]['stream'].empty?).to eq true
+    expect(payload[0]['values'].count).to eq 1
+    expect(payload[0]['values'][0][0]).to eq "1546270458000000000"
+    expect(payload[0]['values'][0][1]).to eq "{\"message\":\"\xC1 rest of line\",\"number\":1.2345,\"stream\":\"stdout\"}"
   end
 
   it 'formats record hash as key_value' do