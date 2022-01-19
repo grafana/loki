c31b9d74d7bcf43e869f2acfe799f6079f6bc457
Jacob-Sakran <jacob.sakran@ibm.com>
2022-01-19 19:21:16 +0200

    Test edited, now both tests pass

--- operator/internal/manifests/ruler_test.go
+++ operator/internal/manifests/ruler_test.go
@@ -44,7 +44,7 @@ func TestNewRulerDeployment_HasTemplateConfigHashAnnotation(t *testing.T) {
 			},
 		},
 	})
-	expected := "loki.openshift.io/config-hash"
+	expected := "loki.grafana.com/config-hash"
 	annotations := ss.Spec.Template.Annotations
 	require.Contains(t, annotations, expected)
 	require.Equal(t, annotations[expected], "deadbeef")
