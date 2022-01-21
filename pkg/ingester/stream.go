f0e4d90d254856df68f8ecdbbd733c44966ba8fa
程迪 <chengdi@xiaomi.com>
2022-01-21 20:18:50 +0800

    Lock consistencyMtx outside locking chunkMtx

--- pkg/ingester/stream.go
+++ pkg/ingester/stream.go
@@ -129,6 +129,7 @@ func newStream(cfg *Config, limits RateLimiterStrategy, tenant string, fp model.
 
 // consumeChunk manually adds a chunk to the stream that was received during
 // ingester chunk transfer.
+// Must hold chunkMtx
 // DEPRECATED: chunk transfers are no longer suggested and remain for compatibility.
 func (s *stream) consumeChunk(_ context.Context, chunk *logproto.Chunk) error {
 	c, err := chunkenc.NewByteChunk(chunk.Data, s.cfg.BlockSize, s.cfg.TargetChunkSize)
@@ -136,8 +137,6 @@ func (s *stream) consumeChunk(_ context.Context, chunk *logproto.Chunk) error {
 		return err
 	}
 
-	s.chunkMtx.Lock()
-	defer s.chunkMtx.Unlock()
 	s.chunks = append(s.chunks, chunkDesc{
 		chunk: c,
 	})
--- pkg/ingester/streams_map_test.go
+++ pkg/ingester/streams_map_test.go
@@ -52,16 +52,18 @@ func TestStreamsMap(t *testing.T) {
 	require.Nil(t, s)
 	require.False(t, loaded)
 
+	// Test LoadOrStoreNew
 	s, loaded, err = streams.LoadOrStoreNew(ss[0].labelsString, func() (*stream, error) {
 		return ss[0], nil
-	})
+	}, nil)
 	require.Equal(t, s, ss[0])
 	require.False(t, loaded)
 	require.Nil(t, err)
 
+	// Test LoadOrStoreNewByFP
 	s, loaded, err = streams.LoadOrStoreNewByFP(ss[1].fp, func() (*stream, error) {
 		return ss[1], nil
-	})
+	}, nil)
 	require.Equal(t, s, ss[1])
 	require.False(t, loaded)
 	require.Nil(t, err)
@@ -78,6 +80,7 @@ func TestStreamsMap(t *testing.T) {
 		require.True(t, loaded)
 	}
 
+	// Test Delete
 	for _, st := range ss {
 		deleted := streams.Delete(st)
 		require.True(t, deleted)
@@ -90,4 +93,30 @@ func TestStreamsMap(t *testing.T) {
 		require.Nil(t, s)
 		require.False(t, loaded)
 	}
+
+	require.Equal(t, 0, streams.Len())
+
+	// Test Store
+	streams.Store(ss[0].labelsString, ss[0])
+
+	s, loaded = streams.Load(ss[0].labelsString)
+	require.Equal(t, ss[0], s)
+	require.True(t, loaded)
+
+	s, loaded = streams.LoadByFP(ss[0].fp)
+	require.Equal(t, ss[0], s)
+	require.True(t, loaded)
+
+	// Test StoreByFP
+	streams.StoreByFP(ss[1].fp, ss[1])
+
+	s, loaded = streams.Load(ss[1].labelsString)
+	require.Equal(t, ss[1], s)
+	require.True(t, loaded)
+
+	s, loaded = streams.LoadByFP(ss[1].fp)
+	require.Equal(t, ss[1], s)
+	require.True(t, loaded)
+
+	require.Equal(t, len(ss), streams.Len())
 }
