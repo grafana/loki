a2d814b875cd45c3e1b4471bf758bf08a3244f5d
Jacob-Sakran <jacob.sakran@ibm.com>
2022-01-25 16:52:03 +0200

    updated

--- operator/hack/deploy-example-rules-configmap.sh
+++ operator/hack/deploy-example-rules-configmap.sh
@@ -5,8 +5,8 @@ set -eou pipefail
 NAMESPACE=$1
 
 create_rules_configmap() {
-  kubectl -n ${NAMESPACE} delete configmap rules ||:
-  kubectl -n ${NAMESPACE} create configmap rules \
+  kubectl -n "${NAMESPACE}" delete configmap rules ||:
+  kubectl -n "${NAMESPACE}" create configmap rules \
 --from-file=rules.yaml=hack/ruler_rules.yaml
 }
 
