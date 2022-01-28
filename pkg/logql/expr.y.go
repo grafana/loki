b36978b04e5af0dd5e186d34947af0a797ef8fb2
Kaviraj <kavirajkanagaraj@gmail.com>
2022-01-28 10:43:27 +0100

    Do not touch generated go files
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- pkg/logql/expr.y.go
+++ pkg/logql/expr.y.go
@@ -2,13 +2,13 @@
 
 package logql
 
+import __yyfmt__ "fmt"
+
+
 import (
-	__yyfmt__ "fmt"
-	"time"
-
-	"github.com/prometheus/prometheus/model/labels"
-
 	"github.com/grafana/loki/pkg/logql/log"
+	"github.com/prometheus/prometheus/model/labels"
+	"time"
 )
 
 type exprSymType struct {
@@ -234,6 +234,7 @@ var exprStatenames = [...]string{}
 const exprEofCode = 1
 const exprErrCode = 2
 const exprInitialStackSize = 16
+
 
 var exprExca = [...]int{
 	-1, 1,
@@ -495,6 +496,7 @@ var exprErrorMessages = [...]struct {
 	token int
 	msg   string
 }{}
+
 
 /*	parser for yacc output	*/
 
