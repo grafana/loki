3e3af509e32c66db905e9ea8b3b6f336fcd8bf4f
Trevor Whitney <trevorjwhitney@gmail.com>
2022-01-31 09:07:27 -0700

    shellcheck create_cluster.sh

--- tools/dev/k3d/scripts/create_cluster.sh
+++ tools/dev/k3d/scripts/create_cluster.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 current_dir="$(cd "$(dirname "$0")" && pwd)"
-k3d_dir="$(cd "$current_dir/.." && pwd)"
+k3d_dir="$(cd "${current_dir}/.." && pwd)"
 
 cluster_name="$1"
-namespace="k3d-$cluster_name"
-environment="$k3d_dir/environments/$cluster_name"
+namespace="k3d-${cluster_name}"
+environment="${k3d_dir}/environments/${cluster_name}"
 registry_port="${REGISTRY_PORT:-45629}"
 
-if [[ -z "$(k3d registry list | grep -m 1 k3d-grafana)" ]]; then
+if k3d registry list | grep -q -m 1 k3d-grafana; then
 	k3d registry create k3d-grafana \
-		--port "$registry_port"
+		--port "${registry_port}"
 fi
 
-if [[ -z "$(k3d cluster list | grep -m 1 "$cluster_name")" ]]; then
-	k3d cluster create "$cluster_name" \
+if k3d cluster list | grep -q -m 1 "${cluster_name}"; then
+	k3d cluster create "${cluster_name}" \
 		--servers 1 \
 		--agents 3 \
-		--registry-use "k3d-grafana:$registry_port" \
+		--registry-use "k3d-grafana:${registry_port}" \
 		--wait
 fi
 
-tk env set "$environment" \
-	--server="https://0.0.0.0:$(k3d node list -o json | jq -r ".[] | select(.name == \"k3d-$cluster_name-serverlb\") | .portMappings.\"6443\"[] | .HostPort")" \
-	--namespace="$namespace"
+tk env set "${environment}" \
+	--server="https://0.0.0.0:$(k3d node list -o json | jq -r ".[] | select(.name == \"k3d-${cluster_name}-serverlb\") | .portMappings.\"6443\"[] | .HostPort")" \
+	--namespace="${namespace}"
 
-kubectl config set-context "$namespace"
+kubectl config set-context "${namespace}"
 
-if [[ -z "$(kubectl get namespaces | grep -m 1 "$namespace")" ]]; then
-	kubectl create namespace "$namespace" || true
+if kubectl get namespaces | grep -q -m 1 "${namespace}"; then
+	kubectl create namespace "${namespace}" || true
 fi
 
-tk apply "$environment"
+tk apply "${environment}"
