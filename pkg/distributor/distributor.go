d0af079aaf9c44333e96a90254f4a0045370065a
Christian Haudum <christian.haudum@gmail.com>
2022-02-08 09:45:40 +0100

    Simplify logic in distributor
    This contains the reverted changes from
    10b93cebe54af5b3af65308d5e0724e5aec2bb50 containing a bug that caused
    incorrect streams to be pushed to the ingesters.
    
    I added this as separate commit to verify the test added in the previous
    commit fails. The bugfix will be in a separate commit after this one.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/kv"
 	"github.com/grafana/dskit/limiter"
 	"github.com/grafana/dskit/ring"
@@ -200,19 +201,23 @@ func (d *Distributor) stopping(_ error) error {
 
 // TODO taken from Cortex, see if we can refactor out an usable interface.
 type streamTracker struct {
-	stream      logproto.Stream
-	minSuccess  int
-	maxFailures int
-	succeeded   atomic.Int32
-	failed      atomic.Int32
+	stream *logproto.Stream
+	// successBucket the number of minimum required successful pushes
+	// each successful push decrements the counter
+	successBucket atomic.Int32
+	// failureBucket holds the number of maximum allowed errors
+	// each erroneous push decrements the counter
+	failureBucket atomic.Int32
 }
 
 // TODO taken from Cortex, see if we can refactor out an usable interface.
 type pushTracker struct {
-	samplesPending atomic.Int32
-	samplesFailed  atomic.Int32
-	done           chan struct{}
-	err            chan error
+	done chan struct{}
+	err  chan error
+	// pending holds the total amount of streams of a push request
+	pending atomic.Int32
+	// failed keeps track of whether the push already failed
+	failed atomic.Bool
 }
 
 // Push a set of streams.
@@ -273,7 +278,7 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 		stream.Entries = stream.Entries[:n]
 
 		keys = append(keys, util.TokenFor(userID, stream.Labels))
-		streams = append(streams, streamTracker{stream: stream})
+		streams = append(streams, streamTracker{stream: &stream})
 	}
 
 	// Return early if none of the streams contained entries
@@ -289,22 +294,22 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 		return nil, httpgrpc.Errorf(http.StatusTooManyRequests, validation.RateLimitedErrorMsg, userID, int(d.ingestionRateLimiter.Limit(now, userID)), validatedSamplesCount, validatedSamplesSize)
 	}
 
-	const maxExpectedReplicationSet = 5 // typical replication factor 3 plus one for inactive plus one for luck
-	var descs [maxExpectedReplicationSet]ring.InstanceDesc
+	streamsByIngester := map[string][]*streamTracker{}
+	descByIngester := map[string]ring.InstanceDesc{}
 
-	samplesByIngester := map[string][]*streamTracker{}
-	ingesterDescs := map[string]ring.InstanceDesc{}
+	bufDescs, bufHosts, bufZones := ring.MakeBuffersForGet()
 	for i, key := range keys {
-		replicationSet, err := d.ingestersRing.Get(key, ring.Write, descs[:0], nil, nil)
+		replicationSet, err := d.ingestersRing.Get(key, ring.Write, bufDescs, bufHosts, bufZones)
 		if err != nil {
 			return nil, err
 		}
 
-		streams[i].minSuccess = len(replicationSet.Instances) - replicationSet.MaxErrors
-		streams[i].maxFailures = replicationSet.MaxErrors
+		streams[i].successBucket.Store(int32(len(replicationSet.Instances) - replicationSet.MaxErrors))
+		streams[i].failureBucket.Store(int32(replicationSet.MaxErrors))
+
 		for _, ingester := range replicationSet.Instances {
-			samplesByIngester[ingester.Addr] = append(samplesByIngester[ingester.Addr], &streams[i])
-			ingesterDescs[ingester.Addr] = ingester
+			streamsByIngester[ingester.Addr] = append(streamsByIngester[ingester.Addr], &streams[i])
+			descByIngester[ingester.Addr] = ingester
 		}
 	}
 
@@ -312,9 +317,10 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 		done: make(chan struct{}, 1), // buffer avoids blocking if caller terminates - sendSamples() only sends once on each
 		err:  make(chan error, 1),
 	}
-	tracker.samplesPending.Store(int32(len(streams)))
-	for ingester, samples := range samplesByIngester {
-		go func(ingester ring.InstanceDesc, samples []*streamTracker) {
+	tracker.pending.Store(int32(len(streams)))
+
+	for addr := range streamsByIngester {
+		go func(ingester ring.InstanceDesc, streams []*streamTracker) {
 			// Use a background context to make sure all ingesters get samples even if we return early
 			localCtx, cancel := context.WithTimeout(context.Background(), d.clientCfg.RemoteTimeout)
 			defer cancel()
@@ -322,9 +328,10 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 			if sp := opentracing.SpanFromContext(ctx); sp != nil {
 				localCtx = opentracing.ContextWithSpan(localCtx, sp)
 			}
-			d.sendSamples(localCtx, ingester, samples, &tracker)
-		}(ingesterDescs[ingester], samples)
+			d.sendToIngester(localCtx, ingester, streams, &tracker)
+		}(descByIngester[addr], streamsByIngester[addr])
 	}
+
 	select {
 	case err := <-tracker.err:
 		return nil, err
@@ -355,51 +362,57 @@ func (d *Distributor) truncateLines(vContext validationContext, stream *logproto
 }
 
 // TODO taken from Cortex, see if we can refactor out an usable interface.
-func (d *Distributor) sendSamples(ctx context.Context, ingester ring.InstanceDesc, streamTrackers []*streamTracker, pushTracker *pushTracker) {
-	err := d.sendSamplesErr(ctx, ingester, streamTrackers)
+func (d *Distributor) sendToIngester(ctx context.Context, ingester ring.InstanceDesc, streams []*streamTracker, pushTracker *pushTracker) {
 
-	// If we succeed, decrement each sample's pending count by one.  If we reach
-	// the required number of successful puts on this sample, then decrement the
-	// number of pending samples by one.  If we successfully push all samples to
-	// min success ingesters, wake up the waiting rpc so it can return early.
-	// Similarly, track the number of errors, and if it exceeds maxFailures
-	// shortcut the waiting rpc.
-	//
-	// The use of atomic increments here guarantees only a single sendSamples
-	// goroutine will write to either channel.
-	for i := range streamTrackers {
-		if err != nil {
-			if streamTrackers[i].failed.Inc() <= int32(streamTrackers[i].maxFailures) {
+	payload := make([]logproto.Stream, len(streams))
+	for i, s := range streams {
+		payload[i] = *s.stream
+	}
+
+	err := d.executePushRequest(ctx, ingester, payload)
+	if err == nil {
+		// If the push rpc call to the ingester succeeds, we decrement the
+		// successBucket counter for each stream sent.
+		// If we reach the amount of required successful pushes for a stream
+		// (successBucket is empty), we decrement the counter of pending streams.
+		// If there are no pending streams, we are done and can notify the tracker.
+		for i := range streams {
+			if streams[i].successBucket.Dec() > 0 {
 				continue
 			}
-			if pushTracker.samplesFailed.Inc() == 1 {
-				pushTracker.err <- err
-			}
-		} else {
-			if streamTrackers[i].succeeded.Inc() != int32(streamTrackers[i].minSuccess) {
-				continue
-			}
-			if pushTracker.samplesPending.Dec() == 0 {
+			if pushTracker.pending.Dec() == 0 {
 				pushTracker.done <- struct{}{}
+			}
+		}
+	} else {
+		// If the push rpc call to the ingester fails, we decrement the
+		// failureBucket counter for each stream sent.
+		// If we reach the amount of maximum allowed erroneous pushes for a stream
+		// (errorBucket is empty), we swap the tracker state to failed.
+		// If the state isn't already failed, we can return early and notify the
+		// tracker.
+		logger := util_log.WithContext(ctx, util_log.Logger)
+		for i := range streams {
+			if streams[i].failureBucket.Dec() >= 0 {
+				level.Warn(logger).Log("msg", "decrement failure bucket for stream", "stream", streams[i].stream.Labels, "counter", streams[i].failureBucket.Load(), "err", err)
+				continue
+			}
+			if !pushTracker.failed.Swap(true) {
+				level.Warn(logger).Log("msg", "mark push tracker as failed", "err", err)
+				pushTracker.err <- err
 			}
 		}
 	}
 }
 
 // TODO taken from Cortex, see if we can refactor out an usable interface.
-func (d *Distributor) sendSamplesErr(ctx context.Context, ingester ring.InstanceDesc, streams []*streamTracker) error {
+func (d *Distributor) executePushRequest(ctx context.Context, ingester ring.InstanceDesc, streams []logproto.Stream) error {
 	c, err := d.pool.GetClientFor(ingester.Addr)
 	if err != nil {
 		return err
 	}
 
-	req := &logproto.PushRequest{
-		Streams: make([]logproto.Stream, len(streams)),
-	}
-	for i, s := range streams {
-		req.Streams[i] = s.stream
-	}
-
+	req := &logproto.PushRequest{Streams: streams}
 	_, err = c.(logproto.PusherClient).Push(ctx, req)
 	d.ingesterAppends.WithLabelValues(ingester.Addr).Inc()
 	if err != nil {
