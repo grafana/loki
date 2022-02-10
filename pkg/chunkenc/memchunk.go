9645f312ee2acec801a399dc2723a73872ed0511
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2022-02-10 13:39:24 +0530

    fix the comment

--- pkg/chunkenc/memchunk.go
+++ pkg/chunkenc/memchunk.go
@@ -911,7 +911,7 @@ func (c *MemChunk) Blocks(mintT, maxtT time.Time) []Block {
 
 // Rebound builds a smaller chunk with logs having timestamp from start and end(both inclusive)
 func (c *MemChunk) Rebound(start, end time.Time) (Chunk, error) {
-	// add a nanosecond to end time because the Chunk.Iterator considers end time to be non-inclusive.
+	// add a millisecond to end time because the Chunk.Iterator considers end time to be non-inclusive.
 	itr, err := c.Iterator(context.Background(), start, end.Add(time.Millisecond), logproto.FORWARD, log.NewNoopPipeline().ForStream(labels.Labels{}))
 	if err != nil {
 		return nil, err
