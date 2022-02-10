2f68217b388befc530b5f54d21fd8b8144621f93
Travis Patterson <travis.patterson@grafana.com>
2022-02-10 11:04:05 -0700

    remove test package from coverage diff

--- tools/diff_coverage.sh
+++ tools/diff_coverage.sh
@@ -6,7 +6,7 @@ if [[ ! -f "$1" ]] || [[ ! -f "$2" ]]; then
 fi
 
 echo '```diff'
-for pkg in ingester distributor querier ruler storage foo; do
+for pkg in ingester distributor querier ruler storage; do
   old=$(grep "pkg/${pkg}\s" "$1" | sed s/%// | awk '{print $5}')
   new=$(grep "pkg/${pkg}\s" "$2" | sed s/%// | awk '{print $5}')
   echo | awk -v pkg="${pkg}" -v old="${old:-0}" -v new="${new:-0}" \
