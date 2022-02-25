7decde435f85d65094a03c5b907e12b5db7899e0
Susana Ferreira <susana.ferreira@grafana.com>
2022-02-25 20:26:26 +0000

    Add vector aggregator and range vector aggregator tests

--- pkg/logql/rangemapper_test.go
+++ pkg/logql/rangemapper_test.go
@@ -33,15 +33,80 @@ func Test_SplitRangeVectorMapping(t *testing.T) {
 			false,
 		},
 		{
-			`sum(avg_over_time({app="foo"} | unwrap bar[3m]))`,
-			`sum(avg_over_time({app="foo"} | unwrap bar[3m]))`,
-			true,
+			`count(rate({app="foo"}[3m]))`,
+			`sum (downstream<count(rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<count(rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<count(rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`count(rate({app="foo"}[3m])) by (bar)`,
+			`sum by (bar) (downstream<count  by (bar) (rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<count by (bar) (rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<count by (bar)(rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`max(rate({app="foo"}[3m]))`,
+			`max (downstream<max(rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<max (rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<max (rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`max(rate({app="foo"}[3m])) by (bar)`,
+			`max by (bar) (downstream<max  by (bar) (rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<max by (bar) (rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<max by (bar)(rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`min(rate({app="foo"}[3m]))`,
+			`min (downstream<min(rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<min (rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<min (rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`min(rate({app="foo"}[3m])) by (bar)`,
+			`min by (bar) (downstream<min  by (bar) (rate({app="foo"}[1m] offset 2m0s)), shard=<nil>>
+				++ downstream<min by (bar) (rate({app="foo"}[1m] offset 1m0s)), shard=<nil>>
+				++ downstream<min by (bar)(rate({app="foo"}[1m])), shard=<nil>>)`,
+			false,
+		},
+		{
+			`bytes_over_time({app="foo"}[3m])`,
+			`sum (downstream<bytes_over_time({app="foo"}[1m] offset 2m0s), shard=<nil>>
+				++ downstream<bytes_over_time({app="foo"}[1m] offset 1m0s), shard=<nil>>
+				++ downstream<bytes_over_time({app="foo"}[1m]), shard=<nil>>)`,
+			false,
+		},
+		{
+			`bytes_rate({app="foo"}[3m])`,
+			`sum (downstream<bytes_rate({app="foo"}[1m] offset 2m0s), shard=<nil>>
+				++ downstream<bytes_rate({app="foo"}[1m] offset 1m0s), shard=<nil>>
+				++ downstream<bytes_rate({app="foo"}[1m]), shard=<nil>>)`,
+			false,
+		},
+		{
+			`count_over_time({app="foo"}[3m])`,
+			`sum (downstream<count_over_time({app="foo"}[1m] offset 2m0s), shard=<nil>>
+				++ downstream<count_over_time({app="foo"}[1m] offset 1m0s), shard=<nil>>
+				++ downstream<count_over_time({app="foo"}[1m]), shard=<nil>>)`,
+			false,
 		},
 		{
 			`rate({app="foo"}[3m])`,
 			`sum(downstream<rate({app="foo"}[1m] offset 2m0s), shard=<nil>>
 				++ downstream<rate({app="foo"}[1m] offset 1m0s), shard=<nil>>
 				++ downstream<rate({app="foo"}[1m]), shard=<nil>>)`,
+			false,
+		},
+		{
+			`sum_over_time({app="foo"} | unwrap bar [3m])`,
+			`sum (downstream<sum_over_time({app="foo"} | unwrap bar [1m] offset 2m0s), shard=<nil>>
+				++ downstream<sum_over_time({app="foo"} | unwrap bar [1m] offset 1m0s), shard=<nil>>
+				++ downstream<sum_over_time({app="foo"} | unwrap bar [1m]), shard=<nil>>)`,
 			false,
 		},
 		{
@@ -52,6 +117,14 @@ func Test_SplitRangeVectorMapping(t *testing.T) {
 			false,
 		},
 		{
+			`max_over_time({app="foo"} | unwrap bar [3m])`,
+			`max(downstream<max_over_time({app="foo"} | unwrap bar [1m] offset 2m0s), shard=<nil>>
+				++ downstream<max_over_time({app="foo"} | unwrap bar [1m] offset 1m0s), shard=<nil>>
+				++ downstream<max_over_time({app="foo"} | unwrap bar[1m]), shard=<nil>>)`,
+			false,
+		},
+		{
+			// TODO: Add more binary operations
 			`rate({app="foo"}[3m]) / rate({app="foo"}[2m])`,
 			`(
 				sum(
@@ -66,6 +139,33 @@ func Test_SplitRangeVectorMapping(t *testing.T) {
 				)
 			)`,
 			false,
+		},
+		{
+			// should go deeper in the AST in case the top function is non-splittable
+			`topk(2, rate({app="foo"}[3m]))`,
+			`topk(2, 
+				sum(downstream<rate({app="foo"}[1m] offset 2m0s), shard=<nil>>
+				++ downstream<rate({app="foo"}[1m] offset 1m0s), shard=<nil>>
+				++ downstream<rate({app="foo"}[1m]), shard=<nil>>)
+			)`,
+			false,
+		},
+		{
+			`quantile_over_time(0.95, {app="foo"} | unwrap bar[3m])`,
+			`quantile_over_time(0.95, {app="foo"} | unwrap bar[3m])`,
+			true,
+		},
+		{
+			// should be noop if function is non-splittable (avg_over_time)
+			`sum(avg_over_time({app="foo"} | unwrap bar[3m]))`,
+			`sum(avg_over_time({app="foo"} | unwrap bar[3m]))`,
+			true,
+		},
+		{
+			// should be noop if range interval is slower or equal to split interval (1m)
+			`rate({app="foo"}[1m])`,
+			`rate({app="foo"}[1m])`,
+			true,
 		},
 	} {
 		tc := tc
