4caf8126227c19e03aae053b08b9b2cca94c599f
Ronen Schaffer <ronen.schaffer@ibm.com>
2022-03-01 17:57:47 +0200

    Make linter happy

--- operator/main.go
+++ operator/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ViaQ/logerr/kverrors"
 	"github.com/ViaQ/logerr/log"
+
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
