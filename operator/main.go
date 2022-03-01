e56f20e80c57859646550b66683a35aaa356d275
Ronen Schaffer <ronen.schaffer@ibm.com>
2022-03-01 17:50:16 +0200

    Remove a blank line to join import blocks

--- operator/main.go
+++ operator/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/ViaQ/logerr/kverrors"
-
 	"github.com/ViaQ/logerr/log"
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
