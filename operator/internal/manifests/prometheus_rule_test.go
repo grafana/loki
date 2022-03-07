2ad2cfd07e500e3b757b3cb1fffed2ebd8374594
Ronen Schaffer <ronen.schaffer@ibm.com>
2022-03-07 14:29:01 +0200

    Add another assert

--- operator/internal/manifests/prometheus_rule_test.go
+++ operator/internal/manifests/prometheus_rule_test.go
@@ -3,19 +3,17 @@ package manifests
 import (
 	"testing"
 
-	"github.com/grafana/loki/operator/api/v1beta1"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewPrometheusRule(t *testing.T) {
 	opts := Options{
-		Stack: v1beta1.LokiStackSpec{
-			Size: v1beta1.SizeOneXSmall,
-		},
+		Name: "test",
 	}
 
 	pr, err := NewPrometheusRule(opts)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, pr)
+	require.NotEmpty(t, pr.Spec)
 }
