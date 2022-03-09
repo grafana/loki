8733010faa75290af28ca397d9651f4d4d65bdb4
Owen Diehl <ow.diehl@gmail.com>
2022-03-09 09:40:40 -0500

    shellcheck

--- tools/tsdb/tsdb-map/diff.sh
+++ tools/tsdb/tsdb-map/diff.sh
@@ -7,16 +7,16 @@ boltdb_base=$1
 branch_a=$2
 branch_b=$3
 
-echo building from ${branch_a}
-git checkout ${branch_a}
-go run tools/tsdb/tsdb-map/main.go  -source ${boltdb_base} -dest /tmp/loki-tsdb-a
-echo benchmarking ${branch_a}
+echo building from "${branch_a}"
+git checkout "${branch_a}"
+go run tools/tsdb/tsdb-map/main.go  -source "${boltdb_base}" -dest /tmp/loki-tsdb-a
+echo benchmarking "${branch_a}"
 LOKI_TSDB_PATH=/tmp/loki-tsdb-a go test github.com/grafana/loki/tools/tsdb/tsdb-map -bench=BenchmarkQuery -run '^$' -benchmem > /tmp/loki-tsdb-bench-a
 
-echo building from ${branch_b}
-git checkout ${branch_b}
-go run tools/tsdb/tsdb-map/main.go  -source ${boltdb_base} -dest /tmp/loki-tsdb-b
-echo benchmarking ${branch_b}
+echo building from "${branch_b}"
+git checkout "${branch_b}"
+go run tools/tsdb/tsdb-map/main.go  -source "${boltdb_base}" -dest /tmp/loki-tsdb-b
+echo benchmarking "${branch_b}"
 LOKI_TSDB_PATH=/tmp/loki-tsdb-b go test github.com/grafana/loki/tools/tsdb/tsdb-map -bench=BenchmarkQuery -run '^$' -benchmem > /tmp/loki-tsdb-bench-b
 
 
