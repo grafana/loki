502347dca934388236a037eaf264c3b2fdd509c4
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-04-06 04:33:22 -0700

    use an empty registry when setting up the logger for the push server (#5761) (#5762)
    Signed-off-by: Ed Welch <edward.welch@grafana.com>
    (cherry picked from commit a9ac01bd0f22ab4e27d39964a0a86f9d47ae93b0)
    
    Co-authored-by: Ed Welch <edward.welch@grafana.com>

--- clients/pkg/promtail/targets/lokipush/pushtarget_test.go
+++ clients/pkg/promtail/targets/lokipush/pushtarget_test.go
@@ -70,7 +70,7 @@ func TestLokiPushTarget(t *testing.T) {
 		},
 	}
 
-	pt, err := NewPushTarget(logger, eh, rlbl, "job1", config, nil)
+	pt, err := NewPushTarget(logger, eh, rlbl, "job1", config)
 	require.NoError(t, err)
 
 	// Build a client to send logs
@@ -162,7 +162,7 @@ func TestPlaintextPushTarget(t *testing.T) {
 		KeepTimestamp: true,
 	}
 
-	pt, err := NewPushTarget(logger, eh, []*relabel.Config{}, "job2", config, nil)
+	pt, err := NewPushTarget(logger, eh, []*relabel.Config{}, "job2", config)
 	require.NoError(t, err)
 
 	// Send some logs
