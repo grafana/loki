2e3add2dd918726fef5d2edb88a8cbd42634c6a3
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-04-06 04:32:41 -0700

    Loki: Revert #4845 which changed the format of errors from the API (#5772) (#5774)
    * This reverts the changes from #4845, unfortunately changing the content type from plain text to JSON is a breaking change of the API and we should wait to do this at a major release.
    
    Signed-off-by: Ed Welch <edward.welch@grafana.com>
    
    * fix tests which were updated after initial change.
    
    Signed-off-by: Ed Welch <edward.welch@grafana.com>
    (cherry picked from commit c158b2c5f5107dc710575c05f5db1889e151114d)
    
    Co-authored-by: Ed Welch <edward.welch@grafana.com>

--- pkg/distributor/http.go
+++ pkg/distributor/http.go
@@ -12,7 +12,6 @@ import (
 	"github.com/grafana/loki/pkg/loghttp/push"
 	"github.com/grafana/loki/pkg/tenant"
 	util_log "github.com/grafana/loki/pkg/util/log"
-	serverutil "github.com/grafana/loki/pkg/util/server"
 	"github.com/grafana/loki/pkg/validation"
 )
 
@@ -29,7 +28,7 @@ func (d *Distributor) PushHandler(w http.ResponseWriter, r *http.Request) {
 				"err", err,
 			)
 		}
-		serverutil.JSONError(w, http.StatusBadRequest, err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -65,7 +64,7 @@ func (d *Distributor) PushHandler(w http.ResponseWriter, r *http.Request) {
 				"err", body,
 			)
 		}
-		serverutil.JSONError(w, int(resp.Code), body)
+		http.Error(w, body, int(resp.Code))
 	} else {
 		if d.tenantConfigs.LogPushRequest(userID) {
 			level.Debug(logger).Log(
@@ -74,7 +73,7 @@ func (d *Distributor) PushHandler(w http.ResponseWriter, r *http.Request) {
 				"err", err.Error(),
 			)
 		}
-		serverutil.JSONError(w, http.StatusInternalServerError, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
