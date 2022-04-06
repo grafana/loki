774b876fc21afa961db5cd26655e4d1b61bd6e98
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-04-06 06:45:08 -0700

    Fix frontend-worker error message (#5567) (#5781)
    (cherry picked from commit 03b734c573aed53d856835ba2395fc9cb348e526)
    
    Co-authored-by: Susana Ferreira <susana.ferreira@grafana.com>

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -177,7 +177,7 @@ func (c *Config) Validate() error {
 		return errors.Wrap(err, "invalid limits config")
 	}
 	if err := c.Worker.Validate(util_log.Logger); err != nil {
-		return errors.Wrap(err, "invalid storage config")
+		return errors.Wrap(err, "invalid frontend-worker config")
 	}
 	if err := c.StorageConfig.BoltDBShipperConfig.Validate(); err != nil {
 		return errors.Wrap(err, "invalid boltdb-shipper config")
