67ba342684294537c80b8c8853f0026e5ce26d60
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2022-04-08 15:57:02 +0530

    improve mutex locking when writing index in boltdb-shipper (#5830)

--- pkg/storage/stores/shipper/uploads/table.go
+++ pkg/storage/stores/shipper/uploads/table.go
@@ -226,15 +226,18 @@ func (lt *Table) MultiQueries(ctx context.Context, queries []chunk.IndexQuery, c
 }
 
 func (lt *Table) getOrAddDB(name string) (*bbolt.DB, error) {
+	lt.dbsMtx.RLock()
+	db, ok := lt.dbs[name]
+	lt.dbsMtx.RUnlock()
+
+	if ok {
+		return db, nil
+	}
+
 	lt.dbsMtx.Lock()
 	defer lt.dbsMtx.Unlock()
 
-	var (
-		db  *bbolt.DB
-		err error
-		ok  bool
-	)
-
+	var err error
 	db, ok = lt.dbs[name]
 	if !ok {
 		db, err = shipper_util.SafeOpenBoltdbFile(filepath.Join(lt.path, name))
