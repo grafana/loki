384c8aa52254ddf264162275aa419f18b118dce0
Dylan Guedes <djmgguedes@gmail.com>
2022-04-08 17:45:39 -0300

    Benchmark Series/GetChunkRef buffer reuse.
    - Add a new benchmark that checks results for Series and GetChunkRef
      - Three scenarios are tested: no buffering given, empty buffer given
        and preallocated buffer given
      - Results aren't too expressive because the test cases are too naive,
        but they still point that reusing a buffer outperform other
        approaches
      - The benchmark isn't just checking performance: it does also checks
        that the implementation is behaving well/returning correct results

--- pkg/storage/tsdb/cacheable_index_test.go
+++ pkg/storage/tsdb/cacheable_index_test.go
@@ -8,12 +8,215 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/loki/pkg/storage/chunk/cache"
 	"github.com/grafana/loki/pkg/storage/tsdb/index"
 )
+
+// buildIndexB is a copy of BuildIndex that expects a *testing.B instead of a *testing.T.
+//
+// Once we fully migrate to Go v1.18 we can rewrite this to use generics.
+func buildIndexB(t *testing.B, cases []LoadableSeries) *TSDBIndex {
+	dir := t.TempDir()
+	b := index.NewBuilder()
+
+	for _, s := range cases {
+		b.AddSeries(s.Labels, s.Chunks)
+	}
+
+	require.Nil(t, b.Build(context.Background(), dir))
+
+	reader, err := index.NewFileReader(dir)
+	require.Nil(t, err)
+
+	return NewTSDBIndex(reader)
+}
+
+func BenchmarkCacheableIndex_Buffering(b *testing.B) {
+	cases := []LoadableSeries{
+		{
+			Labels: mustParseLabels(`{foo="bar"}`),
+			Chunks: []index.ChunkMeta{
+				{
+					MinTime:  0,
+					MaxTime:  3,
+					Checksum: 0,
+				},
+				{
+					MinTime:  1,
+					MaxTime:  4,
+					Checksum: 1,
+				},
+				{
+					MinTime:  2,
+					MaxTime:  5,
+					Checksum: 2,
+				},
+			},
+		},
+		{
+			Labels: mustParseLabels(`{foo="bar", bazz="buzz"}`),
+			Chunks: []index.ChunkMeta{
+				{
+					MinTime:  1,
+					MaxTime:  10,
+					Checksum: 3,
+				},
+			},
+		},
+		{
+			Labels: mustParseLabels(`{foo="bard", bazz="bozz", bonk="borb"}`),
+			Chunks: []index.ChunkMeta{
+				{
+					MinTime:  1,
+					MaxTime:  7,
+					Checksum: 4,
+				},
+			},
+		},
+	}
+
+	b.Run("GetChunkRef", func(b *testing.B) {
+		expected := []ChunkRef{
+			{
+				User:        "fake",
+				Fingerprint: &Fingerprint{int64(mustParseLabels(`{foo="bar"}`).Hash())},
+				Start:       0,
+				End:         3,
+				Checksum:    0,
+			},
+			{
+				User:        "fake",
+				Fingerprint: &Fingerprint{int64(mustParseLabels(`{foo="bar"}`).Hash())},
+				Start:       1,
+				End:         4,
+				Checksum:    1,
+			},
+			{
+				User:        "fake",
+				Fingerprint: &Fingerprint{int64(mustParseLabels(`{foo="bar"}`).Hash())},
+				Start:       2,
+				End:         5,
+				Checksum:    2,
+			},
+			{
+				User:        "fake",
+				Fingerprint: &Fingerprint{int64(mustParseLabels(`{foo="bar", bazz="buzz"}`).Hash())},
+				Start:       1,
+				End:         10,
+				Checksum:    3,
+			},
+		}
+
+		cache := cache.NewFifoCache(b.Name(), cache.FifoCacheConfig{MaxSizeItems: 20}, nil, log.NewNopLogger())
+		idx := buildIndexB(b, cases)
+		defer cache.Stop()
+		l := log.NewNopLogger()
+		nm := NewCacheMetrics(nil)
+		cacheableIndex := NewCacheableIndex(l, idx, cache, nm)
+
+		type getChunkRefCall func(userID string, from model.Time, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]ChunkRef, error)
+
+		ctx := context.Background()
+		var emptyBuf []ChunkRef
+		buf := make([]ChunkRef, 0, len(expected))
+		scenarios := []struct {
+			name       string
+			invocation getChunkRefCall
+		}{
+			{
+				name: "no buffer allocation",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]ChunkRef, error) {
+					return cacheableIndex.GetChunkRefs(ctx, userID, from, through, nil, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+			{
+				name: "empty buffer",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]ChunkRef, error) {
+					return cacheableIndex.GetChunkRefs(ctx, userID, from, through, emptyBuf, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+			{
+				name: "preallocated buffer",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]ChunkRef, error) {
+					return cacheableIndex.GetChunkRefs(ctx, userID, from, through, buf, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+		}
+
+		matchers := labels.MustNewMatcher(labels.MatchEqual, "foo", "bar")
+		for _, scenario := range scenarios {
+			b.Run(scenario.name, func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					emptyBuf = []ChunkRef{}
+					got, err := scenario.invocation("fake", 1, 5, nil, matchers)
+					require.NoError(b, err)
+					require.Equal(b, expected, got)
+				}
+			})
+		}
+	})
+
+	b.Run("Series", func(b *testing.B) {
+		expected := []Series{
+			{
+				Labels:      labelsToLabelsProto(mustParseLabels(`{foo="bar", bazz="buzz"}`), nil),
+				Fingerprint: Fingerprint{int64(mustParseLabels(`{foo="bar", bazz="buzz"}`).Hash())},
+			},
+		}
+
+		cache := cache.NewFifoCache(b.Name(), cache.FifoCacheConfig{MaxSizeItems: 20}, nil, log.NewNopLogger())
+		idx := buildIndexB(b, cases)
+		defer cache.Stop()
+		l := log.NewNopLogger()
+		nm := NewCacheMetrics(nil)
+		cacheableIndex := NewCacheableIndex(l, idx, cache, nm)
+
+		type seriesCall func(userID string, from model.Time, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]Series, error)
+
+		ctx := context.Background()
+		var emptyBuf []Series
+		buf := make([]Series, 0, len(expected))
+		scenarios := []struct {
+			name       string
+			invocation seriesCall
+		}{
+			{
+				name: "no buffer allocation",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]Series, error) {
+					return cacheableIndex.Series(ctx, userID, from, through, nil, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+			{
+				name: "empty buffer",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]Series, error) {
+					return cacheableIndex.Series(ctx, userID, from, through, emptyBuf, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+			{
+				name: "preallocated buffer",
+				invocation: func(userID string, from, through model.Time, shard *index.ShardAnnotation, matcher *labels.Matcher) ([]Series, error) {
+					return cacheableIndex.Series(ctx, userID, from, through, buf, shard, labels.MustNewMatcher(labels.MatchEqual, "foo", "bar"))
+				},
+			},
+		}
+
+		matchers := labels.MustNewMatcher(labels.MatchEqual, "foo", "bar")
+		for _, scenario := range scenarios {
+			b.Run(scenario.name, func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					emptyBuf = []Series{} // reset buffer everytime.
+					got, err := scenario.invocation("fake", 8, 9, nil, matchers)
+					require.NoError(b, err)
+					require.Equal(b, expected, got)
+				}
+			})
+		}
+	})
+}
 
 func TestCacheableIndex(t *testing.T) {
 	cases := []LoadableSeries{
