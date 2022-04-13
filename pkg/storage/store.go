c7d2009e8e7372c31c4f2bf59c2b9b697ab98ffb
Dylan Guedes <djmgguedes@gmail.com>
2022-04-13 18:27:09 -0300

    Apply suggestions from code review
    Rename `shouldRunBoltDBShipperClient` to `shouldUseIndexGatewayClient`.
    
    Co-authored-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -179,7 +179,7 @@ func (s *store) chunkClientForPeriod(p config.PeriodConfig) (client.Client, erro
 	return chunks, nil
 }
 
-func shouldRunBoltDBShipperClient(cfg Config) bool {
+func shouldUseIndexGatewayClient(cfg Config) bool {
 	if cfg.BoltDBShipperConfig.Mode != shipper.ModeReadOnly {
 		return false
 	}
@@ -217,7 +217,7 @@ func (s *store) storeForPeriod(p config.PeriodConfig, chunkClient client.Client,
 		index        stores.Index       = seriesdIndex
 	)
 
-	if shouldRunBoltDBShipperClient(s.cfg) {
+	if shouldUseIndexGatewayClient(s.cfg) {
 		// inject the index-gateway client into the index store
 		gw, err := shipper.NewGatewayClient(s.cfg.BoltDBShipperConfig.IndexGatewayClientConfig, indexClientReg, s.logger)
 		if err != nil {
