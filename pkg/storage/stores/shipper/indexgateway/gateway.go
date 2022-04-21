0f29a6ba12ef62c8626cd6366affbd9c20ec1fa3
Christian Haudum <christian.haudum@gmail.com>
2022-04-21 08:53:42 +0200

    Do not parse string of empty matchers (#5980)
    This fixes a bug in the index gateway when querying values for a label.
    Since the gRPC handler for LabelValuesForMetricName in the index gateway
    allows empty matchers in the LabelValuesForMetricNameRequest, we need to
    check if the matchers string is an empty matcher (`{}`) before we parse
    the string.
    
    This bug was introduced with the index gateway api refactoring in #5892
    
    Fixes: #5965
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/storage/stores/shipper/indexgateway/gateway.go
+++ pkg/storage/stores/shipper/indexgateway/gateway.go
@@ -333,9 +333,14 @@ func (g *Gateway) LabelValuesForMetricName(ctx context.Context, req *indexgatewa
 	if err != nil {
 		return nil, err
 	}
-	matchers, err := syntax.ParseMatchers(req.Matchers)
-	if err != nil {
-		return nil, err
+	var matchers []*labels.Matcher
+	// An empty matchers string cannot be parsed,
+	// therefore we check the string representation of the the matchers.
+	if req.Matchers != syntax.EmptyMatchers {
+		matchers, err = syntax.ParseMatchers(req.Matchers)
+		if err != nil {
+			return nil, err
+		}
 	}
 	names, err := g.indexQuerier.LabelValuesForMetricName(ctx, instanceID, req.From, req.Through, req.MetricName, req.LabelName, matchers...)
 	if err != nil {
