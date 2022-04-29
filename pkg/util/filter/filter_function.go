c861cf40f4e9b56921bdea126b23818c2dbd7796
Michel Hollands <michel.hollands@grafana.com>
2022-04-29 13:51:09 +0100

    Add comments to the Func definition
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>

--- pkg/util/filter/filter_function.go
+++ pkg/util/filter/filter_function.go
@@ -1,3 +1,5 @@
 package filter
 
+// Func is a function type that is the signature of a filter function used in log deletion.
+// This is used as a parameter in the Rebound function.
 type Func func(string) bool
