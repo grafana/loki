31ceaf191a11ab4b176b7ea3972ccc0b8202c0c0
Owen Diehl <ow.diehl@gmail.com>
2022-05-01 18:54:49 -0400

    renames skip -> ok in ProcessString Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/logql/log/metrics_extraction.go
+++ pkg/logql/log/metrics_extraction.go
@@ -249,8 +249,8 @@ func (sp *filteringStreamExtractor) Process(ts int64, line []byte) (float64, Lab
 			continue
 		}
 
-		_, _, skip := filter.pipeline.Process(ts, line)
-		if skip { //When the filter matches, don't run the next step
+		_, _, ok := filter.pipeline.Process(ts, line)
+		if ok { //When the filter matches, don't run the next step
 			return 0, nil, false
 		}
 	}
@@ -264,8 +264,8 @@ func (sp *filteringStreamExtractor) ProcessString(ts int64, line string) (float6
 			continue
 		}
 
-		_, _, skip := filter.pipeline.ProcessString(ts, line)
-		if skip { //When the filter matches, don't run the next step
+		_, _, ok := filter.pipeline.ProcessString(ts, line)
+		if ok { //When the filter matches, don't run the next step
 			return 0, nil, false
 		}
 	}
