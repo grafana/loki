badfc4877b91ef71b51537554d31104fa78cd94b
Periklis Tsirakidis <periklis@redhat.com>
2022-05-19 13:56:40 +0200

    Move gateway secret extract func to gateway package

--- operator/internal/handlers/internal/secrets/secrets.go
+++ operator/internal/handlers/internal/secrets/secrets.go
@@ -3,35 +3,10 @@ package secrets
 import (
 	"github.com/ViaQ/logerr/v2/kverrors"
 	lokiv1beta1 "github.com/grafana/loki/operator/api/v1beta1"
-	"github.com/grafana/loki/operator/internal/manifests"
 	"github.com/grafana/loki/operator/internal/manifests/storage"
 
 	corev1 "k8s.io/api/core/v1"
 )
-
-// ExtractGatewaySecret reads a k8s secret into a manifest tenant secret struct if valid.
-func ExtractGatewaySecret(s *corev1.Secret, tenantName string) (*manifests.TenantSecrets, error) {
-	// Extract and validate mandatory fields
-	clientID, ok := s.Data["clientID"]
-	if !ok {
-		return nil, kverrors.New("missing clientID field", "field", "clientID")
-	}
-	clientSecret, ok := s.Data["clientSecret"]
-	if !ok {
-		return nil, kverrors.New("missing clientSecret field", "field", "clientSecret")
-	}
-	issuerCAPath, ok := s.Data["issuerCAPath"]
-	if !ok {
-		return nil, kverrors.New("missing issuerCAPath field", "field", "issuerCAPath")
-	}
-
-	return &manifests.TenantSecrets{
-		TenantName:   tenantName,
-		ClientID:     string(clientID),
-		ClientSecret: string(clientSecret),
-		IssuerCAPath: string(issuerCAPath),
-	}, nil
-}
 
 // ExtractStorageSecret reads a k8s secret into a manifest object storage struct if valid.
 func ExtractStorageSecret(s *corev1.Secret, secretType lokiv1beta1.ObjectStorageSecretType) (*storage.Options, error) {
