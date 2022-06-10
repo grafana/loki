3ad847d4cb5b38f5facb0830c728c24947a8a128
fuling <fuling.lgz@alibaba-inc.com>
2022-06-10 15:42:45 +0800

    Hide this error message when ctx timeout occurs in s3.getObject

--- pkg/storage/batch.go
+++ pkg/storage/batch.go
@@ -717,6 +717,10 @@ func fetchLazyChunks(ctx context.Context, s config.SchemaConfig, chunks []*LazyC
 			}
 			chks, err := fetcher.FetchChunks(ctx, chks, keys)
 			if err != nil {
+				if ctx.Err() != nil {
+					errChan <- nil
+					return
+				}
 				level.Error(logger).Log("msg", "error fetching chunks", "err", err)
 				if isInvalidChunkError(err) {
 					level.Error(logger).Log("msg", "checksum of chunks does not match", "err", chunk.ErrInvalidChecksum)
