1a45983af5838cde764f70b284994b5e58d2b287
Gerard Vanloo <gvanloo@redhat.com>
2022-06-15 11:08:34 -0400

    Fixing webhook

--- operator/api/v1beta1/lokistack_webhook.go
+++ operator/api/v1beta1/lokistack_webhook.go
@@ -17,7 +17,7 @@ func (s *LokiStack) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-//+kubebuilder:webhook:path=/validate-loki-grafana-com-v1beta1-lokistack,mutating=false,failurePolicy=fail,sideEffects=None,groups=loki.grafana.com,resources=lokistack,verbs=create;update,versions=v1beta1,name=vlokistack.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-loki-grafana-com-v1beta1-lokistack,mutating=false,failurePolicy=fail,sideEffects=None,groups=loki.grafana.com,resources=lokistacks,verbs=create;update,versions=v1beta1,name=vlokistack.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &LokiStack{}
 
