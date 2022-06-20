a50e5e0f35d39d634112a2d14f6a4d9c561651ef
Periklis Tsirakidis <periklis@redhat.com>
2022-06-20 10:49:40 +0200

    Remove obsolete blank lines

--- operator/main.go
+++ operator/main.go
@@ -11,7 +11,6 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
-
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	configv1 "github.com/openshift/api/config/v1"
