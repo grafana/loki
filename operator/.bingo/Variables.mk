381f9d2142677f88054e948fe5fdf1bde0109554
Periklis Tsirakidis <periklis@redhat.com>
2022-06-22 10:31:31 +0200

    operator: Add e2e local development with kind (#6339)

--- operator/.bingo/Variables.mk
+++ operator/.bingo/Variables.mk
@@ -41,11 +41,23 @@ $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@echo "(re)installing $(GOBIN)/golangci-lint-v1.38.0"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.38.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
+KIND := $(GOBIN)/kind-v0.11.0
+$(KIND): $(BINGO_DIR)/kind.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/kind-v0.11.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=kind.mod -o=$(GOBIN)/kind-v0.11.0 "sigs.k8s.io/kind"
+
 KUSTOMIZE := $(GOBIN)/kustomize-v4.2.0
 $(KUSTOMIZE): $(BINGO_DIR)/kustomize.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/kustomize-v4.2.0"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=kustomize.mod -o=$(GOBIN)/kustomize-v4.2.0 "sigs.k8s.io/kustomize/kustomize/v4"
+
+LOGCLI := $(GOBIN)/logcli-v1.6.2-0.20220407212443-2d9d0ee236ea
+$(LOGCLI): $(BINGO_DIR)/logcli.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/logcli-v1.6.2-0.20220407212443-2d9d0ee236ea"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=logcli.mod -o=$(GOBIN)/logcli-v1.6.2-0.20220407212443-2d9d0ee236ea "github.com/grafana/loki/cmd/logcli"
 
 OPERATOR_SDK := $(GOBIN)/operator-sdk-v1.19.1
 $(OPERATOR_SDK): $(BINGO_DIR)/operator-sdk.mod
