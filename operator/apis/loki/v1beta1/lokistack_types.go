578d8d94d16ebaae491e9c5a749ad575032e9f26
Periklis Tsirakidis <periklis@nefeli.eu>
2022-06-24 07:41:48 +0200

    Update operator/apis/loki/v1beta1/lokistack_types.go
    Co-authored-by: Gerard Vanloo <gvanloo@redhat.com>

--- operator/apis/loki/v1beta1/lokistack_types.go
+++ operator/apis/loki/v1beta1/lokistack_types.go
@@ -1079,7 +1079,7 @@ func (src *LokiStack) ConvertTo(dstRaw conversion.Hub) error {
 	return nil
 }
 
-// ConvertFrom converts from the Hub version (v1beta1) to this version (v1).
+// ConvertFrom converts from the Hub version (v1) to this version (v1beta1).
 // nolint:golint
 func (dst *LokiStack) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1.LokiStack)
