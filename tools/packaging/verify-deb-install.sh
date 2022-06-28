e8f915cc26e4930d42eaba9a364c089b740cfb8b
Julien Duchesne <julien.duchesne@grafana.com>
2022-06-27 20:15:15 -0400

    test

--- tools/packaging/verify-deb-install.sh
+++ tools/packaging/verify-deb-install.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-sleep 15
 docker ps
 image="$(docker ps --filter ancestor=jrei/systemd-debian:12 --latest --format {{.ID}})"
 echo "Running on container: $image"
 
+dir="."
+if [ ! -z "$CI" ]; then
+    dir="/drone/src"
+fi
+echo "Running on directory: $dir"
+
 cat <<EOF | docker exec --interactive $image sh
-    ls .
-    ls ./dist
-    ls /drone/src
-
-
     # Install loki and check it's running
-    dpkg -i dist/loki_0.0.0~rc0_amd64.deb
+    dpkg -i ${dir}/dist/loki_0.0.0~rc0_amd64.deb
     [ "\$(systemctl is-active loki)" = "active" ] || exit 1
     # Install promtail and check it's running
-    dpkg -i dist/promtail_0.0.0~rc0_amd64.deb
+    dpkg -i ${dir}/dist/promtail_0.0.0~rc0_amd64.deb
     [ "\$(systemctl is-active promtail)" = "active" ] || exit 1
     # Install logcli
-    dpkg -i dist/logcli_0.0.0~rc0_amd64.deb
+    dpkg -i ${dir}/dist/logcli_0.0.0~rc0_amd64.deb
     # Check that there are logs (from the dpkg install)
     [ \$(logcli query \'{job="varlogs"}\' | wc -l) -gt 0 ] || exit 1
 EOF
