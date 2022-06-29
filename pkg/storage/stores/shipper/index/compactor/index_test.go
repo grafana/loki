d46a1fd38f0d6f9bb5b884a157d2dc7303912bb6
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2022-06-29 16:46:23 +0530

    implement and register compaction for boltdb-shipper index type

--- pkg/storage/stores/shipper/index/compactor/index_test.go
+++ pkg/storage/stores/shipper/index/compactor/index_test.go
@@ -1,42 +1,11 @@
-package retention
+package compactor
 
 import (
-	"fmt"
 	"strconv"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/loki/pkg/storage/config"
 )
-
-func Test_schemaPeriodForTable(t *testing.T) {
-	indexFromTime := func(t time.Time) string {
-		return fmt.Sprintf("%d", t.Unix()/int64(24*time.Hour/time.Second))
-	}
-	tests := []struct {
-		name          string
-		config        config.SchemaConfig
-		tableName     string
-		expected      config.PeriodConfig
-		expectedFound bool
-	}{
-		{"out of scope", schemaCfg, "index_" + indexFromTime(start.Time().Add(-24*time.Hour)), config.PeriodConfig{}, false},
-		{"first table", schemaCfg, "index_" + indexFromTime(dayFromTime(start).Time.Time()), schemaCfg.Configs[0], true},
-		{"4 hour after first table", schemaCfg, "index_" + indexFromTime(dayFromTime(start).Time.Time().Add(4*time.Hour)), schemaCfg.Configs[0], true},
-		{"second schema", schemaCfg, "index_" + indexFromTime(dayFromTime(start.Add(28*time.Hour)).Time.Time()), schemaCfg.Configs[1], true},
-		{"third schema", schemaCfg, "index_" + indexFromTime(dayFromTime(start.Add(75*time.Hour)).Time.Time()), schemaCfg.Configs[2], true},
-		{"now", schemaCfg, "index_" + indexFromTime(time.Now()), schemaCfg.Configs[3], true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			actual, actualFound := schemaPeriodForTable(tt.config, tt.tableName)
-			require.Equal(t, tt.expected, actual)
-			require.Equal(t, tt.expectedFound, actualFound)
-		})
-	}
-}
 
 func TestParseChunkID(t *testing.T) {
 	type resp struct {
