e7fb10f4567f191a8dbc2344b1e62c8072dd695e
Marco Pracucci <marco@pracucci.com>
2022-07-08 18:25:51 +0200

    Loki: Update `grafana/dskit` and `weaveworks/common` (#6643)
    * Upgrade dskit and weaveworks/common
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * 'go mod vendor' again
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- vendor/github.com/weaveworks/common/middleware/grpc_logging.go
+++ vendor/github.com/weaveworks/common/middleware/grpc_logging.go
@@ -20,13 +20,18 @@ const (
 type GRPCServerLog struct {
 	Log logging.Interface
 	// WithRequest will log the entire request rather than just the error
-	WithRequest bool
+	WithRequest              bool
+	DisableRequestSuccessLog bool
 }
 
 // UnaryServerInterceptor returns an interceptor that logs gRPC requests
 func (s GRPCServerLog) UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	begin := time.Now()
 	resp, err := handler(ctx, req)
+	if err == nil && s.DisableRequestSuccessLog {
+		return resp, nil
+	}
+
 	entry := user.LogWith(ctx, s.Log).WithFields(logging.Fields{"method": info.FullMethod, "duration": time.Since(begin)})
 	if err != nil {
 		if s.WithRequest {
@@ -47,6 +52,10 @@ func (s GRPCServerLog) UnaryServerInterceptor(ctx context.Context, req interface
 func (s GRPCServerLog) StreamServerInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	begin := time.Now()
 	err := handler(srv, ss)
+	if err == nil && s.DisableRequestSuccessLog {
+		return nil
+	}
+
 	entry := user.LogWith(ss.Context(), s.Log).WithFields(logging.Fields{"method": info.FullMethod, "duration": time.Since(begin)})
 	if err != nil {
 		if grpcUtils.IsCanceled(err) {
