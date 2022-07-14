762786dc29750fd008391d7bbcf2c66367cbba2d
Pablo Balbi <pablo.l.balbi@gmail.com>
2022-07-14 17:43:53 -0300

    inject tenant ID header

--- clients/pkg/promtail/targets/heroku/target.go
+++ clients/pkg/promtail/targets/heroku/target.go
@@ -3,6 +3,7 @@ package heroku
 import (
 	"flag"
 	"fmt"
+	lokiClient "github.com/grafana/loki/clients/pkg/promtail/client"
 	"net/http"
 	"strings"
 	"time"
@@ -130,12 +131,19 @@ func (h *Target) drain(w http.ResponseWriter, r *http.Request) {
 			ts = message.Timestamp
 		}
 
+		// If the incoming request carries the tenant id, inject it as the reserved label so it's used by the
+		// remote write client.
+		tenantIDHeaderValue := r.Header.Get("X-Scope-OrgID")
+		if tenantIDHeaderValue != "" {
+			lb.Set(lokiClient.ReservedLabelTenantID, tenantIDHeaderValue)
+		}
+
 		processed := relabel.Process(lb.Labels(), h.relabelConfigs...)
 
 		// Start with the set of labels fixed in the configuration
 		filtered := h.Labels().Clone()
 		for _, lbl := range processed {
-			if strings.HasPrefix(lbl.Name, "__") {
+			if strings.HasPrefix(lbl.Name, "__") && lbl.Name != lokiClient.ReservedLabelTenantID {
 				continue
 			}
 			filtered[model.LabelName(lbl.Name)] = model.LabelValue(lbl.Value)
