16cceb574be5ef5b0951d46a5f970e463bccb372
Anatoly Fayngelerin <afayngelerin@dropbox.com>
2022-07-25 12:42:16 -0400

    reroll after rebase

---
+++ pkg/storage/stores/shipper/index/compactor/metrics.go
@@ -0,0 +1,30 @@
+package compactor
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+
+type metrics struct {
+	compactTablesFileIngestLatencyMs        prometheus.Histogram
+	compactTablesFilesIngested            prometheus.Counter
+}
+
+func newMetrics(r prometheus.Registerer) *metrics {
+	m := metrics{
+		compactTablesFileIngestLatencyMs: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
+			Namespace: "loki_boltdb_shipper",
+			Name:      "compact_tables_file_ingest_latency_ms",
+
+		}),
+		compactTablesFilesIngested: promauto.With(r).NewCounter(prometheus.CounterOpts{
+			Namespace: "loki_boltdb_shipper",
+			Name:      "compact_tables_files_ingested",
+
+		}),
+
+	}
+
+	return &m
+}
