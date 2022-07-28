07aa95d703bcc25c05268d38fbd3e6254b3b9209
Kaviraj <kavirajkanagaraj@gmail.com>
2022-07-28 18:47:08 +0200

    update
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- pkg/storage/chunk/cache/cache.go
+++ pkg/storage/chunk/cache/cache.go
@@ -90,7 +90,7 @@ func IsRedisSet(cfg Config) bool {
 }
 
 func IsGroupCacheSet(cfg Config) bool {
-	return cfg.EnableGroupCache && cfg.GroupCache != nil
+	return cfg.EnableGroupCache && !cfg.EnableFifoCache
 }
 
 // IsCacheConfigured determines if memcached, redis, or groupcache have been configured
