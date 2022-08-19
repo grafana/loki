36069bd75a1566a6341fcf05a7bc220d21a7f515
Travis Patterson <travis.patterson@grafana.com>
2022-08-19 10:04:37 -0600

    move streamShard to distributor

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -3,8 +3,12 @@ package distributor
 import (
 	"context"
 	"flag"
+	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/prometheus/model/labels"
 
 	"github.com/grafana/dskit/limiter"
 	"github.com/grafana/dskit/ring"
@@ -33,6 +37,10 @@ import (
 )
 
 const (
+	// ShardLbName is the internal label to be used by Loki when dividing a stream into smaller pieces.
+	// Possible values are only increasing integers starting from 0.
+	ShardLbName = "__stream_shard__"
+
 	ringKey = "distributor"
 )
 
@@ -385,6 +393,42 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
+}
+
+// shardStream shards (divides) the given stream into smaller streams.
+//
+// It will derive N streams (and its entries) from the given stream, where N is the sharding size for the given stream.
+func shardStream(stream logproto.Stream, cfg Config, streamSharder StreamSharder, userID string) ([]uint32, []streamTracker) {
+	logger := util_log.Logger
+	shardsIter := streamSharder.ShardsFor(stream)
+
+	derivedKeys := make([]uint32, 0, shardsIter.NumShards())
+	derivedStreams := make([]streamTracker, 0, shardsIter.NumShards())
+
+	if cfg.ShardStreams.Debug {
+		level.Warn(logger).Log("msg", "sharding request with mode", "mode", cfg.ShardStreams.Mode)
+	}
+
+	for i := 0; i < shardsIter.NumShards(); i++ {
+		idx := shardsIter.NextShardID()
+		streamCopy := stream
+		lbs, err := syntax.ParseLabels(streamCopy.Labels)
+		if err != nil {
+			level.Error(logger).Log("msg", "couldn't extract labels from stream", "stream", streamCopy.Labels)
+			continue
+		}
+		lbs = append(lbs, labels.Label{Name: ShardLbName, Value: fmt.Sprintf("%d", idx)})
+		streamCopy.Labels = lbs.String()
+		streamCopy.Hash = lbs.Hash()
+		if cfg.ShardStreams.Debug {
+			level.Info(logger).Log("msg", "stream derived from sharding", "src-stream", stream.Labels, "derived-stream", streamCopy.Labels)
+		}
+
+		derivedKeys = append(derivedKeys, util.TokenFor(userID, streamCopy.Labels))
+		derivedStreams = append(derivedStreams, streamTracker{stream: streamCopy})
+	}
+
+	return derivedKeys, derivedStreams
 }
 
 // maxT returns the highest between two given timestamps.
