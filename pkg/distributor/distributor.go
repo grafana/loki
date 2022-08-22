dc01ef6c85335777860e4bdd107a6ffab00b8eee
Dylan Guedes <djmgguedes@gmail.com>
2022-08-22 17:56:09 -0300

    Modify shard comparison to be <= 1.

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -420,7 +420,7 @@ func min(x1, x2 int) int {
 func shardStream(stream logproto.Stream, cfg Config, streamSharder StreamSharder, userID string) ([]uint32, []streamTracker) {
 	logger := util_log.Logger
 	shards, ok := streamSharder.ShardsFor(stream)
-	if !ok || shards == 1 {
+	if !ok || shards <= 1 {
 		return []uint32{util.TokenFor(userID, stream.Labels)}, []streamTracker{{stream: stream}}
 	}
 
