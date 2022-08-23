2321da49b580dd00204fe9f19a0db837af8d19f0
Danny Kopping <danny.kopping@grafana.com>
2022-08-23 17:38:14 +0200

    Upgrade prometheus/client_golang and reconfigure to restore `go_sched.*` metrics (#6957)
    * Upgrading prometheus/client_golang to restore go_sched.* metrics
    
    These metrics were removed unintentionally in #6403
    
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>
    
    * Fixing test to match interface correctly
    
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>
    
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>

--- vendor/github.com/prometheus/client_golang/prometheus/get_pid.go
+++ vendor/github.com/prometheus/client_golang/prometheus/get_pid.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Prometheus Authors
+// Copyright 2015 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,17 +11,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !go1.15
-// +build !go1.15
+//go:build !js || wasm
+// +build !js wasm
 
-package collectors
+package prometheus
 
-import (
-	"database/sql"
+import "os"
 
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-func (c *dbStatsCollector) describeNewInGo115(ch chan<- *prometheus.Desc) {}
-
-func (c *dbStatsCollector) collectNewInGo115(ch chan<- prometheus.Metric, stats sql.DBStats) {}
+func getPIDFn() func() (int, error) {
+	pid := os.Getpid()
+	return func() (int, error) {
+		return pid, nil
+	}
+}
