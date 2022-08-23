d0bf5777ac8ccce04effef5059374a52f225e641
Dylan Guedes <djmgguedes@gmail.com>
2022-08-23 11:06:39 -0300

    Add docstring explaining why we use max(shards*2, 2).

--- pkg/distributor/streamsharder.go
+++ pkg/distributor/streamsharder.go
@@ -29,11 +29,15 @@ func (s *streamSharder) ShardsFor(stream logproto.Stream) (int, bool) {
 	return 0, false
 }
 
+// IncreaseShardsFor shards the given stream by doubling its number of shards.
 func (s *streamSharder) IncreaseShardsFor(stream logproto.Stream) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	shards := s.streams[stream.Labels]
+
+	// Since the number of shards of a stream that is being sharded for the first time is 0,
+	// we assign to it shards = max(shards*2, 2) such that its number of shards will be no less than 2.
 	s.streams[stream.Labels] = max(shards*2, 2)
 }
 
