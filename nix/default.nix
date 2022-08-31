efd1731af5952db61471a59b6a938154ea84295a
Trevor Whitney <trevorjwhitney@gmail.com>
2022-08-31 16:14:46 -0600

    Image tag wants the short sha

--- nix/default.nix
+++ nix/default.nix
@@ -4,11 +4,13 @@ let
 
   # self.rev is only set on a clean git tree
   gitRevision = if (self ? rev) then self.rev else buildVars.gitRevision;
+
+  # version is the "short" git sha
   version = with pkgs.lib;
     (strings.concatStrings (lists.take 8 (strings.stringToCharacters gitRevision)));
 
   imageTag = "${buildVars.gitBranch}-${
-      if (self ? rev) then gitRevision else buildVars.gitRevision
+      if (self ? rev) then version else buildVars.gitRevision
     }";
 in
 {
