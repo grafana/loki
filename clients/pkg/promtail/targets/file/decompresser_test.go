8204062202ab8798a2d0957fd553ead576284080
Dylan Guedes <djmgguedes@gmail.com>
2022-09-01 15:06:22 -0300

    Add readLines benchmark.

---
+++ clients/pkg/promtail/targets/file/decompresser_test.go
@@ -0,0 +1,74 @@
+package file
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/grafana/loki/clients/pkg/promtail/api"
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/atomic"
+)
+
+type noopClient struct {
+	noopChan chan api.Entry
+	wg       sync.WaitGroup
+	once     sync.Once
+}
+
+func (n noopClient) Chan() chan<- api.Entry {
+	return n.noopChan
+}
+
+func (n noopClient) Stop() {
+	n.once.Do(func() { close(n.noopChan) })
+}
+
+func newNoopClient() *noopClient {
+	c := &noopClient{noopChan: make(chan api.Entry)}
+	c.wg.Add(1)
+	go func() {
+		defer c.wg.Done()
+		for range c.noopChan {
+			// noop
+		}
+	}()
+	return c
+}
+
+func BenchmarkReadlines(b *testing.B) {
+	entryHandler := newNoopClient()
+
+	scenarios := []struct {
+		name string
+		file string
+	}{
+		{
+			name: "2000 lines of log compressed",
+			file: "test_fixtures/short-access.tar.gz",
+		},
+		{
+			name: "100000 lines of log compressed",
+			file: "test_fixtures/long-access.tar.gz",
+		},
+	}
+
+	for _, tc := range scenarios {
+		b.Run(tc.name, func(b *testing.B) {
+			decBase := &decompressor{
+				logger:  log.NewNopLogger(),
+				running: atomic.NewBool(false),
+				handler: entryHandler,
+				path:    tc.file,
+			}
+
+			for i := 0; i < b.N; i++ {
+				newDec := decBase
+				newDec.metrics = NewMetrics(prometheus.NewRegistry())
+				newDec.done = make(chan struct{})
+				newDec.readLines()
+				<-newDec.done
+			}
+		})
+	}
+}
