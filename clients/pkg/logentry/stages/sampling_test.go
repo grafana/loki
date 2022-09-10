0ed01d0a21135d496bfc8b2c7e9493fb888721eb
李国忠 <249032432@qq.com>
2022-09-10 15:48:53 +0800

    Update sampling_test.go

--- clients/pkg/logentry/stages/sampling_test.go
+++ clients/pkg/logentry/stages/sampling_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	
+
 	util_log "github.com/grafana/loki/pkg/util/log"
 )
 
