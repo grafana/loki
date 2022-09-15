grafana/loki

commit 461f844044048df3160b209b66596072fd3d3e23

[bugfix] fix query_frontend init fail error (#7013)
Co-authored-by: Ashwanth <iamashwanth@gmail.com>

pkg/loki/modules.go
@@ -662,17 +662,20 @@ func (disabledShuffleShardingLimits) MaxQueriersPerUser(userID string) int { ret
 func (t *Loki) initQueryFrontendTripperware() (_ services.Service, err error) {
 	level.Debug(util_log.Logger).Log("msg", "initializing query frontend tripperware")
 
-	cacheGenClient, err := t.cacheGenClient()
-	if err != nil {
-		return nil, err
+	var genLoader *generationnumber.GenNumberLoader
+	if t.supportIndexDeleteRequest() {
+		cacheGenClient, err := t.cacheGenClient()
+		if err != nil {
+			return nil, err
+		}
+		genLoader = generationnumber.NewGenNumberLoader(cacheGenClient, prometheus.DefaultRegisterer)
 	}
 
 	tripperware, stopper, err := queryrange.NewTripperware(
 		t.Cfg.QueryRange,
 		util_log.Logger,
 		t.overrides,
-		t.Cfg.SchemaConfig,
-		generationnumber.NewGenNumberLoader(cacheGenClient, prometheus.DefaultRegisterer),
+		t.Cfg.SchemaConfig, genLoader,
 		prometheus.DefaultRegisterer,
 	)
 	if err != nil {
@@ -684,12 +687,19 @@ func (t *Loki) initQueryFrontendTripperware() (_ services.Service, err error) {
 	return services.NewIdleService(nil, nil), nil
 }
 
+func (t *Loki) supportIndexDeleteRequest() bool {
+	// TODO(owen-d): enable delete request storage in tsdb
+	if config.UsingTSDB(t.Cfg.SchemaConfig.Configs) {
+		return false
+	}
+	return config.UsingBoltdbShipper(t.Cfg.SchemaConfig.Configs)
+}
+
 func (t *Loki) cacheGenClient() (generationnumber.CacheGenClient, error) {
 	compactorAddress, err := t.compactorAddress()
 	if err != nil {
 		return nil, err
 	}
-
 	return generationnumber.NewGenNumberClient(compactorAddress, &http.Client{Timeout: 5 * time.Second})
 }
 
@@ -1093,12 +1103,7 @@ func (t *Loki) initUsageReport() (services.Service, error) {
 }
 
 func (t *Loki) deleteRequestsClient(clientType string, limits *validation.Overrides) (deletion.DeleteRequestsClient, error) {
-	// TODO(owen-d): enable delete request storage in tsdb
-	if config.UsingTSDB(t.Cfg.SchemaConfig.Configs) {
-		return deletion.NewNoOpDeleteRequestsStore(), nil
-	}
-
-	if !config.UsingBoltdbShipper(t.Cfg.SchemaConfig.Configs) {
+	if !t.supportIndexDeleteRequest() {
 		return deletion.NewNoOpDeleteRequestsStore(), nil
 	}
 

commit 2766e0a8c7a0b292e3d5ad86d2611d7aa1d61c33

fixes nil interface value with non-nil type in cache construction (#7175)
Fixes a bug introduced in https://github.com/grafana/loki/pull/7013

This will panic when the interface's _value_ is nil because it's been
assigned a concrete type already and then passed as an interface.
Instead, we treat it as a nil interface with no type bound to it:

```
panic: runtime error: invalid memory address or nil pointer dereference
goroutine 40570 [running]:
github.com/grafana/loki/pkg/util/server.onPanic({0x2209320, 0x3dfae20})
	/src/enterprise-logs/vendor/github.com/grafana/loki/pkg/util/server/recovery.go:41 +0x53
github.com/grafana/loki/pkg/util/server.glob..func1.1.1()
	/src/enterprise-logs/vendor/github.com/grafana/loki/pkg/util/server/recovery.go:29 +0x3a
panic({0x2209320, 0x3dfae20})
	/usr/local/go/src/runtime/panic.go:838 +0x207
github.com/grafana/loki/pkg/storage/stores/indexshipper/compactor/generationnumber.(*GenNumberLoader).getCacheGenNumber(0x0, {0xc0081e14d7, 0x2})
	/src/enterprise-logs/vendor/github.com/grafana/loki/pkg/storage/stores/indexshipper/compactor/generationnumber/gennumber_loader.go:120 +0x60
github.com/grafana/loki/pkg/storage/stores/indexshipper/compactor/generationnumber.(*GenNumberLoader).getCacheGenNumbersPerTenants(0x2?, {0xc000e8c4d0?, 0x1, 0x0?})
	/src/enterprise-logs/vendor/github.com/grafana/loki/pkg/storage/stores/indexshipper/compactor/generationnumber/gennumber_loader.go:98 +0xe7
github.com/grafana/loki/pkg/storage/stores/indexshipper/compactor/generationnumber.(*GenNumberLoader).GetResultsCacheGenNumber(0x2b26298?, {0xc000e8c4d0?, 0xc0050ad080?, 0x2dcdeeb9bcc040e4?})
```

pkg/loki/modules.go
@@ -40,6 +40,7 @@ import (
 	"github.com/grafana/loki/pkg/lokifrontend/frontend/v2/frontendv2pb"
 	"github.com/grafana/loki/pkg/querier"
 	"github.com/grafana/loki/pkg/querier/queryrange"
+	"github.com/grafana/loki/pkg/querier/queryrange/queryrangebase"
 	"github.com/grafana/loki/pkg/ruler"
 	base_ruler "github.com/grafana/loki/pkg/ruler/base"
 	"github.com/grafana/loki/pkg/runtime"
@@ -663,7 +664,7 @@ func (disabledShuffleShardingLimits) MaxQueriersPerUser(userID string) int { ret
 func (t *Loki) initQueryFrontendTripperware() (_ services.Service, err error) {
 	level.Debug(util_log.Logger).Log("msg", "initializing query frontend tripperware")
 
-	var genLoader *generationnumber.GenNumberLoader
+	var genLoader queryrangebase.CacheGenNumberLoader
 	if t.supportIndexDeleteRequest() {
 		cacheGenClient, err := t.cacheGenClient()
 		if err != nil {