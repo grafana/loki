188e7d52af55beb1f15130bf66e7a8d112586193
Michel Hollands <michel.hollands@grafana.com>
2022-09-30 15:49:27 +0100

    Handle empty slice
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>

--- pkg/storage/stores/indexshipper/compactor/deletion/delete_requests_manager_test.go
+++ pkg/storage/stores/indexshipper/compactor/deletion/delete_requests_manager_test.go
@@ -485,9 +485,10 @@ func TestDeleteRequestsManager_IntervalMayHaveExpiredChunks(t *testing.T) {
 
 type mockDeleteRequestsStore struct {
 	DeleteRequestsStore
-	deleteRequests []DeleteRequest
-	addReqs        []DeleteRequest
-	addErr         error
+	deleteRequests           []DeleteRequest
+	addReqs                  []DeleteRequest
+	addErr                   error
+	returnZeroDeleteRequests bool
 
 	removeReqs []DeleteRequest
 	removeErr  error
@@ -508,6 +509,9 @@ func (m *mockDeleteRequestsStore) GetDeleteRequestsByStatus(_ context.Context, _
 
 func (m *mockDeleteRequestsStore) AddDeleteRequestGroup(ctx context.Context, reqs []DeleteRequest) ([]DeleteRequest, error) {
 	m.addReqs = reqs
+	if m.returnZeroDeleteRequests {
+		return []DeleteRequest{}, m.addErr
+	}
 	return m.addReqs, m.addErr
 }
 
--- pkg/storage/stores/indexshipper/compactor/deletion/request_handler.go
+++ pkg/storage/stores/indexshipper/compactor/deletion/request_handler.go
@@ -81,6 +81,12 @@ func (dm *DeleteRequestHandler) AddDeleteRequestHandler(w http.ResponseWriter, r
 		return
 	}
 
+	if len(createdDeleteRequests) == 0 {
+		level.Error(util_log.Logger).Log("msg", "zero delete requests created", "user", userID, "query", query)
+		http.Error(w, "Zero delete requests were created due to an internal error. Please contact support.", http.StatusInternalServerError)
+		return
+	}
+
 	level.Info(util_log.Logger).Log(
 		"msg", "delete request for user added",
 		"delete_request_id", createdDeleteRequests[0].RequestID,
