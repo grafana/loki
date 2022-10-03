6b71d0aed3fe9141128731d6db4abffc87113e35
Dylan Guedes <djmgguedes@gmail.com>
2022-10-03 15:25:42 -0300

    Add JSON tags to variables.

--- pkg/distributor/shardstreams/config.go
+++ pkg/distributor/shardstreams/config.go
@@ -7,16 +7,17 @@ import (
 )
 
 type Config struct {
-	Enabled        bool `yaml:"enabled"`
-	LoggingEnabled bool `yaml:"logging_enabled"`
+	Enabled        bool `yaml:"enabled" json:"enabled"`
+	LoggingEnabled bool `yaml:"logging_enabled" json:"logging_enabled"`
 
 	// DesiredRate is the threshold used to shard the stream into smaller pieces.
 	// Expected to be in bytes.
-	DesiredRate flagext.ByteSize `yaml:"desired_rate"`
+	DesiredRate flagext.ByteSize `yaml:"desired_rate" json:"desired_rate"`
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, fs *flag.FlagSet) {
 	fs.BoolVar(&cfg.Enabled, prefix+".enabled", false, "Automatically shard streams to keep them under the per-stream rate limit")
 	fs.BoolVar(&cfg.LoggingEnabled, prefix+".logging-enabled", false, "Enable logging when sharding streams")
+	cfg.DesiredRate.Set("3mb")
 	fs.Var(&cfg.DesiredRate, prefix+".desired-rate", "threshold used to cut a new shard. Default (3MB) means if a rate is above 3MB, it will be sharded.")
 }
