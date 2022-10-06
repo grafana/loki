7979cfbe076da067ab4057b513f1df7502eb4404
Travis Patterson <travis.patterson@grafana.com>
2022-10-06 16:12:38 -0600

    Implement Distributor Rate Store (#7349)
    The `RateStore` concurrently queries all ingester `GetStreamRate` APIs
    and stores the results. `RateStore` deduplicates replicas and combines
    sharded streams so a caller of `RateFor` gets an unsharded view of a
    stream's rate.

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -47,7 +47,8 @@ import (
 
 const (
 	// RingKey is the key under which we store the ingesters ring in the KVStore.
-	RingKey = "ring"
+	RingKey            = "ring"
+	internalInstanceID = "internal"
 )
 
 // ErrReadOnly is returned when the ingester is shutting down and a push was
@@ -192,10 +193,11 @@ type Ingester struct {
 	clientConfig  client.Config
 	tenantConfigs *runtime.TenantConfigs
 
-	shutdownMtx  sync.Mutex // Allows processes to grab a lock and prevent a shutdown
-	instancesMtx sync.RWMutex
-	instances    map[string]*instance
-	readonly     bool
+	shutdownMtx      sync.Mutex // Allows processes to grab a lock and prevent a shutdown
+	instancesMtx     sync.RWMutex
+	instances        map[string]*instance
+	internalInstance *instance // used for non-user communication from the distributors
+	readonly         bool
 
 	lifecycler        *ring.Lifecycler
 	lifecyclerWatcher *services.FailureWatcher
@@ -623,14 +625,7 @@ func (i *Ingester) Push(ctx context.Context, req *logproto.PushRequest) (*logpro
 // GetStreamRates returns a response containing all streams and their current rate
 // TODO: It might be nice for this to be human readable, eventually: Sort output and return labels, too?
 func (i *Ingester) GetStreamRates(ctx context.Context, req *logproto.StreamRatesRequest) (*logproto.StreamRatesResponse, error) {
-	instanceID, err := tenant.TenantID(ctx)
-	if err != nil {
-		return nil, err
-	} else if i.readonly {
-		return nil, ErrReadOnly
-	}
-
-	instance, err := i.GetOrCreateInstance(instanceID)
+	instance, err := i.getOrCreateInternalInstance()
 	if err != nil {
 		return &logproto.StreamRatesResponse{}, err
 	}
@@ -657,6 +652,25 @@ func (i *Ingester) GetOrCreateInstance(instanceID string) (*instance, error) { /
 		activeTenantsStats.Set(int64(len(i.instances)))
 	}
 	return inst, nil
+}
+
+func (i *Ingester) getOrCreateInternalInstance() (*instance, error) { //nolint:revive
+	if inst, ok := i.getInternalInstance(); ok {
+		return inst, nil
+	}
+
+	i.instancesMtx.Lock()
+	defer i.instancesMtx.Unlock()
+
+	if i.internalInstance == nil {
+		inst, err := newInstance(&i.cfg, i.periodicConfigs, internalInstanceID, i.limiter, i.tenantConfigs, i.wal, i.metrics, i.flushOnShutdownSwitch, i.chunkFilter)
+		if err != nil {
+			return nil, err
+		}
+		i.internalInstance = inst
+	}
+
+	return i.internalInstance, nil
 }
 
 // Query the ingests for log streams matching a set of matchers.
@@ -953,6 +967,17 @@ func (i *Ingester) getInstanceByID(id string) (*instance, bool) {
 
 	inst, ok := i.instances[id]
 	return inst, ok
+}
+
+func (i *Ingester) getInternalInstance() (*instance, bool) {
+	i.instancesMtx.RLock()
+	defer i.instancesMtx.RUnlock()
+
+	if i.internalInstance != nil {
+		return i.internalInstance, true
+	}
+
+	return nil, false
 }
 
 func (i *Ingester) getInstances() []*instance {
