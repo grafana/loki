f8e6c54fb3cced6861224b9cf56f448ef59a1318
Dylan Guedes <djmgguedes@gmail.com>
2022-10-13 11:37:43 -0300

    Introduce new shardStreams rate limit.

--- pkg/distributor/shardstreams/config.go
+++ pkg/distributor/shardstreams/config.go
@@ -13,6 +13,8 @@ type Config struct {
 	// DesiredRate is the threshold used to shard the stream into smaller pieces.
 	// Expected to be in bytes.
 	DesiredRate flagext.ByteSize `yaml:"desired_rate" json:"desired_rate"`
+
+	RateLimit flagext.ByteSize `yaml:"rate_limit" json:"rate_limit"`
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, fs *flag.FlagSet) {
@@ -20,4 +22,5 @@ func (cfg *Config) RegisterFlagsWithPrefix(prefix string, fs *flag.FlagSet) {
 	fs.BoolVar(&cfg.LoggingEnabled, prefix+".logging-enabled", false, "Enable logging when sharding streams")
 	cfg.DesiredRate.Set("3mb") //nolint:errcheck
 	fs.Var(&cfg.DesiredRate, prefix+".desired-rate", "threshold used to cut a new shard. Default (3MB) means if a rate is above 3MB, it will be sharded.")
+	fs.Var(&cfg.RateLimit, prefix+".rate-limit", "Limit used by an ingester to reject a stream in case sharding is enabled (per-stream is ignored). If this isn't set, rely on the per-stream-rate-limit instead, so no default.")
 }
