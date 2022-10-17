2b1e4d4c1b859b378c9e4c18e52b1b8fb931bf62
Travis Patterson <travis.patterson@grafana.com>
2022-10-17 15:58:09 -0600

    Move middleware close to where it's used

--- pkg/util/server/middleware.go
+++ pkg/util/server/middleware.go
@@ -3,11 +3,8 @@ package server
 import (
 	"net/http"
 
-	"github.com/grafana/dskit/tenant"
 	"github.com/weaveworks/common/httpgrpc"
 	"github.com/weaveworks/common/middleware"
-
-	"github.com/grafana/loki/pkg/querier/queryrange/queryrangebase"
 )
 
 // NewPrepopulateMiddleware creates a middleware which will parse incoming http forms.
@@ -31,24 +28,6 @@ func ResponseJSONMiddleware() middleware.Interface {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 			next.ServeHTTP(w, req)
-		})
-	})
-}
-
-// middleware for setting cache gen header to let consumer of response know all previous responses could be invalid due to delete operation
-func CacheGenNumberHeaderSetterMiddleware(cacheGenNumbersLoader queryrangebase.CacheGenNumberLoader) middleware.Interface {
-	return middleware.Func(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userIDs, err := tenant.TenantIDs(r.Context())
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusUnauthorized)
-				return
-			}
-
-			cacheGenNumber := cacheGenNumbersLoader.GetResultsCacheGenNumber(userIDs)
-
-			w.Header().Set(queryrangebase.ResultsCacheGenNumberHeaderName, cacheGenNumber)
-			next.ServeHTTP(w, r)
 		})
 	})
 }
