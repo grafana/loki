2f6fb739af6e2576ffeb3d4818b057a3ac872354
MarNicGit <47538428+MarNicGit@users.noreply.github.com>
2022-10-19 00:41:42 +0200

    Edited test to use unique values

--- clients/pkg/promtail/targets/windows/target_test.go
+++ clients/pkg/promtail/targets/windows/target_test.go
@@ -134,10 +134,10 @@ func Test_renderEntries(t *testing.T) {
 		{
 			Source:        win_eventlog.Provider{Name: "Application"},
 			EventID:       10,
-			Version:       10,
-			Level:         10,
-			Task:          10,
-			Opcode:        10,
+			Version:       20,
+			Level:         30,
+			Task:          40,
+			Opcode:        50,
 			Keywords:      "keywords",
 			TimeCreated:   win_eventlog.TimeCreated{SystemTime: time.Unix(0, 1).UTC().Format(time.RFC3339Nano)},
 			EventRecordID: 11,
@@ -156,7 +156,7 @@ func Test_renderEntries(t *testing.T) {
 			Labels: model.LabelSet{"channel": "channel", "computer": "local", "job": "windows-events"},
 			Entry: logproto.Entry{
 				Timestamp: time.Unix(0, 1).UTC(),
-				Line:      `{"source":"Application","channel":"channel","computer":"local","event_id":10,"version":10,"level":10,"task":10,"opCode":10,"keywords":"keywords","timeCreated":"1970-01-01T00:00:00.000000001Z","eventRecordID":11,"correlation":{"activityID":"some activity","relatedActivityID":"some related activity"},"execution":{"processId":1,"threadId":5},"security":{"userId":"1"},"user_data":"eventdata","event_data":"eventdata","message":"message"}`,
+				Line:      `{"source":"Application","channel":"channel","computer":"local","event_id":10,"version":20,"level":30,"task":40,"opCode":50,"keywords":"keywords","timeCreated":"1970-01-01T00:00:00.000000001Z","eventRecordID":11,"correlation":{"activityID":"some activity","relatedActivityID":"some related activity"},"execution":{"processId":1,"threadId":5},"security":{"userId":"1"},"user_data":"eventdata","event_data":"eventdata","message":"message"}`,
 			},
 		},
 	}, entries)
