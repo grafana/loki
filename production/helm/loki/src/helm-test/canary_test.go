0d10a58cd2afb5d50092c54355f37fb70e0a86d9
Trevor Whitney <trevorjwhitney@gmail.com>
2022-10-21 10:39:29 -0600

    Refactor canary_test

--- production/helm/loki/src/helm-test/canary_test.go
+++ production/helm/loki/src/helm-test/canary_test.go
@@ -14,6 +14,45 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestCanary(t *testing.T) {
+	totalEntriesQuery := "sum(loki_canary_entries_total)"
+	totalEntriesMissingQuery := "sum(loki_canary_missing_entries_total)"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	t.Cleanup(func() {
+		cancel()
+	})
+
+	t.Run("Canary should have entries", func(t *testing.T) {
+		client := newClient(t)
+
+		eventually(t, func() error {
+			result, _, err := client.Query(ctx, totalEntriesQuery, time.Now(), v1.WithTimeout(5*time.Second))
+			if err != nil {
+				return err
+			}
+			return testResult(t, result, totalEntriesQuery, func(v model.SampleValue) bool {
+				return v > 0
+			}, fmt.Sprintf("Expected %s to be greater than 0", totalEntriesQuery))
+		}, "Expected Loki Canary to have entries")
+	})
+
+	t.Run("Canary should not have missed any entries", func(t *testing.T) {
+		client := newClient(t)
+
+		eventually(t, func() error {
+			result, _, err := client.Query(ctx, totalEntriesMissingQuery, time.Now(), v1.WithTimeout(5*time.Second))
+			if err != nil {
+				return err
+			}
+			return testResult(t, result, totalEntriesMissingQuery, func(v model.SampleValue) bool {
+				return v == 0
+			}, fmt.Sprintf("Expected %s to equal 0", totalEntriesMissingQuery))
+		}, "Expected Loki Canary to not have any missing entries")
+	})
+}
+
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
@@ -21,10 +60,10 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-func testResult(result model.Value, query string, test func(model.SampleValue) bool, msg string) error {
+func testResult(t *testing.T, result model.Value, query string, test func(model.SampleValue) bool, msg string) error {
 	if v, ok := result.(model.Vector); ok {
 		for _, s := range v {
-			fmt.Printf("%s => %v\n", query, s.Value)
+			t.Logf("%s => %v\n", query, s.Value)
 			if !test(s.Value) {
 				return errors.New(msg)
 			}
@@ -36,67 +75,28 @@ func testResult(result model.Value, query string, test func(model.SampleValue) b
 	return fmt.Errorf("unexpected Prometheus result type: %v ", result.Type())
 }
 
-func TestCanary(t *testing.T) {
-	totalEntriesQuery := "sum(loki_canary_entries_total)"
-	totalEntriesMissingQuery := "sum(loki_canary_missing_entries_total)"
+func newClient(t *testing.T) v1.API {
+	address := os.Getenv("CANARY_PROMETHEUS_ADDRESS")
+	require.NotEmpty(t, address, "CANARY_PROMETHEUS_ADDRESS must be set to a valid prometheus address")
 
-	testContext := func(t *testing.T) v1.API {
-		address := os.Getenv("CANARY_PROMETHEUS_ADDRESS")
-		require.NotEmpty(t, address, "CANARY_PROMETHEUS_ADDRESS must be set to a valid prometheus address")
-
-		client, err := api.NewClient(api.Config{
-			Address: address,
-		})
-		require.NoError(t, err, "Failed to create Loki Canary client")
-
-		return v1.NewAPI(client)
-	}
-
-	eventually := func(t *testing.T, test func() error, msg string) {
-		timeout := getEnv("CANARY_TEST_TIMEOUT", "1m")
-		timeoutDuration, err := time.ParseDuration(timeout)
-		require.NoError(t, err, "Failed to parse timeout. Please set CANARY_TEST_TIMEOUT to a valid duration.")
-
-		require.Eventually(t, func() bool {
-			queryError := test()
-			if queryError != nil {
-				fmt.Printf("Query failed\n%+v\n", queryError)
-			}
-			return queryError == nil
-		}, timeoutDuration, 5*time.Second, msg)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
-	t.Cleanup(func() {
-		cancel()
+	client, err := api.NewClient(api.Config{
+		Address: address,
 	})
+	require.NoError(t, err, "Failed to create Loki Canary client")
 
-	t.Run("Canary should have entries", func(t *testing.T) {
-		client := testContext(t)
+	return v1.NewAPI(client)
+}
 
-		eventually(t, func() error {
-			result, _, err := client.Query(ctx, totalEntriesQuery, time.Now(), v1.WithTimeout(5*time.Second))
-			if err != nil {
-				return err
-			}
-			return testResult(result, totalEntriesQuery, func(v model.SampleValue) bool {
-				return v > 0
-			}, fmt.Sprintf("Expected %s to be greater than 0", totalEntriesQuery))
-		}, "Expected Loki Canary to have entries")
-	})
+func eventually(t *testing.T, test func() error, msg string) {
+	timeout := getEnv("CANARY_TEST_TIMEOUT", "1m")
+	timeoutDuration, err := time.ParseDuration(timeout)
+	require.NoError(t, err, "Failed to parse timeout. Please set CANARY_TEST_TIMEOUT to a valid duration.")
 
-	t.Run("Canary should not have missed any entries", func(t *testing.T) {
-		client := testContext(t)
-
-		eventually(t, func() error {
-			result, _, err := client.Query(ctx, totalEntriesMissingQuery, time.Now(), v1.WithTimeout(5*time.Second))
-			if err != nil {
-				return err
-			}
-			return testResult(result, totalEntriesMissingQuery, func(v model.SampleValue) bool {
-				return v == 0
-			}, fmt.Sprintf("Expected %s to equal 0", totalEntriesMissingQuery))
-		}, "Expected Loki Canary to not have any missing entries")
-	})
+	require.Eventually(t, func() bool {
+		queryError := test()
+		if queryError != nil {
+			t.Logf("Query failed\n%+v\n", queryError)
+		}
+		return queryError == nil
+	}, timeoutDuration, 5*time.Second, msg)
 }
