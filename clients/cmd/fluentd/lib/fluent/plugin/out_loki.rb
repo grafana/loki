0f5dd2bceb28ddd44f1202205e0e450c9581c3b3
Christian Haudum <christian.haudum@gmail.com>
2022-10-21 13:49:28 +0200

    Revert "fluentd: Add un-escaping of control characters in JSON" (#7468)
    Reverts grafana/loki#7396 according to discussing on PR

--- clients/cmd/fluentd/lib/fluent/plugin/out_loki.rb
+++ clients/cmd/fluentd/lib/fluent/plugin/out_loki.rb
@@ -278,7 +278,6 @@ module Fluent
           case @line_format
           when :json
             line = Yajl.dump(record)
-            line = line.gsub("\\n", "\n").gsub("\\r", "\r").gsub("\\s", "\s").gsub("\\t", "\t")
           when :key_value
             formatted_labels = []
             record.each do |k, v|
