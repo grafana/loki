7b3a178c281c99a22462b3e40aa9fc753e302f33
Periklis Tsirakidis <periklis@redhat.com>
2022-11-03 20:12:07 +0100

    operator: Add support for built-in-cert-rotation for all internal lokistack encryption (#7064)

--- operator/internal/manifests/query-frontend.go
+++ operator/internal/manifests/query-frontend.go
@@ -32,6 +32,13 @@ func BuildQueryFrontend(opts Options) ([]client.Object, error) {
 		}
 	}
 
+	if opts.Gates.HTTPEncryption || opts.Gates.GRPCEncryption {
+		caBundleName := signingCABundleName(opts.Name)
+		if err := configureServiceCA(&deployment.Spec.Template.Spec, caBundleName); err != nil {
+			return nil, err
+		}
+	}
+
 	return []client.Object{
 		deployment,
 		NewQueryFrontendGRPCService(opts),
@@ -129,7 +136,7 @@ func NewQueryFrontendDeployment(opts Options) *appsv1.Deployment {
 	}
 
 	l := ComponentLabels(LabelQueryFrontendComponent, opts.Name)
-	a := commonAnnotations(opts.ConfigSHA1)
+	a := commonAnnotations(opts.ConfigSHA1, opts.CertRotationRequiredAt)
 
 	return &appsv1.Deployment{
 		TypeMeta: metav1.TypeMeta{
@@ -171,9 +178,8 @@ func NewQueryFrontendGRPCService(opts Options) *corev1.Service {
 			APIVersion: corev1.SchemeGroupVersion.String(),
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:        serviceName,
-			Labels:      labels,
-			Annotations: serviceAnnotations(serviceName, opts.Gates.OpenShift.ServingCertsService),
+			Name:   serviceName,
+			Labels: labels,
 		},
 		Spec: corev1.ServiceSpec{
 			ClusterIP: "None",
@@ -201,9 +207,8 @@ func NewQueryFrontendHTTPService(opts Options) *corev1.Service {
 			APIVersion: corev1.SchemeGroupVersion.String(),
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:        serviceName,
-			Labels:      labels,
-			Annotations: serviceAnnotations(serviceName, opts.Gates.OpenShift.ServingCertsService),
+			Name:   serviceName,
+			Labels: labels,
 		},
 		Spec: corev1.ServiceSpec{
 			Ports: []corev1.ServicePort{
@@ -220,80 +225,36 @@ func NewQueryFrontendHTTPService(opts Options) *corev1.Service {
 }
 
 func configureQueryFrontendHTTPServicePKI(deployment *appsv1.Deployment, opts Options) error {
-	serviceName := serviceNameQueryFrontendHTTP(opts.Name)
-	caBundleName := signingCABundleName(opts.Name)
-
-	err := configureTailCA(
-		deployment,
-		lokiFrontendContainerName,
-		caBundleName,
-		caBundleDir,
-		caFile,
-		opts.TLSProfile.MinTLSVersion,
-		opts.TLSCipherSuites(),
-	)
-	if err != nil {
-		return err
-	}
-
-	return configureHTTPServicePKI(&deployment.Spec.Template.Spec, serviceName)
-}
-
-func configureQueryFrontendGRPCServicePKI(deployment *appsv1.Deployment, opts Options) error {
-	serviceName := serviceNameQueryFrontendGRPC(opts.Name)
-	return configureGRPCServicePKI(&deployment.Spec.Template.Spec, serviceName)
-}
-
-// ConfigureQueryFrontendDeployment configures CA certificate when TLS is enabled.
-func configureTailCA(d *appsv1.Deployment,
-	qfContainerName, caBundleVolumeName, caDir, caFile, minTLSVersion, cipherSuites string,
-) error {
 	var qfIdx int
-	for i, c := range d.Spec.Template.Spec.Containers {
-		if c.Name == qfContainerName {
+	for i, c := range deployment.Spec.Template.Spec.Containers {
+		if c.Name == lokiFrontendContainerName {
 			qfIdx = i
 			break
 		}
 	}
 
+	url := fmt.Sprintf("https://%s:%d", fqdn(serviceNameQuerierHTTP(opts.Name), opts.Namespace), httpPort)
+
 	containerSpec := corev1.Container{
 		Args: []string{
-			fmt.Sprintf("-frontend.tail-tls-config.tls-cipher-suites=%s", cipherSuites),
-			fmt.Sprintf("-frontend.tail-tls-config.tls-min-version=%s", minTLSVersion),
-			fmt.Sprintf("-frontend.tail-tls-config.tls-ca-path=%s/%s", caDir, caFile),
-		},
-		VolumeMounts: []corev1.VolumeMount{
-			{
-				Name:      caBundleVolumeName,
-				ReadOnly:  true,
-				MountPath: caDir,
-			},
+			fmt.Sprintf("-frontend.tail-proxy-url=%s", url),
+			fmt.Sprintf("-frontend.tail-tls-config.tls-min-version=%s", opts.TLSProfile.MinTLSVersion),
+			fmt.Sprintf("-frontend.tail-tls-config.tls-cipher-suites=%s", opts.TLSCipherSuites()),
+			fmt.Sprintf("-frontend.tail-tls-config.tls-ca-path=%s", signingCAPath()),
+			fmt.Sprintf("-frontend.tail-tls-config.tls-cert-path=%s", lokiServerHTTPTLSCert()),
+			fmt.Sprintf("-frontend.tail-tls-config.tls-key-path=%s", lokiServerHTTPTLSKey()),
 		},
 	}
 
-	p := corev1.PodSpec{
-		Volumes: []corev1.Volume{
-			{
-				Name: caBundleVolumeName,
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						DefaultMode: &defaultConfigMapMode,
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: caBundleVolumeName,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	if err := mergo.Merge(&d.Spec.Template.Spec.Containers[qfIdx], containerSpec, mergo.WithAppendSlice); err != nil {
+	if err := mergo.Merge(&deployment.Spec.Template.Spec.Containers[qfIdx], containerSpec, mergo.WithAppendSlice); err != nil {
 		return kverrors.Wrap(err, "failed to add tls config args")
 	}
 
-	if err := mergo.Merge(&d.Spec.Template.Spec, p, mergo.WithAppendSlice); err != nil {
-		return kverrors.Wrap(err, "failed to add tls volumes")
-	}
+	serviceName := serviceNameQueryFrontendHTTP(opts.Name)
+	return configureHTTPServicePKI(&deployment.Spec.Template.Spec, serviceName, opts.TLSProfile.MinTLSVersion, opts.TLSCipherSuites())
+}
 
-	return nil
+func configureQueryFrontendGRPCServicePKI(deployment *appsv1.Deployment, opts Options) error {
+	serviceName := serviceNameQueryFrontendGRPC(opts.Name)
+	return configureGRPCServicePKI(&deployment.Spec.Template.Spec, serviceName)
 }
--- operator/internal/manifests/service.go
+++ operator/internal/manifests/service.go
@@ -2,12 +2,49 @@ package manifests
 
 import (
 	"fmt"
-	"path"
 
 	"github.com/ViaQ/logerr/v2/kverrors"
 	"github.com/imdario/mergo"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
+
+func configureServiceCA(podSpec *corev1.PodSpec, caBundleName string) error {
+	secretVolumeSpec := corev1.PodSpec{
+		Volumes: []corev1.Volume{
+			{
+				Name: caBundleName,
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: caBundleName,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	secretContainerSpec := corev1.Container{
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      caBundleName,
+				ReadOnly:  false,
+				MountPath: caBundleDir,
+			},
+		},
+	}
+
+	if err := mergo.Merge(podSpec, secretVolumeSpec, mergo.WithAppendSlice); err != nil {
+		return kverrors.Wrap(err, "failed to merge volumes")
+	}
+
+	if err := mergo.Merge(&podSpec.Containers[0], secretContainerSpec, mergo.WithAppendSlice); err != nil {
+		return kverrors.Wrap(err, "failed to merge container")
+	}
+
+	return nil
+}
 
 func configureGRPCServicePKI(podSpec *corev1.PodSpec, serviceName string) error {
 	secretVolumeSpec := corev1.PodSpec{
@@ -27,12 +64,14 @@ func configureGRPCServicePKI(podSpec *corev1.PodSpec, serviceName string) error 
 			{
 				Name:      serviceName,
 				ReadOnly:  false,
-				MountPath: grpcTLSDir,
+				MountPath: lokiServerGRPCTLSDir(),
 			},
 		},
 		Args: []string{
-			fmt.Sprintf("-server.grpc-tls-cert-path=%s", path.Join(grpcTLSDir, tlsCertFile)),
-			fmt.Sprintf("-server.grpc-tls-key-path=%s", path.Join(grpcTLSDir, tlsKeyFile)),
+			fmt.Sprintf("-server.grpc-tls-ca-path=%s", signingCAPath()),
+			fmt.Sprintf("-server.grpc-tls-cert-path=%s", lokiServerGRPCTLSCert()),
+			fmt.Sprintf("-server.grpc-tls-key-path=%s", lokiServerGRPCTLSKey()),
+			"-server.grpc-tls-client-auth=RequireAndVerifyClientCert",
 		},
 	}
 
@@ -47,7 +86,7 @@ func configureGRPCServicePKI(podSpec *corev1.PodSpec, serviceName string) error 
 	return nil
 }
 
-func configureHTTPServicePKI(podSpec *corev1.PodSpec, serviceName string) error {
+func configureHTTPServicePKI(podSpec *corev1.PodSpec, serviceName, minTLSVersion, tlsCipherSuites string) error {
 	secretVolumeSpec := corev1.PodSpec{
 		Volumes: []corev1.Volume{
 			{
@@ -60,24 +99,44 @@ func configureHTTPServicePKI(podSpec *corev1.PodSpec, serviceName string) error 
 			},
 		},
 	}
+
 	secretContainerSpec := corev1.Container{
 		VolumeMounts: []corev1.VolumeMount{
 			{
 				Name:      serviceName,
 				ReadOnly:  false,
-				MountPath: httpTLSDir,
+				MountPath: lokiServerHTTPTLSDir(),
 			},
 		},
 		Args: []string{
-			fmt.Sprintf("-server.http-tls-cert-path=%s", path.Join(httpTLSDir, tlsCertFile)),
-			fmt.Sprintf("-server.http-tls-key-path=%s", path.Join(httpTLSDir, tlsKeyFile)),
+			// Expose ready handler through internal server without requiring mTLS
+			"-internal-server.enable=true",
+			"-internal-server.http-listen-address=",
+			fmt.Sprintf("-internal-server.http-tls-min-version=%s", minTLSVersion),
+			fmt.Sprintf("-internal-server.http-tls-cipher-suites=%s", tlsCipherSuites),
+			fmt.Sprintf("-internal-server.http-tls-cert-path=%s", lokiServerHTTPTLSCert()),
+			fmt.Sprintf("-internal-server.http-tls-key-path=%s", lokiServerHTTPTLSKey()),
+			// Require mTLS for any other handler
+			fmt.Sprintf("-server.http-tls-ca-path=%s", signingCAPath()),
+			fmt.Sprintf("-server.http-tls-cert-path=%s", lokiServerHTTPTLSCert()),
+			fmt.Sprintf("-server.http-tls-key-path=%s", lokiServerHTTPTLSKey()),
+			"-server.http-tls-client-auth=RequireAndVerifyClientCert",
+		},
+		Ports: []corev1.ContainerPort{
+			{
+				Name:          lokiInternalHTTPPortName,
+				ContainerPort: internalHTTPPort,
+				Protocol:      protocolTCP,
+			},
 		},
 	}
+
 	uriSchemeContainerSpec := corev1.Container{
 		ReadinessProbe: &corev1.Probe{
 			ProbeHandler: corev1.ProbeHandler{
 				HTTPGet: &corev1.HTTPGetAction{
 					Scheme: corev1.URISchemeHTTPS,
+					Port:   intstr.FromInt(internalHTTPPort),
 				},
 			},
 		},
@@ -85,6 +144,7 @@ func configureHTTPServicePKI(podSpec *corev1.PodSpec, serviceName string) error 
 			ProbeHandler: corev1.ProbeHandler{
 				HTTPGet: &corev1.HTTPGetAction{
 					Scheme: corev1.URISchemeHTTPS,
+					Port:   intstr.FromInt(internalHTTPPort),
 				},
 			},
 		},
