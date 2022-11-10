061ca8678807ecc597e709158b47380aa1413396
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2022-11-10 17:12:38 +0100

    Configure bucket policy.

--- production/terraform/modules/s3/main.tf
+++ production/terraform/modules/s3/main.tf
@@ -42,6 +42,32 @@ resource "aws_s3_bucket" "loki-data" {
   bucket = "${var.bucket_name}"
 }
 
+resource "aws_s3_bucket_policy" "grant-access" {
+  bucket = aws_s3_bucket.loki-data.id
+  policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Sid: "Statement1",
+            Effect: "Allow",
+            Principal: {
+                AWS: aws_iam_role.loki.arn  
+            },
+            Action: [
+              "s3:PutObject",
+              "s3:GetObject",
+              "s3:DeleteObject",
+              "s3:ListBucket"
+            ],
+            Resource: [
+	      aws_s3_bucket.loki-data.arn,
+	      "${aws_s3_bucket.loki-data.arn}/*"
+            ]
+        }
+    ]
+  })
+}
+
 resource "aws_iam_role" "loki" {
   name               = "LokiStorage-${var.cluster_name}"
   assume_role_policy = data.aws_iam_policy_document.oidc.json
