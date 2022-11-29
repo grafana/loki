691b8be6c74da1c55d17ebf8d0b004b3072b5a76
Karsten Jeschkies <k@jeschkies.xyz>
2022-11-29 17:06:52 +0100

    Provide Terraform script for setting up S3 and document role based access. (#7656)
    **What this PR does / why we need it**:
    Role based access to S3 on an EKS cluster via service account annotation
    was not documented well and let to some confusion. This change documents
    how a role based accessis achieved with providing an AWS secret access
    key and access key id. Thus there is no change of accidentally leaking
    credentials.
    
    A Terraform module is provided under `production/terraform/modules/s3`
    that provisions and S3 bucket and IAM role and policy.
    
    **Which issue(s) this PR fixes**:
    Fixes #7279
    
    **Special notes for your reviewer**:
    
    **Checklist**
    - [ ] Reviewed the `CONTRIBUTING.md` guide
    - [x] Documentation added
    - [ ] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/upgrading/_index.md`
    
    Co-authored-by: Pablo Angulo <pablo.angulo@grafana.com>

---
+++ production/terraform/modules/s3/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = "> 1.2.0"
+}
