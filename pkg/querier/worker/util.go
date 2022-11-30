5c6262211c2db7b960683a47eed84e3be2f0f4a1
fuling <fuling.lgz@alibaba-inc.com>
2022-11-30 14:21:54 +0800

    add a small comment summarizing Marco's answer?

--- pkg/querier/worker/util.go
+++ pkg/querier/worker/util.go
@@ -36,7 +36,28 @@ func newExecutionContext(workerCtx context.Context, logger log.Logger) (execCtx 
 		select {
 		case <-workerCtx.Done():
 			level.Debug(logger).Log("msg", "querier worker context has been canceled, waiting until there's no inflight query")
-
+			//
+			// TODO：a potential race condition
+			//
+			// summarizing Marco's answer.
+			//   Question 1：
+			//     isn't there a potential race condition between testing the flag and setting it in the querier loop?
+			//     It could be false here but then the next query is received.
+			//   Answer 1：
+			//     When the querier shutdowns it's expected to cancel the context and so the call to request,
+			//         err := c.Recv() (done in schedulerProcessor.querierLoop())
+			//     to return error because of the canceled context (I mean the querier context, not the query execution context).
+			//
+			//   Question 2：
+			//     Is there a race？
+			//   Answer 2：
+			//     Yes, there's a race between the call to c.Recv() and the sequent call to inflightQuery.Store(true),
+			//     but the time window is very short and we ignored it in Mimir (all in all we want to gracefully handle the 99.9% of cases).
+			//
+			//  Question Q3：
+			//    I was wondering if we can end up in a state were the query is inflight but we shut down.I guess it times out.
+			//  Answer 3：
+			//    I think that race condition still exists (I found it very hard to guarantee to never happen) but in practice should be very unlikely.
 			for inflightQuery.Load() {
 				select {
 				case <-execCtx.Done():
