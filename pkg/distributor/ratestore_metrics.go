a1e9e4cdd03ee5637994e48068344c80acb2e1fe
Travis Patterson <travis.patterson@grafana.com>
2022-12-02 16:05:56 -0700

    Revert "Make some stream shard metrics per-tenant (#7838)" (#7842)
    Because these metrics will be coming from each distributor, this is
    going to have an adverse impact on cardinality. Consider doing this with
    logging

--- pkg/distributor/ratestore_metrics.go
+++ pkg/distributor/ratestore_metrics.go
@@ -8,13 +8,13 @@ import (
 
 type ratestoreMetrics struct {
 	rateRefreshFailures *prometheus.CounterVec
-	expiredCount        prometheus.Counter
 	streamCount         prometheus.Gauge
-	maxStreamShardCount *prometheus.GaugeVec
-	maxStreamRate       *prometheus.GaugeVec
-	maxUniqueStreamRate *prometheus.GaugeVec
+	expiredCount        prometheus.Counter
+	maxStreamShardCount prometheus.Gauge
 	streamShardCount    prometheus.Histogram
+	maxStreamRate       prometheus.Gauge
 	streamRate          prometheus.Histogram
+	maxUniqueStreamRate prometheus.Gauge
 	refreshDuration     *instrument.HistogramCollector
 }
 
@@ -35,33 +35,33 @@ func newRateStoreMetrics(reg prometheus.Registerer) *ratestoreMetrics {
 			Name:      "rate_store_expired_streams_total",
 			Help:      "The number of streams that have been expired by the ratestore",
 		}),
-		maxStreamShardCount: promauto.With(reg).NewGaugeVec(prometheus.GaugeOpts{
+		maxStreamShardCount: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
 			Namespace: "loki",
 			Name:      "rate_store_max_stream_shards",
 			Help:      "The number of shards for a single stream reported by ingesters during a sync operation.",
-		}, []string{"tenant"}),
+		}),
 		streamShardCount: promauto.With(reg).NewHistogram(prometheus.HistogramOpts{
 			Namespace: "loki",
 			Name:      "rate_store_stream_shards",
 			Help:      "The distribution of number of shards for a single stream reported by ingesters during a sync operation.",
 			Buckets:   []float64{0, 2, 4, 8, 16, 32, 64, 128},
 		}),
-		maxStreamRate: promauto.With(reg).NewGaugeVec(prometheus.GaugeOpts{
+		maxStreamRate: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
 			Namespace: "loki",
 			Name:      "rate_store_max_stream_rate_bytes",
 			Help:      "The maximum stream rate for any stream reported by ingesters during a sync operation. Sharded Streams are combined.",
-		}, []string{"tenant"}),
+		}),
 		streamRate: promauto.With(reg).NewHistogram(prometheus.HistogramOpts{
 			Namespace: "loki",
 			Name:      "rate_store_stream_rate_bytes",
 			Help:      "The distribution of stream rates for any stream reported by ingesters during a sync operation. Sharded Streams are combined.",
 			Buckets:   prometheus.ExponentialBuckets(20000, 2, 14), // biggest bucket is 20000*2^(14-1) = 163,840,000 (~163.84MB)
 		}),
-		maxUniqueStreamRate: promauto.With(reg).NewGaugeVec(prometheus.GaugeOpts{
+		maxUniqueStreamRate: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
 			Namespace: "loki",
 			Name:      "rate_store_max_unique_stream_rate_bytes",
 			Help:      "The maximum stream rate for any stream reported by ingesters during a sync operation. Sharded Streams are considered separate.",
-		}, []string{"tenant"}),
+		}),
 		refreshDuration: instrument.NewHistogramCollector(
 			promauto.With(reg).NewHistogramVec(
 				prometheus.HistogramOpts{
