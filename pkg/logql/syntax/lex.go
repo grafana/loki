4a2b4c84db5831802e092fbe7ab7d38dd796f219
Mohamed-Amine Bouqsimi <mbouqsim@redhat.com>
2022-12-05 18:44:08 +0100

    lint

--- pkg/logql/syntax/lex.go
+++ pkg/logql/syntax/lex.go
@@ -66,8 +66,8 @@ var tokens = map[string]int{
 	OpParserTypePattern: PATTERN,
 
 	// fmt
-	OpFmtLabel:   LABEL_FMT,
-	OpFmtLine:    LINE_FMT,
+	OpFmtLabel: LABEL_FMT,
+	OpFmtLine:  LINE_FMT,
 
 	// filter functions
 	OpFilterIP:   IP,
