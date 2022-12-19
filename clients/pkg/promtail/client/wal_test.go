921f62be60a03eb651df2a3bb4e67e84b4116eee
Pablo Balbi <pablo.l.balbi@gmail.com>
2022-12-19 19:09:24 -0300

    test not passing

--- clients/pkg/promtail/client/wal_test.go
+++ clients/pkg/promtail/client/wal_test.go
@@ -153,3 +153,62 @@ func Test_WAL_WriteAndConsume(t *testing.T) {
 	t.Logf("series: %v", consumer.Series)
 	t.Logf("entries: %v", consumer.Entries)
 }
+
+func Test_WAL_WriteAfterWatcherIsStarted(t *testing.T) {
+	dir := t.TempDir()
+	expectedWALPath := path.Join(
+		dir,       // wal base dir
+		"foo",     // client config name
+		"tenant1", // tenant ID
+	)
+	t.Logf("Expected wal path: %s", expectedWALPath)
+
+	consumer := newSimpleConsumer()
+	watcher := NewWALWatcher(expectedWALPath, consumer)
+	watcher.Start()
+	defer watcher.Stop()
+
+	c, err := New(nilMetrics, Config{
+		Name:      "foo",
+		URL:       flagext.URLValue{URL: &url.URL{}},
+		BatchWait: 100 * time.Millisecond,
+		BatchSize: int(1e6),
+		WAL: WALConfig{
+			Enabled: true,
+			Dir:     dir,
+		},
+	}, nil, 0, log.NewNopLogger())
+	require.NoError(t, err)
+
+	// defer c.Stop()
+	c.Chan() <- api.Entry{
+		Labels: model.LabelSet{
+			"foo": "bar",
+		},
+		Entry: logproto.Entry{
+			Line: "test",
+		},
+	}
+	c.Chan() <- api.Entry{
+		Labels: model.LabelSet{
+			ReservedLabelTenantID: "tenant1",
+		},
+		Entry: logproto.Entry{
+			Line: "test 2",
+		},
+	}
+
+	//// testing logs
+	//dirs, err := os.ReadDir(expectedWALPath)
+	//require.NoError(t, err)
+	//t.Logf("files in wal dir: %v", dirs)
+	//
+	// todo: why is this propagation time needed
+	time.Sleep(2 * time.Second)
+
+	require.Len(t, consumer.Entries["tenant1"], 2)
+
+	// todo assert as well over the series
+	t.Logf("series: %v", consumer.Series)
+	t.Logf("entries: %v", consumer.Entries)
+}
