33e0ee0d6a04c6574ed6dfece3dce6420e5f4c00
Christian Haudum <christian.haudum@gmail.com>
2023-01-18 10:21:00 +0100

    fixup! Add timeout for waiting for component to start
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- integration/cluster/cluster.go
+++ integration/cluster/cluster.go
@@ -14,8 +14,8 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/multierror"
 	"github.com/prometheus/client_golang/prometheus"
 
