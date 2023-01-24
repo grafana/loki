5bd764f657af21af2f8d4479e3d8e058d940afc8
Mateusz Misiewicz <mateusz.misiewicz@gemini.pl>
2023-01-24 10:12:13 +0100

    Lambda-promtail: add support for AWS WAF S3 logs

--- tools/lambda-promtail/lambda-promtail/s3.go
+++ tools/lambda-promtail/lambda-promtail/s3.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -18,24 +20,65 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-var (
-	// AWS Application Load Balancers
-	// source:  https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-log-file-format
-	// format:  bucket[/prefix]/AWSLogs/aws-account-id/elasticloadbalancing/region/yyyy/mm/dd/aws-account-id_elasticloadbalancing_region_app.load-balancer-id_end-time_ip-address_random-string.log.gz
-	// example: my-bucket/AWSLogs/123456789012/elasticloadbalancing/us-east-1/2022/01/24/123456789012_elasticloadbalancing_us-east-1_app.my-loadbalancer.b13ea9d19f16d015_20220124T0000Z_0.0.0.0_2et2e1mx.log.gz
-	// VPC Flow Logs
-	// source: https://docs.aws.amazon.com/vpc/latest/userguide/flow-logs-s3.html#flow-logs-s3-path
-	// format: bucket-and-optional-prefix/AWSLogs/account_id/vpcflowlogs/region/year/month/day/aws_account_id_vpcflowlogs_region_flow_log_id_YYYYMMDDTHHmmZ_hash.log.gz
-	// example: 123456789012_vpcflowlogs_us-east-1_fl-1234abcd_20180620T1620Z_fe123456.log.gz
-	filenameRegex = regexp.MustCompile(`AWSLogs\/(?P<account_id>\d+)\/(?P<type>\w+)\/(?P<region>[\w-]+)\/(?P<year>\d+)\/(?P<month>\d+)\/(?P<day>\d+)\/\d+\_(?:elasticloadbalancing|vpcflowlogs)\_\w+-\w+-\d_(?:(?:app|nlb|net)\.*?)?(?P<src>[a-zA-Z0-9\-]+)`)
-
-	// regex that extracts the timestamp (RFC3339) from message log
-	timestampRegex = regexp.MustCompile(`\w+ (?P<timestamp>\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z)`)
-)
+type logDefinition struct {
+	logType        string
+	timestampType  string
+	filenameRegex  *regexp.Regexp
+	timestampRegex *regexp.Regexp
+	skipHeader     bool
+}
 
 const (
-	FLOW_LOG_TYPE string = "vpcflowlogs"
-	LB_LOG_TYPE   string = "elasticloadbalancing"
+	S3_LB       string = "s3_lb"
+	S3_VPC_FLOW string = "s3_vpc_flow"
+	S3_WAF             = "s3_waf"
+)
+
+var (
+	logDefinitionList = []logDefinition{
+		logDefinition{
+			logType:       S3_LB,
+			timestampType: "rfc3339",
+			// regex that parses the log file name fields for AWS Loadbalancer logs in S3
+			// source:  https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-log-file-format
+			// format:  bucket[/prefix]/AWSLogs/aws-account-id/elasticloadbalancing/region/yyyy/mm/dd/aws-account-id_elasticloadbalancing_region_app.load-balancer-id_end-time_ip-address_random-string.log.gz
+			// example: my-bucket/AWSLogs/123456789012/elasticloadbalancing/us-east-1/2022/01/24/123456789012_elasticloadbalancing_us-east-1_app.my-loadbalancer.b13ea9d19f16d015_20220124T0000Z_0.0.0.0_2et2e1mx.log.gz
+			filenameRegex: regexp.MustCompile(`AWSLogs\/(?P<account_id>\d+)\/elasticloadbalancing\/(?P<region>[\w-]+)\/(?P<year>\d+)\/(?P<month>\d+)\/(?P<day>\d+)\/\d+\_elasticloadbalancing\_\w+-\w+-\d_(?:(?:app|nlb|net)\.*?)?(?P<src>[a-zA-Z0-9\-]+)`),
+			// regex that extracts the timestamp from the AWS Loadbalancer message log
+			// format: timestamp (RFC3339)
+			// example: h2 2022-12-20T23:55:02.599911Z ...
+			timestampRegex: regexp.MustCompile(`\w+ (?P<timestamp>\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z)`),
+			skipHeader:     false,
+		},
+		logDefinition{
+			logType:       S3_VPC_FLOW,
+			timestampType: "seconds",
+			// regex that parses the log file name fields for AWS VPC Flow Logs in S3
+			// source: https://docs.aws.amazon.com/vpc/latest/userguide/flow-logs-s3.html#flow-logs-s3-path
+			// format: bucket-and-optional-prefix/AWSLogs/account_id/vpcflowlogs/region/year/month/day/aws_account_id_vpcflowlogs_region_flow_log_id_YYYYMMDDTHHmmZ_hash.log.gz
+			// example: 123456789012_vpcflowlogs_us-east-1_fl-1234abcd_20180620T1620Z_fe123456.log.gz
+			filenameRegex: regexp.MustCompile(`AWSLogs\/(?P<account_id>\d+)\/vpcflowlogs\/(?P<region>[\w-]+)\/(?P<year>\d+)\/(?P<month>\d+)\/(?P<day>\d+)\/\d+\_vpcflowlogs\_\w+-\w+-\d_(?:(?:app|nlb|net)\.*?)?(?P<src>[a-zA-Z0-9\-]+)`),
+			// regex that extracts the timestamp from the AWS VPC Flow Logs message log
+			// format: timestamp (seconds)
+			// example: ... 1669842701 1669842702 ACCEPT
+			timestampRegex: regexp.MustCompile(`(?P<begin_date>\d+) (?P<end_date>\d+) (?:ACCEPT|REJECT)`),
+			skipHeader:     true,
+		},
+		logDefinition{
+			logType:       S3_WAF,
+			timestampType: "milliseconds",
+			// regex that parses the log file name fields for AWS WAFv2 logs in S3
+			// source:  https://docs.aws.amazon.com/waf/latest/developerguide/logging-s3.html
+			// format:  bucket[/prefix]/AWSLogs/aws-account-id/WAFLogs/region/webacl-name/YYYY/MM/dd/HH/mm/aws-account-id_waflogs_region_webacl-name_YYYYMMddTHHmmZ_random-string.log.gz
+			// example: aws-waf-logs-test/AWSLogs/11111111111/WAFLogs/us-east-1/TEST-WEBACL/2021/10/28/19/50/11111111111_waflogs_us-east-1_TEST-WEBACL_20211028T1950Z_e0ca43b5.log.gz
+			filenameRegex: regexp.MustCompile(`AWSLogs\/(?P<account_id>\d+)\/WAFLogs\/(?P<region>[\w-]+)\/(?P<src>[a-zA-Z0-9\-]+)\/(?P<year>\d+)\/(?P<month>\d+)\/(?P<day>\d+)\/(?P<hour>\d+)\/(?P<minute>\d+)\/\d+\_waflogs\_`),
+			// regex that extracts the timestamp from the AWS WAFv2 message log
+			// format: timestamp (milliseconds)
+			// example: {"timestamp":1671624901861,...
+			timestampRegex: regexp.MustCompile(`"timestamp":(?P<timestamp>\d+)`),
+			skipHeader:     false,
+		},
+	}
 )
 
 func getS3Object(ctx context.Context, labels map[string]string) (io.ReadCloser, error) {
@@ -75,56 +118,79 @@ func parseS3Log(ctx context.Context, b *batch, labels map[string]string, obj io.
 
 	scanner := bufio.NewScanner(gzreader)
 
-	skipHeader := false
 	logType := labels["type"]
-	if labels["type"] == FLOW_LOG_TYPE {
-		skipHeader = true
-		logType = "s3_vpc_flow"
-	} else if labels["type"] == LB_LOG_TYPE {
-		logType = "s3_lb"
-	}
 
 	ls := model.LabelSet{
 		model.LabelName("__aws_log_type"):                          model.LabelValue(logType),
 		model.LabelName(fmt.Sprintf("__aws_%s_lb", logType)):       model.LabelValue(labels["src"]),
 		model.LabelName(fmt.Sprintf("__aws_%s_lb_owner", logType)): model.LabelValue(labels["account_id"]),
 	}
-
 	ls = applyExtraLabels(ls)
 
-	timestamp := time.Now()
-	var lineCount int
-	for scanner.Scan() {
-		log_line := scanner.Text()
-		lineCount++
-		if lineCount == 1 && skipHeader {
-			continue
-		}
-		if printLogLine {
-			fmt.Println(log_line)
-		}
+	logDef, found := findLogDefinitionByType(logType)
+	if found {
+		timestamp := time.Now()
+		var err error
+		var lineCount int
+		for scanner.Scan() {
+			log_line := scanner.Text()
+			lineCount++
+			if lineCount == 1 && logDef.skipHeader {
+				continue
+			}
+			if printLogLine {
+				fmt.Println(log_line)
+			}
+			match := logDef.timestampRegex.FindStringSubmatch(log_line)
+			if len(match) > 0 {
+				timestamp, err = parseLogTime(logDef.timestampType, match[1])
+				if err != nil {
+					return err
+				}
+			}
 
-		match := timestampRegex.FindStringSubmatch(log_line)
-		if len(match) > 0 {
-			timestamp, err = time.Parse(time.RFC3339, match[1])
-			if err != nil {
+			if err := b.add(ctx, entry{ls, logproto.Entry{
+				Line:      log_line,
+				Timestamp: timestamp,
+			}}); err != nil {
 				return err
 			}
 		}
-
-		if err := b.add(ctx, entry{ls, logproto.Entry{
-			Line:      log_line,
-			Timestamp: timestamp,
-		}}); err != nil {
-			return err
-		}
 	}
-
 	return nil
 }
 
-func getLabels(record events.S3EventRecord) (map[string]string, error) {
+func parseLogTime(timestampType string, timeToParse string) (time.Time, error) {
+	switch timestampType {
+	case "rfc3339":
+		return time.Parse(time.RFC3339, timeToParse)
+	case "seconds":
+		timeToParseNumber, err := strconv.ParseInt(timeToParse, 10, 64)
+		if err != nil {
+			return time.Time{}, err
+		}
+		return time.Unix(timeToParseNumber, 0), nil
+	case "milliseconds":
+		timeToParseNumber, err := strconv.ParseInt(timeToParse, 10, 64)
+		if err != nil {
+			return time.Time{}, err
+		}
+		return time.UnixMilli(timeToParseNumber), nil
+	default:
+		return time.Time{}, errors.New("cannot parse log timestamp string")
+	}
+}
 
+func findLogDefinitionByType(logType string) (logDefinition, bool) {
+	for _, e := range logDefinitionList {
+		if e.logType == logType {
+			return e, true
+		}
+	}
+	return logDefinition{}, false
+}
+
+func getLabels(record events.S3EventRecord) (map[string]string, error) {
 	labels := make(map[string]string)
 
 	labels["key"] = record.S3.Object.Key
@@ -132,14 +198,31 @@ func getLabels(record events.S3EventRecord) (map[string]string, error) {
 	labels["bucket_owner"] = record.S3.Bucket.OwnerIdentity.PrincipalID
 	labels["bucket_region"] = record.AWSRegion
 
-	match := filenameRegex.FindStringSubmatch(labels["key"])
-	for i, name := range filenameRegex.SubexpNames() {
-		if i != 0 && name != "" {
-			labels[name] = match[i]
+	logType, match, matchNames := parseLogFilename(labels["key"])
+
+	if logType != "" {
+		labels["type"] = logType
+
+		for i, name := range matchNames {
+			if i != 0 && name != "" {
+				labels[name] = match[i]
+			}
 		}
 	}
 
 	return labels, nil
+}
+
+func parseLogFilename(bucketKey string) (string, []string, []string) {
+	for _, e := range logDefinitionList {
+		match := e.filenameRegex.FindStringSubmatch(bucketKey)
+		if match != nil {
+			return e.logType, match, e.filenameRegex.SubexpNames()
+		}
+	}
+
+	fmt.Printf("Unknown S3 log format: %s\n", bucketKey)
+	return "", nil, nil
 }
 
 func processS3Event(ctx context.Context, ev *events.S3Event) error {
