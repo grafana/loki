43ef3beb953bc3df93111552c6546450a7c5bd89
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-01-26 14:49:09 -0300

    restorin main changes under pkg/ingester

--- pkg/ingester/wal.go
+++ pkg/ingester/wal.go
@@ -11,6 +11,7 @@ import (
 	"github.com/prometheus/prometheus/tsdb/wlog"
 
 	"github.com/grafana/loki/pkg/ingester/wal"
+	"github.com/grafana/loki/pkg/logproto"
 	"github.com/grafana/loki/pkg/util/flagext"
 	util_log "github.com/grafana/loki/pkg/util/log"
 )
@@ -117,7 +118,7 @@ func (w *walWrapper) Log(record *wal.Record) error {
 
 		// Always write series then entries.
 		if len(record.Series) > 0 {
-			buf = record.EncodeSeries(buf)
+			buf = record.encodeSeries(buf)
 			if err := w.wal.Log(buf); err != nil {
 				return err
 			}
@@ -165,4 +166,56 @@ func (w *walWrapper) run() {
 	)
 	checkpointer.Run()
 
+}
+
+type resettingPool struct {
+	rPool *sync.Pool // records
+	ePool *sync.Pool // entries
+	bPool *sync.Pool // bytes
+}
+
+func (p *resettingPool) GetRecord() *WALRecord {
+	rec := p.rPool.Get().(*WALRecord)
+	rec.Reset()
+	return rec
+}
+
+func (p *resettingPool) PutRecord(r *WALRecord) {
+	p.rPool.Put(r)
+}
+
+func (p *resettingPool) GetEntries() []logproto.Entry {
+	return p.ePool.Get().([]logproto.Entry)
+}
+
+func (p *resettingPool) PutEntries(es []logproto.Entry) {
+	p.ePool.Put(es[:0]) // nolint:staticcheck
+}
+
+func (p *resettingPool) GetBytes() []byte {
+	return p.bPool.Get().([]byte)
+}
+
+func (p *resettingPool) PutBytes(b []byte) {
+	p.bPool.Put(b[:0]) // nolint:staticcheck
+}
+
+func newRecordPool() *resettingPool {
+	return &resettingPool{
+		rPool: &sync.Pool{
+			New: func() interface{} {
+				return &WALRecord{}
+			},
+		},
+		ePool: &sync.Pool{
+			New: func() interface{} {
+				return make([]logproto.Entry, 0, 512)
+			},
+		},
+		bPool: &sync.Pool{
+			New: func() interface{} {
+				return make([]byte, 0, 1<<10) // 1kb
+			},
+		},
+	}
 }
