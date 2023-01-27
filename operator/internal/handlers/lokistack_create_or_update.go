f6d6ad28bdb644824d426706fe9579ab640614db
Mohamed-Amine Bouqsimi <mbouqsim@redhat.com>
2023-01-27 10:49:18 +0100

    cleanup ruler resources when disabled

--- operator/internal/handlers/lokistack_create_or_update.go
+++ operator/internal/handlers/lokistack_create_or_update.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/ViaQ/logerr/v2/kverrors"
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -250,6 +251,19 @@ func CreateOrUpdateLokiStack(
 			ll.Error(err, "failed to check OCP User Workload AlertManager")
 			return err
 		}
+	} else {
+		// Clean up ruler resources
+		err = removeRulesConfigMap(ctx, req, k)
+		if err != nil {
+			ll.Error(err, "failed to remove rules configmap")
+			return err
+		}
+
+		err = removeRuler(ctx, req, k)
+		if err != nil {
+			ll.Error(err, "failed to remove ruler statefulset")
+			return err
+		}
 	}
 
 	certRotationRequiredAt := ""
@@ -415,4 +429,50 @@ func isNamespaceScoped(obj client.Object) bool {
 	default:
 		return true
 	}
+}
+
+func removeRulesConfigMap(ctx context.Context, req ctrl.Request, c client.Client) error {
+	// Check if the CM exists before proceeding.
+	key := client.ObjectKey{Name: manifests.RulesConfigMapName(req.Name), Namespace: req.Namespace}
+
+	var rulesCm corev1.ConfigMap
+	if err := c.Get(ctx, key, &rulesCm); err != nil {
+		if apierrors.IsNotFound(err) {
+			// resource doesnt exist, so nothing to do.
+			return nil
+		}
+		return kverrors.Wrap(err, "failed to lookup configmap", "name", key)
+	}
+
+	if err := c.Delete(ctx, &rulesCm, &client.DeleteOptions{}); err != nil {
+		return kverrors.Wrap(err, "failed to delete configmap",
+			"name", rulesCm.Name,
+			"namespace", rulesCm.Namespace,
+		)
+	}
+
+	return nil
+}
+
+func removeRuler(ctx context.Context, req ctrl.Request, c client.Client) error {
+	// Check if the Statefulset exists before proceeding.
+	key := client.ObjectKey{Name: manifests.RulerName(req.Name), Namespace: req.Namespace}
+
+	var ruler appsv1.StatefulSet
+	if err := c.Get(ctx, key, &ruler); err != nil {
+		if apierrors.IsNotFound(err) {
+			// resource doesnt exist, so nothing to do.
+			return nil
+		}
+		return kverrors.Wrap(err, "failed to lookup Statefulset", "name", key)
+	}
+
+	if err := c.Delete(ctx, &ruler, &client.DeleteOptions{}); err != nil {
+		return kverrors.Wrap(err, "failed to delete statefulset",
+			"name", ruler.Name,
+			"namespace", ruler.Namespace,
+		)
+	}
+
+	return nil
 }
