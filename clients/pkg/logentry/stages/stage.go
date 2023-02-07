85b3e5d4abf5d6e483f107c02ea750a1cb1cc5c9
Nick Moore <shiny.nickel@gmail.com>
2023-02-07 13:55:47 +0000

    Promtail: Add event log message stage (#8382)
    The Windows Event Log includes a field named Message that include a large amount of additional data, stored in a simple key-value format:
    
        key-value pairs are separated with Windows-style \r\n newlines,
        the key and value are separated with : (or just : if the key has no associated value).
    
    The key-value data in the Message field is commonly utilised when investigating Windows events, e.g., in security use-cases, but the existing windows_events pipeline stage keeps the Message data as a string, leading to requiring significant effort at the query time to extract that data to be used in field filters.
    
    This PR request introduces a new promtail stage that parses a specified Source (defaulting to message used by the windows_events) and extracts all the key-value pairs into the extracted fields, which can then be used in latter stages in the pipeline.

--- clients/pkg/logentry/stages/stage.go
+++ clients/pkg/logentry/stages/stage.go
@@ -15,28 +15,29 @@ import (
 )
 
 const (
-	StageTypeJSON         = "json"
-	StageTypeLogfmt       = "logfmt"
-	StageTypeRegex        = "regex"
-	StageTypeReplace      = "replace"
-	StageTypeMetric       = "metrics"
-	StageTypeLabel        = "labels"
-	StageTypeLabelDrop    = "labeldrop"
-	StageTypeTimestamp    = "timestamp"
-	StageTypeOutput       = "output"
-	StageTypeDocker       = "docker"
-	StageTypeCRI          = "cri"
-	StageTypeMatch        = "match"
-	StageTypeTemplate     = "template"
-	StageTypePipeline     = "pipeline"
-	StageTypeTenant       = "tenant"
-	StageTypeDrop         = "drop"
-	StageTypeLimit        = "limit"
-	StageTypeMultiline    = "multiline"
-	StageTypePack         = "pack"
-	StageTypeLabelAllow   = "labelallow"
-	StageTypeStaticLabels = "static_labels"
-	StageTypeDecolorize   = "decolorize"
+	StageTypeJSON            = "json"
+	StageTypeLogfmt          = "logfmt"
+	StageTypeRegex           = "regex"
+	StageTypeReplace         = "replace"
+	StageTypeMetric          = "metrics"
+	StageTypeLabel           = "labels"
+	StageTypeLabelDrop       = "labeldrop"
+	StageTypeTimestamp       = "timestamp"
+	StageTypeOutput          = "output"
+	StageTypeDocker          = "docker"
+	StageTypeCRI             = "cri"
+	StageTypeMatch           = "match"
+	StageTypeTemplate        = "template"
+	StageTypePipeline        = "pipeline"
+	StageTypeTenant          = "tenant"
+	StageTypeDrop            = "drop"
+	StageTypeLimit           = "limit"
+	StageTypeMultiline       = "multiline"
+	StageTypePack            = "pack"
+	StageTypeLabelAllow      = "labelallow"
+	StageTypeStaticLabels    = "static_labels"
+	StageTypeDecolorize      = "decolorize"
+	StageTypeEventLogMessage = "eventlogmessage"
 )
 
 // Processor takes an existing set of labels, timestamp and log entry and returns either a possibly mutated
@@ -212,6 +213,11 @@ func New(logger log.Logger, jobName *string, stageType string,
 		}
 	case StageTypeDecolorize:
 		s, err = newDecolorizeStage(cfg)
+		if err != nil {
+			return nil, err
+		}
+	case StageTypeEventLogMessage:
+		s, err = newEventLogMessageStage(logger, cfg)
 		if err != nil {
 			return nil, err
 		}
