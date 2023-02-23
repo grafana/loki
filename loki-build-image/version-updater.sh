eeca4589fbb439c7d5c391c46886dff81fe619a2
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-02-23 17:31:33 +0100

    Define Container for Codespaces (#8561)

--- loki-build-image/version-updater.sh
+++ loki-build-image/version-updater.sh
@@ -19,7 +19,7 @@ fi
 
 echo "Updating loki-build-image references to '${VERSION}'"
 
-find . -type f \( -name '*.yml' -o -name '*.yaml' -o -name '*Dockerfile*' \) -exec grep -lE "grafana/loki-build-image:[0-9]+" {} \; | grep -ve '.drone' |
+find . -type f \( -name '*.yml' -o -name '*.yaml' -o -name '*Dockerfile*' -o -name '*devcontainer.json' \) -exec grep -lE "grafana/loki-build-image:[0-9]+" {} \; | grep -ve '.drone' |
   while read -r x; do
     echo "Updating ${x}"
     ${SED} -i -re "s,grafana/loki-build-image:[0-9]+\.[0-9]+\.[0-9]+,grafana/loki-build-image:${VERSION},g" "${x}"
