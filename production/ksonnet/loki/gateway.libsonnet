grafana/loki

commit 027c2ef9570935262ab100e0cc5ea29b353b0997

Jsonnet: Allows gateway tenant ID to be configured (#7624)
Co-authored-by: Karsten Jeschkies <karsten.jeschkies@grafana.com>

production/ksonnet/loki/gateway.libsonnet
@@ -7,6 +7,7 @@ local k = import 'ksonnet-util/kausal.libsonnet';
     // This is inserted into the gateway Nginx config file
     // under the server directive
     gateway_server_snippet: '',
+    gateway_tenant_id: '1',
   },
 
   _images+:: {
@@ -49,7 +50,7 @@ local k = import 'ksonnet-util/kausal.libsonnet';
             listen               80;
             auth_basic           “Prometheus”;
             auth_basic_user_file /etc/nginx/secrets/.htpasswd;
-            proxy_set_header     X-Scope-OrgID 1;
+            proxy_set_header     X-Scope-OrgID %(gateway_tenant_id);
 
             location = /api/prom/push {
               proxy_pass       http://distributor.%(namespace)s.svc.cluster.local:%(http_listen_port)s$request_uri;

commit d59792ffa315f5a76dd1b253180d0adc819b6892

production: Add missing string formatter token to nginx.conf config map (#8580)
The `nginx.conf` config map server settings has a `proxy_set_header` line setting the X-Scope-OrgID using the gateway tenant ID. The Jsonnet string formatter is missing the `s` token.

This PR adds the missing formatter token to the `nginx.conf` config map that's causing an `Unrecognized conversion type: ;` when trying to deploy.

Fixes a bug introduced in #7624

production/ksonnet/loki/gateway.libsonnet
@@ -50,7 +50,7 @@ local k = import 'ksonnet-util/kausal.libsonnet';
             listen               80;
             auth_basic           “Prometheus”;
             auth_basic_user_file /etc/nginx/secrets/.htpasswd;
-            proxy_set_header     X-Scope-OrgID %(gateway_tenant_id);
+            proxy_set_header     X-Scope-OrgID %(gateway_tenant_id)s;
 
             location = /api/prom/push {
               proxy_pass       http://distributor.%(namespace)s.svc.cluster.local:%(http_listen_port)s$request_uri;