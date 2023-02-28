85227feb7965012a9cd56013f80484292396a90a
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-02-28 20:55:49 +0100

    using two maps preventing contetion

--- clients/pkg/promtail/client/client_writeto.go
+++ clients/pkg/promtail/client/client_writeto.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/tsdb/chunks"
 	"github.com/prometheus/prometheus/tsdb/record"
 
 	"github.com/grafana/loki/clients/pkg/promtail/api"
@@ -14,70 +15,69 @@ import (
 	"github.com/grafana/loki/pkg/util"
 )
 
-type seriesCacheEntry struct {
-	labels            model.LabelSet
-	lastSeenInSegment int
-}
-
-type seriesCache = map[uint64]*seriesCacheEntry
-
 // clientWriteTo implements a wal.WriteTo that re-builds entries with the stored series, and the received entries. After,
 // sends each to the provided Client channel.
 type clientWriteTo struct {
-	series    seriesCache
-	cacheLock sync.RWMutex
-	logger    log.Logger
-	toClient  chan<- api.Entry
+	series     map[chunks.HeadSeriesRef]model.LabelSet
+	seriesLock sync.RWMutex
+
+	// seriesSegment keeps track of the last segment in which the series pointed by each key in this map was seen. Keeping
+	// this in a separate map avoids unnecessary contention.
+	//
+	// Even though it doesn't present a difference right now according to benchmarks, it will help when we introduce other
+	// calls from the wal.Watcher to the wal.WriteTo like `UpdateSeriesSegment`.
+	seriesSegment     map[chunks.HeadSeriesRef]int
+	seriesSegmentLock sync.RWMutex
+
+	logger   log.Logger
+	toClient chan<- api.Entry
 }
 
 // newClientWriteTo creates a new clientWriteTo
 func newClientWriteTo(toClient chan<- api.Entry, logger log.Logger) *clientWriteTo {
 	return &clientWriteTo{
-		series:   make(seriesCache),
-		toClient: toClient,
-		logger:   logger,
+		series:        make(map[chunks.HeadSeriesRef]model.LabelSet),
+		seriesSegment: make(map[chunks.HeadSeriesRef]int),
+		toClient:      toClient,
+		logger:        logger,
 	}
 }
 
 func (c *clientWriteTo) StoreSeries(series []record.RefSeries, segment int) {
-	c.cacheLock.Lock()
-	defer c.cacheLock.Unlock()
+	c.seriesLock.Lock()
+	defer c.seriesLock.Unlock()
+	c.seriesSegmentLock.Lock()
+	defer c.seriesSegmentLock.Unlock()
 	for _, seriesRec := range series {
-		entry, ok := c.series[uint64(seriesRec.Ref)]
-		if ok && entry.lastSeenInSegment < segment {
-			// entry is present, touch if required
-			entry.lastSeenInSegment = segment
-			continue
-		}
-		// entry is not present
-		c.series[uint64(seriesRec.Ref)] = &seriesCacheEntry{
-			labels:            util.MapToModelLabelSet(seriesRec.Labels.Map()),
-			lastSeenInSegment: segment,
-		}
+		c.seriesSegment[seriesRec.Ref] = segment
+		// maybe we can avoid doing this below if already present
+		labels := util.MapToModelLabelSet(seriesRec.Labels.Map())
+		c.series[seriesRec.Ref] = labels
 	}
 }
 
 // SeriesReset will delete all cache entries that were last seen in segments numbered equal or lower than segmentNum
 func (c *clientWriteTo) SeriesReset(segmentNum int) {
-	c.cacheLock.Lock()
-	defer c.cacheLock.Unlock()
-	for ref, entry := range c.series {
-		// Since segmentNum means all segment with number i <= segmentNum are safe to be reclaimed, evict all cache
-		// entries that were last seen in those
-		if entry.lastSeenInSegment <= segmentNum {
+	c.seriesLock.Lock()
+	defer c.seriesLock.Unlock()
+	c.seriesSegmentLock.Lock()
+	defer c.seriesSegmentLock.Unlock()
+	for k, v := range c.seriesSegment {
+		if v < segmentNum {
 			level.Debug(c.logger).Log("msg", fmt.Sprintf("reclaiming series under segment %d", segmentNum))
-			delete(c.series, ref)
+			delete(c.seriesSegment, k)
+			delete(c.series, k)
 		}
 	}
 }
 
 func (c *clientWriteTo) AppendEntries(entries wal.RefEntries) error {
 	var entry api.Entry
-	c.cacheLock.RLock()
-	l, ok := c.series[uint64(entries.Ref)]
-	c.cacheLock.RUnlock()
+	c.seriesLock.RLock()
+	l, ok := c.series[entries.Ref]
+	c.seriesLock.RUnlock()
 	if ok {
-		entry.Labels = l.labels
+		entry.Labels = l
 		for _, e := range entries.Entries {
 			entry.Entry = e
 			c.toClient <- entry
