2ed5a46cf2df957fa652443990f547ce9ac045bb
Travis Patterson <travis.patterson@grafana.com>
2023-02-28 13:44:22 -0700

    don't include noop label filters

--- pkg/logql/syntax/ast.go
+++ pkg/logql/syntax/ast.go
@@ -82,7 +82,7 @@ func (m MultiStageExpr) stages() ([]log.Stage, error) {
 		if err != nil {
 			return nil, logqlmodel.NewStageError(e.String(), err)
 		}
-		if p == log.NoopStage {
+		if p == log.NoopStage || p == log.NoopLabelFilter {
 			continue
 		}
 		c = append(c, p)
