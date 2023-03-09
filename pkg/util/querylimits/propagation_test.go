a55e58f4d02e41e8c6d63fab3db77a20a8f3e864
Callum Styan <callumstyan@gmail.com>
2023-03-09 13:36:43 -0800

    allow omitempty for the query limits struct
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- pkg/util/querylimits/propagation_test.go
+++ pkg/util/querylimits/propagation_test.go
@@ -24,13 +24,25 @@ func TestInjectAndExtractQueryLimits(t *testing.T) {
 }
 
 func TestDeserializingQueryLimits(t *testing.T) {
-	payload := `{"maxQueryLength":"1h"}`
+	// full limits
+	payload := `{"maxEntriesLimitPerQuery": 100, "maxQueryLength": "2d", "maxQueryLookback": "2w", "queryTimeout": "5s"}`
 	limits, err := UnmarshalQueryLimits([]byte(payload))
 	require.NoError(t, err)
+	require.Equal(t, model.Duration(2*24*time.Hour), limits.MaxQueryLength)
+	require.Equal(t, model.Duration(14*24*time.Hour), limits.MaxQueryLookback)
+	require.Equal(t, model.Duration(5*time.Second), limits.QueryTimeout)
+	require.Equal(t, 100, limits.MaxEntriesLimitPerQuery)
+	// some limits are empty
+	payload = `{"maxQueryLength":"1h"}`
+	limits, err = UnmarshalQueryLimits([]byte(payload))
+	require.NoError(t, err)
 	require.Equal(t, model.Duration(3600000000000), limits.MaxQueryLength)
+	require.Equal(t, model.Duration(0), limits.MaxQueryLookback)
+	require.Equal(t, 0, limits.MaxEntriesLimitPerQuery)
 }
 
 func TestSerializingQueryLimits(t *testing.T) {
+	// full struct
 	limits := QueryLimits{
 		MaxQueryLength:          model.Duration(2 * 24 * time.Hour),
 		MaxQueryLookback:        model.Duration(14 * 24 * time.Hour),
@@ -41,5 +53,17 @@ func TestSerializingQueryLimits(t *testing.T) {
 	actual, err := MarshalQueryLimits(&limits)
 	require.NoError(t, err)
 	expected := `{"maxEntriesLimitPerQuery": 100, "maxQueryLength": "2d", "maxQueryLookback": "2w", "queryTimeout": "5s"}`
+	require.JSONEq(t, expected, string(actual))
+
+	// some limits are empty
+	limits = QueryLimits{
+		MaxQueryLength:          model.Duration(2 * 24 * time.Hour),
+		MaxQueryLookback:        model.Duration(14 * 24 * time.Hour),
+		MaxEntriesLimitPerQuery: 100,
+	}
+
+	actual, err = MarshalQueryLimits(&limits)
+	require.NoError(t, err)
+	expected = `{"maxEntriesLimitPerQuery": 100, "maxQueryLength": "2d", "maxQueryLookback": "2w"}`
 	require.JSONEq(t, expected, string(actual))
 }
