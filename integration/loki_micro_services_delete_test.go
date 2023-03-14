43a1f96a00d0389bc9e58d4fcf2a20c3c0597ae9
Danny Kopping <danny.kopping@grafana.com>
2023-03-14 15:12:58 +0200

    Allow loki logs to be optionally output in integration tests
    Signed-off-by: Danny Kopping <danny.kopping@grafana.com>

--- integration/loki_micro_services_delete_test.go
+++ integration/loki_micro_services_delete_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 func TestMicroServicesDeleteRequest(t *testing.T) {
-	clu := cluster.New()
+	clu := cluster.New(false, nil)
 	defer func() {
 		assert.NoError(t, clu.Cleanup())
 		storage.ResetBoltDBIndexClientWithShipper()
