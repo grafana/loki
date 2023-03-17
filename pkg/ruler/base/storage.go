13f6abe831beda25cd93a3850f5887011426ed47
Aditya C S <aditya.gnu@gmail.com>
2023-03-17 13:16:11 +0530

    code cleanup

--- pkg/ruler/base/storage.go
+++ pkg/ruler/base/storage.go
@@ -71,11 +71,6 @@ func (cfg *RuleStoreConfig) Validate() error {
 	if err := cfg.S3.Validate(); err != nil {
 		return errors.Wrap(err, "invalid S3 Storage config")
 	}
-	/* currently cos does not support storage class so we don't add validation
-	if err := cfg.COS.Validate(); err != nil {
-		return errors.Wrap(err, "invalid COS Storage config")
-	}
-	*/
 	return nil
 }
 
