eca437e49d3cfbb384a3c82d91981e8ad93c0da5
Salva Corts <salva.corts@grafana.com>
2023-03-29 12:16:16 +0200

    Update pkg/util/querylimits/propagation.go
    Co-authored-by: Dylan Guedes <djmgguedes@gmail.com>

--- pkg/util/querylimits/propagation.go
+++ pkg/util/querylimits/propagation.go
@@ -26,7 +26,7 @@ type QueryLimits struct {
 	MaxEntriesLimitPerQuery int              `json:"maxEntriesLimitPerQuery,omitempty"`
 	QueryTimeout            model.Duration   `json:"queryTimeout,omitempty"`
 	RequiredLabels          []string         `json:"requiredLabels,omitempty"`
-	RequiredNumberLabels    int              `json:"requiredNumberLabelsMatchers,omitempty"`
+	RequiredNumberLabels    int              `json:"requiredNumberLabelMatchers,omitempty"`
 	MaxQueryBytesRead       flagext.ByteSize `json:"maxQueryBytesRead,omitempty"`
 }
 
