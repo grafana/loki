cf0f4488751df3ba27a86623d41d700d5797c7e6
Trevor Whitney <trevorjwhitney@gmail.com>
2023-05-01 10:01:06 -0600

    Fix journald support in promtail

--- nix/default.nix
+++ nix/default.nix
@@ -67,17 +67,28 @@
         '';
       });
 
-      promtail = loki.overrideAttrs (oldAttrs: rec {
+      promtail = loki.overrideAttrs (oldAttrs: with prev; rec {
         pname = "promtail";
+
+        nativeBuildInputs = [ makeWrapper ];
+        buildInputs = oldAttrs.buildInputs ++ lib.optionals stdenv.isLinux [ systemd.dev ];
 
         buildPhase = ''
           export GOCACHE=$TMPDIR/go-cache
+          ${lib.optionalString stdenv.isLinux ''
+            export PROMTAIL_JOURNAL_ENABLED=true
+          ''}
           make clean promtail
         '';
 
         installPhase = ''
           mkdir -p $out/bin
           install -m755 clients/cmd/promtail/promtail $out/bin/promtail
+        '';
+
+        preFixup = lib.optionalString stdenv.isLinux ''
+          wrapProgram $out/bin/promtail \
+            --prefix LD_LIBRARY_PATH : "${lib.getLib systemd}/lib"
         '';
       });
     };
