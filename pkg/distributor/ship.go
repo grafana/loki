2b5cd5f75589524a1f8fa743c5d0946341cb72a4
fuling <fuling.lgz@alibaba-inc.com>
2023-06-02 15:38:13 +0800

    format

--- pkg/distributor/ship.go
+++ pkg/distributor/ship.go
@@ -4,35 +4,31 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/grafana/loki/pkg/logproto"
-
 	"github.com/prometheus/prometheus/model/labels"
+
+	"github.com/grafana/loki/pkg/logproto"
 )
 
 var DefaultLogSender LogSender
 
-//send log to ES or kafka
 type LogSender interface {
 	Send(ctx context.Context, tenantID string, req *logproto.PushRequest, header http.Header) error
 }
 
 var DefaultLogPipeline LogPipeline
 
-//send log to ES or kafka
 type LogPipeline interface {
 	Pipeline(ctx context.Context, tenantID string, labels string, entry logproto.Entry) (bool, error)
 }
 
 var DefaultLabelPipeline LabelPipeline
 
-//label filter base server
 type LabelPipeline interface {
 	Pipeline(tenantID string, labels labels.Labels) (labels.Labels, error)
 }
 
 var DefaultLogShip LogShip
 
-//send log to ES or kafka
 type LogShip interface {
 	Ship(ctx context.Context, tenantID string, req *logproto.PushRequest) (*logproto.PushRequest, error)
 }
