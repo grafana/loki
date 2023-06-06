22759469145284314638d6cf913690723fe8daa5
Periklis Tsirakidis <periklis@nefeli.eu>
2023-06-06 20:16:03 +0200

    Update operator/internal/manifests/openshift/alertingrule.go
    Co-authored-by: Mohamed-Amine Bouqsimi <bouqsimiamine@gmail.com>

--- operator/internal/manifests/openshift/alertingrule.go
+++ operator/internal/manifests/openshift/alertingrule.go
@@ -5,21 +5,14 @@ import lokiv1 "github.com/grafana/loki/operator/apis/loki/v1"
 func AlertingRuleTenantLabels(ar *lokiv1.AlertingRule) {
 	switch ar.Spec.TenantID {
 	case tenantApplication:
-		tenantLabels := map[string]string{
-			opaDefaultLabelMatcher: ar.Namespace,
-		}
-
 		for groupIdx, group := range ar.Spec.Groups {
 			group := group
 			for ruleIdx, rule := range group.Rules {
 				rule := rule
-				for key, value := range tenantLabels {
-					if rule.Labels == nil {
-						rule.Labels = map[string]string{}
-					}
-
-					rule.Labels[key] = value
+				if rule.Labels == nil {
+					rule.Labels = map[string]string{}
 				}
+				rule.Labels[opaDefaultLabelMatcher] = ar.Namespace
 				group.Rules[ruleIdx] = rule
 			}
 			ar.Spec.Groups[groupIdx] = group
