0a5e149ea540d9b034ff7023ca6f95ce09805080
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2023-06-06 12:06:50 +0530

    query-scheduler: fix query distribution in SSD mode (#9471)
    **What this PR does / why we need it**:
    When we run the `query-scheduler` in `ring` mode, `queriers` and
    `query-frontend` discover the available `query-scheduler` instances
    using the ring. However, we have a problem when `query-schedulers` are
    not running in the same process as queriers and query-frontend since [we
    try to get the ring client interface from the scheduler
    instance](https://github.com/grafana/loki/blob/abd6131bba18db7f3575241c5e6dc4eed879fbc0/pkg/loki/modules.go#L358).
    
    This causes queries not to be spread across all the available queriers
    when running in SSD mode because [we point querier workers to query
    frontend when there is no ring client and scheduler address
    configured](https://github.com/grafana/loki/blob/b05f4fced305800b32641ae84e3bed5f1794fa7d/pkg/querier/worker_service.go#L115).
    
    I have fixed this issue by adding a new hidden target to initialize the
    ring client in `reader`/`member` mode based on which service is
    initializing it. `reader` mode will be used by `queriers` and
    `query-frontend` for discovering `query-scheduler` instances from the
    ring. `member` mode will be used by `query-schedulers` for registering
    themselves in the ring.
    
    I have also made a couple of changes not directly related to the issue
    but it fixes some problems:
    * [reset metric registry for each integration
    test](https://github.com/grafana/loki/commit/18c4fe59078b649ad6a788a48765b101d0b97618)
    - Previously we were reusing the same registry for all the tests and
    just [ignored the attempts to register same
    metrics](https://github.com/grafana/loki/blob/01f0ded7fcb57e3a7b26ffc1e8e3abf04a403825/integration/cluster/cluster.go#L113).
    This causes the registry to have metrics registered only from the first
    test so any updates from subsequent tests won't reflect in the metrics.
    metrics was the only reliable way for me to verify that
    `query-schedulers` were connected to `queriers` and `query-frontend`
    when running in ring mode in the integration test that I added to test
    my changes. This should also help with other tests where earlier it was
    hard to reliably check the metrics.
    * [load config from cli as well before applying dynamic
    config](https://github.com/grafana/loki/commit/f9e2448fc7e718db107165cd908054c806b84337)
    - Previously we were applying dynamic config considering just the config
    from config file. This results in unexpected config changes, for
    example, [this config
    change](https://github.com/grafana/loki/blob/4148dd2c51cb827ec3889298508b95ec7731e7fd/integration/loki_micro_services_test.go#L66)
    was getting ignored and [dynamic config tuning was unexpectedly turning
    on ring
    mode](https://github.com/grafana/loki/blob/52cd0a39b8266564352c61ab9b845ab597008770/pkg/loki/config_wrapper.go#L94)
    in the config. It is better to do any config tuning based on both file
    and cli args configs.
    
    **Which issue(s) this PR fixes**:
    Fixes #9195

---
+++ pkg/scheduler/lifecycle.go
@@ -0,0 +1,28 @@
+package scheduler
+
+import (
+	"github.com/grafana/dskit/ring"
+)
+
+func (rm *RingManager) OnRingInstanceRegister(_ *ring.BasicLifecycler, ringDesc ring.Desc, instanceExists bool, instanceID string, instanceDesc ring.InstanceDesc) (ring.InstanceState, ring.Tokens) {
+	// When we initialize the scheduler instance in the ring we want to start from
+	// a clean situation, so whatever is the state we set it JOINING, while we keep existing
+	// tokens (if any) or the ones loaded from file.
+	var tokens []uint32
+	if instanceExists {
+		tokens = instanceDesc.GetTokens()
+	}
+
+	takenTokens := ringDesc.GetTokens()
+	newTokens := ring.GenerateTokens(ringNumTokens-len(tokens), takenTokens)
+
+	// Tokens sorting will be enforced by the parent caller.
+	tokens = append(tokens, newTokens...)
+
+	return ring.JOINING, tokens
+}
+
+func (rm *RingManager) OnRingInstanceTokens(_ *ring.BasicLifecycler, _ ring.Tokens) {}
+func (rm *RingManager) OnRingInstanceStopping(_ *ring.BasicLifecycler)              {}
+func (rm *RingManager) OnRingInstanceHeartbeat(_ *ring.BasicLifecycler, _ *ring.Desc, _ *ring.InstanceDesc) {
+}
