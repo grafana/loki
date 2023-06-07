af287ac3eba46e04ad1d5cf8051262ea0f2a25de
Salva Corts <salva.corts@grafana.com>
2023-06-07 11:25:51 +0200

    Add summary stats and metrics for stats cache (#9536)
    **What this PR does / why we need it**:
    When a query finishes, we return (and log) the following stats:
    ```go
    Cache.Chunk.Requests             0
    Cache.Chunk.EntriesRequested     0
    Cache.Chunk.EntriesFound         0
    Cache.Chunk.EntriesStored        0
    Cache.Chunk.BytesSent            0 B
    Cache.Chunk.BytesReceived        0 B
    Cache.Chunk.DownloadTime         0s
    Cache.Index.Requests             0
    Cache.Index.EntriesRequested     0
    Cache.Index.EntriesFound         0
    Cache.Index.EntriesStored        0
    Cache.Index.BytesSent            0 B
    Cache.Index.BytesReceived        0 B
    Cache.Index.DownloadTime         0s
    Cache.Result.Requests            13
    Cache.Result.EntriesRequested    13
    Cache.Result.EntriesFound        13
    Cache.Result.EntriesStored       0
    Cache.Result.BytesSent   0 B
    Cache.Result.BytesReceived       2.5 kB
    Cache.Result.DownloadTime        4.600266ms
    ```
    
    In addition to that, we log the following in metrics.go:
    ```
    level=info ts=2023-05-29T09:17:10.93029945Z caller=metrics.go:152 component=frontend org_id=145265 traceID=52d59b78fe6b9221 sampled=true latency=fast query="{cluster=\"dev-us-central-0\", namespace=~\"loki.*\", container=~\"distributor|ingester
    |promtail|index-gateway|compactor\"} |= \"thislinewillnotexist\"" query_hash=1194136170 query_type=filter range_type=range length=3h0m0s start_delta=165h37m24.930289434s end_delta=162h37m24.930289612s step=43s duration=2.473055ms status=200 lim
    it=30 returned_lines=0 throughput=0B total_bytes=0B lines_per_second=0 total_lines=0 total_entries=0 store_chunks_download_time=0s queue_time=0s splits=13 shards=0 cache_chunk_req=0 cache_chunk_hit=0 cache_chunk_bytes_stored=0 cache_chunk_bytes
    _fetched=0 cache_chunk_download_time=0s cache_index_req=0 cache_index_hit=0 cache_index_download_time=0s cache_result_req=13 cache_result_hit=13 cache_result_download_time=4.600266ms
    ```
    
    With the goal of being able to better monitor how the stats cache is
    performing; this PR adds stats for the index stats cache, similarly to
    how it's done for the results cache.
    
    Here's an example of the new stats being returned and printed:
    ```go
    ...
    Cache.StatsResult.Requests               180
    Cache.StatsResult.EntriesRequested       129
    Cache.StatsResult.EntriesFound   129
    Cache.StatsResult.EntriesStored          51
    Cache.StatsResult.BytesSent              0 B
    Cache.StatsResult.BytesReceived          75 kB
    ...
    ```
    
    And the new stats from metrics.go
    ```
    ... caller=metrics.go:155 ... cache_stats_results_req=129 cache_stats_results_hit=129 cache_stats_results_download_ti
    me=156.864429ms ...
    ```
    
    **Special notes for your reviewer**:
    - Blocked by https://github.com/grafana/loki/pull/9535
    - Note the new`stats.GetOrCreateContext` func. It's used inside the
    `query.Exec` method so we don't overwrite the stats added in the stats
    middleware.
    
    **Checklist**
    - [x] Reviewed the
    [`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
    guide (**required**)
    - [ ] Documentation added
    - [x] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/upgrading/_index.md`
    - [ ] For Helm chart changes bump the Helm chart version in
    `production/helm/loki/Chart.yaml` and update
    `production/helm/loki/CHANGELOG.md` and
    `production/helm/loki/README.md`. [Example
    PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)

--- pkg/util/marshal/marshal_test.go
+++ pkg/util/marshal/marshal_test.go
@@ -106,6 +106,15 @@ var queryTests = []struct {
 							"requests": 0,
 							"downloadTime": 0
 						},
+						"statsResult": {
+							"entriesFound": 0,
+							"entriesRequested": 0,
+							"entriesStored": 0,
+							"bytesReceived": 0,
+							"bytesSent": 0,
+							"requests": 0,
+							"downloadTime": 0
+						},
 						"result": {
 							"entriesFound": 0,
 							"entriesRequested": 0,
@@ -235,6 +244,15 @@ var queryTests = []struct {
 						"downloadTime": 0
 					},
 					"index": {
+						"entriesFound": 0,
+						"entriesRequested": 0,
+						"entriesStored": 0,
+						"bytesReceived": 0,
+						"bytesSent": 0,
+						"requests": 0,
+						"downloadTime": 0
+					},
+					"statsResult": {
 						"entriesFound": 0,
 						"entriesRequested": 0,
 						"entriesStored": 0,
@@ -393,6 +411,15 @@ var queryTests = []struct {
 						"downloadTime": 0
 					},
 					"index": {
+						"entriesFound": 0,
+						"entriesRequested": 0,
+						"entriesStored": 0,
+						"bytesReceived": 0,
+						"bytesSent": 0,
+						"requests": 0,
+						"downloadTime": 0
+					},
+					"statsResult": {
 						"entriesFound": 0,
 						"entriesRequested": 0,
 						"entriesStored": 0,
