6897943ba7813b5d5b7aba4f6f362ee8f41bab91
Jack Baldry <jack.baldry@grafana.com>
2023-06-08 13:13:34 +0100

    Update make-docs procedure
    ## 3.0.0 (2023-05-18)
    
    ### Fixed
    
    - Compatibility with the updated Make targets in the `website` repository.
      `docs` now runs this script itself, `server-docs` builds the site with the `docs` Hugo environment.
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,2 +1,5 @@
 # List of projects to provide to the make-docs script.
-PROJECTS = loki
+PROJECTS := loki
+
+# Use alternative image until make-docs 3.0.0 is rolled out.
+export DOCS_IMAGE := grafana/docs-base:dbd975af06
