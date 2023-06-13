6e0633a4f4d442e202de4b193a3e84695a3bcf26
Salva Corts <salva.corts@grafana.com>
2023-06-13 09:31:47 +0200

    Add keep stage to LogQL

---
+++ pkg/logql/log/keep_labels.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"github.com/prometheus/prometheus/model/labels"
+)
+
+type KeepLabels struct {
+	keepLabels []KeepLabel
+}
+
+type KeepLabel struct {
+	Matcher *labels.Matcher
+	Name    string
+}
+
+func NewKeepLabel(matcher *labels.Matcher, name string) KeepLabel {
+	return KeepLabel{
+		Matcher: matcher,
+		Name:    name,
+	}
+}
+
+func NewKeepLabels(dl []KeepLabel) *KeepLabels {
+	return &KeepLabels{keepLabels: dl}
+}
+
+func (kl *KeepLabels) Process(ts int64, line []byte, lbls *LabelsBuilder) ([]byte, bool) {
+	if len(kl.keepLabels) == 0 {
+		return line, true
+	}
+
+	// TODO: Reuse buf?
+	for _, lb := range lbls.UnsortedLabels(nil) {
+		var keep bool
+		for _, keepLabel := range kl.keepLabels {
+			if keepLabel.Matcher != nil && keepLabel.Matcher.Name == lb.Name && keepLabel.Matcher.Matches(lb.Value) {
+				keep = true
+				break
+			}
+
+			if keepLabel.Name == lb.Name {
+				keep = true
+				break
+			}
+		}
+
+		if !keep {
+			lbls.Del(lb.Name)
+		}
+	}
+
+	return line, true
+}
+
+func (kl *KeepLabels) RequiredLabelNames() []string {
+	return []string{}
+}
---
+++ pkg/logql/log/keep_labels_test.go
@@ -0,0 +1,89 @@
+package log
+
+import (
+	"testing"
+
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_KeepLabels(t *testing.T) {
+	for _, tc := range []struct {
+		Name       string
+		keepLabels []KeepLabel
+		lbs        labels.Labels
+
+		want labels.Labels
+	}{
+		{
+			"keep all",
+			[]KeepLabel{},
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+				"env", "prod",
+				"pod_uuid", "foo",
+			),
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+				"env", "prod",
+				"pod_uuid", "foo",
+			),
+		},
+		{
+			"keep by name",
+			[]KeepLabel{
+				{
+					nil,
+					"app",
+				},
+				{
+					nil,
+					"namespace",
+				},
+			},
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+				"env", "prod",
+				"pod_uuid", "foo",
+			),
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+			),
+		},
+		{
+			"keep labels with names and matcher",
+			[]KeepLabel{
+				{
+					labels.MustNewMatcher(labels.MatchEqual, "namespace", "prod"),
+					"",
+				},
+				{
+					nil,
+					"app",
+				},
+			},
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+				"env", "prod",
+				"pod_uuid", "foo",
+			),
+			labels.FromStrings(
+				"app", "foo",
+				"namespace", "prod",
+			),
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			keepLabels := NewKeepLabels(tc.keepLabels)
+			lbls := NewBaseLabelsBuilder().ForLabels(tc.lbs, tc.lbs.Hash())
+			lbls.Reset()
+			keepLabels.Process(0, []byte(""), lbls)
+			require.Equal(t, tc.want, lbls.LabelsResult().Labels())
+		})
+	}
+}
