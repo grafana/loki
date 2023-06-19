90732e1474867d1bd1ca7be4ff5b580638755d4b
Callum Styan <callumstyan@gmail.com>
2023-06-19 14:19:31 -0700

    simplify heap implementation
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- pkg/util/sketch/heap.go
+++ pkg/util/sketch/heap.go
@@ -1,143 +1,76 @@
 package sketch
 
+import (
+	"container/heap"
+)
+
 type node struct {
 	event string
-	count int64
+	count uint32
+	// used for the container heap Fix function
+	index uint16
 }
 
-// MinHeap is a binary heap implementation, we could switch to something with better than O(n) merging if we need to
-type MinHeap struct {
-	max  int
-	heap []*node
+type MinHeap []*node
+
+func (h MinHeap) Len() int {
+	return len(h)
 }
 
-func NewMinHeap(max int) *MinHeap {
-	return &MinHeap{
-		max:  max,
-		heap: make([]*node, 0, max),
+// less is only used in the underlying pop implementation
+func (h MinHeap) Less(i, j int) bool {
+	return h[i].count < h[j].count
+}
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = uint16(i)
+	h[j].index = uint16(j)
+}
+
+func (h *MinHeap) Push(x interface{}) {
+	n := len(*h)
+	item := x.(*node)
+	item.index = uint16(n)
+	*h = append(*h, item)
+}
+
+func (h *MinHeap) Pop() interface{} {
+	if len(*h) == 0 {
+		return nil
 	}
-}
-
-func (mh *MinHeap) Push(event string, count int64) {
-	mh.heap = append(mh.heap, &node{event: event, count: count})
-	mh.siftUp(len(mh.heap) - 1)
-}
-
-func (mh *MinHeap) Pop() *node {
-	top := mh.min()
-	heapSize := len(mh.heap)
+	top := (*h)[0]
+	heapSize := len(*h)
 
 	if heapSize > 1 {
-		mh.heap[0] = mh.heap[len(mh.heap)-1]
+		(*h)[0] = (*h)[heapSize-1]
 	}
-
-	mh.heap = mh.heap[:len(mh.heap)-1]
-	mh.siftDown(0)
+	(*h) = (*h)[:len(*h)-1]
+	heap.Fix(h, 0)
 	return top
 }
 
-func (mh *MinHeap) UpdateValue(event string) {
-	for _, k := range mh.heap {
+func (h *MinHeap) Peek() interface{} {
+	return (*h)[0]
+}
+
+// update modifies the count and value of an Item in the queue.
+func (h *MinHeap) update(event string, count uint32) {
+	updateNode := -1
+	for i, k := range *h {
 		if k.event == event {
-			k.count += 1
+			k.count = count
+			updateNode = i
+			break
 		}
 	}
-	// When we added something to the heap it might have become the
-	// new minimum element, but updating it's value could have changed that
-	// so we should re-heapify.
-	mh.siftDown(0)
+	heap.Fix(h, updateNode)
 }
 
-func (mh *MinHeap) Peek() *node {
-	return mh.min()
-}
-
-func (mh *MinHeap) parent(i int) (*node, int) {
-	if i == 0 {
-		return mh.heap[0], 0
-	}
-	return mh.heap[(i-1)/2], (i - 1) / 2
-}
-
-func (mh *MinHeap) leftChild(i int) (*node, int) {
-	// todo nicer interface for nil node
-	if len(mh.heap) <= (2*i + 1) {
-		return &node{}, -1
-	}
-	return mh.heap[2*i+1], 2*i + 1
-}
-
-func (mh *MinHeap) rightChild(i int) (*node, int) {
-	// todo nicer interface for nil node
-	if len(mh.heap) <= (2*i + 2) {
-		return &node{}, -1
-	}
-	return mh.heap[2*i+2], 2*i + 2
-}
-
-func (mh *MinHeap) minChildIndex(index int) int {
-	leftChild, leftChildIndex := mh.leftChild(index)
-	if leftChildIndex == -1 {
-		// no right child yet
-		return index
-	}
-	if leftChildIndex >= len(mh.heap) {
-		return leftChildIndex
-	}
-
-	rightChild, rightChildIndex := mh.rightChild(index)
-	if rightChildIndex == -1 {
-		// no right child yet
-		return leftChildIndex
-	}
-	if rightChild.count < leftChild.count {
-		return rightChildIndex
-	}
-
-	return leftChildIndex
-}
-
-func (mh *MinHeap) min() *node {
-	if len(mh.heap) == 0 {
-		return &node{
-			count: -1,
+func (h *MinHeap) Find(e string) (int, bool) {
+	for i := 0; i < len(*h); i++ {
+		if (*h)[i].event == e {
+			return i, true
 		}
 	}
-	return mh.heap[0]
-}
-
-// move a node up the tree
-func (mh *MinHeap) siftUp(index int) {
-	for index > 0 {
-		parentIndex := (index - 1) / 2
-
-		if mh.heap[parentIndex].count < mh.heap[index].count {
-			return
-		}
-
-		mh.heap[parentIndex], mh.heap[index] = mh.heap[index], mh.heap[parentIndex]
-		index = parentIndex
-	}
-}
-
-// move a node down the tree
-func (mh *MinHeap) siftDown(index int) {
-	_, i := mh.leftChild(index)
-	for i < len(mh.heap) {
-		_, i = mh.leftChild(index)
-		if i == -1 {
-			return
-		}
-		minChildIndex := mh.minChildIndex(index)
-		if minChildIndex == -1 {
-			return
-		}
-
-		if mh.heap[minChildIndex].count >= mh.heap[index].count {
-			return
-		}
-
-		mh.heap[minChildIndex], mh.heap[index] = mh.heap[index], mh.heap[minChildIndex]
-		index = minChildIndex
-	}
+	return 0, false
 }
