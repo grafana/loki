0d4ea173daf5b55e10c2b5575c4a14b88fa3b9d8
DylanGuedes <djmgguedes@gmail.com>
2023-06-27 15:12:49 -0300

    Change internal errors handling.

--- pkg/distributor/writefailures/manager.go
+++ pkg/distributor/writefailures/manager.go
@@ -1,8 +1,6 @@
 package writefailures
 
 import (
-	"context"
-	"errors"
 	"time"
 
 	"github.com/go-kit/log"
@@ -42,29 +40,8 @@ func (m *Manager) Log(tenantID string, err error) {
 		return
 	}
 
-	if m.skipError(err) {
-		return
-	}
-
 	errMsg := err.Error()
 	if m.limiter.AllowN(time.Now(), tenantID, len(errMsg)) {
 		level.Error(m.logger).Log("msg", "write operation failed", "details", errMsg)
 	}
-}
-
-// skipError dictates if a given error should be ignored or not.
-//
-// We should ignore errors that weren't caused by an inappropriate write.
-// An inappropriate write would cause other errors instead.
-//
-// Common scenarios where we shouldn't report errors:
-// - Overloaded ingesters causing write timeouts
-// - Distributor OOMing causing cancellations
-// - Ingester OOMing causing cancellations
-func (m *Manager) skipError(err error) bool {
-	if errors.Is(err, context.DeadlineExceeded) || errors.Is(err, context.Canceled) {
-		return true
-	}
-
-	return false
 }
