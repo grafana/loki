grafana/loki

commit b35bbd80d65f09eb6f1e6ba1bfe05bf4af712a97

Support content negotiation between query frontend and querier. (#9813)
**What this PR does / why we need it**:
Currently, the querier sends results to the query frontend in JSON which
is then decoded to Protobuf. It is more efficient to send the results as
Protobuf.

This will also allow to extend the results with custom data structures.

The change is backwards compatible through content negotiation.

**Special notes for your reviewer**:

**Checklist**
- [x] Reviewed the
[`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
guide (**required**)
- [x] Documentation added
- [x] Tests updated
- [x] `CHANGELOG.md` updated
- [ ] If the change is worth mentioning in the release notes, add
`add-to-release-notes` label
- [ ] Changes that require user attention or interaction to upgrade are
documented in `docs/sources/upgrading/_index.md`
- [ ] For Helm chart changes bump the Helm chart version in
`production/helm/loki/Chart.yaml` and update
`production/helm/loki/CHANGELOG.md` and
`production/helm/loki/README.md`. [Example
PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)

pkg/querier/queryrange/marshal.go
@@ -1,0 +1,226 @@
+// Package contains methods to marshal logqmodel types to queryrange Protobuf types.
+// Its cousing is util/marshal which converts them to JSON.
+package queryrange
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/prometheus/prometheus/promql"
+
+	"github.com/grafana/loki/pkg/loghttp"
+	"github.com/grafana/loki/pkg/logproto"
+	"github.com/grafana/loki/pkg/logql"
+	"github.com/grafana/loki/pkg/logqlmodel"
+	"github.com/grafana/loki/pkg/querier/queryrange/queryrangebase"
+	"github.com/grafana/loki/pkg/storage/stores/index/stats"
+	"github.com/grafana/loki/pkg/util/marshal"
+)
+
+const (
+	JSONType     = `application/json; charset=utf-8`
+	ProtobufType = `application/vnd.google.protobuf`
+)
+
+func WriteResponse(req *http.Request, params *logql.LiteralParams, v any, w http.ResponseWriter) error {
+	if req.Header.Get("Accept") == ProtobufType {
+		w.Header().Add("Content-Type", ProtobufType)
+		return WriteResponseProtobuf(req, params, v, w)
+	}
+
+	w.Header().Add("Content-Type", JSONType)
+	return marshal.WriteResponseJSON(req, v, w)
+}
+
+func WriteResponseProtobuf(req *http.Request, params *logql.LiteralParams, v any, w http.ResponseWriter) error {
+	switch result := v.(type) {
+	case logqlmodel.Result:
+		return WriteQueryResponseProtobuf(params, result, w)
+	case *logproto.LabelResponse:
+		version := loghttp.GetVersion(req.RequestURI)
+		return WriteLabelResponseProtobuf(version, *result, w)
+	case logproto.SeriesResponse:
+		version := loghttp.GetVersion(req.RequestURI)
+		return WriteSeriesResponseProtobuf(version, result, w)
+	case *stats.Stats:
+		return WriteIndexStatsResponseProtobuf(result, w)
+	case *logproto.VolumeResponse:
+		return WriteSeriesVolumeResponseProtobuf(result, w)
+	}
+	return fmt.Errorf("unknown response type %T", v)
+}
+
+// WriteQueryResponseProtobuf marshals the promql.Value to queryrange QueryResonse and then
+// writes it to the provided io.Writer.
+func WriteQueryResponseProtobuf(params *logql.LiteralParams, v logqlmodel.Result, w io.Writer) error {
+	p, err := ResultToResponse(v, params)
+	if err != nil {
+		return err
+	}
+
+	buf, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf)
+	return err
+}
+
+// WriteLabelResponseProtobuf marshals a logproto.LabelResponse to queryrange LokiLabelNamesResponse
+// and then writes it to the provided io.Writer.
+func WriteLabelResponseProtobuf(version loghttp.Version, l logproto.LabelResponse, w io.Writer) error {
+	p := QueryResponse{
+		Response: &QueryResponse_Labels{
+			Labels: &LokiLabelNamesResponse{
+				Status:  "success",
+				Data:    l.Values,
+				Version: uint32(version),
+				// Statistics: statResult,
+			},
+		},
+	}
+	buf, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf)
+	return err
+}
+
+// WriteSeriesResponseProtobuf marshals a logproto.SeriesResponse to queryrange LokiSeriesResponse
+// and then writes it to the provided io.Writer.
+func WriteSeriesResponseProtobuf(version loghttp.Version, r logproto.SeriesResponse, w io.Writer) error {
+	p := QueryResponse{
+		Response: &QueryResponse_Series{
+			Series: &LokiSeriesResponse{
+				Status:  "success",
+				Version: uint32(version),
+				Data:    r.Series,
+				// Statistics: statResult,
+			}},
+	}
+	buf, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf)
+	return err
+}
+
+// WriteIndexStatsResponseProtobuf marshals a gatewaypb.Stats to queryrange IndexStatsResponse
+// and then writes it to the provided io.Writer.
+func WriteIndexStatsResponseProtobuf(r *stats.Stats, w io.Writer) error {
+	p := QueryResponse{
+		Response: &QueryResponse_Stats{
+			Stats: &IndexStatsResponse{
+				Response: r,
+			}},
+	}
+	buf, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf)
+	return err
+}
+
+// WriteIndexStatsResponseProtobuf marshals a logproto.VolumeResponse to queryrange.QueryResponse
+// and then writes it to the provided io.Writer.
+func WriteSeriesVolumeResponseProtobuf(r *logproto.VolumeResponse, w io.Writer) error {
+	p := QueryResponse{
+		Response: &QueryResponse_Volume{
+			Volume: &VolumeResponse{
+				Response: r,
+			}},
+	}
+	buf, err := p.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf)
+	return err
+}
+
+// ResultToResponse is the reverse of ResponseToResult in downstreamer.
+func ResultToResponse(result logqlmodel.Result, params *logql.LiteralParams) (*QueryResponse, error) {
+	switch data := result.Data.(type) {
+	case promql.Vector:
+		sampleStream, err := queryrangebase.FromValue(data)
+		if err != nil {
+			return nil, err
+		}
+
+		return &QueryResponse{
+			Response: &QueryResponse_Prom{
+				Prom: &LokiPromResponse{
+					Response: &queryrangebase.PrometheusResponse{
+						Status: "success",
+						Data: queryrangebase.PrometheusData{
+							ResultType: loghttp.ResultTypeVector,
+							Result:     sampleStream,
+						},
+					},
+					Statistics: result.Statistics,
+				},
+			},
+		}, nil
+	case promql.Matrix:
+		sampleStream, err := queryrangebase.FromValue(data)
+		if err != nil {
+			return nil, err
+		}
+		return &QueryResponse{
+			Response: &QueryResponse_Prom{
+				Prom: &LokiPromResponse{
+					Response: &queryrangebase.PrometheusResponse{
+						Status: "success",
+						Data: queryrangebase.PrometheusData{
+							ResultType: loghttp.ResultTypeMatrix,
+							Result:     sampleStream,
+						},
+					},
+					Statistics: result.Statistics,
+				},
+			},
+		}, nil
+	case promql.Scalar:
+		sampleStream, err := queryrangebase.FromValue(data)
+		if err != nil {
+			return nil, err
+		}
+
+		return &QueryResponse{
+			Response: &QueryResponse_Prom{
+				Prom: &LokiPromResponse{
+					Response: &queryrangebase.PrometheusResponse{
+						Status: "success",
+						Data: queryrangebase.PrometheusData{
+							ResultType: loghttp.ResultTypeScalar,
+							Result:     sampleStream,
+						},
+					},
+					Statistics: result.Statistics,
+				},
+			},
+		}, nil
+	case logqlmodel.Streams:
+		return &QueryResponse{
+			Response: &QueryResponse_Streams{
+				// Note: we are omitting the Version here because the Protobuf already defines a schema.
+				Streams: &LokiResponse{
+					Direction: params.Direction(),
+					Limit:     params.Limit(),
+					Data: LokiData{
+						ResultType: loghttp.ResultTypeStream,
+						Result:     data,
+					},
+					Status:     "success",
+					Statistics: result.Statistics,
+				},
+			},
+		}, nil
+	}
+
+	return nil, fmt.Errorf("unsupported data type: %t", result.Data)
+}

pkg/util/marshal/marshal.go
@@ -5,6 +5,7 @@ package marshal
 import (
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/gorilla/websocket"
 	jsoniter "github.com/json-iterator/go"
@@ -14,7 +15,34 @@ import (
 	"github.com/grafana/loki/pkg/logproto"
 	"github.com/grafana/loki/pkg/logqlmodel"
 	"github.com/grafana/loki/pkg/storage/stores/index/stats"
+	marshal_legacy "github.com/grafana/loki/pkg/util/marshal/legacy"
 )
+
+func WriteResponseJSON(r *http.Request, v any, w http.ResponseWriter) error {
+	switch result := v.(type) {
+	case logqlmodel.Result:
+		version := loghttp.GetVersion(r.RequestURI)
+		if version == loghttp.VersionV1 {
+			return WriteQueryResponseJSON(result, w)
+		}
+
+		return marshal_legacy.WriteQueryResponseJSON(result, w)
+	case *logproto.LabelResponse:
+		version := loghttp.GetVersion(r.RequestURI)
+		if version == loghttp.VersionV1 {
+			return WriteLabelResponseJSON(*result, w)
+		}
+
+		return marshal_legacy.WriteLabelResponseJSON(*result, w)
+	case logproto.SeriesResponse:
+		return WriteSeriesResponseJSON(result, w)
+	case *stats.Stats:
+		return WriteIndexStatsResponseJSON(result, w)
+	case *logproto.VolumeResponse:
+		return WriteSeriesVolumeResponseJSON(result, w)
+	}
+	return fmt.Errorf("unknown response type %T", v)
+}
 
 // WriteQueryResponseJSON marshals the promql.Value to v1 loghttp JSON and then
 // writes it to the provided io.Writer.
@@ -99,7 +127,7 @@ func WriteIndexStatsResponseJSON(r *stats.Stats, w io.Writer) error {
 	return s.Flush()
 }
 
-// WriteIndexStatsResponseJSON marshals a gatewaypb.Stats to JSON and then
+// WriteSeriesVolumeResponseJSON marshals a logproto.VolumeResponse to JSON and then
 // writes it to the provided io.Writer.
 func WriteSeriesVolumeResponseJSON(r *logproto.VolumeResponse, w io.Writer) error {
 	s := jsoniter.ConfigFastest.BorrowStream(w)

integration/loki_micro_services_test.go
@@ -68,6 +68,7 @@ func TestMicroServicesIngestQuery(t *testing.T) {
 			"-boltdb.shipper.index-gateway-client.server-address="+tIndexGateway.GRPCURL(),
 			"-common.compactor-address="+tCompactor.HTTPURL(),
 			"-querier.per-request-limits-enabled=true",
+			"-frontend.required-query-response-format=protobuf",
 		)
 		_ = clu.AddComponent(
 			"querier",

pkg/querier/http.go
@@ -7,15 +7,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/prometheus/common/model"
-
-	"github.com/grafana/loki/pkg/logproto"
-
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/gorilla/websocket"
 	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/promql/parser"
 	"github.com/weaveworks/common/httpgrpc"
@@ -25,10 +22,12 @@ import (
 
 	"github.com/grafana/loki/pkg/loghttp"
 	loghttp_legacy "github.com/grafana/loki/pkg/loghttp/legacy"
+	"github.com/grafana/loki/pkg/logproto"
 	"github.com/grafana/loki/pkg/logql"
 	"github.com/grafana/loki/pkg/logql/syntax"
 	"github.com/grafana/loki/pkg/logqlmodel"
 	"github.com/grafana/loki/pkg/logqlmodel/stats"
+	"github.com/grafana/loki/pkg/querier/queryrange"
 	index_stats "github.com/grafana/loki/pkg/storage/stores/index/stats"
 	"github.com/grafana/loki/pkg/util/httpreq"
 	util_log "github.com/grafana/loki/pkg/util/log"
@@ -97,9 +96,9 @@ func (q *QuerierAPI) RangeQueryHandler(w http.ResponseWriter, r *http.Request) {
 		serverutil.WriteError(err, w)
 		return
 	}
-	if err := marshal.WriteQueryResponseJSON(result, w); err != nil {
+
+	if err := queryrange.WriteResponse(r, &params, result, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -134,9 +133,8 @@ func (q *QuerierAPI) InstantQueryHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	if err := marshal.WriteQueryResponseJSON(result, w); err != nil {
+	if err := queryrange.WriteResponse(r, &params, result, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -189,9 +187,8 @@ func (q *QuerierAPI) LogQueryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := marshal_legacy.WriteQueryResponseJSON(result, w); err != nil {
+	if err := queryrange.WriteResponse(r, &params, result, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -233,14 +230,8 @@ func (q *QuerierAPI) LabelHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if loghttp.GetVersion(r.RequestURI) == loghttp.VersionV1 {
-		err = marshal.WriteLabelResponseJSON(*resp, w)
-	} else {
-		err = marshal_legacy.WriteLabelResponseJSON(*resp, w)
-	}
-	if err != nil {
+	if err := queryrange.WriteResponse(r, nil, resp, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -407,10 +398,8 @@ func (q *QuerierAPI) SeriesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = marshal.WriteSeriesResponseJSON(*resp, w)
-	if err != nil {
+	if err := queryrange.WriteResponse(r, nil, resp, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -434,10 +423,8 @@ func (q *QuerierAPI) IndexStatsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = marshal.WriteIndexStatsResponseJSON(resp, w)
-	if err != nil {
+	if err := queryrange.WriteResponse(r, nil, resp, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 
@@ -461,7 +448,7 @@ func (q *QuerierAPI) SeriesVolumeRangeHandler(w http.ResponseWriter, r *http.Req
 		Limit:    int32(rawReq.Limit),
 	}
 
-	q.seriesVolumeHandler(r.Context(), req, w)
+	q.seriesVolumeHandler(r.Context(), r, req, w)
 }
 
 // SeriesVolumeInstantHandler queries the index label volumes related to the passed matchers and given time range.
@@ -482,10 +469,10 @@ func (q *QuerierAPI) SeriesVolumeInstantHandler(w http.ResponseWriter, r *http.R
 		Limit:    int32(rawReq.Limit),
 	}
 
-	q.seriesVolumeHandler(r.Context(), req, w)
+	q.seriesVolumeHandler(r.Context(), r, req, w)
 }
 
-func (q *QuerierAPI) seriesVolumeHandler(ctx context.Context, req *logproto.VolumeRequest, w http.ResponseWriter) {
+func (q *QuerierAPI) seriesVolumeHandler(ctx context.Context, r *http.Request, req *logproto.VolumeRequest, w http.ResponseWriter) {
 	resp, err := q.querier.SeriesVolume(ctx, req)
 	if err != nil {
 		serverutil.WriteError(err, w)
@@ -496,9 +483,8 @@ func (q *QuerierAPI) seriesVolumeHandler(ctx context.Context, req *logproto.Volu
 		resp = &logproto.VolumeResponse{Volumes: []logproto.Volume{}}
 	}
 
-	if marshal.WriteSeriesVolumeResponseJSON(resp, w) != nil {
+	if err := queryrange.WriteResponse(r, nil, resp, w); err != nil {
 		serverutil.WriteError(err, w)
-		return
 	}
 }
 

pkg/querier/queryrange/codec.go
@@ -35,9 +35,13 @@ import (
 	marshal_legacy "github.com/grafana/loki/pkg/util/marshal/legacy"
 )
 
-var LokiCodec = &Codec{}
+var DefaultCodec = &Codec{}
 
 type Codec struct{}
+
+type RequestProtobufCodec struct {
+	Codec
+}
 
 func (r *LokiRequest) GetEnd() int64 {
 	return r.EndTs.UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
@@ -302,7 +306,7 @@ func (Codec) DecodeRequest(_ context.Context, r *http.Request, _ []string) (quer
 	}
 }
 
-func (Codec) EncodeRequest(ctx context.Context, r queryrangebase.Request) (*http.Request, error) {
+func (c Codec) EncodeRequest(ctx context.Context, r queryrangebase.Request) (*http.Request, error) {
 	header := make(http.Header)
 	queryTags := getQueryTags(ctx)
 	if queryTags != "" {
@@ -462,6 +466,16 @@ func (Codec) EncodeRequest(ctx context.Context, r queryrangebase.Request) (*http
 	}
 }
 
+func (p RequestProtobufCodec) EncodeRequest(ctx context.Context, r queryrangebase.Request) (*http.Request, error) {
+	req, err := p.Codec.EncodeRequest(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/vnd.google.protobuf")
+	return req, nil
+}
+
 type Buffer interface {
 	Bytes() []byte
 }
@@ -471,6 +485,16 @@ func (Codec) DecodeResponse(_ context.Context, r *http.Response, req queryrangeb
 		body, _ := io.ReadAll(r.Body)
 		return nil, httpgrpc.Errorf(r.StatusCode, string(body))
 	}
+
+	if r.Header.Get("Content-Type") == ProtobufType {
+		return decodeResponseProtobuf(r, req)
+	}
+
+	// Default to JSON.
+	return decodeResponseJSON(r, req)
+}
+
+func decodeResponseJSON(r *http.Response, req queryrangebase.Request) (queryrangebase.Response, error) {
 
 	var buf []byte
 	var err error
@@ -609,7 +633,55 @@ func (Codec) DecodeResponse(_ context.Context, r *http.Response, req queryrangeb
 	}
 }
 
-func (Codec) EncodeResponse(ctx context.Context, res queryrangebase.Response) (*http.Response, error) {
+func decodeResponseProtobuf(r *http.Response, req queryrangebase.Request) (queryrangebase.Response, error) {
+	var buf []byte
+	var err error
+	if buffer, ok := r.Body.(Buffer); ok {
+		buf = buffer.Bytes()
+	} else {
+		buf, err = io.ReadAll(r.Body)
+		if err != nil {
+			return nil, httpgrpc.Errorf(http.StatusInternalServerError, "error decoding response: %v", err)
+		}
+	}
+
+	resp := &QueryResponse{}
+	err = resp.Unmarshal(buf)
+	if err != nil {
+		return nil, httpgrpc.Errorf(http.StatusInternalServerError, "error decoding response: %v", err)
+	}
+
+	headers := httpResponseHeadersToPromResponseHeaders(r.Header)
+	switch req.(type) {
+	case *LokiSeriesRequest:
+		return resp.GetSeries().WithHeaders(headers), nil
+	case *LokiLabelNamesRequest:
+		return resp.GetLabels().WithHeaders(headers), nil
+	case *logproto.IndexStatsRequest:
+		return resp.GetStats().WithHeaders(headers), nil
+	default:
+		switch concrete := resp.Response.(type) {
+		case *QueryResponse_Prom:
+			return concrete.Prom.WithHeaders(headers), nil
+		case *QueryResponse_Streams:
+			return concrete.Streams.WithHeaders(headers), nil
+		default:
+			return nil, httpgrpc.Errorf(http.StatusInternalServerError, "unsupported response type, got (%t)", resp.Response)
+		}
+	}
+}
+
+func (Codec) EncodeResponse(ctx context.Context, req *http.Request, res queryrangebase.Response) (*http.Response, error) {
+	if req.Header.Get("Accept") == ProtobufType {
+		return encodeResponseProtobuf(ctx, res)
+	}
+
+	// Default to JSON.
+	version := loghttp.GetVersion(req.RequestURI)
+	return encodeResponseJSON(ctx, version, res)
+}
+
+func encodeResponseJSON(ctx context.Context, version loghttp.Version, res queryrangebase.Response) (*http.Response, error) {
 	sp, _ := opentracing.StartSpanFromContext(ctx, "codec.EncodeResponse")
 	defer sp.Finish()
 	var buf bytes.Buffer
@@ -630,7 +702,7 @@ func (Codec) EncodeResponse(ctx context.Context, res queryrangebase.Response) (*
 			Data:       logqlmodel.Streams(streams),
 			Statistics: response.Statistics,
 		}
-		if loghttp.Version(response.Version) == loghttp.VersionLegacy {
+		if version == loghttp.VersionLegacy {
 			if err := marshal_legacy.WriteQueryResponseJSON(result, &buf); err != nil {
 				return nil, err
 			}
@@ -673,9 +745,45 @@ func (Codec) EncodeResponse(ctx context.Context, res queryrangebase.Response) (*
 
 	resp := http.Response{
 		Header: http.Header{
-			"Content-Type": []string{"application/json"},
+			"Content-Type": []string{"application/json; charset=UTF-8"},
 		},
 		Body:       io.NopCloser(&buf),
+		StatusCode: http.StatusOK,
+	}
+	return &resp, nil
+}
+
+func encodeResponseProtobuf(ctx context.Context, res queryrangebase.Response) (*http.Response, error) {
+	sp, _ := opentracing.StartSpanFromContext(ctx, "codec.EncodeResponse")
+	defer sp.Finish()
+
+	p := QueryResponse{}
+
+	switch response := res.(type) {
+	case *LokiPromResponse:
+		p.Response = &QueryResponse_Prom{response}
+	case *LokiResponse:
+		p.Response = &QueryResponse_Streams{response}
+	case *LokiSeriesResponse:
+		p.Response = &QueryResponse_Series{response}
+	case *LokiLabelNamesResponse:
+		p.Response = &QueryResponse_Labels{response}
+	case *IndexStatsResponse:
+		p.Response = &QueryResponse_Stats{response}
+	default:
+		return nil, httpgrpc.Errorf(http.StatusInternalServerError, "invalid response format")
+	}
+
+	buf, err := p.Marshal()
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal protobuf: %w", err)
+	}
+
+	resp := http.Response{
+		Header: http.Header{
+			"Content-Type": []string{ProtobufType},
+		},
+		Body:       io.NopCloser(bytes.NewBuffer(buf)),
 		StatusCode: http.StatusOK,
 	}
 	return &resp, nil

commit f5992274f4c41544cbe6f677dfa1f4d909b12193

Switch on pointer `*logproto.SeriesResponse` in JSON serialization. (#9868)
**What this PR does / why we need it**:
Commit b35bbd80d65f09eb6f1e6ba1bfe05bf4af712a97 introduced a regression
in the deserialization code. It would switch on
`logproto.SeriesResponse` instead of the pointer type
`*logproto.SeriesResponse`.

**Checklist**
- [ ] Reviewed the
[`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
guide (**required**)
- [ ] Documentation added
- [x] Tests updated
- [ ] `CHANGELOG.md` updated
- [ ] If the change is worth mentioning in the release notes, add
`add-to-release-notes` label
- [ ] Changes that require user attention or interaction to upgrade are
documented in `docs/sources/upgrading/_index.md`
- [ ] For Helm chart changes bump the Helm chart version in
`production/helm/loki/Chart.yaml` and update
`production/helm/loki/CHANGELOG.md` and
`production/helm/loki/README.md`. [Example
PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)

pkg/querier/queryrange/marshal.go
@@ -40,9 +40,9 @@ func WriteResponseProtobuf(req *http.Request, params *logql.LiteralParams, v any
 	case *logproto.LabelResponse:
 		version := loghttp.GetVersion(req.RequestURI)
 		return WriteLabelResponseProtobuf(version, *result, w)
-	case logproto.SeriesResponse:
+	case *logproto.SeriesResponse:
 		version := loghttp.GetVersion(req.RequestURI)
-		return WriteSeriesResponseProtobuf(version, result, w)
+		return WriteSeriesResponseProtobuf(version, *result, w)
 	case *stats.Stats:
 		return WriteIndexStatsResponseProtobuf(result, w)
 	case *logproto.VolumeResponse:

pkg/util/marshal/marshal.go
@@ -34,8 +34,8 @@ func WriteResponseJSON(r *http.Request, v any, w http.ResponseWriter) error {
 		}
 
 		return marshal_legacy.WriteLabelResponseJSON(*result, w)
-	case logproto.SeriesResponse:
-		return WriteSeriesResponseJSON(result, w)
+	case *logproto.SeriesResponse:
+		return WriteSeriesResponseJSON(*result, w)
 	case *stats.Stats:
 		return WriteIndexStatsResponseJSON(result, w)
 	case *logproto.VolumeResponse:

pkg/querier/http_test.go
@@ -337,6 +337,43 @@ func TestQueryWrapperMiddleware(t *testing.T) {
 	})
 }
 
+func TestSeriesHandler(t *testing.T) {
+	t.Run("instant queries set a step of 0", func(t *testing.T) {
+		ret := func() *logproto.SeriesResponse {
+			return &logproto.SeriesResponse{
+				Series: []logproto.SeriesIdentifier{
+					{
+						Labels: map[string]string{
+							"a": "1",
+							"b": "2",
+						},
+					},
+					{
+						Labels: map[string]string{
+							"c": "3",
+							"d": "4",
+						},
+					},
+				},
+			}
+		}
+		expected := `{"status":"success","data":[{"a":"1","b":"2"},{"c":"3","d":"4"}]}`
+
+		querier := newQuerierMock()
+		querier.On("Series", mock.Anything, mock.Anything).Return(ret, nil)
+		api := setupAPI(querier)
+
+		req := httptest.NewRequest(http.MethodGet, "/loki/api/v1/series"+
+			"?start=0"+
+			"&end=1"+
+			"&step=42"+
+			"&query=%7Bfoo%3D%22bar%22%7D", nil)
+		res := makeRequest(t, api.SeriesHandler, req)
+
+		require.Equalf(t, 200, res.Code, "response was not HTTP OK: %s", res.Body.String())
+		require.JSONEq(t, expected, res.Body.String())
+	})
+}
 func TestSeriesVolumeHandler(t *testing.T) {
 	ret := &logproto.VolumeResponse{
 		Volumes: []logproto.Volume{

pkg/util/marshal/marshal_test.go
@@ -675,7 +675,7 @@ func Test_WriteSeriesResponseJSON(t *testing.T) {
 			err := WriteSeriesResponseJSON(tc.input, &b)
 			require.NoError(t, err)
 
-			require.JSONEqf(t, tc.expected, b.String(), "Label Test %d failed", i)
+			require.JSONEqf(t, tc.expected, b.String(), "Series Test %d failed", i)
 		})
 	}
 }