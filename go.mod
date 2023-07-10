df018826f75f2eba6ed5f1a5f60c36845f08c84f
Edward Welch <edward.welch@grafana.com>
2023-07-10 20:42:41 +0000

    updates to my index stats stuff
    Signed-off-by: Edward Welch <edward.welch@grafana.com>

--- go.mod
+++ go.mod
@@ -143,6 +143,7 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.0 // indirect
 	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20180226025133-644b8db467af // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
@@ -160,6 +161,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.1 // indirect
 	github.com/aws/smithy-go v1.11.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bsipos/thist v1.0.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
 	github.com/cncf/udpa/go v0.0.0-20210930031921-04548b0d99d4 // indirect
 	github.com/cncf/xds/go v0.0.0-20220314180256-7f1daf1720fc // indirect
@@ -184,6 +186,7 @@ require (
 	github.com/envoyproxy/go-control-plane v0.10.3 // indirect
 	github.com/envoyproxy/protoc-gen-validate v0.9.1 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/fogleman/gg v1.2.1-0.20190220221249-0403632d5b90 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -200,6 +203,7 @@ require (
 	github.com/gofrs/flock v0.7.1 // indirect
 	github.com/gogo/googleapis v1.4.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
@@ -231,6 +235,7 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/jung-kurt/gofpdf v1.0.3-0.20190309125859-24315acbbda5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.14 // indirect
 	github.com/leodido/ragel-machinery v0.0.0-20181214104525-299bdde78165 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -282,10 +287,12 @@ require (
 	go.uber.org/zap v1.21.0 // indirect
 	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
 	go4.org/unsafe/assume-no-moving-gc v0.0.0-20220617031537-928513b29760 // indirect
+	golang.org/x/image v0.0.0-20190802002840-cff245a6509b // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/term v0.4.0 // indirect
 	golang.org/x/tools v0.4.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gonum.org/v1/plot v0.0.0-20190515093506-e2840ee46a6b // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- tools/query-label-analyzer/main.go
+++ tools/query-label-analyzer/main.go
@@ -5,11 +5,27 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/bsipos/thist"
+	"github.com/dustin/go-humanize"
+	"github.com/prometheus/prometheus/model/labels"
 	"golang.org/x/exp/slices"
 
+	"github.com/grafana/loki/pkg/logql/log"
 	"github.com/grafana/loki/pkg/logql/syntax"
 )
+
+type parser struct {
+	labelInMatcher      map[string]int
+	labelValueInMatcher map[string]int
+	labelInGroup        map[string]int
+	matchers            map[string]int
+	matchLabel          string
+	labelCountHist      *thist.Hist
+	lengthHist          *thist.Hist
+	bytesHist           *thist.Hist
+}
 
 func main() {
 
@@ -23,72 +39,144 @@ func main() {
 
 	//testQuery := `avg(sum(count_over_time({stream_filter="JPAGEC",environment="pro",platform=~"Openshift-Meccano Interxion 2|Openshift-Meccano Telecity 2",response_status!~"5..",route_paths_1="/itxrest/2/order/store/[^/]+/shipping/unique/?$",sre_obs_perf=~"1"} |="apigw-swapi.md.apps" | json | request_method="GET" | request_headers_user_agent=~".*Android.*" [1m])) by (response_status,request_method,route_paths_1,itxsessionid)) by (response_status,request_method,route_paths_1)`
 
-	labelValueInMatcher := map[string]int{}
-	labelInGroup := map[string]int{}
+	p := parser{
+		labelInMatcher:      map[string]int{},
+		labelValueInMatcher: map[string]int{},
+		labelInGroup:        map[string]int{},
+		matchers:            map[string]int{},
+		matchLabel:          matchLabel,
+		labelCountHist:      thist.NewHist(nil, "Matchers Per Query", "fixed", 10, false),
+		lengthHist:          thist.NewHist(nil, "Query Length", "fixed", 10, false),
+		bytesHist:           thist.NewHist(nil, "Query Bytes", "fixed", 10, false),
+	}
 
 	scanner := bufio.NewScanner(os.Stdin)
+	//sb := strings.Builder{}
 	for scanner.Scan() {
 		query := scanner.Text()
+		pr := log.NewLogfmtParser()
+		builder := log.NewBaseLabelsBuilder()
+		lb := builder.ForLabels(labels.Labels{}, 0)
+		pr.Process(0, []byte(query), lb)
+		//fmt.Println(lb.LabelsResult().String())
+		q, _ := lb.Get("query")
+		p.parseQuery(q)
+		p.parseMetris(lb)
+		//if strings.HasPrefix(query, "QUERY:") {
+		//	query = strings.TrimPrefix(query, "QUERY:")
+		//	if sb.Len() > 0 {
+		//		p.parseQuery(sb.String())
+		//	}
+		//	sb.Reset()
+		//	sb.WriteString(query)
+		//	sb.WriteString("\n")
+		//} else {
+		//	sb.WriteString(query)
+		//	sb.WriteString("\n")
+		//}
+	}
 
-		e, err := syntax.ParseExpr(query)
-		if err != nil {
-			fmt.Println("failed to parse query", query)
-			continue
+	p.printResult()
+
+}
+
+func (p *parser) parseMetris(lb *log.LabelsBuilder) {
+	if l, ok := lb.Get("length"); ok {
+		d, err := time.ParseDuration(l)
+		if err == nil {
+			p.lengthHist.Update(d.Hours())
 		}
+	}
+	if b, ok := lb.Get("total_bytes"); ok {
+		by, err := humanize.ParseBytes(b)
+		if err == nil {
+			p.bytesHist.Update(float64(by) / 1e9)
+		}
+	}
+}
 
-		e.Walk(func(e interface{}) {
-			switch expr := e.(type) {
-			case *syntax.MatchersExpr:
-				for _, l := range expr.Mts {
-					if matchLabel == "" || l.Name == matchLabel {
-						if _, ok := labelValueInMatcher[l.Name+"="+l.Value]; ok {
-							labelValueInMatcher[l.Name+"="+l.Value]++
-						} else {
-							labelValueInMatcher[l.Name+"="+l.Value] = 1
-						}
+func (p *parser) parseQuery(query string) {
+	e, err := syntax.ParseExpr(query)
+	if err != nil {
+		fmt.Println("failed to parse query", query, "err", err)
+		return
+	}
+
+	matchersInQuery := 0
+
+	e.Walk(func(e interface{}) {
+		switch expr := e.(type) {
+		case *syntax.MatchersExpr:
+
+			if _, ok := p.matchers[expr.String()]; ok {
+				p.matchers[expr.String()]++
+			} else {
+				p.matchers[expr.String()] = 1
+			}
+
+			for _, l := range expr.Mts {
+				matchersInQuery++
+				if p.matchLabel == "" || l.Name == p.matchLabel {
+					if _, ok := p.labelInMatcher[l.Name]; ok {
+						p.labelInMatcher[l.Name]++
+					} else {
+						p.labelInMatcher[l.Name] = 1
 					}
-				}
-			case *syntax.VectorAggregationExpr:
-				for _, g := range expr.Grouping.Groups {
-					if matchLabel == "" || g == matchLabel {
-						if _, ok := labelInGroup[g]; ok {
-							labelInGroup[g]++
-						} else {
-							labelInGroup[g] = 1
-						}
-					}
-				}
-			case syntax.RangeAggregationExpr:
-				for _, g := range expr.Grouping.Groups {
-					if matchLabel == "" || g == matchLabel {
-						if _, ok := labelInGroup[g]; ok {
-							labelInGroup[g]++
-						} else {
-							labelInGroup[g] = 1
-						}
+
+					if _, ok := p.labelValueInMatcher[l.Name+"="+l.Value]; ok {
+						p.labelValueInMatcher[l.Name+"="+l.Value]++
+					} else {
+						p.labelValueInMatcher[l.Name+"="+l.Value] = 1
 					}
 				}
 			}
-		})
+		case *syntax.VectorAggregationExpr:
+			for _, g := range expr.Grouping.Groups {
+				if p.matchLabel == "" || g == p.matchLabel {
+					if _, ok := p.labelInGroup[g]; ok {
+						p.labelInGroup[g]++
+					} else {
+						p.labelInGroup[g] = 1
+					}
+				}
+			}
+		case syntax.RangeAggregationExpr:
+			for _, g := range expr.Grouping.Groups {
+				if p.matchLabel == "" || g == p.matchLabel {
+					if _, ok := p.labelInGroup[g]; ok {
+						p.labelInGroup[g]++
+					} else {
+						p.labelInGroup[g] = 1
+					}
+				}
+			}
+		}
+	})
 
-	}
+	//if matchersInQuery > 2 {
+	//	fmt.Println(query)
+	//}
 
+	p.labelCountHist.Update(float64(matchersInQuery))
+}
+
+func (p *parser) printResult() {
 	type labelCount struct {
 		name  string
 		count int
 	}
 
 	// make slices so we can sort
-	labelValueInMatherSlice := make([]*labelCount, 0, len(labelValueInMatcher))
-	for k, v := range labelValueInMatcher {
+	labelValueInMatherSlice := make([]*labelCount, 0, len(p.labelValueInMatcher))
+	for k, v := range p.labelValueInMatcher {
 		labelValueInMatherSlice = append(labelValueInMatherSlice, &labelCount{k, v})
 	}
 	slices.SortFunc[*labelCount](labelValueInMatherSlice, func(a *labelCount, b *labelCount) bool {
 		return a.count < b.count
 	})
 
-	labelInGroupSlice := make([]*labelCount, 0, len(labelInGroup))
-	for k, v := range labelInGroup {
+	labelInGroupSlice := make([]*labelCount, 0, len(p.labelInGroup))
+	for k, v := range p.labelInGroup {
 		labelInGroupSlice = append(labelInGroupSlice, &labelCount{k, v})
 	}
 	slices.SortFunc[*labelCount](labelInGroupSlice, func(a *labelCount, b *labelCount) bool {
@@ -102,12 +190,48 @@ func main() {
 		fmt.Printf("%s %d\n", v.name, v.count)
 	}
 
+	//fmt.Println()
+	//fmt.Println()
+	//fmt.Println("Labels in groupings")
+	//fmt.Println()
+	//for _, v := range labelInGroupSlice {
+	//	fmt.Printf("%s %d\n", v.name, v.count)
+	//}
+
+	// make slices of labelInMatcher so we can sort
+	labelInMatcherSlice := make([]*labelCount, 0, len(p.labelInMatcher))
+	for k, v := range p.labelInMatcher {
+		labelInMatcherSlice = append(labelInMatcherSlice, &labelCount{k, v})
+	}
+	slices.SortFunc[*labelCount](labelInMatcherSlice, func(a *labelCount, b *labelCount) bool {
+		return a.count < b.count
+	})
 	fmt.Println()
 	fmt.Println()
-	fmt.Println("Labels in groupings")
+	fmt.Println("Labels in matchers")
 	fmt.Println()
-	for _, v := range labelInGroupSlice {
+	for _, v := range labelInMatcherSlice {
 		fmt.Printf("%s %d\n", v.name, v.count)
 	}
 
+	// make slices of matchers so we can sort
+	matchersSlice := make([]*labelCount, 0, len(p.matchers))
+	for k, v := range p.matchers {
+		matchersSlice = append(matchersSlice, &labelCount{k, v})
+	}
+	slices.SortFunc[*labelCount](matchersSlice, func(a *labelCount, b *labelCount) bool {
+		return a.count < b.count
+	})
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Matchers")
+	fmt.Println()
+	for _, v := range matchersSlice {
+		fmt.Printf("%s %d\n", v.name, v.count)
+	}
+
+	fmt.Println(p.labelCountHist.Draw())
+	fmt.Println(p.lengthHist.Draw())
+	fmt.Println(p.bytesHist.Draw())
 }
