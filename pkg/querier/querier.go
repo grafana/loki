5a570cb921545aa5aaff7532b2cad8799f151c33
Trevor Whitney <trevorjwhitney@gmail.com>
2023-07-11 10:18:46 -0600

    Query ingesters for SeriesVolume in single binary (#9888)
    This change is necessary to get recent SeriesVolume data from the
    ingesters when running without an `AsyncStore` in single binary.
    It changes the queriers to query both the ingester and the store,
    rather than relying on the `AsyncStore` to query the ingesters.

--- pkg/querier/querier.go
+++ pkg/querier/querier.go
@@ -802,12 +802,54 @@ func (q *SingleTenantQuerier) SeriesVolume(ctx context.Context, req *logproto.Vo
 		"limit", req.Limit,
 	)
 
-	return q.store.SeriesVolume(
-		ctx,
-		userID,
-		req.From,
-		req.Through,
-		req.Limit,
-		matchers...,
-	)
+	ingesterQueryInterval, storeQueryInterval := q.buildQueryIntervals(req.From.Time(), req.Through.Time())
+
+	queryIngesters := !q.cfg.QueryStoreOnly && ingesterQueryInterval != nil
+	queryStore := !q.cfg.QueryIngesterOnly && storeQueryInterval != nil
+
+	numResponses := 0
+	if queryIngesters {
+		numResponses++
+	}
+	if queryStore {
+		numResponses++
+	}
+	responses := make([]*logproto.VolumeResponse, 0, numResponses)
+
+	if queryIngesters {
+		// Make a copy of the request before modifying
+		// because the initial request is used below to query stores
+
+		resp, err := q.ingesterQuerier.SeriesVolume(
+			ctx,
+			userID,
+			model.TimeFromUnix(ingesterQueryInterval.start.Unix()),
+			model.TimeFromUnix(ingesterQueryInterval.end.Unix()),
+			req.Limit,
+			matchers...,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		responses = append(responses, resp)
+	}
+
+	if queryStore {
+		resp, err := q.store.SeriesVolume(
+			ctx,
+			userID,
+			model.TimeFromUnix(storeQueryInterval.start.Unix()),
+			model.TimeFromUnix(storeQueryInterval.end.Unix()),
+			req.Limit,
+			matchers...,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		responses = append(responses, resp)
+	}
+
+	return seriesvolume.Merge(responses, req.Limit), nil
 }
