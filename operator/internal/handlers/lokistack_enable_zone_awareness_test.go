616cee7bd14222b59c1b7e2f2a2023bcbf35333a
Shweta Padubidri <spadubid@redhat.com>
2023-07-13 00:22:10 -0400

    Fix the unit tests for Zone Aware Handler
    Signed-off-by: Shweta Padubidri <spadubid@redhat.com>

--- operator/internal/handlers/lokistack_enable_zone_awareness_test.go
+++ operator/internal/handlers/lokistack_enable_zone_awareness_test.go
@@ -2,6 +2,7 @@ package handlers_test
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"testing"
 
@@ -11,21 +12,12 @@ import (
 	"github.com/grafana/loki/operator/internal/handlers"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	// apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// "k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	// ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
-	defaultZones = []lokiv1.ZoneSpec{
-		{
-			TopologyKey: corev1.LabelTopologyZone,
-		},
-	}
-
 	defaultPod = corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test-pod",
@@ -42,16 +34,6 @@ var (
 			NodeName: "test-node",
 		},
 	}
-
-	defaultNode = corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-node",
-			Namespace: "some-ns",
-			Labels: map[string]string{
-				corev1.LabelTopologyZone: "us-east-zone-2c",
-			},
-		},
-	}
 )
 
 func TestAnnotatePodWithAvailabilityZone_WhenGetReturnsAnErrorOtherThanNotFound_ReturnsTheError(t *testing.T) {
@@ -66,8 +48,8 @@ func TestAnnotatePodWithAvailabilityZone_WhenGetReturnsAnErrorOtherThanNotFound_
 	err := handlers.AnnotatePodWithAvailabilityZone(context.TODO(), logger, k, &defaultPod)
 	require.Equal(t, badRequestErr, errors.Unwrap(err))
 
-	// make sure create was NOT called because the Get failed
-	require.Zero(t, k.CreateCallCount())
+	// make sure patch was NOT called because the Get failed
+	require.Zero(t, k.PatchCallCount())
 }
 
 func TestAnnotatePodWithAvailabilityZone_WhenGetReturnsNode_DoesNotError(t *testing.T) {
@@ -82,7 +64,6 @@ func TestAnnotatePodWithAvailabilityZone_WhenGetReturnsNode_DoesNotError(t *test
 			},
 			Annotations: map[string]string{
 				lokiv1.AnnotationAvailabilityZoneLabels: corev1.LabelHostname + "," + corev1.LabelTopologyZone + "," + corev1.LabelTopologyRegion,
-				lokiv1.AnnotationAvailabilityZone:       "test-node_us-east-2c_us-east-2",
 			},
 		},
 		Spec: corev1.PodSpec{
@@ -103,13 +84,27 @@ func TestAnnotatePodWithAvailabilityZone_WhenGetReturnsNode_DoesNotError(t *test
 	}
 
 	k.GetStub = func(ctx context.Context, name types.NamespacedName, object client.Object, _ ...client.GetOption) error {
-		k.SetClientObject(object, &testNode)
-		return nil
+		if name.Name == testPod.Spec.NodeName {
+			k.SetClientObject(object, &testNode)
+			return nil
+		}
+		return kverrors.New("failed to lookup node")
 	}
 
 	err := handlers.AnnotatePodWithAvailabilityZone(context.TODO(), logger, k, &testPod)
 	require.NoError(t, err)
+	expectedPatch, err := json.Marshal(map[string]interface{}{
+		"metadata": map[string]interface{}{
+			"annotations": map[string]string{
+				lokiv1.AnnotationAvailabilityZone: "test-node_us-east-2c_us-east-2",
+			},
+		},
+	})
 
-	// make sure create was NOT called because the Get failed
-	require.Zero(t, k.CreateCallCount())
+	// make sure patch was called because the Get succeeded
+	require.Equal(t, 1, k.PatchCallCount())
+	_, p, patch, _ := k.PatchArgsForCall(0)
+	require.Equal(t, p, &testPod)
+	actualPatch, err := patch.Data(nil)
+	require.Equal(t, actualPatch, expectedPatch)
 }
