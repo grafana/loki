863ff043aa02252702f1723cde3b08df438b3d78
Vladyslav Diachenko <82767850+vlad-diachenko@users.noreply.github.com>
2023-07-17 09:35:36 +0300

    Revert "updated to the latest version of dskit" (#9953)
    Reverts grafana/loki#9920
    
    (cherry picked from commit be1a9c4ec535df7b25b090ab1bb1dd0fa4d359dc)

--- vendor/github.com/grafana/dskit/kv/mock.go
+++ vendor/github.com/grafana/dskit/kv/mock.go
@@ -16,24 +16,24 @@ func buildMockClient(logger log.Logger) (Client, error) {
 	return mockClient{}, nil
 }
 
-func (m mockClient) List(_ context.Context, _ string) ([]string, error) {
+func (m mockClient) List(ctx context.Context, prefix string) ([]string, error) {
 	return []string{}, nil
 }
 
-func (m mockClient) Get(_ context.Context, _ string) (interface{}, error) {
+func (m mockClient) Get(ctx context.Context, key string) (interface{}, error) {
 	return "", nil
 }
 
-func (m mockClient) Delete(_ context.Context, _ string) error {
+func (m mockClient) Delete(ctx context.Context, key string) error {
 	return nil
 }
 
-func (m mockClient) CAS(_ context.Context, _ string, _ func(in interface{}) (out interface{}, retry bool, err error)) error {
+func (m mockClient) CAS(ctx context.Context, key string, f func(in interface{}) (out interface{}, retry bool, err error)) error {
 	return nil
 }
 
-func (m mockClient) WatchKey(_ context.Context, _ string, _ func(interface{}) bool) {
+func (m mockClient) WatchKey(ctx context.Context, key string, f func(interface{}) bool) {
 }
 
-func (m mockClient) WatchPrefix(_ context.Context, _ string, _ func(string, interface{}) bool) {
+func (m mockClient) WatchPrefix(ctx context.Context, prefix string, f func(string, interface{}) bool) {
 }
