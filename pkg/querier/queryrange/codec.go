a35ecfc1d9a802af0c9b0d331d39f5d56a37e562
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-07-19 20:24:49 +0200

    Just copy hashing method.

--- pkg/querier/queryrange/codec.go
+++ pkg/querier/queryrange/codec.go
@@ -840,6 +840,29 @@ func (Codec) MergeResponse(responses ...queryrangebase.Response) (queryrangebase
 					key = series.Hash.Value
 				} else {
 					key = 0 // TODO(karsten): hash labels.
+
+	// Use xxhash.Sum64(b) for fast path as it's faster.
+	b := make([]byte, 0, 1024)
+	for i, v := range ls {
+		if len(b)+len(v.Name)+len(v.Value)+2 >= cap(b) {
+			// If labels entry is 1KB+ do not allocate whole entry.
+			h := xxhash.New()
+			_, _ = h.Write(b)
+			for _, v := range ls[i:] {
+				_, _ = h.WriteString(v.Name)
+				_, _ = h.Write(seps)
+				_, _ = h.WriteString(v.Value)
+				_, _ = h.Write(seps)
+			}
+			return h.Sum64()
+		}
+
+		b = append(b, v.Name...)
+		b = append(b, seps[0])
+		b = append(b, v.Value...)
+		b = append(b, seps[0])
+	}
+	return xxhash.Sum64(b)
 				}
 				if _, ok := uniqueSeries[key]; !ok {
 					lokiSeriesData = append(lokiSeriesData, series)
