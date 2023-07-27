a9e0c70776fb0f47a9bf12e35bc804534a4966ed
fuling <fuling.lgz@alibaba-inc.com>
2023-07-27 15:53:54 +0800

    add metrics query test

--- pkg/querier/fanout/fanout_test.go
+++ pkg/querier/fanout/fanout_test.go
@@ -10,11 +10,14 @@ import (
 
 	"github.com/google/uuid"
 	config_util "github.com/prometheus/common/config"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/promql"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/loki/pkg/loghttp"
 	"github.com/grafana/loki/pkg/logproto"
 	"github.com/grafana/loki/pkg/logql"
+	"github.com/grafana/loki/pkg/logql/syntax"
 	"github.com/grafana/loki/pkg/logqlmodel"
 	"github.com/grafana/loki/pkg/logqlmodel/stats"
 	"github.com/grafana/loki/pkg/querier/remote"
@@ -203,11 +206,68 @@ func TestQuerier_Read(t *testing.T) {
 
 }
 
+func TestQuerier_MetricsQuery(t *testing.T) {
+	server := &mockLokiHTTPServer{server: &http.Server{Addr: ":3100", Handler: nil}}
+	from := time.Now().Add(time.Minute * -5)
+	server.Run(t, from)
+	time.Sleep(time.Second)
+	defer server.Stop(t)
+	remoteConf := remote.ReadConfig{
+		Name:          "remote-read-1",
+		RemoteTimeout: queryTimeout,
+		URL: &config_util.URL{
+			URL: &url.URL{
+				Scheme: "http",
+				Host:   "localhost:3100",
+			},
+		},
+		OrgID: "team1",
+	}
+
+	querier, err := remote.NewQuerier("test", remoteConf)
+	require.NoError(t, err)
+
+	remoteConf2 := remote.ReadConfig{
+		Name:          "remote-read-2",
+		RemoteTimeout: queryTimeout,
+		URL: &config_util.URL{
+			URL: &url.URL{
+				Scheme: "http",
+				Host:   "localhost:3100",
+			},
+		},
+		OrgID: "team2",
+	}
+
+	querier2, err := remote.NewQuerier("test", remoteConf2)
+	require.NoError(t, err)
+
+	fanoutQuerier := NewQuerier(querier, 5, 0, querier2)
+
+	request := logproto.SampleQueryRequest{
+		Selector: `sum(count_over_time({app="distributor"}[1m]))`,
+		Start:    time.Now().Add(time.Minute * -5),
+		End:      time.Now(),
+	}
+
+	iter, err := fanoutQuerier.SelectSamples(
+		context.Background(), logql.SelectSampleParams{SampleQueryRequest: &request},
+	)
+	require.NoError(t, err)
+	count := 0
+	for iter.Next() {
+		require.Equal(t, true, len(iter.Labels()) > 10)
+		count++
+	}
+	require.Equal(t, 6, count)
+
+}
+
 type mockLokiHTTPServer struct {
 	server *http.Server
 }
 
-func (s *mockLokiHTTPServer) Run(_ *testing.T, from time.Time) {
+func (s *mockLokiHTTPServer) Run(t *testing.T, from time.Time) {
 	var mux http.ServeMux
 	mux.HandleFunc("/loki/api/v1/query_range", func(w http.ResponseWriter, r *http.Request) {
 
@@ -219,30 +279,21 @@ func (s *mockLokiHTTPServer) Run(_ *testing.T, from time.Time) {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
-		limit := request.Limit
-		data := make([]logproto.Entry, 0)
-		for i := 0; i < int(limit); i++ {
-			dur := time.Duration(i) * time.Millisecond
-			newUUID, err := uuid.NewUUID()
-			if err != nil {
-				panic(err)
-			}
-			data = append(data, logproto.Entry{
-				Timestamp: from.Add(dur),
-				Line:      "log+" + strconv.Itoa(i) + newUUID.String(),
-			})
+		expr, err := syntax.ParseExpr(request.Query)
+		if err != nil {
+			serverutil.WriteError(err, w)
+			return
+		}
+		var mockData logqlmodel.Result
+		switch expr.(type) {
+		case syntax.LogSelectorExpr:
+			mockData = morkSelectData(request.Limit, from)
+		case syntax.SampleExpr:
+			mockData = morkMetricsQueryData()
+		default:
+			t.Fatal("error expr type")
 		}
 
-		mockData := logqlmodel.Result{
-			Statistics: stats.Result{
-				Summary: stats.Summary{QueueTime: 1, ExecTime: 2},
-			},
-
-			Data: logqlmodel.Streams{{
-				Labels:  `{foo="bar"}`,
-				Entries: data,
-			}},
-		}
 		if err := marshal.WriteQueryResponseJSON(mockData, w); err != nil {
 			serverutil.WriteError(err, w)
 			return
@@ -274,4 +325,81 @@ func (s *mockLokiHTTPServer) Run(_ *testing.T, from time.Time) {
 func (s *mockLokiHTTPServer) Stop(t *testing.T) {
 	err := s.server.Shutdown(context.Background())
 	require.NoError(t, err)
+}
+func morkSelectData(limit uint32, from time.Time) logqlmodel.Result {
+	data := make([]logproto.Entry, 0)
+	for i := 0; i < int(limit); i++ {
+		dur := time.Duration(i) * time.Millisecond
+		newUUID, err := uuid.NewUUID()
+		if err != nil {
+			panic(err)
+		}
+		data = append(data, logproto.Entry{
+			Timestamp: from.Add(dur),
+			Line:      "log+" + strconv.Itoa(i) + newUUID.String(),
+		})
+	}
+
+	mockData := logqlmodel.Result{
+		Statistics: stats.Result{
+			Summary: stats.Summary{QueueTime: 1, ExecTime: 2},
+		},
+
+		Data: logqlmodel.Streams{{
+			Labels:  `{foo="bar"}`,
+			Entries: data,
+		}},
+	}
+	return mockData
+}
+
+func morkMetricsQueryData() logqlmodel.Result {
+	mockData := logqlmodel.Result{
+		Statistics: stats.Result{
+			Summary: stats.Summary{QueueTime: 1, ExecTime: 2},
+		},
+		Data: promql.Matrix{
+			{
+				Floats: []promql.FPoint{
+					{
+						T: 1568404331324,
+						F: 0.013333333333333334,
+					},
+				},
+				Metric: []labels.Label{
+					{
+						Name:  "filename",
+						Value: `/var/hostlog/apport.log`,
+					},
+					{
+						Name:  "job",
+						Value: "varlogs",
+					},
+				},
+			},
+			{
+				Floats: []promql.FPoint{
+					{
+						T: 1568404331324,
+						F: 3.45,
+					},
+					{
+						T: 1568404331339,
+						F: 4.45,
+					},
+				},
+				Metric: []labels.Label{
+					{
+						Name:  "filename",
+						Value: `/var/hostlog/syslog`,
+					},
+					{
+						Name:  "job",
+						Value: "varlogs",
+					},
+				},
+			},
+		},
+	}
+	return mockData
 }
