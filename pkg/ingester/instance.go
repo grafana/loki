420a2344350b5390c31bd3832c08285e9104a5c2
Trevor Whitney <trevorjwhitney@gmail.com>
2023-07-27 09:59:49 -0600

    Cherry pick volume memory fix onto k160

--- pkg/ingester/instance.go
+++ pkg/ingester/instance.go
@@ -641,6 +641,8 @@ func (i *instance) GetVolume(ctx context.Context, req *logproto.VolumeRequest) (
 
 	from, through := req.From.Time(), req.Through.Time()
 	volumes := make(map[string]uint64)
+	aggregateBySeries := seriesvolume.AggregateBySeries(req.AggregateBy) || req.AggregateBy == ""
+
 	if err = i.forMatchingStreams(ctx, from, matchers, nil, func(s *stream) error {
 		// Consider streams which overlap our time range
 		if shouldConsiderStream(s, from, through) {
@@ -660,12 +662,20 @@ func (i *instance) GetVolume(ctx context.Context, req *logproto.VolumeRequest) (
 				}
 			}
 
-			seriesLabels = seriesLabels[:0]
-			labelVolumes := make(map[string]uint64, len(s.labels))
-			for _, l := range s.labels {
-				if _, ok := labelsToMatch[l.Name]; matchAny || ok {
-					seriesLabels = append(seriesLabels, l)
-					labelVolumes[l.Name] += size
+			var labelVolumes map[string]uint64
+			if aggregateBySeries {
+				seriesLabels = seriesLabels[:0]
+				for _, l := range s.labels {
+					if _, ok := labelsToMatch[l.Name]; matchAny || ok {
+						seriesLabels = append(seriesLabels, l)
+					}
+				}
+			} else {
+				labelVolumes = make(map[string]uint64, len(s.labels))
+				for _, l := range s.labels {
+					if _, ok := labelsToMatch[l.Name]; matchAny || ok {
+						labelVolumes[l.Name] += size
+					}
 				}
 			}
 
@@ -676,7 +686,7 @@ func (i *instance) GetVolume(ctx context.Context, req *logproto.VolumeRequest) (
 				seriesNames[hash] = seriesLabels.String()
 			}
 
-			if seriesvolume.AggregateBySeries(req.AggregateBy) || req.AggregateBy == "" {
+			if aggregateBySeries {
 				volumes[seriesNames[hash]] += size
 			} else {
 				for k, v := range labelVolumes {
