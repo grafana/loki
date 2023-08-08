e03d92c5b1e8f22938974751b0330ae220ab10f8
DylanGuedes <djmgguedes@gmail.com>
2023-08-08 16:07:49 -0300

    Add configuration for default runtime.

--- pkg/loki/runtime_config_test.go
+++ pkg/loki/runtime_config_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/stretchr/testify/require"
 
+	"github.com/grafana/loki/pkg/runtime"
 	"github.com/grafana/loki/pkg/validation"
 )
 
@@ -81,6 +82,65 @@ overrides:
               priority: 10
 `))
 	require.Equal(t, "invalid override for tenant 29: retention period must be >= 24h was 5h", err.Error())
+}
+
+func Test_DefaultConfig(t *testing.T) {
+	runtimeGetter := newTestRuntimeconfig(t,
+		`
+default:
+    log_push_request: true
+    limited_log_push_errors: false
+configs:
+    "1":
+        log_push_request: false
+        limited_log_push_errors: false
+    "2":
+        log_push_request: true
+`)
+
+	user1 := runtimeGetter("1")
+	user2 := runtimeGetter("2")
+	user3 := runtimeGetter("3")
+
+	require.Equal(t, false, user1.LogPushRequest)
+	require.Equal(t, false, user1.LimitedLogPushErrors)
+	require.Equal(t, false, user2.LimitedLogPushErrors)
+	require.Equal(t, true, user2.LogPushRequest)
+	require.Equal(t, false, user3.LimitedLogPushErrors)
+	require.Equal(t, true, user3.LogPushRequest)
+}
+
+func newTestRuntimeconfig(t *testing.T, yaml string) runtime.TenantConfig {
+	t.Helper()
+	f, err := os.CreateTemp(t.TempDir(), "bar")
+	require.NoError(t, err)
+	path := f.Name()
+	// fake loader to load from string instead of file.
+	loader := func(_ io.Reader) (interface{}, error) {
+		return loadRuntimeConfig(strings.NewReader(yaml))
+	}
+	cfg := runtimeconfig.Config{
+		ReloadPeriod: 1 * time.Second,
+		Loader:       loader,
+		LoadPath:     []string{path},
+	}
+	flagset := flag.NewFlagSet("", flag.PanicOnError)
+	var defaults validation.Limits
+	defaults.RegisterFlags(flagset)
+	require.NoError(t, flagset.Parse(nil))
+
+	reg := prometheus.NewPedanticRegistry()
+	runtimeConfig, err := runtimeconfig.New(cfg, prometheus.WrapRegistererWithPrefix("loki_", reg), log.NewNopLogger())
+	require.NoError(t, err)
+
+	require.NoError(t, runtimeConfig.StartAsync(context.Background()))
+	require.NoError(t, runtimeConfig.AwaitRunning(context.Background()))
+	defer func() {
+		runtimeConfig.StopAsync()
+		require.NoError(t, runtimeConfig.AwaitTerminated(context.Background()))
+	}()
+
+	return tenantConfigFromRuntimeConfig(runtimeConfig)
 }
 
 func newTestOverrides(t *testing.T, yaml string) *validation.Overrides {
