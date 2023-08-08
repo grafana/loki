395998c821b3a1a9a11e58de5153831a30304955
Periklis Tsirakidis <periklis@nefeli.eu>
2023-08-08 20:59:00 +0200

    Update operator/internal/operator/operator.go
    Co-authored-by: Robert Jacob <xperimental@solidproject.de>

--- operator/internal/operator/operator.go
+++ operator/internal/operator/operator.go
@@ -4,8 +4,8 @@ import "os"
 
 const inClusterNamespaceFile = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 
-// GetNamespace returns the cluster namespace from the
-// `/var/run/secrets/kubernetes.io/serviceaccount/namespace` file.
+// GetNamespace returns the namespace the operator is running in from
+// the `/var/run/secrets/kubernetes.io/serviceaccount/namespace` file.
 func GetNamespace() (string, error) {
 	b, err := os.ReadFile(inClusterNamespaceFile)
 	if err != nil {
