ee3f3260283bdef6a99c720bf4e6b819850c1ded
Christian Haudum <christian.haudum@gmail.com>
2023-08-25 11:09:59 +0200

    Remove bigchunk encoding dependency in series tests
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/storage/chunk/chunk.go
+++ pkg/storage/chunk/chunk.go
@@ -40,11 +40,10 @@ type Chunk struct {
 	logproto.ChunkRef
 
 	Metric labels.Labels `json:"metric"`
+	Data   Data          `json:"-"`
 
-	// We never use Delta encoding (the zero value), so if this entry is
-	// missing, we default to DoubleDelta.
+	// Deprecated: Always use Encoding(129) -> LogChunk
 	Encoding Encoding `json:"encoding"`
-	Data     Data     `json:"-"`
 
 	// The encoded version of the chunk, held so we don't need to re-encode it
 	encoded []byte
