b481611b2092be2214f6669240d3603ae9e86980
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2023-08-28 09:12:24 -0600

    [release-2.9.x] config: deprecates unordered-writes and few unused configs (#10369)
    Backport cc6634972f9ee8af669bc128de3d42293ba33d65 from #10324
    
    ---
    
    **What this PR does / why we need it**:
    
    Deprecates the following configs:
    
    out of order writes will be the default and the only format with the
    next major release
    - ingester.unordered-writes
    
    these need not be configurable, were added for testing
    - log.use-buffered
    - log.use-sync
    
    unused
    - frontend.forward-headers-list
    
    
    **Which issue(s) this PR fixes**:
    Fixes #<issue number>
    
    **Special notes for your reviewer**:
    
    **Checklist**
    - [x] Reviewed the
    [`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
    guide (**required**)
    - [x] Documentation added
    - [ ] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] If the change is worth mentioning in the release notes, add
    `add-to-release-notes` label
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/setup/upgrade/_index.md`
    - [ ] For Helm chart changes bump the Helm chart version in
    `production/helm/loki/Chart.yaml` and update
    `production/helm/loki/CHANGELOG.md` and
    `production/helm/loki/README.md`. [Example
    PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)
    
    ---------
    
    Co-authored-by: Ashwanth <iamashwanth@gmail.com>

--- pkg/querier/queryrange/queryrangebase/roundtrip.go
+++ pkg/querier/queryrange/queryrangebase/roundtrip.go
@@ -62,7 +62,7 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	f.BoolVar(&cfg.AlignQueriesWithStep, "querier.align-querier-with-step", false, "Mutate incoming queries to align their start and end with their step.")
 	f.BoolVar(&cfg.CacheResults, "querier.cache-results", false, "Cache query results.")
 	f.BoolVar(&cfg.ShardedQueries, "querier.parallelise-shardable-queries", true, "Perform query parallelisations based on storage sharding configuration and query ASTs. This feature is supported only by the chunks storage engine.")
-	f.Var(&cfg.ForwardHeaders, "frontend.forward-headers-list", "List of headers forwarded by the query Frontend to downstream querier.")
+	f.Var(&cfg.ForwardHeaders, "frontend.forward-headers-list", "Deprecated. List of headers forwarded by the query Frontend to downstream querier.")
 
 	f.StringVar(&cfg.RequiredQueryResponseFormat, "frontend.required-query-response-format", "json", "The downstream querier is required to answer in the accepted format. Can be 'json' or 'protobuf'. Note: Both will still be routed over GRPC.")
 
