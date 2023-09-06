c299550d0398e524af31157bd044423e988984c8
Christian Haudum <christian.haudum@gmail.com>
2023-09-06 08:38:36 +0200

    Make the Store field of the Loki struct an interface type
    Applications that extends the `Loki` struct must be able to also
    override the `Store`. In order to allow this, the field must be an
    interface type, so any `storage.Store` implementation can be assigned.
    
    This reverts the change from interface type to struct type made in PR #10435.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -295,7 +295,7 @@ type Loki struct {
 	cacheGenerationLoader     queryrangebase.CacheGenNumberLoader
 	querierAPI                *querier.QuerierAPI
 	ingesterQuerier           *querier.IngesterQuerier
-	Store                     *storage.LokiStore
+	Store                     storage.Store
 	tableManager              *index.TableManager
 	frontend                  Frontend
 	ruler                     *base_ruler.Ruler
