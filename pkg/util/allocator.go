d4ac956aab85209f77112d00ff68b88e1dc5c227
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-09-13 12:08:18 +0200

    Define slab allocator.

--- pkg/util/allocator.go
+++ pkg/util/allocator.go
@@ -5,17 +5,18 @@ import (
 	"sync"
 	"sync/atomic"
 
+	"github.com/prometheus/prometheus/util/pool"
 	"golang.org/x/exp/slices"
 )
 
 var Closed atomic.Bool
 
-var ChunkAllocator = NewPoolAllocator(2 << 10) // 2KiB
+//var ChunkAllocator = NewPoolAllocator(2 << 10) // 2KiB
+var ChunkAllocator = NewSlabAllocator()
 
 // TODO comment
 type PoolAllocator struct {
 	pool    *sync.Pool
-	maxSize int
 }
 
 func (p *PoolAllocator) Get(sz int) *[]byte {
@@ -56,5 +57,41 @@ func NewPoolAllocator(initialSize int) *PoolAllocator {
 				return &bytes
 			},
 		},
+	}
+}
+
+type SlabAllocator struct {
+	pool *pool.Pool
+}
+
+func (p *SlabAllocator) Get(sz int) *[]byte {
+	sl := p.pool.Get(sz).(*[]byte)
+	*sl = (*sl)[:0:sz]
+
+	return sl
+}
+
+func (p *SlabAllocator) Put(b *[]byte) {
+	if *b == nil {
+		return
+	}
+
+	fmt.Printf("recycling %d bytes\n", cap(*b))
+
+	// TODO(dannyk): once we upgrade to go1.21, replace with clear()
+	for i := range *b {
+		(*b)[i] = 0
+	}
+
+	p.pool.Put(b)
+}
+
+func NewSlabAllocator() *SlabAllocator {
+	return &SlabAllocator{
+		pool: pool.New(2 << 10, 2 << 20, 2, func(size int) any {
+			fmt.Println("allocating...")
+			bytes := make([]byte, 0, size)
+			return &bytes
+		}),
 	}
 }
