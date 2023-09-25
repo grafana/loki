a46bdad329db5d3943903e6c6e6f6b916e3d2f8f
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-09-25 20:25:28 +0200

    Check for nil StepResult.

--- pkg/logql/evaluator.go
+++ pkg/logql/evaluator.go
@@ -1062,10 +1062,10 @@ type LabelReplaceEvaluator struct {
 
 func (e *LabelReplaceEvaluator) Next() (bool, int64, StepResult) {
 	next, ts, r := e.nextEvaluator.Next()
-	vec := r.SampleVector()
 	if !next {
 		return false, 0, SampleVector{}
 	}
+	vec := r.SampleVector()
 	if e.labelCache == nil {
 		e.labelCache = make(map[uint64]labels.Labels, len(vec))
 	}
