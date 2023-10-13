15a318577f543b1b0cc35fa496dd46aa57ca83fd
Owen Diehl <ow.diehl@gmail.com>
2023-10-12 17:05:20 -0700

    chunk ref extensions Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/storage/bloom/v1/index_test.go
+++ pkg/storage/bloom/v1/index_test.go
@@ -72,3 +72,65 @@ func TestSeriesEncoding(t *testing.T) {
 	require.Equal(t, src.Offset, offset)
 	require.Equal(t, src, dst)
 }
+
+func TestChunkRefUnless(t *testing.T) {
+	for _, tc := range []struct {
+		desc             string
+		left, right, exp ChunkRefs
+	}{
+		{
+			desc:  "empty",
+			left:  nil,
+			right: nil,
+			exp:   nil,
+		},
+		{
+			desc:  "left empty",
+			left:  nil,
+			right: ChunkRefs{{Start: 1, End: 2}},
+			exp:   nil,
+		},
+		{
+			desc:  "right empty",
+			left:  ChunkRefs{{Start: 1, End: 2}},
+			right: nil,
+			exp:   ChunkRefs{{Start: 1, End: 2}},
+		},
+		{
+			desc:  "left before right",
+			left:  ChunkRefs{{Start: 1, End: 2}},
+			right: ChunkRefs{{Start: 3, End: 4}},
+			exp:   ChunkRefs{{Start: 1, End: 2}},
+		},
+		{
+			desc:  "left after right",
+			left:  ChunkRefs{{Start: 3, End: 4}},
+			right: ChunkRefs{{Start: 1, End: 2}},
+			exp:   ChunkRefs{{Start: 3, End: 4}},
+		},
+		{
+			desc: "left overlaps right",
+			left: ChunkRefs{
+				{Start: 1, End: 3},
+				{Start: 2, End: 4},
+				{Start: 3, End: 5},
+				{Start: 4, End: 6},
+				{Start: 5, End: 7},
+			},
+			right: ChunkRefs{
+				{Start: 2, End: 4},
+				{Start: 4, End: 6},
+				{Start: 5, End: 6}, // not in left
+			},
+			exp: ChunkRefs{
+				{Start: 1, End: 3},
+				{Start: 3, End: 5},
+				{Start: 5, End: 7},
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			require.Equal(t, tc.exp, tc.left.Unless(tc.right))
+		})
+	}
+}
