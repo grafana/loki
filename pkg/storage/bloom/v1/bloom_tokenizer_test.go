22963a9244713f80e25dd80b2e0234ce990eed64
Paul Rogers <paul.rogers@grafana.com>
2023-10-24 16:13:10 -0400

    Another set of benchmarks

---
+++ pkg/storage/bloom/v1/bloom_tokenizer_test.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"testing"
+)
+
+func BenchmarkMapClear(b *testing.B) {
+	bt, _ := NewBloomTokenizer(prometheus.DefaultRegisterer)
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < CacheSize; k++ {
+			bt.cache[fmt.Sprint(k)] = k
+		}
+
+		clearCache(bt.cache)
+	}
+}
+
+func BenchmarkNewMap(b *testing.B) {
+	bt, _ := NewBloomTokenizer(prometheus.DefaultRegisterer)
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < CacheSize; k++ {
+			bt.cache[fmt.Sprint(k)] = k
+		}
+
+		bt.cache = make(map[string]interface{}, CacheSize)
+	}
+}
