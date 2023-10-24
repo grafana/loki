1bda0a9731ea0464bd6002a443a51445257752b7
Michel Hollands <michel.hollands@gmail.com>
2023-10-24 15:07:33 +0100

    Make cortex the default metrics namespace
    Signed-off-by: Michel Hollands <michel.hollands@gmail.com>

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -147,7 +147,7 @@ func (c *Config) RegisterFlags(f *flag.FlagSet) {
 
 	f.DurationVar(&c.ShutdownDelay, "shutdown-delay", 0, "How long to wait between SIGTERM and shutdown. After receiving SIGTERM, Loki will report 503 Service Unavailable status via /ready endpoint.")
 
-	f.StringVar(&c.MetricsNamespace, "metrics-namespace", "loki", "Namespace of the metrics.")
+	f.StringVar(&c.MetricsNamespace, "metrics-namespace", "cortex", "Namespace of the metrics.")
 
 	c.registerServerFlagsWithChangedDefaultValues(f)
 	c.Common.RegisterFlags(f)
