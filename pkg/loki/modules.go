bcab696bd3c162f4d60ba91eb55f9a55e3b61595
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-10-31 11:42:18 +0100

    Instrument querier inflight requests. (#11085)
    **What this PR does / why we need it**:
    means inflight requests are not instrumented anymore. This change adds a
    middleware that does that.
    
    This will add metrics such as
    ```
    cortex_inflight_requests{method="gRPC",route="*logproto.IndexStatsRequest"} 0
    cortex_inflight_requests{method="gRPC",route="*queryrange.LabelRequest"} 0
    cortex_inflight_requests{method="gRPC",route="*queryrange.LokiRequest"} 0
    cortex_inflight_requests{method="gRPC",route="*queryrange.LokiSeriesRequest"} 0
    ```
    
    **Checklist**
    - [ ] Reviewed the
    [`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
    guide (**required**)
    - [ ] Documentation added
    - [ ] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] If the change is worth mentioning in the release notes, add
    `add-to-release-notes` label
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/setup/upgrade/_index.md`
    - [ ] For Helm chart changes bump the Helm chart version in
    `production/helm/loki/Chart.yaml` and update
    `production/helm/loki/CHANGELOG.md` and
    `production/helm/loki/README.md`. [Example
    PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)
    - [ ] If the change is deprecating or removing a configuration option,
    update the `deprecated-config.yaml` and `deleted-config.yaml` files
    respectively in the `tools/deprecated-config-checker` directory.
    [Example
    PR](https://github.com/grafana/loki/pull/10840/commits/0d4416a4b03739583349934b96f272fb4f685d15)

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -504,10 +504,19 @@ func (t *Loki) initQuerier() (services.Service, error) {
 		t.Codec = queryrange.DefaultCodec
 	}
 
+	internalHandler := queryrangebase.MergeMiddlewares(
+		queryrange.Instrument{
+			QueryHandlerMetrics: queryrange.NewQueryHandlerMetrics(
+				prometheus.DefaultRegisterer,
+				t.Cfg.MetricsNamespace,
+			),
+		},
+	).Wrap(handler)
+
 	svc, err := querier.InitWorkerService(
 		querierWorkerServiceConfig,
 		prometheus.DefaultRegisterer,
-		handler,
+		internalHandler,
 		t.Codec,
 	)
 	if err != nil {
