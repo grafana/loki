d7b8ca7621067c82c4880bb1a6b1dc870ca5bc16
Joao Marcal <jmarcal@redhat.com>
2023-11-23 16:51:09 +0000

    Improve readability

--- operator/hack/deploy-gcp-storage-secret.sh
+++ operator/hack/deploy-gcp-storage-secret.sh
@@ -2,18 +2,18 @@
 
 set -euo pipefail
 
-if [ -z "${1-}" ]; then
+readonly bucket_name=${1-}
+readonly google_application_credentials=${2-}
+
+if [ -z "${bucket_name}" ]; then
     echo "Provide a bucket name"
     exit 1
 fi
 
-if [ -z "${2-}" ]; then
+if [ -z "${google_application_credentials}" ]; then
     echo "Provide a path to the Google application credentials file"
     exit 1
 fi
-
-readonly bucket_name=$1
-readonly google_application_credentials=$2
 
 readonly namespace=${NAMESPACE:-openshift-logging}
 
