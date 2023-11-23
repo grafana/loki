4c6ce98aa412e9e91b6b5e541b972b131ceae99e
Joao Marcal <jmarcal@redhat.com>
2023-11-23 09:32:38 +0000

    Operator add tools to test on GCP

---
+++ operator/hack/deploy-gcp-storage-secret.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ ! "$1" ]; then
+    echo "Provide a bucket name"
+    exit 1
+fi
+
+if [ ! "$2" ]; then
+    echo "Provide a path to the Google application credentials file"
+    exit 1
+fi
+
+BUCKET_NAME=$1
+GOOGLE_APPLICATION_CREDENTIALS=$2
+
+NAMESPACE=${NAMESPACE:-openshift-logging}
+
+echo kubectl --ignore-not-found=true -n "${NAMESPACE}" delete secret test
+echo kubectl -n "${NAMESPACE}" create secret generic test \
+  --from-literal=bucketname="$(echo -n "${BUCKET_NAME}")" \
+  --from-file=key.json="${GOOGLE_APPLICATION_CREDENTIALS}"
