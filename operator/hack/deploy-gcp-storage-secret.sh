557057151160e55f6ecac25670257d95af7c1771
Joao Marcal <jmarcal@redhat.com>
2023-11-23 15:53:51 +0000

    Address Robert review

--- operator/hack/deploy-gcp-storage-secret.sh
+++ operator/hack/deploy-gcp-storage-secret.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
-if [ ! "$1" ]; then
+if [ -z "${1-}" ]; then
     echo "Provide a bucket name"
     exit 1
 fi
 
-if [ ! "$2" ]; then
+if [ -z "${2-}" ]; then
     echo "Provide a path to the Google application credentials file"
     exit 1
 fi
 
-BUCKET_NAME=$1
-GOOGLE_APPLICATION_CREDENTIALS=$2
+readonly bucket_name=$1
+readonly google_application_credentials=$2
 
-NAMESPACE=${NAMESPACE:-openshift-logging}
+readonly namespace=${NAMESPACE:-openshift-logging}
 
-kubectl --ignore-not-found=true -n "${NAMESPACE}" delete secret test
-kubectl -n "${NAMESPACE}" create secret generic test \
-  --from-literal=bucketname="$(echo -n "${BUCKET_NAME}")" \
-  --from-file=key.json="${GOOGLE_APPLICATION_CREDENTIALS}"
+kubectl --ignore-not-found=true -n "${namespace}" delete secret test
+kubectl -n "${namespace}" create secret generic test \
+    --from-literal=bucketname="$(echo -n "${bucket_name}")" \
+    --from-file=key.json="${google_application_credentials}"
