489ac8d529ec393526dfa2e5a32da2aac842b7d9
Salva Corts <salva.corts@grafana.com>
2023-12-02 08:47:12 +0100

    Extract results cache into new pkg (#11343)
    **What this PR does / why we need it**:
    
    This extracts the results cache from `queryrangebase` into its own pkg
    so we can reuse it in other components such as the bloom-gateway without
    having to import `queryrangebase`.
    
    - Most of the logic inside
    `pkg/querier/queryrange/queryrangebase/results_cache.go` now lives in
    `pkg/storage/chunk/cache/results_cache/cache.go`.
    - Some of the tests in
    `pkg/querier/queryrange/queryrangebase/results_cache.go` are moved into
    pkg/storage/chunk/cache/results_cache/cache_test.go.
    - Note that here we don't have access to the types we use in
    `queryrangebase` so we created a new set of mock request/response types
    to test with.

---
+++ pkg/storage/chunk/cache/resultscache/config.go
@@ -0,0 +1,41 @@
+package resultscache
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/grafana/loki/pkg/storage/chunk/cache"
+)
+
+// Config is the config for the results cache.
+type Config struct {
+	CacheConfig cache.Config `yaml:"cache"`
+	Compression string       `yaml:"compression"`
+}
+
+func (cfg *Config) RegisterFlagsWithPrefix(f *flag.FlagSet, prefix string) {
+	cfg.CacheConfig.RegisterFlagsWithPrefix(prefix, "", f)
+	f.StringVar(&cfg.Compression, prefix+"compression", "", "Use compression in cache. The default is an empty value '', which disables compression. Supported values are: 'snappy' and ''.")
+}
+
+func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
+	cfg.RegisterFlagsWithPrefix(f, "")
+}
+
+func (cfg *Config) Validate() error {
+	switch cfg.Compression {
+	case "snappy", "":
+		// valid
+	default:
+		return errors.Errorf("unsupported compression type: %s", cfg.Compression)
+	}
+
+	return nil
+}
+
+type Limits interface {
+	MaxCacheFreshness(ctx context.Context, tenantID string) time.Duration
+}
