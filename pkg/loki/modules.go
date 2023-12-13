8c2842c62836e31566263cf0bf225675b8e6c882
Michel Hollands <michel.hollands@gmail.com>
2023-12-13 16:10:28 +0000

    Support GET for /ingester/shutdown
    Signed-off-by: Michel Hollands <michel.hollands@gmail.com>

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -579,7 +579,7 @@ func (t *Loki) initIngester() (_ services.Service, err error) {
 	t.Server.HTTP.Methods("POST", "GET", "DELETE").Path("/ingester/prepare_shutdown").Handler(
 		httpMiddleware.Wrap(http.HandlerFunc(t.Ingester.PrepareShutdown)),
 	)
-	t.Server.HTTP.Methods("POST").Path("/ingester/shutdown").Handler(
+	t.Server.HTTP.Methods("POST", "GET").Path("/ingester/shutdown").Handler(
 		httpMiddleware.Wrap(http.HandlerFunc(t.Ingester.ShutdownHandler)),
 	)
 	return t.Ingester, nil
