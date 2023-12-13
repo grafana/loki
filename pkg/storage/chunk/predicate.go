3a605e611784b0c76f92b233bf658b116acab74f
Christian Haudum <christian.haudum@gmail.com>
2023-12-13 21:28:39 +0100

    Extract line filters during query execution
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/storage/chunk/predicate.go
+++ pkg/storage/chunk/predicate.go
@@ -1,15 +1,15 @@
 package chunk
 
 import (
-	"github.com/grafana/loki/pkg/logproto"
+	"github.com/grafana/loki/pkg/logql/syntax"
 	"github.com/prometheus/prometheus/model/labels"
 )
 
 type Predicate struct {
 	Matchers []*labels.Matcher
-	Filters  []*logproto.LineFilterExpression
+	Filters  []syntax.LineFilter // Should also be a slice of pointers??
 }
 
-func NewPredicate(m []*labels.Matcher, f []*logproto.LineFilterExpression) Predicate {
+func NewPredicate(m []*labels.Matcher, f []syntax.LineFilter) Predicate {
 	return Predicate{Matchers: m, Filters: f}
 }
