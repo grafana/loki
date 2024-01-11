9281f4479ef5db068b5d42aa37949fdcc9448dc1
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-01-11 10:44:49 +0100

    Step 7

--- pkg/logql/first_over_time.go
+++ pkg/logql/first_over_time.go
@@ -33,3 +33,29 @@ type firstWithTimestampBatchRangeVectorIterator struct {
 	*batchRangeVectorIterator
 	at []promql.Sample
 }
+
+// Step 7
+func (r *firstWithTimestampBatchRangeVectorIterator) At() (int64, StepResult) {
+	if r.at == nil {
+		r.at = make([]promql.Sample, 0, len(r.window))
+	}
+	r.at = r.at[:0]
+	// convert ts from nano to milli seconds as the iterator work with nanoseconds
+	ts := r.current/1e+6 + r.offset/1e+6
+	for _, series := range r.window {
+		s := r.agg(series.Floats)
+		r.at = append(r.at, promql.Sample{
+			F:      s.F,
+			T:      s.T / int64(time.Millisecond),
+			Metric: series.Metric,
+		})
+	}
+	return ts, SampleVector(r.at)
+}
+
+func (r *firstWithTimestampBatchRangeVectorIterator) agg(samples []promql.FPoint) promql.FPoint {
+	if len(samples) == 0 {
+		return promql.FPoint{F: math.NaN(), T: 0}
+	}
+	return samples[0]
+}
