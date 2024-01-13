f89ea658c2ff0c2ca0ca6c861772d42e1080abd8
Callum Styan <callumstyan@gmail.com>
2024-01-12 17:21:54 -0800

    add iterators functionality
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- pkg/logql/first_last_over_time.go
+++ pkg/logql/first_last_over_time.go
@@ -213,3 +213,28 @@ type lastWithTimestampBatchRangeVectorIterator struct {
 	*batchRangeVectorIterator
 	at []promql.Sample
 }
+
+func (r *lastWithTimestampBatchRangeVectorIterator) At() (int64, StepResult) {
+	if r.at == nil {
+		r.at = make([]promql.Sample, 0, len(r.window))
+	}
+	r.at = r.at[:0]
+	// convert ts from nano to milli seconds as the iterator work with nanoseconds
+	ts := r.current/1e+6 + r.offset/1e+6
+	for _, series := range r.window {
+		s := r.agg(series.Floats)
+		r.at = append(r.at, promql.Sample{
+			F:      s.F,
+			T:      s.T / int64(time.Millisecond),
+			Metric: series.Metric,
+		})
+	}
+	return ts, SampleVector(r.at)
+}
+
+func (r *lastWithTimestampBatchRangeVectorIterator) agg(samples []promql.FPoint) promql.FPoint {
+	if len(samples) == 0 {
+		return promql.FPoint{F: math.NaN(), T: 0}
+	}
+	return samples[0]
+}
