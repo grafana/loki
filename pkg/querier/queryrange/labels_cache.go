09a93eb508acf8020730132f6bfb395a608257f9
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2024-01-16 12:14:47 +0100

    [k185] feat(metadata cache): adds max_metadata_cache_freshness (#11683)
    Backport 51899b521b8610d6ecd062b8f2f9e5978b18660b from #11682
    
    ---
    
    **What this PR does / why we need it**:
    Adds `max_metadata_cache_freshness` to limit the metadata requests that
    get cached. When configured, only metadata requests with end time before
    `now - max_metadata_cache_freshness` are cacheable.
    
    _reason for setting the default to 24h?_
    metric results cache can [extract samples for the desired time range
    from an
    extent](https://github.com/grafana/loki/blob/b6e64e1ef1fb2a2155661c815d0198e147579c8e/pkg/querier/queryrange/queryrangebase/results_cache.go#L78)
    since the samples are associated with a timestamp. But the same is not
    true for metadata caching, it is not possible to extract a subset of
    labels/series from a cached extent. As a result, we could return
    inaccurate results, more that what was requested. for ex: returning
    results from an entire 1h extent for a 5m query
    
    Setting `max_metadata_cache_freshness` to 24h should help us avoid
    caching recent data. For anything older, we would report cached metadata
    results at a granularity controlled by
    `split_metadata_queries_by_interval`
    
    **Which issue(s) this PR fixes**:
    Fixes #<issue number>
    
    **Special notes for your reviewer**:
    
    **Checklist**
    - [x] Reviewed the
    [`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
    guide (**required**)
    - [x] Documentation added
    - [x] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] If the change is worth mentioning in the release notes, add
    `add-to-release-notes` label
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/setup/upgrade/_index.md`
    - [ ] For Helm chart changes bump the Helm chart version in
    `production/helm/loki/Chart.yaml` and update
    `production/helm/loki/CHANGELOG.md` and
    `production/helm/loki/README.md`. [Example
    PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)
    - [ ] If the change is deprecating or removing a configuration option,
    update the `deprecated-config.yaml` and `deleted-config.yaml` files
    respectively in the `tools/deprecated-config-checker` directory.
    [Example
    PR](https://github.com/grafana/loki/pull/10840/commits/0d4416a4b03739583349934b96f272fb4f685d15)
    
    Co-authored-by: Ashwanth <iamashwanth@gmail.com>

--- pkg/querier/queryrange/labels_cache.go
+++ pkg/querier/queryrange/labels_cache.go
@@ -91,7 +91,9 @@ func NewLabelsCacheMiddleware(
 		merger,
 		labelsExtractor{},
 		cacheGenNumberLoader,
-		shouldCache,
+		func(ctx context.Context, r queryrangebase.Request) bool {
+			return shouldCacheMetadataReq(ctx, logger, shouldCache, r, limits)
+		},
 		parallelismForReq,
 		retentionEnabled,
 		metrics,
