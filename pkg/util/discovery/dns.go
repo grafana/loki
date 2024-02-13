fc3f9de93cf00770da9b17fb5f0c24fba13573cd
Periklis Tsirakidis <periklis@redhat.com>
2024-02-13 10:10:08 +0100

    Revert "fix[compat-1.20]: Remove context.WitTimeoutCause occurences"
    This reverts commit 60cdd365b4cc33ed57a3d4da55e6c9ce50804ac9.

--- pkg/util/discovery/dns.go
+++ pkg/util/discovery/dns.go
@@ -2,6 +2,7 @@ package discovery
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
@@ -69,7 +70,7 @@ func (d *DNS) discoveryLoop() {
 }
 
 func (d *DNS) runDiscovery() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeoutCause(context.Background(), 5*time.Second, fmt.Errorf("DNS lookup timeout: %s", d.address))
 	defer cancel()
 	err := d.dnsProvider.Resolve(ctx, []string{d.address})
 	if err != nil {
