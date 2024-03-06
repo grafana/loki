82952b5394e10ceec98f1c0f595897228f0793a7
Owen Diehl <ow.diehl@gmail.com>
2024-03-06 10:20:26 -0800

    remove unused helper from bloomgateway Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/bloomgateway/util.go
+++ pkg/bloomgateway/util.go
@@ -5,11 +5,9 @@ import (
 	"time"
 
 	"github.com/prometheus/common/model"
-	"github.com/prometheus/prometheus/model/labels"
 	"golang.org/x/exp/slices"
 
 	"github.com/grafana/loki/pkg/logproto"
-	"github.com/grafana/loki/pkg/logql/syntax"
 	v1 "github.com/grafana/loki/pkg/storage/bloom/v1"
 	"github.com/grafana/loki/pkg/storage/config"
 	"github.com/grafana/loki/pkg/storage/stores/shipper/bloomshipper"
@@ -44,29 +42,6 @@ func getFromThrough(refs []*logproto.ShortRef) (model.Time, model.Time) {
 	})
 
 	return refs[0].From, maxItem.Through
-}
-
-// convertToSearches converts a list of line filter expressions to a list of
-// byte slices that can be used with the bloom filters.
-func convertToSearches(t *v1.NGramTokenizer, filters ...syntax.LineFilterExpr) [][]byte {
-	searches := make([][]byte, 0, (13-t.N)*len(filters))
-	for _, f := range filters {
-		if f.Left != nil {
-			searches = append(searches, convertToSearches(t, *f.Left)...)
-		}
-		if f.Or != nil {
-			searches = append(searches, convertToSearches(t, *f.Or)...)
-		}
-		if f.Ty == labels.MatchEqual {
-			it := t.Tokens(f.Match)
-			for it.Next() {
-				key := make([]byte, t.N)
-				_ = copy(key, it.At())
-				searches = append(searches, key)
-			}
-		}
-	}
-	return searches
 }
 
 // convertToShortRefs converts a v1.ChunkRefs into []*logproto.ShortRef
