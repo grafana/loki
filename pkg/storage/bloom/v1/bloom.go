fe1b357cc2f1f755b0709d3ccad168e7cd2869fa
Owen Diehl <ow.diehl@gmail.com>
2024-03-21 12:30:56 -0700

    fix(blooms): leaky ref (#12300)

--- pkg/storage/bloom/v1/bloom.go
+++ pkg/storage/bloom/v1/bloom.go
@@ -86,7 +86,7 @@ func LazyDecodeBloomPage(r io.Reader, pool chunkenc.ReaderPool, page BloomPageHe
 	}
 	defer pool.PutReader(decompressor)
 
-	b := BlockPool.Get(page.DecompressedLen)[:page.DecompressedLen]
+	b := make([]byte, page.DecompressedLen)
 
 	if _, err = io.ReadFull(decompressor, b); err != nil {
 		return nil, errors.Wrap(err, "decompressing bloom page")
@@ -97,10 +97,6 @@ func LazyDecodeBloomPage(r io.Reader, pool chunkenc.ReaderPool, page BloomPageHe
 	return decoder, nil
 }
 
-// NewBloomPageDecoder returns a decoder for a bloom page.
-// If the byte slice passed in the constructor is from the BlockPool pool, then
-// the caller needs to ensure that Close() is called to put the slice back to
-// its pool.
 func NewBloomPageDecoder(data []byte) *BloomPageDecoder {
 	// last 8 bytes are the number of blooms in this page
 	dec := encoding.DecWith(data[len(data)-8:])
@@ -121,6 +117,11 @@ func NewBloomPageDecoder(data []byte) *BloomPageDecoder {
 }
 
 // Decoder is a seekable, reset-able iterator
+// TODO(owen-d): use buffer pools. The reason we don't currently
+// do this is because the `data` slice currently escapes the decoder
+// via the returned bloom, so we can't know when it's safe to return it to the pool.
+// This happens via `data ([]byte) -> dec (*encoding.Decbuf) -> bloom (Bloom)` where
+// the final Bloom has a reference to the data slice.
 // We could optimize this by encoding the mode (read, write) into our structs
 // and doing copy-on-write shenannigans, but I'm avoiding this for now.
 type BloomPageDecoder struct {
@@ -164,14 +165,6 @@ func (d *BloomPageDecoder) At() *Bloom {
 
 func (d *BloomPageDecoder) Err() error {
 	return d.err
-}
-
-func (d *BloomPageDecoder) Close() {
-	d.err = nil
-	d.cur = nil
-	BlockPool.Put(d.data)
-	d.data = nil
-	d.dec.B = nil
 }
 
 type BloomPageHeader struct {
