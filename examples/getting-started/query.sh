017bf4ab574aa1222075ff6c8d4433190268f510
Cyril Tovena <cyril.tovena@gmail.com>
2024-03-28 17:10:12 +0100

    Cleanup old examples

--- examples/getting-started/query.sh
+++
@@ -1,8 +0,0 @@
-service=getting-started-gateway-1
-
-curl -X POST http://localhost:3102/loki/api/v1/patterns \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "query={service=\"${service}\"}" \
-  -d 'from=2020-09-14T15:22:25.479Z' \
-  -d 'to=2025-09-14T15:23:25.479Z' \
-  | jq .
