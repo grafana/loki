5ab6f970dfc4a8527ebf333a3a016502427c8e2b
Trevor Whitney <trevorjwhitney@gmail.com>
2024-03-29 15:04:24 -0600

    feat: cast to constrained type

--- pkg/querier/querier.go
+++ pkg/querier/querier.go
@@ -905,7 +905,7 @@ func (q *SingleTenantQuerier) DetectedFields(_ context.Context, _ *logproto.Dete
 		Fields: []*logproto.DetectedField{
 			{
 				Label:       "foo",
-				Type:        logproto.DetectedFieldString.String(),
+				Type:        logproto.DetectedFieldString,
 				Cardinality: 1,
 			},
 		},
