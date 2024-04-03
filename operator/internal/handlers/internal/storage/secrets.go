3849bcb022f59258d16379d1de14bcf2754cbe6e
btaani <btaani@redhat.com>
2024-04-03 12:24:20 +0200

    fix indentation

--- operator/internal/handlers/internal/storage/secrets.go
+++ operator/internal/handlers/internal/storage/secrets.go
@@ -436,12 +436,11 @@ func extractS3ConfigSecret(s *corev1.Secret, credentialMode lokiv1.CredentialMod
 
 		if len(endpoint) == 0 {
 			return nil, fmt.Errorf("%w: %s", errSecretMissingField, storage.KeyAWSEndpoint)
-		} 
+		}
 		if len(region) != 0 {
-		   if err := validateS3Endpoint(string(endpoint), string(region)); err != nil {
-		      return nil, err
-		   }
-	        }
+			if err := validateS3Endpoint(string(endpoint), string(region)); err != nil {
+				return nil, err
+			}
 		}
 		if len(id) == 0 {
 			return nil, fmt.Errorf("%w: %s", errSecretMissingField, storage.KeyAWSAccessKeyID)
