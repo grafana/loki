85afb617bda063975aa69ff0c7933800608e46b1
Owen Diehl <ow.diehl@gmail.com>
2024-04-06 10:01:30 -0700

    cleanup Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/storage/bloom/v1/bloom_tester_test.go
+++ pkg/storage/bloom/v1/bloom_tester_test.go
@@ -8,232 +8,11 @@ import (
 	"github.com/grafana/loki/v3/pkg/logql/syntax"
 )
 
-// func TestFiltersToBloomTests(t *testing.T) {
-// 	for _, tc := range []struct {
-// 		name        string
-// 		query       string
-// 		bloom       filter.Checker
-// 		expectMatch bool
-// 	}{
-// 		{
-// 			name:        "No filters",
-// 			query:       `{app="fake"}`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "Single filter",
-// 			query:       `{app="fake"} |= "foo"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "Single filter no match",
-// 			query:       `{app="fake"} |= "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: false,
-// 		},
-// 		{
-// 			name:        "two filters",
-// 			query:       `{app="fake"} |= "foo" |= "bar"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "two filters no match",
-// 			query:       `{app="fake"} |= "foo" |= "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: false,
-// 		},
-// 		{
-// 			name:        "notEq doesnt exist",
-// 			query:       `{app="fake"} != "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "notEq exists",
-// 			query:       `{app="fake"} != "foo"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "or filter both match",
-// 			query:       `{app="fake"} |= "foo" or "bar"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "or filter one right match",
-// 			query:       `{app="fake"} |= "nope" or "foo"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "or filter one left match",
-// 			query:       `{app="fake"} |= "foo" or "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "or filter no match",
-// 			query:       `{app="fake"} |= "no" or "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: false,
-// 		},
-// 		{
-// 			name:        "Not or filter match",
-// 			query:       `{app="fake"} != "nope" or "no"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "NotEq OR filter right exists",
-// 			query:       `{app="fake"} != "nope" or "bar"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "Not OR filter left exists",
-// 			query:       `{app="fake"} != "foo" or "nope"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "NotEq OR filter both exists",
-// 			query:       `{app="fake"} != "foo" or "bar"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "complex filter match",
-// 			query:       `{app="fake"} |= "foo" |= "bar" or "baz" |= "fuzz" or "not" != "nope" != "no" or "none"`,
-// 			bloom:       fakeBloom{"foo", "bar", "baz", "fuzz"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "regex match all star",
-// 			query:       `{app="fake"} |~ ".*"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "regex match all plus",
-// 			query:       `{app="fake"} |~ ".+"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "regex match all notEq",
-// 			query:       `{app="fake"} !~ ".*"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match,
-// 		},
-// 		{
-// 			name:        "regex match",
-// 			query:       `{app="fake"} |~ "nope|.*foo.*"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "regex no match",
-// 			query:       `{app="fake"} |~ ".*not.*"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: false,
-// 		},
-// 		{
-// 			name:        "regex notEq right exists",
-// 			query:       `{app="fake"} !~ "nope|.*foo.*"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "complex regex match",
-// 			query:       `{app="fake"} |~ "(nope|.*not.*|.*foo.*)" or "(no|ba)" !~ "noz.*" or "(nope|not)"`,
-// 			bloom:       fakeBloom{"foo", "bar", "baz", "fuzz"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "complex regex with notEq exists",
-// 			query:       `{app="fake"} |~ "(nope|.*not.*|.*foo.*)" or "(no|ba)" !~ "noz.*"`,
-// 			bloom:       fakeBloom{"foo", "bar", "baz", "fuzz", "noz"},
-// 			expectMatch: true, // Still should match because it's NotEQ
-// 		},
-// 		{
-// 			name:        "line filter after line format",
-// 			query:       `{app="fake"} |= "foo" | line_format "thisNewTextShouldMatch" |= "thisNewTextShouldMatch"`,
-// 			bloom:       fakeBloom{"foo"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "pattern match exists",
-// 			query:       `{app="fake"} |> "<_>foo<bar>"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "pattern match does not exist",
-// 			query:       `{app="fake"} |> "<_>foo<bar>"`,
-// 			bloom:       fakeBloom{"bar", "baz"},
-// 			expectMatch: false,
-// 		},
-// 		{
-// 			name:        "pattern not match exists",
-// 			query:       `{app="fake"} !> "<_>foo<bar>"`,
-// 			bloom:       fakeBloom{"foo", "bar"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "pattern not match does not exist",
-// 			query:       `{app="fake"} !> "<_>foo<bar>"`,
-// 			bloom:       fakeBloom{"bar", "baz"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "pattern all",
-// 			query:       `{app="fake"} |> "<_>"`,
-// 			bloom:       fakeBloom{"bar", "baz"},
-// 			expectMatch: true,
-// 		},
-// 		{
-// 			name:        "pattern empty",
-// 			query:       `{app="fake"} |> ""`,
-// 			bloom:       fakeBloom{"bar", "baz"},
-// 			expectMatch: true,
-// 		},
-// 	} {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			expr, err := syntax.ParseExpr(tc.query)
-// 			assert.NoError(t, err)
-// 			filters := ExtractTestableLineFilters(expr)
+type fakeBloom []string
 
-// 			bloomTests := FiltersToBloomTest(fakeNgramBuilder{}, filters...)
-// 			assert.Equal(t, tc.expectMatch, bloomTests.Matches(tc.bloom))
-// 		})
-// 	}
-// }
-
-// type fakeNgramBuilder struct{}
-
-// func (f fakeNgramBuilder) Tokens(line string) Iterator[[]byte] {
-// 	return NewSliceIter[[]byte]([][]byte{[]byte(line)})
-// }
-
-// type fakeBloom []string
-
-// func (f fakeBloom) Test(data []byte) bool {
-// 	str := string(data)
-// 	for _, match := range f {
-// 		if str == match {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-type fakeBloom2 []string
-
-func newFakeBloom2(tokenizer *NGramTokenizer, line string) (res fakeBloom2) {
+// fakeBloom is a fake bloom filter that matches tokens exactly.
+// It uses a tokenizer to build the tokens for a line
+func newFakeBloom(tokenizer *NGramTokenizer, line string) (res fakeBloom) {
 	toks := tokenizer.Tokens(line)
 	for toks.Next() {
 		res = append(res, string(toks.At()))
@@ -241,7 +20,7 @@ func newFakeBloom2(tokenizer *NGramTokenizer, line string) (res fakeBloom2) {
 	return
 }
 
-func (f fakeBloom2) Test(data []byte) bool {
+func (f fakeBloom) Test(data []byte) bool {
 	str := string(data)
 	for _, match := range f {
 		if str == match {
@@ -251,7 +30,7 @@ func (f fakeBloom2) Test(data []byte) bool {
 	return false
 }
 
-func TestIdk(t *testing.T) {
+func TestBloomQueryingLogic(t *testing.T) {
 	// All tested on 4skip1
 	n := 4
 	skip := 1
@@ -326,7 +105,7 @@ func TestIdk(t *testing.T) {
 			continue
 		}
 		t.Run(tc.desc, func(t *testing.T) {
-			bloom := newFakeBloom2(tokenizer, tc.line)
+			bloom := newFakeBloom(tokenizer, tc.line)
 			expr, err := syntax.ParseExpr(tc.query)
 			require.NoError(t, err)
 			filters := ExtractTestableLineFilters(expr)
