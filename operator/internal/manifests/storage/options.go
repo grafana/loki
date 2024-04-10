0084262269f4e2cb94d04e0cc0d40e9666177f06
Bayan Taani <86984560+btaani@users.noreply.github.com>
2024-04-10 10:51:11 +0200

    fix(operator): Configure Loki to use virtual-host-style URLs for S3 AWS endpoints (#12469)

--- operator/internal/manifests/storage/options.go
+++ operator/internal/manifests/storage/options.go
@@ -42,12 +42,13 @@ type GCSStorageConfig struct {
 
 // S3StorageConfig for S3 storage config
 type S3StorageConfig struct {
-	Endpoint string
-	Region   string
-	Buckets  string
-	Audience string
-	STS      bool
-	SSE      S3SSEConfig
+	Endpoint       string
+	Region         string
+	Buckets        string
+	Audience       string
+	STS            bool
+	SSE            S3SSEConfig
+	ForcePathStyle bool
 }
 
 type S3SSEType string
