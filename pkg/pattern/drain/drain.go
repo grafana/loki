ac8af3964a3a6dc978430c8d5814d1a45d2a4bda
Ben Clive <ben.clive@grafana.com>
2024-04-19 10:23:08 +0100

    Use backticks for raw strings & unify config params

--- pkg/pattern/drain/drain.go
+++ pkg/pattern/drain/drain.go
@@ -102,6 +102,50 @@ type Node struct {
 	clusterIDs     []int
 }
 
+func DefaultConfig() *Config {
+	// TODO(kolesnikovae):
+	//
+	// This is crucial for Drain to ensure that the first LogClusterDepth tokens
+	// are constant (see https://jiemingzhu.github.io/pub/pjhe_icws2017.pdf).
+	// We should remove any variables such as timestamps, IDs, IPs, counters, etc.
+	// from these tokens.
+	//
+	// Moreover, Drain is not designed for structured logs. Therefore, we should
+	// handle logfmt (and, probably, JSON) logs in a special way:
+	//
+	// The parse tree should have a fixed length, and the depth should be
+	// determined by the number of fields in the logfmt message.
+	// A parsing tree should be maintained for each unique field set.
+	return &Config{
+		// At training, if at the depth of LogClusterDepth there is a cluster with
+		// similarity coefficient greater that SimTh, then the log message is added
+		// to that cluster. Otherwise, a new cluster is created.
+		//
+		// LogClusterDepth should be equal to the number of constant tokens from
+		// the beginning of the message that likely determine the message contents.
+		//
+		//  > In this step, Drain traverses from a 1-st layer node, which
+		//  > is searched in step 2, to a leaf node. This step is based on
+		//  > the assumption that tokens in the beginning positions of a log
+		//  > message are more likely to be constants. Specifically, Drain
+		//  > selects the next internal node by the tokens in the beginning
+		//  > positions of the log message
+		LogClusterDepth: 8,
+		// SimTh is basically a ratio of matching/total in the cluster.
+		// Cluster tokens: "foo <*> bar fred"
+		//       Log line: "foo bar baz qux"
+		//                  *   *   *   x
+		// Similarity of these sequences is 0.75 (the distance)
+		// Both SimTh and MaxClusterDepth impact branching factor: the greater
+		// MaxClusterDepth and SimTh, the less the chance that there will be
+		// "similar" clusters, but the greater the footprint.
+		SimTh:       0.3,
+		MaxChildren: 100,
+		ParamString: `<_>`,
+		MaxClusters: 300,
+	}
+}
+
 func New(config *Config) *Drain {
 	if config.LogClusterDepth < 3 {
 		panic("depth argument must be at least 3")
