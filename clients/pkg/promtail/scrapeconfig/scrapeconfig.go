79e87e91e87448faa987438c0b099bf341c58ddd
ChengenH <hce19970702@gmail.com>
2024-04-21 20:02:07 +0800

    chore: use errors.New to replace fmt.Errorf with no parameters will much better
    Signed-off-by: ChengenH <hce19970702@gmail.com>

--- clients/pkg/promtail/scrapeconfig/scrapeconfig.go
+++ clients/pkg/promtail/scrapeconfig/scrapeconfig.go
@@ -1,7 +1,7 @@
 package scrapeconfig
 
 import (
-	"fmt"
+	"errors"
 	"reflect"
 	"time"
 
@@ -462,7 +462,7 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	}
 
 	if len(c.JobName) == 0 {
-		return fmt.Errorf("job_name is empty")
+		return errors.New("job_name is empty")
 	}
 
 	return nil
