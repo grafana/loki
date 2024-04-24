grafana/loki

commit c36b1142c7acd6a13a3634ddbef71254040cff73

fix(blooms): Correctly return unfiltered chunks for series that are not mapped to any block (#12774)
This PR fixes a conceptual mistake in the code of resolving blocks on the index gateways. Currently, a series does not resolve to any block is discarded instead of being kept for the response.
This change adds the chunks of the skipped series to the bloom querier response.


Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

pkg/bloomgateway/querier.go
@@ -99,7 +99,7 @@ func (bq *BloomQuerier) FilterChunkRefs(ctx context.Context, tenant string, from
 	// only covers a single day, and if not, it's at most two days.
 	for _, s := range partitionSeriesByDay(from, through, grouped) {
 		day := bloomshipper.NewInterval(s.day.Time, s.day.Time.Add(Day))
-		blocks, err := bq.blockResolver.Resolve(ctx, tenant, day, s.series)
+		blocks, skipped, err := bq.blockResolver.Resolve(ctx, tenant, day, s.series)
 		if err != nil {
 			return nil, err
 		}
@@ -120,6 +120,9 @@ func (bq *BloomQuerier) FilterChunkRefs(ctx context.Context, tenant string, from
 		if err != nil {
 			return nil, err
 		}
+
+		// add chunk refs from series that were not mapped to any blocks
+		refs = append(refs, skipped...)
 
 		for i := range refs {
 			seriesSeen[refs[i].Fingerprint] = struct{}{}

pkg/bloomgateway/resolver.go
@@ -15,7 +15,7 @@ import (
 )
 
 type BlockResolver interface {
-	Resolve(context.Context, string, bloomshipper.Interval, []*logproto.GroupedChunkRefs) ([]blockWithSeries, error)
+	Resolve(context.Context, string, bloomshipper.Interval, []*logproto.GroupedChunkRefs) ([]blockWithSeries, []*logproto.GroupedChunkRefs, error)
 }
 
 type blockWithSeries struct {
@@ -28,7 +28,7 @@ type defaultBlockResolver struct {
 	logger log.Logger
 }
 
-func (r *defaultBlockResolver) Resolve(ctx context.Context, tenant string, interval bloomshipper.Interval, series []*logproto.GroupedChunkRefs) ([]blockWithSeries, error) {
+func (r *defaultBlockResolver) Resolve(ctx context.Context, tenant string, interval bloomshipper.Interval, series []*logproto.GroupedChunkRefs) ([]blockWithSeries, []*logproto.GroupedChunkRefs, error) {
 	minFp, maxFp := getFirstLast(series)
 	metaSearch := bloomshipper.MetaSearchParams{
 		TenantID: tenant,
@@ -52,10 +52,12 @@ func (r *defaultBlockResolver) Resolve(ctx context.Context, tenant string, inter
 	)
 
 	if err != nil {
-		return nil, err
+		return nil, series, err
 	}
 
-	return blocksMatchingSeries(metas, interval, series), nil
+	mapped := blocksMatchingSeries(metas, interval, series)
+	skipped := unassignedSeries(mapped, series)
+	return mapped, skipped, nil
 }
 
 func blocksMatchingSeries(metas []bloomshipper.Meta, interval bloomshipper.Interval, series []*logproto.GroupedChunkRefs) []blockWithSeries {
@@ -94,6 +96,31 @@ func blocksMatchingSeries(metas []bloomshipper.Meta, interval bloomshipper.Inter
 	}
 
 	return result
+}
+
+func unassignedSeries(mapped []blockWithSeries, series []*logproto.GroupedChunkRefs) []*logproto.GroupedChunkRefs {
+	skipped := make([]*logproto.GroupedChunkRefs, len(series))
+	_ = copy(skipped, series)
+
+	for _, block := range mapped {
+		minFp, maxFp := getFirstLast(block.series)
+
+		minIdx := sort.Search(len(skipped), func(i int) bool {
+			return skipped[i].Fingerprint >= minFp.Fingerprint
+		})
+
+		maxIdx := sort.Search(len(skipped), func(i int) bool {
+			return skipped[i].Fingerprint >= maxFp.Fingerprint
+		})
+
+		if minIdx == len(skipped) || maxIdx == 0 || minIdx == maxIdx {
+			continue
+		}
+
+		skipped = append(skipped[0:minIdx], skipped[maxIdx+1:]...)
+	}
+
+	return skipped
 }
 
 func NewBlockResolver(store bloomshipper.Store, logger log.Logger) BlockResolver {

pkg/bloomgateway/resolver_test.go
@@ -11,18 +11,22 @@ import (
 	"github.com/grafana/loki/v3/pkg/storage/stores/shipper/bloomshipper"
 )
 
+func makeBlockRef(minFp, maxFp model.Fingerprint, from, through model.Time) bloomshipper.BlockRef {
+	return bloomshipper.BlockRef{
+		Ref: bloomshipper.Ref{
+			TenantID:       "tenant",
+			TableName:      "table",
+			Bounds:         v1.NewBounds(minFp, maxFp),
+			StartTimestamp: from,
+			EndTimestamp:   through,
+		},
+	}
+}
+
 func makeMeta(minFp, maxFp model.Fingerprint, from, through model.Time) bloomshipper.Meta {
 	return bloomshipper.Meta{
 		Blocks: []bloomshipper.BlockRef{
-			{
-				Ref: bloomshipper.Ref{
-					TenantID:       "tenant",
-					TableName:      "table",
-					Bounds:         v1.NewBounds(minFp, maxFp),
-					StartTimestamp: from,
-					EndTimestamp:   through,
-				},
-			},
+			makeBlockRef(minFp, maxFp, from, through),
 		},
 	}
 }
@@ -112,4 +116,138 @@ func TestBlockResolver_BlocksMatchingSeries(t *testing.T) {
 		}
 		require.Equal(t, expected, res)
 	})
+}
+
+func TestBlockResolver_UnassignedSeries(t *testing.T) {
+	series := []*logproto.GroupedChunkRefs{
+		{Fingerprint: 0x00},
+		{Fingerprint: 0x20},
+		{Fingerprint: 0x40},
+		{Fingerprint: 0x60},
+		{Fingerprint: 0x80},
+		{Fingerprint: 0xa0},
+		{Fingerprint: 0xc0},
+		{Fingerprint: 0xe0},
+	}
+
+	testCases := []struct {
+		desc     string
+		mapped   []blockWithSeries
+		expected []*logproto.GroupedChunkRefs
+	}{
+		{
+			desc:     "no blocks - all unassigned",
+			mapped:   []blockWithSeries{},
+			expected: series,
+		},
+		{
+			desc: "block has no overlapping series - all unassigned",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0xf0},
+						{Fingerprint: 0xff},
+					},
+				},
+			},
+			expected: series,
+		},
+		{
+			desc: "single block covering all series - no unassigned",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x00},
+						{Fingerprint: 0x20},
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+						{Fingerprint: 0x80},
+						{Fingerprint: 0xa0},
+						{Fingerprint: 0xc0},
+						{Fingerprint: 0xe0},
+					},
+				},
+			},
+			expected: []*logproto.GroupedChunkRefs{},
+		},
+		{
+			desc: "multiple blocks covering all series - no unassigned",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x00},
+						{Fingerprint: 0x20},
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+					},
+				},
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+						{Fingerprint: 0x80},
+						{Fingerprint: 0xa0},
+					},
+				},
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x80},
+						{Fingerprint: 0xa0},
+						{Fingerprint: 0xc0},
+						{Fingerprint: 0xe0},
+					},
+				},
+			},
+			expected: []*logproto.GroupedChunkRefs{},
+		},
+		{
+			desc: "single block overlapping some series",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x00},
+						{Fingerprint: 0x20},
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+					},
+				},
+			},
+			expected: []*logproto.GroupedChunkRefs{
+				{Fingerprint: 0x80},
+				{Fingerprint: 0xa0},
+				{Fingerprint: 0xc0},
+				{Fingerprint: 0xe0},
+			},
+		},
+		{
+			desc: "multiple blocks overlapping some series",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x20},
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+					},
+				},
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x80},
+						{Fingerprint: 0xa0},
+						{Fingerprint: 0xc0},
+					},
+				},
+			},
+			expected: []*logproto.GroupedChunkRefs{
+				{Fingerprint: 0x00},
+				{Fingerprint: 0xe0},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			result := unassignedSeries(tc.mapped, series)
+			require.Equal(t, result, tc.expected)
+		})
+	}
 }

pkg/bloomgateway/querier_test.go
@@ -40,7 +40,7 @@ func (c *noopClient) FilterChunks(_ context.Context, _ string, _ bloomshipper.In
 type mockBlockResolver struct{}
 
 // Resolve implements BlockResolver.
-func (*mockBlockResolver) Resolve(_ context.Context, tenant string, interval bloomshipper.Interval, series []*logproto.GroupedChunkRefs) ([]blockWithSeries, error) {
+func (*mockBlockResolver) Resolve(_ context.Context, tenant string, interval bloomshipper.Interval, series []*logproto.GroupedChunkRefs) ([]blockWithSeries, []*logproto.GroupedChunkRefs, error) {
 	day := truncateDay(interval.Start)
 	first, last := getFirstLast(series)
 	block := bloomshipper.BlockRef{
@@ -53,7 +53,7 @@ func (*mockBlockResolver) Resolve(_ context.Context, tenant string, interval blo
 			Checksum:       0,
 		},
 	}
-	return []blockWithSeries{{block: block, series: series}}, nil
+	return []blockWithSeries{{block: block, series: series}}, nil, nil
 }
 
 var _ BlockResolver = &mockBlockResolver{}

commit ecefb495084a59d25778af520041766e087598ba

fix(blooms): Fix a regression introduced with #12774 (#12776)
This PR fixes a regression introduced with #12774 where series where wrongly identified as "skipped" even though there were part of blocks.

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

pkg/bloomgateway/querier.go
@@ -22,6 +22,7 @@ type querierMetrics struct {
 	chunksFiltered prometheus.Counter
 	seriesTotal    prometheus.Counter
 	seriesFiltered prometheus.Counter
+	seriesSkipped  prometheus.Counter
 }
 
 func newQuerierMetrics(registerer prometheus.Registerer, namespace, subsystem string) *querierMetrics {
@@ -49,6 +50,12 @@ func newQuerierMetrics(registerer prometheus.Registerer, namespace, subsystem st
 			Subsystem: subsystem,
 			Name:      "series_filtered_total",
 			Help:      "Total amount of series that have been filtered out. Does not count series in failed requests.",
+		}),
+		seriesSkipped: promauto.With(registerer).NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "series_skipped_total",
+			Help:      "Total amount of series that have been skipped and returned unfiltered, because no block matched the series.",
 		}),
 	}
 }
@@ -114,6 +121,7 @@ func (bq *BloomQuerier) FilterChunkRefs(ctx context.Context, tenant string, from
 			"series", len(s.series),
 			"chunks", chunks,
 			"blocks", len(blocks),
+			"skipped", len(skipped),
 		)
 
 		refs, err := bq.c.FilterChunks(ctx, tenant, s.interval, blocks, queryPlan)
@@ -123,6 +131,7 @@ func (bq *BloomQuerier) FilterChunkRefs(ctx context.Context, tenant string, from
 
 		// add chunk refs from series that were not mapped to any blocks
 		refs = append(refs, skipped...)
+		bq.metrics.seriesSkipped.Add(float64(len(skipped)))
 
 		for i := range refs {
 			seriesSeen[refs[i].Fingerprint] = struct{}{}

pkg/bloomgateway/resolver.go
@@ -110,14 +110,14 @@ func unassignedSeries(mapped []blockWithSeries, series []*logproto.GroupedChunkR
 		})
 
 		maxIdx := sort.Search(len(skipped), func(i int) bool {
-			return skipped[i].Fingerprint >= maxFp.Fingerprint
+			return skipped[i].Fingerprint > maxFp.Fingerprint
 		})
 
 		if minIdx == len(skipped) || maxIdx == 0 || minIdx == maxIdx {
 			continue
 		}
 
-		skipped = append(skipped[0:minIdx], skipped[maxIdx+1:]...)
+		skipped = append(skipped[0:minIdx], skipped[maxIdx:]...)
 	}
 
 	return skipped

pkg/bloomgateway/resolver_test.go
@@ -242,12 +242,34 @@ func TestBlockResolver_UnassignedSeries(t *testing.T) {
 				{Fingerprint: 0xe0},
 			},
 		},
+		{
+			desc: "block overlapping single remaining series",
+			mapped: []blockWithSeries{
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0x00},
+						{Fingerprint: 0x20},
+						{Fingerprint: 0x40},
+						{Fingerprint: 0x60},
+						{Fingerprint: 0x80},
+						{Fingerprint: 0xa0},
+						{Fingerprint: 0xc0},
+					},
+				},
+				{
+					series: []*logproto.GroupedChunkRefs{
+						{Fingerprint: 0xe0},
+					},
+				},
+			},
+			expected: []*logproto.GroupedChunkRefs{},
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			result := unassignedSeries(tc.mapped, series)
-			require.Equal(t, result, tc.expected)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }