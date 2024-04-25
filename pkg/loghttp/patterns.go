3e3a11e53d4054fdaeff62b94171ff15a25fca1c
Ben Clive <ben.clive@grafana.com>
2024-04-25 14:40:03 +0100

    Correctly parse step param from HTTP

--- pkg/loghttp/patterns.go
+++ pkg/loghttp/patterns.go
@@ -2,7 +2,6 @@ package loghttp
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/grafana/loki/v3/pkg/logproto"
 )
@@ -10,14 +9,27 @@ import (
 func ParsePatternsQuery(r *http.Request) (*logproto.QueryPatternsRequest, error) {
 	req := &logproto.QueryPatternsRequest{}
 
+	req.Query = query(r)
 	start, end, err := bounds(r)
 	if err != nil {
 		return nil, err
 	}
 	req.Start = start
 	req.End = end
-	req.Step = (time.Duration(defaultQueryRangeStep(start, end)) * time.Second).Milliseconds()
 
-	req.Query = query(r)
+	calculatedStep, err := step(r, start, end)
+	if err != nil {
+		return nil, err
+	}
+	if calculatedStep <= 0 {
+		return nil, errZeroOrNegativeStep
+	}
+	// For safety, limit the number of returned points per timeseries.
+	// This is sufficient for 60s resolution for a week or 1h resolution for a year.
+	if (req.End.Sub(req.Start) / calculatedStep) > 11000 {
+		return nil, errStepTooSmall
+	}
+	req.Step = calculatedStep.Milliseconds()
+
 	return req, nil
 }
