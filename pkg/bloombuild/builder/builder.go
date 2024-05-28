34fcd850fac88341c37e177520737d30481cb338
Salva Corts <salva.corts@grafana.com>
2024-05-28 12:46:15 +0200

    typo

--- pkg/bloombuild/builder/builder.go
+++ pkg/bloombuild/builder/builder.go
@@ -36,106 +36,106 @@ func New(
 ) (*Builder, error) {
 	utillog.WarnExperimentalUse("Bloom Builder", logger)
 
-	w := &Builder{
+	b := &Builder{
 		ID:      uuid.NewString(),
 		cfg:     cfg,
 		metrics: NewMetrics(r),
 		logger:  logger,
 	}
 
-	w.Service = services.NewBasicService(w.starting, w.running, w.stopping)
-	return w, nil
+	b.Service = services.NewBasicService(b.starting, b.running, b.stopping)
+	return b, nil
 }
 
-func (w *Builder) starting(_ context.Context) error {
-	w.metrics.running.Set(1)
+func (b *Builder) starting(_ context.Context) error {
+	b.metrics.running.Set(1)
 	return nil
 }
 
-func (w *Builder) stopping(_ error) error {
-	if w.client != nil {
+func (b *Builder) stopping(_ error) error {
+	if b.client != nil {
 		req := &protos.NotifyBuilderShutdownRequest{
-			BuilderID: w.ID,
+			BuilderID: b.ID,
 		}
-		if _, err := w.client.NotifyBuilderShutdown(context.Background(), req); err != nil {
-			level.Error(w.logger).Log("msg", "failed to notify planner about builder shutdown", "err", err)
+		if _, err := b.client.NotifyBuilderShutdown(context.Background(), req); err != nil {
+			level.Error(b.logger).Log("msg", "failed to notify planner about builder shutdown", "err", err)
 		}
 	}
 
-	w.metrics.running.Set(0)
+	b.metrics.running.Set(0)
 	return nil
 }
 
-func (w *Builder) running(ctx context.Context) error {
-	opts, err := w.cfg.GrpcConfig.DialOption(nil, nil)
+func (b *Builder) running(ctx context.Context) error {
+	opts, err := b.cfg.GrpcConfig.DialOption(nil, nil)
 	if err != nil {
 		return fmt.Errorf("failed to create grpc dial options: %w", err)
 	}
 
 	// TODO: Wrap hereafter in retry logic
-	conn, err := grpc.DialContext(ctx, w.cfg.PlannerAddress, opts...)
+	conn, err := grpc.DialContext(ctx, b.cfg.PlannerAddress, opts...)
 	if err != nil {
 		return fmt.Errorf("failed to dial bloom planner: %w", err)
 	}
 
-	w.client = protos.NewPlannerForBuilderClient(conn)
+	b.client = protos.NewPlannerForBuilderClient(conn)
 
-	c, err := w.client.BuilderLoop(ctx)
+	c, err := b.client.BuilderLoop(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to start builder loop: %w", err)
 	}
 
 	// Start processing tasks from planner
-	if err := w.builderLoop(c); err != nil {
+	if err := b.builderLoop(c); err != nil {
 		return fmt.Errorf("builder loop failed: %w", err)
 	}
 
 	return nil
 }
 
-func (w *Builder) builderLoop(c protos.PlannerForBuilder_BuilderLoopClient) error {
+func (b *Builder) builderLoop(c protos.PlannerForBuilder_BuilderLoopClient) error {
 	// Send ready message to planner
-	if err := c.Send(&protos.BuilderToPlanner{BuilderID: w.ID}); err != nil {
+	if err := c.Send(&protos.BuilderToPlanner{BuilderID: b.ID}); err != nil {
 		return fmt.Errorf("failed to send ready message to planner: %w", err)
 	}
 
-	for w.State() == services.Running {
+	for b.State() == services.Running {
 		// When the planner connection closes or the builder stops, the context
 		// will be canceled and the loop will exit.
 		protoTask, err := c.Recv()
 		if err != nil {
 			if errors.Is(c.Context().Err(), context.Canceled) {
-				level.Debug(w.logger).Log("msg", "builder loop context canceled")
+				level.Debug(b.logger).Log("msg", "builder loop context canceled")
 				return nil
 			}
 
 			return fmt.Errorf("failed to receive task from planner: %w", err)
 		}
 
-		w.metrics.taskStarted.Inc()
+		b.metrics.taskStarted.Inc()
 		start := time.Now()
 		status := statusSuccess
 
-		err = w.processTask(c.Context(), protoTask.Task)
+		err = b.processTask(c.Context(), protoTask.Task)
 		if err != nil {
 			status = statusFailure
-			level.Error(w.logger).Log("msg", "failed to process task", "err", err)
+			level.Error(b.logger).Log("msg", "failed to process task", "err", err)
 		}
 
-		w.metrics.taskCompleted.WithLabelValues(status).Inc()
-		w.metrics.taskTime.WithLabelValues(status).Observe(time.Since(start).Seconds())
+		b.metrics.taskCompleted.WithLabelValues(status).Inc()
+		b.metrics.taskTime.WithLabelValues(status).Observe(time.Since(start).Seconds())
 
 		// Acknowledge task completion to planner
-		if err = w.notifyTaskCompletedToPlanner(c, err); err != nil {
+		if err = b.notifyTaskCompletedToPlanner(c, err); err != nil {
 			return fmt.Errorf("failed to notify task completion to planner: %w", err)
 		}
 	}
 
-	level.Debug(w.logger).Log("msg", "builder loop stopped")
+	level.Debug(b.logger).Log("msg", "builder loop stopped")
 	return nil
 }
 
-func (w *Builder) notifyTaskCompletedToPlanner(c protos.PlannerForBuilder_BuilderLoopClient, err error) error {
+func (b *Builder) notifyTaskCompletedToPlanner(c protos.PlannerForBuilder_BuilderLoopClient, err error) error {
 	var errMsg string
 	if err != nil {
 		errMsg = err.Error()
@@ -143,7 +143,7 @@ func (w *Builder) notifyTaskCompletedToPlanner(c protos.PlannerForBuilder_Builde
 
 	// TODO: Implement retry
 	if err := c.Send(&protos.BuilderToPlanner{
-		BuilderID: w.ID,
+		BuilderID: b.ID,
 		Error:     errMsg,
 	}); err != nil {
 		return fmt.Errorf("failed to acknowledge task completion to planner: %w", err)
@@ -151,13 +151,13 @@ func (w *Builder) notifyTaskCompletedToPlanner(c protos.PlannerForBuilder_Builde
 	return nil
 }
 
-func (w *Builder) processTask(_ context.Context, protoTask *protos.ProtoTask) error {
+func (b *Builder) processTask(_ context.Context, protoTask *protos.ProtoTask) error {
 	task, err := protos.FromProtoTask(protoTask)
 	if err != nil {
 		return fmt.Errorf("failed to convert proto task to task: %w", err)
 	}
 
-	level.Debug(w.logger).Log("msg", "received task", "task", task.ID)
+	level.Debug(b.logger).Log("msg", "received task", "task", task.ID)
 
 	// TODO: Implement task processing
 
