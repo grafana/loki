1121d22fb0332cfffbcc37625b42140cc98ef1af
Christian Haudum <christian.haudum@gmail.com>
2024-06-06 12:22:48 +0200

    fixup! Add metrics to memory pool
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

---
+++ pkg/util/mempool/metrics.go
@@ -0,0 +1,31 @@
+package mempool
+
+import (
+	"github.com/grafana/loki/v3/pkg/util/constants"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type metrics struct {
+	availableBuffersPerSlab *prometheus.CounterVec
+	errorsCounter           *prometheus.CounterVec
+}
+
+func newMetrics(r prometheus.Registerer, name string) *metrics {
+	return &metrics{
+		availableBuffersPerSlab: promauto.With(r).NewCounterVec(prometheus.CounterOpts{
+			Namespace:   constants.Loki,
+			Subsystem:   "mempool",
+			Name:        "available_buffers_per_slab",
+			Help:        "The amount of available buffers per slab.",
+			ConstLabels: prometheus.Labels{"pool": name},
+		}, []string{"slab"}),
+		errorsCounter: promauto.With(r).NewCounterVec(prometheus.CounterOpts{
+			Namespace:   constants.Loki,
+			Subsystem:   "mempool",
+			Name:        "errors_total",
+			Help:        "The total amount of errors returned from the pool.",
+			ConstLabels: prometheus.Labels{"pool": name},
+		}, []string{"slab", "reason"}),
+	}
+}
