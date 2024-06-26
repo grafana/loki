6438215fa0d7118c9ec6449aad3e96edaf42e2c7
Christian Haudum <christian.haudum@gmail.com>
2024-06-26 09:44:10 +0200

    Remove specialized, unused iterator implementations
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/iter/v2/iter_test.go
+++ pkg/iter/v2/iter_test.go
@@ -6,35 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSliceIterWithIndex(t *testing.T) {
-	t.Parallel()
-	t.Run("SliceIterWithIndex implements PeekingIterator interface", func(t *testing.T) {
-		xs := []string{"a", "b", "c"}
-		it := NewSliceIterWithIndex(xs, 123)
-
-		// peek at first item
-		p, ok := it.Peek()
-		require.True(t, ok)
-		require.Equal(t, "a", p.val)
-		require.Equal(t, 123, p.idx)
-
-		// proceed to first item
-		require.True(t, it.Next())
-		require.Equal(t, "a", it.At().val)
-		require.Equal(t, 123, it.At().idx)
-
-		// proceed to second and third item
-		require.True(t, it.Next())
-		require.True(t, it.Next())
-
-		// peek at non-existing fourth item
-		p, ok = it.Peek()
-		require.False(t, ok)
-		require.Equal(t, "", p.val) // "" is zero value for type string
-		require.Equal(t, 123, p.idx)
-	})
-}
-
 func TestPeekingIterator(t *testing.T) {
 	t.Parallel()
 	data := []int{1, 2, 3, 4, 5}
