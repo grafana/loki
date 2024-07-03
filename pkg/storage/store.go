7f35179cd3fd3627057d916b7f00c92cee400339
George Robinson <george.robinson@grafana.com>
2024-07-03 16:53:19 +0100

    feat: Ingester RF-1 (#13365)

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/grafana/loki/v3/pkg/storage/types"
+	"github.com/grafana/loki/v3/pkg/storage/wal"
 	"github.com/grafana/loki/v3/pkg/util/httpreq"
 
 	lokilog "github.com/grafana/loki/v3/pkg/logql/log"
@@ -606,5 +607,9 @@ func (f failingChunkWriter) Put(_ context.Context, _ []chunk.Chunk) error {
 }
 
 func (f failingChunkWriter) PutOne(_ context.Context, _, _ model.Time, _ chunk.Chunk) error {
+	return errWritingChunkUnsupported
+}
+
+func (f failingChunkWriter) PutWal(_ context.Context, _ *wal.SegmentWriter) error {
 	return errWritingChunkUnsupported
 }
