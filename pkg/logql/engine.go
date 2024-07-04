3598d664f641a6cca01c2c04ad5f3752c2737b5b
Ashwanth Goli <iamashwanth@gmail.com>
2024-07-04 16:55:23 +0530

    fixup! review suggestions

--- pkg/logql/engine.go
+++ pkg/logql/engine.go
@@ -265,11 +265,7 @@ func (q *query) Exec(ctx context.Context) (logqlmodel.Result, error) {
 	statResult := statsCtx.Result(time.Since(start), queueTime, q.resultLength(data))
 	sp.LogKV(statResult.KVList()...)
 
-	status := 200
-	if err != nil {
-		status, _ = server.ClientHTTPStatusAndError(err)
-	}
-
+	status, _ := server.ClientHTTPStatusAndError(err)
 	if q.record {
 		RecordRangeAndInstantQueryMetrics(ctx, q.logger, q.params, strconv.Itoa(status), statResult, data)
 	}
