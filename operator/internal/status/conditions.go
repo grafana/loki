527510d1a84a981250047dbabba8d492177b8452
Robert Jacob <rojacob@redhat.com>
2024-07-12 20:24:35 +0200

    fix(operator): Remove duplicate conditions from status (#13497)

--- operator/internal/status/conditions.go
+++ operator/internal/status/conditions.go
@@ -2,36 +2,35 @@ package status
 
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+type conditionKey struct {
+	Type   string
+	Reason string
+}
+
 func mergeConditions(old, active []metav1.Condition, now metav1.Time) []metav1.Condition {
+	conditions := map[conditionKey]bool{}
 	merged := make([]metav1.Condition, 0, len(old)+len(active))
-	for len(old) > 0 {
-		c := old[0]
-		found := -1
-		for i, ac := range active {
-			if c.Type == ac.Type && c.Reason == ac.Reason {
-				found = i
-				break
-			}
-		}
-
-		if found != -1 {
-			c = active[found]
-			active = append(active[:found], active[found+1:]...)
-
-			c.Status = metav1.ConditionTrue
-		} else {
-			c.Status = metav1.ConditionFalse
-		}
-
-		c.LastTransitionTime = now
-		merged = append(merged, c)
-		old = old[1:]
-	}
-
 	for _, c := range active {
 		c.Status = metav1.ConditionTrue
 		c.LastTransitionTime = now
+
 		merged = append(merged, c)
+		conditions[conditionKey{Type: c.Type, Reason: c.Reason}] = true
 	}
+
+	for _, c := range old {
+		if conditions[conditionKey{c.Type, c.Reason}] {
+			continue
+		}
+
+		if c.Status != metav1.ConditionFalse {
+			c.LastTransitionTime = now
+			c.Status = metav1.ConditionFalse
+		}
+
+		merged = append(merged, c)
+		conditions[conditionKey{c.Type, c.Reason}] = true
+	}
+
 	return merged
 }
