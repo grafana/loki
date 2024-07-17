d3d9923410c3a724d3dea7472407b977f50d9f69
George Robinson <george.robinson@grafana.com>
2024-07-17 13:57:07 +0100

    chore: Rename PendingItem to PendingSegment and clean up flush.go (#13554)

--- pkg/storage/wal/manager.go
+++ pkg/storage/wal/manager.go
@@ -114,7 +114,7 @@ type Manager struct {
 	clock quartz.Clock
 }
 
-// item is similar to PendingItem, but it is an internal struct used in the
+// item is similar to PendingSegment, but it is an internal struct used in the
 // available and pending lists. It contains a single-use result that is returned
 // to callers appending to the WAL and a re-usable segment that is reset after
 // each flush.
@@ -123,8 +123,8 @@ type item struct {
 	w *SegmentWriter
 }
 
-// PendingItem contains a result and the segment to be flushed.
-type PendingItem struct {
+// PendingSegment contains a result and the segment to be flushed.
+type PendingSegment struct {
 	Result *AppendResult
 	Writer *SegmentWriter
 }
@@ -196,7 +196,7 @@ func (m *Manager) Close() {
 // It returns nil if there are no segments waiting to be flushed. If the WAL
 // is closed it returns all remaining segments from the pending list and then
 // ErrClosed.
-func (m *Manager) NextPending() (*PendingItem, error) {
+func (m *Manager) NextPending() (*PendingSegment, error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	if m.pending.Len() == 0 && !m.moveFrontIfExpired() {
@@ -210,12 +210,12 @@ func (m *Manager) NextPending() (*PendingItem, error) {
 	m.pending.Remove(el)
 	m.metrics.NumPending.Dec()
 	m.metrics.NumFlushing.Inc()
-	return &PendingItem{Result: it.r, Writer: it.w}, nil
+	return &PendingSegment{Result: it.r, Writer: it.w}, nil
 }
 
 // Put resets the segment and puts it back in the available list to accept
-// writes. A PendingItem should not be put back until it has been flushed.
-func (m *Manager) Put(it *PendingItem) {
+// writes. A PendingSegment should not be put back until it has been flushed.
+func (m *Manager) Put(it *PendingSegment) {
 	it.Writer.Reset()
 	m.mu.Lock()
 	defer m.mu.Unlock()
