9ff4612014bc083e20fdf4e5196f833a8429011c
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2024-07-30 08:02:10 +0530

    fix: Fix validation error message for push request

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -429,7 +429,7 @@ func (d *Distributor) Push(ctx context.Context, req *logproto.PushRequest) (*log
 
 	// Return early if request does not contain any streams
 	if len(req.Streams) == 0 {
-		return &logproto.PushResponse{}, nil
+		return &logproto.PushResponse{}, httpgrpc.Errorf(http.StatusUnprocessableEntity, validation.MissingStreams)
 	}
 
 	// First we flatten out the request into a list of samples.
