47a1bdfb5378d1da206452b01c88e4837e6a704e
shantanualshi <shantanu.alshi@grafana.com>
2024-08-05 21:40:36 +0530

    Fix bucket param

--- pkg/loghttp/query_plan.go
+++ pkg/loghttp/query_plan.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/grafana/loki/v3/pkg/logproto"
+	"github.com/pkg/errors"
 )
 
 func ParseQueryPlanRequest(r *http.Request) (*logproto.QueryPlanRequest, error) {
@@ -22,5 +23,21 @@ func ParseQueryPlanRequest(r *http.Request) (*logproto.QueryPlanRequest, error) 
 		return nil, err
 	}
 	req.Direction = dir
+	b, err := buckets(r)
+	if err != nil {
+		return nil, err
+	}
+	req.Buckets = b
 	return req, nil
+}
+
+func buckets(r *http.Request) (uint32, error) {
+	b, err := parseInt(r.Form.Get("buckets"), 1)
+	if err != nil {
+		return 0, err
+	}
+	if b <= 0 {
+		return 0, errors.New("buckets must be a positive value")
+	}
+	return uint32(b), nil
 }
