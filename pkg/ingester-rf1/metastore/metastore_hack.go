6ffd294d94161271e6afe6ad75d43331a16775a4
George Robinson <george.robinson@grafana.com>
2024-08-07 10:01:13 +0100

    feat: Delete WAL objects on truncate
    Until we have a working compactor, we should delete objects from the
    WAL when truncated from the metastore. This complements, but does not
    replace the lifecycle rules on the bucket.

--- pkg/ingester-rf1/metastore/metastore_hack.go
+++ pkg/ingester-rf1/metastore/metastore_hack.go
@@ -1,14 +1,20 @@
 package metastore
 
 import (
+	"context"
+	"fmt"
+	"path"
 	"time"
 
 	"github.com/go-kit/log/level"
 	"github.com/hashicorp/raft"
 	"github.com/oklog/ulid"
+	"go.etcd.io/bbolt"
 	"google.golang.org/protobuf/types/known/anypb"
 
+	"github.com/grafana/loki/v3/pkg/ingester-rf1/metastore/metastorepb"
 	"github.com/grafana/loki/v3/pkg/ingester-rf1/metastore/raftlogpb"
+	"github.com/grafana/loki/v3/pkg/storage/wal"
 )
 
 // FIXME(kolesnikovae):
@@ -60,10 +66,10 @@ func (m *metastoreState) applyTruncate(request *raftlogpb.TruncateCommand) (*any
 		_ = level.Error(m.logger).Log("msg", "failed to get metadata bucket", "err", err)
 		return nil, err
 	}
-	for k, segment := range m.segments {
-		if ulid.MustParse(segment.Id).Time() < request.Timestamp {
-			if err = bucket.Delete([]byte(segment.Id)); err != nil {
-				_ = level.Error(m.logger).Log("msg", "failed to delete stale segments", "err", err)
+	for k, v := range m.segments {
+		if ulid.MustParse(v.Id).Time() < request.Timestamp {
+			if err = m.deleteSegment(context.TODO(), bucket, v); err != nil {
+				_ = level.Error(m.logger).Log("msg", "failed to delete segment", "id", v.Id, "err", err)
 				continue
 			}
 			delete(m.segments, k)
@@ -71,4 +77,16 @@ func (m *metastoreState) applyTruncate(request *raftlogpb.TruncateCommand) (*any
 		}
 	}
 	return &anypb.Any{}, nil
+}
+
+func (m *metastoreState) deleteSegment(ctx context.Context, bucket *bbolt.Bucket, segment *metastorepb.BlockMeta) error {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancelFunc()
+	if err := m.store.DeleteObject(ctx, path.Join(wal.Dir, segment.Id)); err != nil && !m.store.IsObjectNotFoundErr(err) {
+		return fmt.Errorf("failed to delete segment from object store: %w", err)
+	}
+	if err := bucket.Delete([]byte(segment.Id)); err != nil {
+		return fmt.Errorf("failed to delete segment from boltdb: %w", err)
+	}
+	return nil
 }
