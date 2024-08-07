5cf15a23fbc6a2c1e07f993f6682512f804d3e6c
Max Forasteiro <18661016+maxforasteiro@users.noreply.github.com>
2024-08-07 13:35:23 +0200

    !feat: Remove unused variable and update docs

--- tools/lambda-promtail/variables.tf
+++ tools/lambda-promtail/variables.tf
@@ -28,12 +28,6 @@ variable "log_group_names" {
   default     = []
 }
 
-variable "lambda_promtail_image" {
-  type        = string
-  description = "The ECR image URI to pull and use for lambda-promtail."
-  default     = ""
-}
-
 variable "username" {
   type        = string
   description = "The basic auth username, necessary if writing directly to Grafana Cloud Loki."
