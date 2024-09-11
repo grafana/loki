6a95af2e2a949ad55f884446dae82e4e3125b553
Robert Fratto <robertfratto@gmail.com>
2024-09-11 12:19:38 -0400

    storage/bloom/v1: add ExtractTestableLabelMatchers
    ExtractTestableLabelMatchers examines a LogQL expression and extracts
    any label filters that can be used for bloom testing.
    
    The returned value is a higher level type for separation of concerns and
    easier testing.
    
    This commit also fixes a bug where DropLabelsExpr was not implemented
    properly.
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>

---
+++ pkg/storage/bloom/v1/ast_extractor_test.go
@@ -0,0 +1,97 @@
+package v1_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/loki/v3/pkg/logql/syntax"
+	v1 "github.com/grafana/loki/v3/pkg/storage/bloom/v1"
+)
+
+func TestExtractLabelMatchers(t *testing.T) {
+	tt := []struct {
+		name   string
+		input  string
+		expect []v1.LabelMatcher
+	}{
+		{
+			name:  "basic label matcher",
+			input: `{app="foo"} | key="value"`,
+			expect: []v1.LabelMatcher{
+				v1.PlainLabelMatcher{Key: "key", Value: "value"},
+			},
+		},
+
+		{
+			name:  "or label matcher",
+			input: `{app="foo"} | key1="value1" or key2="value2"`,
+			expect: []v1.LabelMatcher{
+				v1.OrLabelMatcher{
+					Left:  v1.PlainLabelMatcher{Key: "key1", Value: "value1"},
+					Right: v1.PlainLabelMatcher{Key: "key2", Value: "value2"},
+				},
+			},
+		},
+
+		{
+			name:  "and label matcher",
+			input: `{app="foo"} | key1="value1" and key2="value2"`,
+			expect: []v1.LabelMatcher{
+				v1.AndLabelMatcher{
+					Left:  v1.PlainLabelMatcher{Key: "key1", Value: "value1"},
+					Right: v1.PlainLabelMatcher{Key: "key2", Value: "value2"},
+				},
+			},
+		},
+
+		{
+			name:  "multiple label matchers",
+			input: `{app="foo"} | key1="value1" | key2="value2"`,
+			expect: []v1.LabelMatcher{
+				v1.PlainLabelMatcher{Key: "key1", Value: "value1"},
+				v1.PlainLabelMatcher{Key: "key2", Value: "value2"},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			expr, err := syntax.ParseExpr(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expect, v1.ExtractTestableLabelMatchers(expr))
+		})
+	}
+}
+
+func TestExtractLabelMatchers_IgnoreAfterParse(t *testing.T) {
+	tt := []struct {
+		name string
+		expr string
+	}{
+		{"after json parser", `json`},
+		{"after logfmt parser", `logfmt`},
+		{"after pattern parser", `pattern "<msg>"`},
+		{"after regexp parser", `regexp "(?P<message>.*)"`},
+		{"after unpack parser", `unpack`},
+		{"after label_format", `label_format foo="bar"`},
+		{"after drop labels stage", `drop foo`},
+		{"after keep labels stage", `keep foo`},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			fullInput := fmt.Sprintf(`{app="foo"} | key1="value1" | %s | key2="value2"`, tc.expr)
+			expect := []v1.LabelMatcher{
+				v1.PlainLabelMatcher{Key: "key1", Value: "value1"},
+				// key2="value2" should be ignored following tc.expr
+			}
+
+			expr, err := syntax.ParseExpr(fullInput)
+			require.NoError(t, err)
+
+			require.Equal(t, expect, v1.ExtractTestableLabelMatchers(expr), "key2=value2 should be ignored with query %s", fullInput)
+		})
+	}
+}
