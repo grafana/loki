51e5f5eae4119b8ae18a8347c644638bcb9b6cd1
Trevor Whitney <trevorjwhitney@gmail.com>
2024-09-18 14:27:47 -0600

    chore: lint and format

--- pkg/logql/engine.go
+++ pkg/logql/engine.go
@@ -265,10 +265,10 @@ func (q *query) Exec(ctx context.Context) (logqlmodel.Result, error) {
 	statResult := statsCtx.Result(time.Since(start), queueTime, q.resultLength(data))
 	sp.LogKV(statResult.KVList()...)
 
-  status := http.StatusOK
-  if err != nil {
-	  status, _ = server.ClientHTTPStatusAndError(err)
-  }
+	status := http.StatusOK
+	if err != nil {
+		status, _ = server.ClientHTTPStatusAndError(err)
+	}
 
 	if q.record {
 		RecordRangeAndInstantQueryMetrics(ctx, q.logger, q.params, strconv.Itoa(status), statResult, data)
