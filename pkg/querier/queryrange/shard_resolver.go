f1a83801df696a9884241e0792c5aa04c830b307
Ashwanth Goli <iamashwanth@gmail.com>
2024-09-25 17:50:02 +0530

    fix(sharding): offset both from and through in shard request

--- pkg/querier/queryrange/shard_resolver.go
+++ pkg/querier/queryrange/shard_resolver.go
@@ -226,6 +226,7 @@ func (r *dynamicShardResolver) ShardingRanges(expr syntax.Expr, targetBytesPerSh
 	log := spanlogger.FromContext(r.ctx)
 
 	adjustedFrom := r.from
+	adjustedThrough := r.through
 
 	// NB(owen-d): there should only ever be 1 matcher group passed
 	// to this call as we call it separately for different legs
@@ -236,17 +237,23 @@ func (r *dynamicShardResolver) ShardingRanges(expr syntax.Expr, targetBytesPerSh
 	}
 
 	for _, grp := range grps {
-		diff := grp.Interval + grp.Offset
+		diff := grp.Interval
 
 		// For instant queries, when start == end,
 		// we have a default lookback which we add here
-		if grp.Interval == 0 {
-			diff = diff + r.defaultLookback
+		if diff == 0 {
+			diff = r.defaultLookback
 		}
+
+		diff += grp.Offset
 
 		// use the oldest adjustedFrom
 		if r.from.Add(-diff).Before(adjustedFrom) {
 			adjustedFrom = r.from.Add(-diff)
+		}
+
+		if r.through.Add(-grp.Offset).After(adjustedThrough) {
+			adjustedThrough = r.through.Add(-grp.Offset)
 		}
 	}
 
@@ -256,7 +263,7 @@ func (r *dynamicShardResolver) ShardingRanges(expr syntax.Expr, targetBytesPerSh
 	// use the retry handler here to retry transient errors
 	resp, err := r.retryNextHandler.Do(r.ctx, &logproto.ShardsRequest{
 		From:                adjustedFrom,
-		Through:             r.through,
+		Through:             adjustedThrough,
 		Query:               expr.String(),
 		TargetBytesPerShard: targetBytesPerShard,
 	})
