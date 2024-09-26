7bfa0bd4fe590d7fa3d9cae44f2676267da5bf5c
Periklis Tsirakidis <periklis@redhat.com>
2024-09-26 10:32:44 +0200

    fix(operator): Add missing groupBy label for all rules on OpenShift

--- operator/internal/manifests/openshift/recordngrule.go
+++ operator/internal/manifests/openshift/recordngrule.go
@@ -5,23 +5,34 @@ import lokiv1 "github.com/grafana/loki/operator/apis/loki/v1"
 func RecordingRuleTenantLabels(r *lokiv1.RecordingRule) {
 	switch r.Spec.TenantID {
 	case tenantApplication:
-		for groupIdx, group := range r.Spec.Groups {
-			group := group
-			for ruleIdx, rule := range group.Rules {
-				rule := rule
-				if rule.Labels == nil {
-					rule.Labels = map[string]string{}
-				}
-				rule.Labels[opaDefaultLabelMatcher] = r.Namespace
-				group.Rules[ruleIdx] = rule
-			}
-			r.Spec.Groups[groupIdx] = group
-		}
-	case tenantInfrastructure, tenantAudit:
-		// Do nothing
-	case tenantNetwork:
-		// Do nothing
+		appendRecordingRuleLabels(r, map[string]string{
+			opaDefaultLabelMatcher:    r.Namespace,
+			ocpMonitoringGroupByLabel: r.Namespace,
+		})
+	case tenantInfrastructure, tenantAudit, tenantNetwork:
+		appendRecordingRuleLabels(r, map[string]string{
+			ocpMonitoringGroupByLabel: r.Namespace,
+		})
 	default:
 		// Do nothing
+	}
+}
+
+func appendRecordingRuleLabels(ar *lokiv1.RecordingRule, labels map[string]string) {
+	for groupIdx, group := range ar.Spec.Groups {
+		group := group
+		for ruleIdx, rule := range group.Rules {
+			rule := rule
+			if rule.Labels == nil {
+				rule.Labels = map[string]string{}
+			}
+
+			for name, value := range labels {
+				rule.Labels[name] = value
+			}
+
+			group.Rules[ruleIdx] = rule
+		}
+		ar.Spec.Groups[groupIdx] = group
 	}
 }
