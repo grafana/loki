225866f2846a3c24acd4c440881fc2d442a6202c
Ashwanth Goli <iamashwanth@gmail.com>
2024-10-09 17:32:25 +0530

    wire http conf + transport for gcs correctly

--- pkg/storage/bucket/gcs/bucket_client.go
+++ pkg/storage/bucket/gcs/bucket_client.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"github.com/go-kit/log"
+	"github.com/prometheus/common/model"
 	"github.com/thanos-io/objstore"
+	"github.com/thanos-io/objstore/exthttp"
 	"github.com/thanos-io/objstore/providers/gcs"
-	yaml "gopkg.in/yaml.v2"
 )
 
 // NewBucketClient creates a new GCS bucket client
@@ -15,14 +16,18 @@ func NewBucketClient(ctx context.Context, cfg Config, name string, logger log.Lo
 		Bucket:         cfg.BucketName,
 		ServiceAccount: cfg.ServiceAccount.String(),
 		ChunkSizeBytes: cfg.ChunkBufferSize,
+		HTTPConfig: exthttp.HTTPConfig{
+			IdleConnTimeout:       model.Duration(cfg.HTTP.IdleConnTimeout),
+			ResponseHeaderTimeout: model.Duration(cfg.HTTP.ResponseHeaderTimeout),
+			InsecureSkipVerify:    cfg.HTTP.InsecureSkipVerify,
+			TLSHandshakeTimeout:   model.Duration(cfg.HTTP.TLSHandshakeTimeout),
+			ExpectContinueTimeout: model.Duration(cfg.HTTP.ExpectContinueTimeout),
+			MaxIdleConns:          cfg.HTTP.MaxIdleConns,
+			MaxIdleConnsPerHost:   cfg.HTTP.MaxIdleConnsPerHost,
+			MaxConnsPerHost:       cfg.HTTP.MaxConnsPerHost,
+			Transport:             cfg.HTTP.Transport,
+		},
 	}
 
-	// Thanos currently doesn't support passing the config as is, but expects a YAML,
-	// so we're going to serialize it.
-	serialized, err := yaml.Marshal(bucketConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return gcs.NewBucket(ctx, logger, serialized, name)
+	return gcs.NewBucketWithConfig(ctx, logger, bucketConfig, name)
 }
