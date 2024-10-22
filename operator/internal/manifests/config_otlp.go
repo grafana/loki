f635ba6bda17e61650795962ecba690ae6b47e83
Robert Jacob <rojacob@redhat.com>
2024-10-22 18:28:30 +0200

    Fix new code after merge

--- operator/internal/manifests/config_otlp.go
+++ operator/internal/manifests/config_otlp.go
@@ -4,7 +4,7 @@ import (
 	"slices"
 	"strings"
 
-	lokiv1 "github.com/grafana/loki/operator/apis/loki/v1"
+	lokiv1 "github.com/grafana/loki/operator/api/loki/v1"
 	"github.com/grafana/loki/operator/internal/manifests/internal/config"
 	"github.com/grafana/loki/operator/internal/manifests/openshift"
 )
