674901843409bcc3ebde11f9fe96203c6b4bd3cb
Salva Corts <salva.corts@grafana.com>
2024-10-30 14:18:24 +0100

    fix: Add tenant limits as dependency to pattern ingester

--- pkg/loki/loki.go
+++ pkg/loki/loki.go
@@ -740,7 +740,7 @@ func (t *Loki) setupModuleManager() error {
 		BloomStore:               {IndexGatewayRing},
 		PatternRingClient:        {Server, MemberlistKV, Analytics},
 		PatternIngesterTee:       {Server, MemberlistKV, Analytics, PatternRingClient},
-		PatternIngester:          {Server, MemberlistKV, Analytics, PatternRingClient, PatternIngesterTee},
+		PatternIngester:          {Server, MemberlistKV, Analytics, PatternRingClient, PatternIngesterTee, Overrides},
 		IngesterQuerier:          {Ring, PartitionRing, Overrides},
 		QuerySchedulerRing:       {Overrides, MemberlistKV},
 		IndexGatewayRing:         {Overrides, MemberlistKV},
