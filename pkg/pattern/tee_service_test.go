492d5bbfd347aec883015c497ca9a5f7d3c40220
Trevor Whitney <trevorjwhitney@gmail.com>
2024-11-01 11:23:36 -0600

    feat: Improve pattern ingester tracing (#14707)
    (cherry picked from commit 80aec2548203957dbb834ba69e6d734d9054416d)

--- pkg/pattern/tee_service_test.go
+++ pkg/pattern/tee_service_test.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/grafana/loki/v3/pkg/distributor"
 	"github.com/grafana/loki/v3/pkg/logproto"
+	"github.com/grafana/loki/v3/pkg/runtime"
 
 	"github.com/grafana/loki/pkg/push"
 )
@@ -51,6 +52,7 @@ func getTestTee(t *testing.T) (*TeeService, *mockPoolClient) {
 			metricAggregationEnabled: true,
 		},
 		ringClient,
+		runtime.DefaultTenantConfigs(),
 		"test",
 		nil,
 		log.NewNopLogger(),
