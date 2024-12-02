575415bc08ab067d2d0f9b8133bf2fa9344ac966
Paul Rogers <paul.rogers@grafana.com>
2024-12-02 08:51:08 -0500

    Update nix

--- nix/packages/loki.nix
+++ nix/packages/loki.nix
@@ -5,7 +5,7 @@ let
     pname = "lambda-promtail";
 
     src = ./../../tools/lambda-promtail;
-    vendorHash = "sha256-d4EMPbUuw3m+M/9lkxQOJGhN8qvvmeoIwfBGJX1Xrl8=";
+    vendorHash = "sha256-90pY7nXU92K3HC2tr1oHT2AYpiaimbcdnIhyP9awkTk=";
 
     doCheck = false;
 
