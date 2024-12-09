130ae9ec60bb4c79b9b3afd1c51ada91877d251e
Trevor Whitney <trevorjwhitney@gmail.com>
2024-12-09 10:46:29 -0700

    Reapply "fix(memberlist): Respect interface names config for advertise address (#13940)"
    This reverts commit 523f99157f7098b5963d062237ee9afbe4c2d116.

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -23,6 +23,7 @@ import (
 	"github.com/grafana/dskit/kv/codec"
 	"github.com/grafana/dskit/kv/memberlist"
 	"github.com/grafana/dskit/middleware"
+	"github.com/grafana/dskit/netutil"
 	"github.com/grafana/dskit/ring"
 	"github.com/grafana/dskit/runtimeconfig"
 	"github.com/grafana/dskit/server"
@@ -1386,6 +1387,15 @@ func (t *Loki) initMemberlistKV() (services.Service, error) {
 	)
 	dnsProvider := dns.NewProvider(util_log.Logger, dnsProviderReg, dns.GolangResolverType)
 
+	var err error
+	t.Cfg.MemberlistKV.AdvertiseAddr, err = GetInstanceAddr(
+		t.Cfg.MemberlistKV.AdvertiseAddr,
+		t.Cfg.Common.InstanceInterfaceNames,
+		util_log.Logger,
+	)
+	if err != nil {
+		return nil, err
+	}
 	t.MemberlistKV = memberlist.NewKVInitService(&t.Cfg.MemberlistKV, util_log.Logger, dnsProvider, reg)
 
 	t.Cfg.CompactorConfig.CompactorRing.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
@@ -2017,4 +2027,12 @@ func schemaHasBoltDBShipperConfig(scfg config.SchemaConfig) bool {
 	}
 
 	return false
+}
+
+func GetInstanceAddr(addr string, netInterfaces []string, logger log.Logger) (string, error) {
+	if addr != "" {
+		return addr, nil
+	}
+
+	return netutil.GetFirstAddressOf(netInterfaces, logger, false)
 }
