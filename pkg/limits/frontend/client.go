b44dcc0e0fe3dc06ad1dcb1cc1b8389abac7eabe
George Robinson <george.robinson@grafana.com>
2025-01-10 15:58:08 +0000

    feat: add client for ingest limits (#15684)

---
+++ pkg/limits/frontend/client.go
@@ -0,0 +1,48 @@
+package frontend
+
+import (
+	"github.com/grafana/loki/v3/pkg/logproto"
+)
+
+type IngestLimitsClient interface {
+	// GetLimits returns the ingestion limits for the tenant.
+	GetLimits(tenantID string) (*logproto.IngestionLimits, error)
+
+	// HasStream returns true if the stream is known to the limits service,
+	// otherwise false. This helps distributors reject new streams when stream
+	// limits are exceeded while still allowing push requests for existing streams.
+	HasStream(tenantID string, streamHash uint64) (bool, error)
+
+	// HasStreams returns a slice of booleans for each stream and if it is known
+	// to the limits service.
+	HasStreams(tenantID string, streamHashes []uint64) ([]bool, error)
+}
+
+type GRPCIngestLimitsClient struct {
+	cfg     Config
+	metrics *IngestLimitsClientMetrics
+}
+
+func NewGRPCIngestLimitsClient(cfg Config, metrics *IngestLimitsClientMetrics) *GRPCIngestLimitsClient {
+	return &GRPCIngestLimitsClient{
+		cfg:     cfg,
+		metrics: metrics,
+	}
+}
+
+func (c *GRPCIngestLimitsClient) GetLimits(tenantID string) (*logproto.IngestionLimits, error) {
+	_ = tenantID
+	return nil, nil
+}
+
+func (c *GRPCIngestLimitsClient) HasStream(tenantID string, streamHash uint64) (bool, error) {
+	_ = tenantID
+	_ = streamHash
+	return false, nil
+}
+
+func (c *GRPCIngestLimitsClient) HasStreams(tenantID string, streamHashes []uint64) ([]bool, error) {
+	_ = tenantID
+	b := make([]bool, 0, len(streamHashes))
+	return b, nil
+}
--- pkg/limits/frontend/config.go
+++ pkg/limits/frontend/config.go
@@ -1,4 +1,15 @@
 package frontend
 
+import (
+	"flag"
+
+	"github.com/grafana/dskit/grpcclient"
+)
+
 type Config struct {
+	GRPCClientConfig grpcclient.Config `yaml:"grpc_client_config"`
+}
+
+func (cfg Config) RegisterFlags(f *flag.FlagSet) {
+	cfg.GRPCClientConfig.RegisterFlagsWithPrefix("limits-frontend.grpc-client-config", f)
 }
