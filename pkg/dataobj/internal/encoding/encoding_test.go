bcec63d9c22f64ffc8e4f6de7e9ef634c72830c4
Robert Fratto <robertfratto@gmail.com>
2025-01-14 08:20:45 -0500

    chore(dataobj): logs section metadata and encoding/decoding (#15720)

--- pkg/dataobj/internal/encoding/encoding_test.go
+++ pkg/dataobj/internal/encoding/encoding_test.go
@@ -11,11 +11,12 @@ import (
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/encoding"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/datasetmd"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/filemd"
+	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/logsmd"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/streamsmd"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/result"
 )
 
-func Test(t *testing.T) {
+func TestStreams(t *testing.T) {
 	type Country struct {
 		Name    string
 		Capital string
@@ -105,5 +106,81 @@ func Test(t *testing.T) {
 		}
 
 		require.Equal(t, countries, actual)
+	})
+}
+
+func TestLogs(t *testing.T) {
+	var (
+		columnA = &dataset.MemColumn{
+			Pages: []*dataset.MemPage{
+				{Data: []byte("Hello")},
+				{Data: []byte("World")},
+				{Data: []byte("!")},
+			},
+		}
+		columnB = &dataset.MemColumn{
+			Pages: []*dataset.MemPage{
+				{Data: []byte("metadata")},
+				{Data: []byte("column")},
+			},
+		}
+	)
+
+	var buf bytes.Buffer
+
+	t.Run("Encode", func(t *testing.T) {
+		enc := encoding.NewEncoder(&buf)
+		logsEnc, err := enc.OpenLogs()
+		require.NoError(t, err)
+
+		colEnc, err := logsEnc.OpenColumn(logsmd.COLUMN_TYPE_MESSAGE, &columnA.Info)
+		require.NoError(t, err)
+		for _, page := range columnA.Pages {
+			require.NoError(t, colEnc.AppendPage(page))
+		}
+		require.NoError(t, colEnc.Commit())
+
+		colEnc, err = logsEnc.OpenColumn(logsmd.COLUMN_TYPE_METADATA, &columnB.Info)
+		require.NoError(t, err)
+		for _, page := range columnB.Pages {
+			require.NoError(t, colEnc.AppendPage(page))
+		}
+		require.NoError(t, colEnc.Commit())
+
+		require.NoError(t, logsEnc.Commit())
+		require.NoError(t, enc.Flush())
+	})
+
+	t.Run("Decode", func(t *testing.T) {
+		dec := encoding.ReadSeekerDecoder(bytes.NewReader(buf.Bytes()))
+		sections, err := dec.Sections(context.TODO())
+		require.NoError(t, err)
+		require.Len(t, sections, 1)
+		require.Equal(t, filemd.SECTION_TYPE_LOGS, sections[0].Type)
+
+		logsDec := dec.LogsDecoder()
+		columns, err := logsDec.Columns(context.TODO(), sections[0])
+		require.NoError(t, err)
+		require.Len(t, columns, 2)
+
+		pageSets, err := result.Collect(logsDec.Pages(context.TODO(), columns))
+		require.NoError(t, err)
+		require.Len(t, pageSets, 2)
+
+		columnAPages, err := result.Collect(logsDec.ReadPages(context.TODO(), pageSets[0]))
+		require.NoError(t, err)
+		require.Len(t, columnAPages, len(columnA.Pages))
+
+		for i := range columnA.Pages {
+			require.Equal(t, columnA.Pages[i].Data, columnAPages[i])
+		}
+
+		columnBPages, err := result.Collect(logsDec.ReadPages(context.TODO(), pageSets[1]))
+		require.NoError(t, err)
+		require.Len(t, columnBPages, len(columnB.Pages))
+
+		for i := range columnB.Pages {
+			require.Equal(t, columnB.Pages[i].Data, columnBPages[i])
+		}
 	})
 }
