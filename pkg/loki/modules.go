e51256274176b222a603caeef4b79bc11c7053ea
Owen Diehl <ow.diehl@gmail.com>
2025-01-14 11:35:15 -0800

    fixes signature

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -1869,7 +1869,6 @@ func (t *Loki) initBlockScheduler() (services.Service, error) {
 
 	s, err := blockscheduler.NewScheduler(
 		t.Cfg.BlockScheduler,
-		blockscheduler.NewJobQueue(t.Cfg.BlockScheduler.JobQueueConfig, logger, prometheus.DefaultRegisterer),
 		offsetManager,
 		logger,
 		prometheus.DefaultRegisterer,
