2dbd6f75bf730b8c696fe1eaa5d09bdee8a4af0c
George Robinson <george.robinson@grafana.com>
2025-01-30 16:00:44 +0000

    Perhaps it needs to be both?

--- pkg/loki/modules.go
+++ pkg/loki/modules.go
@@ -453,6 +453,9 @@ func (t *Loki) initIngestLimitsFrontendRing() (_ services.Service, err error) {
 		return nil, nil
 	}
 
+	// Members of the ring are expected to listen on their gRPC server port.
+	t.Cfg.IngestLimits.LifecyclerConfig.ListenPort = t.Cfg.Server.GRPCListenPort
+
 	reg := prometheus.WrapRegistererWithPrefix(t.Cfg.MetricsNamespace+"_", prometheus.DefaultRegisterer)
 
 	if t.ingestLimitsFrontendRing, err = ring.New(
