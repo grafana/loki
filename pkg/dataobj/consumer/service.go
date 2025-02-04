093e6e8386150b9bebb1a31a3e692e09d5fcb875
benclive <ben.clive@grafana.com>
2025-02-04 15:36:48 +0000

    chore(dataobj): Refactor processor & builder to separate concerns (#16055)

--- pkg/dataobj/consumer/service.go
+++ pkg/dataobj/consumer/service.go
@@ -95,7 +95,7 @@ func (s *Service) handlePartitionsAssigned(ctx context.Context, client *kgo.Clie
 		}
 
 		for _, partition := range parts {
-			processor := newPartitionProcessor(ctx, client, s.cfg.BuilderConfig, s.bucket, tenant, virtualShard, topic, partition, s.logger, s.reg)
+			processor := newPartitionProcessor(ctx, client, s.cfg.BuilderConfig, s.cfg.UploaderConfig, s.bucket, tenant, virtualShard, topic, partition, s.logger, s.reg)
 			s.partitionHandlers[topic][partition] = processor
 			processor.start()
 		}
