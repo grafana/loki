fb579bc558c3da914c8fbe4e940e9c174147c097
Ben Clive <ben.clive@grafana.com>
2025-02-07 11:11:06 +0000

    try again

--- pkg/kafka/partitionring/consumer/client.go
+++ pkg/kafka/partitionring/consumer/client.go
@@ -39,7 +39,7 @@ func NewGroupClient(kafkaCfg kafka.Config, partitionRing ring.PartitionRingReade
 		kgo.ConsumerGroup(groupName),
 		kgo.ConsumeRegex(),
 		kgo.ConsumeTopics(kafkaCfg.Topic),
-		//kgo.Balancers(kgo.CooperativeStickyBalancer()),
+		kgo.Balancers(kgo.CooperativeStickyBalancer()),
 		kgo.ConsumeResetOffset(kgo.NewOffset().AtStart()),
 		kgo.DisableAutoCommit(),
 		kgo.RebalanceTimeout(5 * time.Minute),
