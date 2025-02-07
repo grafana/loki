208438d5219750496bb3542fa4e11ab8cf803652
DylanGuedes <djmgguedes@gmail.com>
2025-02-07 12:55:29 -0300

    remove unused function

--- pkg/validation/limits.go
+++ pkg/validation/limits.go
@@ -1124,10 +1124,6 @@ func (o *Overrides) EnforcedLabels(userID string) []string {
 	return o.getOverridesForUser(userID).EnforcedLabels
 }
 
-func (o *Overrides) PolicyFor(userID string, lbs labels.Labels) string {
-	return o.getOverridesForUser(userID).PolicyStreamMapping.PolicyFor(lbs)
-}
-
 func (o *Overrides) PoliciesStreamMapping(userID string) PolicyStreamMapping {
 	return o.getOverridesForUser(userID).PolicyStreamMapping
 }
