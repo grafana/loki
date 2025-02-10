ba052df57f4d856519dea9dffba145ac505e2362
Robert Fratto <robertfratto@gmail.com>
2025-02-10 12:11:47 -0500

    chore(dataobj): pool instances of dataobj encoders (#16166)

--- pkg/dataobj/builder.go
+++ pkg/dataobj/builder.go
@@ -110,8 +110,6 @@ type Builder struct {
 	logs    *logs.Logs
 
 	state builderState
-
-	encoder *encoding.Encoder
 }
 
 type builderState int
@@ -160,8 +158,6 @@ func NewBuilder(cfg BuilderConfig) (*Builder, error) {
 			BufferSize:   int(cfg.BufferSize),
 			SectionSize:  int(cfg.TargetSectionSize),
 		}),
-
-		encoder: encoding.NewEncoder(nil),
 	}, nil
 }
 
@@ -290,13 +286,15 @@ func (b *Builder) buildObject(output *bytes.Buffer) error {
 
 	initialBufferSize := output.Len()
 
-	b.encoder.Reset(output)
+	enc := encoderPool.Get().(*encoding.Encoder)
+	enc.Reset(output)
+	defer encoderPool.Put(enc)
 
-	if err := b.streams.EncodeTo(b.encoder); err != nil {
+	if err := b.streams.EncodeTo(enc); err != nil {
 		return fmt.Errorf("encoding streams: %w", err)
-	} else if err := b.logs.EncodeTo(b.encoder); err != nil {
+	} else if err := b.logs.EncodeTo(enc); err != nil {
 		return fmt.Errorf("encoding logs: %w", err)
-	} else if err := b.encoder.Flush(); err != nil {
+	} else if err := enc.Flush(); err != nil {
 		return fmt.Errorf("encoding object: %w", err)
 	}
 
