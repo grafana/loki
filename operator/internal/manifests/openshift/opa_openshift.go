ee244bdb8d72e5ca4c3c804c098d9b089fd3f04b
Joao Marcal <jmarcal@redhat.com>
2025-02-20 16:20:51 +0000

    refactor: move to OPA change vs API

--- operator/internal/manifests/openshift/opa_openshift.go
+++ operator/internal/manifests/openshift/opa_openshift.go
@@ -54,7 +54,7 @@ func newOPAOpenShiftContainer(mode lokiv1.ModeType, secretVolumeName, tlsDir, mi
 	if mode != lokiv1.OpenshiftNetwork {
 		args = append(args, []string{
 			fmt.Sprintf("--opa.matcher=%s", opaDefaultLabelMatchers),
-			"--opa.matcher-op=and",
+			"--opa.viaq-to-otel-migration=true",
 		}...)
 	} else {
 		args = append(args, []string{
