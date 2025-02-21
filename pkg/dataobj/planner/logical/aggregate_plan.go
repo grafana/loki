23968687e3cec7c0ec4a48a003f13ae4765fd65a
Owen Diehl <ow.diehl@gmail.com>
2025-02-21 09:44:51 -0800

    aggregate plans & dataframe init

---
+++ pkg/dataobj/planner/logical/aggregate_plan.go
@@ -0,0 +1,56 @@
+package logical
+
+import "github.com/grafana/loki/v3/pkg/dataobj/planner/schema"
+
+// Compile-time check to ensure Aggregate implements Plan
+var (
+	_ Plan = &Aggregate{}
+)
+
+// Aggregate represents a plan node that performs aggregation operations.
+// The output schema is organized with grouping columns followed by aggregate expressions.
+// It often needs to be wrapped in a projection to achieve the desired column ordering.
+type Aggregate struct {
+	// input is the child plan node providing data to aggregate
+	input Plan
+	// groupExprs are the expressions to group by
+	groupExprs []Expr
+	// aggExprs are the aggregate expressions to compute
+	aggExprs []AggregateExpr
+}
+
+// NewAggregate creates a new Aggregate plan node.
+// The Aggregate logical plan calculates aggregates of underlying data such as
+// calculating minimum, maximum, averages, and sums of data. Aggregates are often
+// grouped by other columns (or expressions).
+// A simple example would be SELECT region, SUM(sales) FROM orders GROUP BY region.
+func NewAggregate(input Plan, groupExprs []Expr, aggExprs []AggregateExpr) *Aggregate {
+	return &Aggregate{
+		input:      input,
+		groupExprs: groupExprs,
+		aggExprs:   aggExprs,
+	}
+}
+
+// Schema returns the schema of the data produced by this aggregate.
+// The schema consists of group-by expressions followed by aggregate expressions.
+func (a *Aggregate) Schema() schema.Schema {
+	var columns []schema.ColumnSchema
+
+	// Group expressions come first
+	for _, expr := range a.groupExprs {
+		columns = append(columns, expr.ToField(a.input))
+	}
+
+	// Followed by aggregate expressions
+	for _, expr := range a.aggExprs {
+		columns = append(columns, expr.ToField(a.input))
+	}
+
+	return schema.SchemaFromColumns(columns)
+}
+
+// Children returns the child plan nodes
+func (a *Aggregate) Children() []Plan {
+	return []Plan{a.input}
+}
---
+++ pkg/dataobj/planner/logical/dataframe.go
@@ -0,0 +1,46 @@
+package logical
+
+import (
+	"github.com/grafana/loki/v3/pkg/dataobj/planner/schema"
+)
+
+// DataFrame provides a fluent interface for building logical query plans
+type DataFrame struct {
+	plan Plan
+}
+
+// NewDataFrame creates a new DataFrame from a logical plan
+func NewDataFrame(plan Plan) *DataFrame {
+	return &DataFrame{plan: plan}
+}
+
+// Project applies a projection to the DataFrame
+func (df *DataFrame) Project(exprs []Expr) *DataFrame {
+	return &DataFrame{
+		plan: NewProjection(df.plan, exprs),
+	}
+}
+
+// Filter applies a filter to the DataFrame
+func (df *DataFrame) Filter(expr Expr) *DataFrame {
+	return &DataFrame{
+		plan: NewFilter(df.plan, expr),
+	}
+}
+
+// Aggregate applies grouping and aggregation to the DataFrame
+func (df *DataFrame) Aggregate(groupBy []Expr, aggExprs []AggregateExpr) *DataFrame {
+	return &DataFrame{
+		plan: NewAggregate(df.plan, groupBy, aggExprs),
+	}
+}
+
+// Schema returns the schema of the data that will be produced by this DataFrame
+func (df *DataFrame) Schema() schema.Schema {
+	return df.plan.Schema()
+}
+
+// LogicalPlan returns the underlying logical plan
+func (df *DataFrame) LogicalPlan() Plan {
+	return df.plan
+}
