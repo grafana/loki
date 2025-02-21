b419d813def9e96af8a9909175b72517cdb6d007
Owen Diehl <ow.diehl@gmail.com>
2025-02-21 11:03:09 -0800

    format pkg

---
+++ pkg/dataobj/planner/logical/format/format.go
@@ -0,0 +1,67 @@
+package format
+
+import "strings"
+
+// Format represents something that can describe itself as a formatted node
+// and its children in a tree
+type Format interface {
+	Format(f Formatter)
+}
+
+// Formatter controls how nodes are formatted and maintains the format tree
+type Formatter interface {
+	// WriteNode starts a new node in the format tree and returns a new Formatter
+	// targeting the newly created node.
+	// This allows for adding children to the newly created node later
+	WriteNode(node Node) Formatter
+	// Format renders the complete format tree
+	Format() string
+}
+
+// An abstract formatted node
+// in the tree. All of these are formatted with the same indent, but
+// we allow for different _types_ of entities, e.g. Singletons, tuples, etc
+type Node struct {
+	// e.g. "SELECT"
+	Singletons []string
+	// e.g. "GROUP BY (Foo, Bar)",
+	// "foo=bar"
+	Tuples []ContentTuple
+}
+
+type Content interface {
+	Content() string
+}
+
+type ContentTuple struct {
+	Key   string
+	Value Content
+}
+
+func (t ContentTuple) Content() string {
+	var sb strings.Builder
+	sb.WriteString(t.Key)
+	sb.WriteString("=(")
+	sb.WriteString(t.Value.Content())
+	sb.WriteString(")")
+	return sb.String()
+}
+
+type GroupContent []Content
+
+func (g GroupContent) Content() string {
+	var sb strings.Builder
+	for i, c := range g {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		sb.WriteString(c.Content())
+	}
+	return sb.String()
+}
+
+type SingleContent string
+
+func (s SingleContent) Content() string {
+	return string(s)
+}
