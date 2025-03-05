d504b079db577e7873a6835034da45a1b115941d
Trevor Whitney <trevorjwhitney@gmail.com>
2025-03-05 12:48:14 -0700

    fix(nix): build with go 1.24

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Grafana Loki";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
