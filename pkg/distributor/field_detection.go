3e799aeeecd070117f85f8457349e9c6ef95bb65
Sven Grossmann <Svennergr@gmail.com>
2025-04-02 16:53:49 +0200

    fix: revert remove colons from level detection (#16999)
    (cherry picked from commit e678b615c18f52a23862d5d3ff39102989ec8374)

--- pkg/distributor/field_detection.go
+++ pkg/distributor/field_detection.go
@@ -309,21 +309,22 @@ func isJSON(line string) bool {
 }
 
 func detectLevelFromLogLine(log string) string {
-	if strings.Contains(log, "info") || strings.Contains(log, "INFO") {
+	if strings.Contains(log, "info:") || strings.Contains(log, "INFO:") ||
+		strings.Contains(log, "info") || strings.Contains(log, "INFO") {
 		return constants.LogLevelInfo
 	}
-	if strings.Contains(log, "err") || strings.Contains(log, "ERR") ||
+	if strings.Contains(log, "err:") || strings.Contains(log, "ERR:") ||
 		strings.Contains(log, "error") || strings.Contains(log, "ERROR") {
 		return constants.LogLevelError
 	}
-	if strings.Contains(log, "warn") || strings.Contains(log, "WARN") ||
+	if strings.Contains(log, "warn:") || strings.Contains(log, "WARN:") ||
 		strings.Contains(log, "warning") || strings.Contains(log, "WARNING") {
 		return constants.LogLevelWarn
 	}
-	if strings.Contains(log, "CRITICAL") || strings.Contains(log, "critical") {
+	if strings.Contains(log, "CRITICAL:") || strings.Contains(log, "critical:") {
 		return constants.LogLevelCritical
 	}
-	if strings.Contains(log, "debug") || strings.Contains(log, "DEBUG") {
+	if strings.Contains(log, "debug:") || strings.Contains(log, "DEBUG:") {
 		return constants.LogLevelDebug
 	}
 	return constants.LogLevelUnknown
