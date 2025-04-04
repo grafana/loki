ba61aa88dea99e3371ba0c6ec2a2b3cd48219bcb
Christian Haudum <christian.haudum@gmail.com>
2025-04-04 08:03:33 +0200

    chore(engine): Integrate metastore into physical query planning (#17016)
    This PR integrates the data object lookup using the metastore into the query planning.
    
    To achieve that, I extended the `Metastore` interface with an additional function that not only returns the data object paths, but also the stream IDs of streams that match the given label matchers.
    
    ```go
    type Metastore interface {
    	...
    
    	// StreamsIDs returns object store paths and stream IDs for all matching objects for the given matchers between [start,end]
    	StreamIDs(ctx context.Context, start, end time.Time, matchers ...*labels.Matcher) ([]string, [][]int64, error)
    
    	...
    }
    ```
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/planner/logical/node_literal.go
+++ pkg/engine/planner/logical/node_literal.go
@@ -1,8 +1,6 @@
 package logical
 
 import (
-	"fmt"
-
 	"github.com/grafana/loki/v3/pkg/engine/internal/types"
 	"github.com/grafana/loki/v3/pkg/engine/planner/schema"
 )
@@ -42,47 +40,6 @@ func (l Literal) String() string {
 // Value returns lit's value as untyped interface{}.
 func (l Literal) Value() any {
 	return l.val.Value
-}
-
-// IsNull returns true if lit is a [ValueTypeNull] value.
-func (l Literal) IsNull() bool {
-	return l.Kind() == types.ValueTypeNull
-}
-
-// Int64 returns lit's value as an int64. It panics if lit is not a
-// [ValueTypeFloat].
-func (l Literal) Float() float64 {
-	if expect, actual := types.ValueTypeFloat, l.Kind(); expect != actual {
-		panic(fmt.Sprintf("literal type is %s, not %s", actual, expect))
-	}
-	return l.val.Value.(float64)
-}
-
-// Int returns lit's value as an int64. It panics if lit is not a
-// [ValueTypeInt].
-func (l Literal) Int() int64 {
-	if expect, actual := types.ValueTypeInt, l.Kind(); expect != actual {
-		panic(fmt.Sprintf("literal type is %s, not %s", actual, expect))
-	}
-	return l.val.Value.(int64)
-}
-
-// Timestamp returns lit's value as a uint64. It panics if lit is not a
-// [ValueTypeTimestamp].
-func (l Literal) Timestamp() uint64 {
-	if expect, actual := types.ValueTypeTimestamp, l.Kind(); expect != actual {
-		panic(fmt.Sprintf("literal type is %s, not %s", actual, expect))
-	}
-	return l.val.Value.(uint64)
-}
-
-// ByteArray returns lit's value as a byte slice. It panics if lit is not a
-// [ValueTypeByteArray].
-func (l Literal) ByteArray() []byte {
-	if expect, actual := types.ValueTypeByteArray, l.Kind(); expect != actual {
-		panic(fmt.Sprintf("literal type is %s, not %s", actual, expect))
-	}
-	return l.val.Value.([]byte)
 }
 
 func (l *Literal) Schema() *schema.Schema {
