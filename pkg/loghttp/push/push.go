cd0afb1978364fd41febe9b553a519b62cb707fa
matthewhudsonedb <matthew.hudson@enterprisedb.com>
2025-04-22 16:11:49 +1000

    fix(push): fix guard clauses to prevent error spam in logs

--- pkg/loghttp/push/push.go
+++ pkg/loghttp/push/push.go
@@ -172,7 +172,7 @@ func ParseRequest(
 		for retentionPeriod, size := range retentionToSizeMapping {
 			retentionHours := RetentionPeriodToString(retentionPeriod)
 			// Add guard clause to prevent negative values from being passed to Prometheus counters
-			if size > 0 {
+			if size >= 0 {
 				bytesIngested.WithLabelValues(userID, retentionHours, isAggregatedMetric, policyName).Add(float64(size))
 				bytesReceivedStats.Inc(size)
 			} else {
@@ -193,7 +193,7 @@ func ParseRequest(
 			retentionHours := RetentionPeriodToString(retentionPeriod)
 
 			// Add guard clause to prevent negative values from being passed to Prometheus counters
-			if size > 0 {
+			if size >= 0 {
 				structuredMetadataBytesIngested.WithLabelValues(userID, retentionHours, isAggregatedMetric, policyName).Add(float64(size))
 				bytesIngested.WithLabelValues(userID, retentionHours, isAggregatedMetric, policyName).Add(float64(size))
 				bytesReceivedStats.Inc(size)
