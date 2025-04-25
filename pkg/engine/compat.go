6b4430afd0c98ab21024a9fa4b9a50139bfce57c
Christian Haudum <christian.haudum@gmail.com>
2025-04-25 12:09:37 +0200

    chore(engine): Collect pipeline records into Loki result (#17447)
    This PR adds the missing piece between existing HTTP handler and the new execution engine, which converts the Apache Arrow records from the vectorized, columnar, pull-based iterator pipeline into the existing (row-based) Loki response format.
    
    Since the engine only supports logs queries at the moment, the transformation logic does not take possible metric query results into account.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

---
+++ pkg/engine/compat.go
@@ -0,0 +1,54 @@
+package engine
+
+import (
+	"github.com/prometheus/prometheus/model/labels"
+
+	"github.com/grafana/loki/pkg/push"
+	"github.com/grafana/loki/v3/pkg/logproto"
+	"github.com/grafana/loki/v3/pkg/logqlmodel"
+	"github.com/grafana/loki/v3/pkg/logqlmodel/stats"
+)
+
+func newResultBuilder() *resultBuilder {
+	return &resultBuilder{
+		streams: make(map[string]int),
+	}
+}
+
+type resultBuilder struct {
+	streams map[string]int
+	data    logqlmodel.Streams
+	stats   stats.Result
+	count   int
+}
+
+func (b *resultBuilder) add(lbs labels.Labels, entry logproto.Entry) {
+	key := lbs.String()
+	idx, ok := b.streams[key]
+	if !ok {
+		idx = len(b.data)
+		b.streams[key] = idx
+		b.data = append(b.data, push.Stream{Labels: key})
+	}
+	b.data[idx].Entries = append(b.data[idx].Entries, entry)
+	b.count++
+}
+
+func (b *resultBuilder) setStats(s stats.Result) {
+	b.stats = s
+}
+
+func (b *resultBuilder) build() logqlmodel.Result {
+	return logqlmodel.Result{
+		Data:       b.data,
+		Statistics: b.stats,
+	}
+}
+
+func (b *resultBuilder) empty() logqlmodel.Result {
+	return logqlmodel.Result{}
+}
+
+func (b *resultBuilder) len() int {
+	return b.count
+}
