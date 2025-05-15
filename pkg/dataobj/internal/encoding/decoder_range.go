897df11e6309a8007e7a73fc7c34f5a723fcee2c
Robert Fratto <robertfratto@gmail.com>
2025-05-15 12:19:46 -0400

    chore(dataobj): add low-level SectionReader to Decoder
    For decoding to be split out into section packages, the Decoder must
    expose a way for packages to read their own sections.

--- pkg/dataobj/internal/encoding/decoder_range.go
+++ pkg/dataobj/internal/encoding/decoder_range.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"math"
 
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/dataset"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/filemd"
@@ -87,12 +88,73 @@ func (rd *rangeDecoder) tailer(ctx context.Context) (tailer, error) {
 	}, nil
 }
 
+func (rd *rangeDecoder) SectionReader(metadata *filemd.Metadata, section *filemd.SectionInfo) SectionReader {
+	return &rangeSectionReader{rr: rd.r, md: metadata, sec: section}
+}
+
 func (rd *rangeDecoder) StreamsDecoder(metadata *filemd.Metadata, section *filemd.SectionInfo) StreamsDecoder {
 	return &rangeStreamsDecoder{rr: rd.r, md: metadata, sec: section}
 }
 
 func (rd *rangeDecoder) LogsDecoder(metadata *filemd.Metadata, section *filemd.SectionInfo) LogsDecoder {
 	return &rangeLogsDecoder{rr: rd.r, md: metadata, sec: section}
+}
+
+type rangeSectionReader struct {
+	rr  rangeReader // Reader for absolute ranges within the file.
+	md  *filemd.Metadata
+	sec *filemd.SectionInfo
+}
+
+func (rd *rangeSectionReader) Type() (SectionType, error) {
+	return GetSectionType(rd.md, rd.sec)
+}
+
+func (rd *rangeSectionReader) DataRange(ctx context.Context, offset, length int64) (io.ReadCloser, error) {
+	if offset < 0 || length < 0 {
+		return nil, fmt.Errorf("parameters must not be negative: offset=%d length=%d", offset, length)
+	}
+
+	// In newer versions of data objects, the offset to read is relative to the
+	// beginning of the section. In older versions, it's an absolute offset.
+	//
+	// We default to the older interpretation and adjust to the correct relative
+	// offset if a layout is provided.
+	var absoluteOffset = offset
+
+	if layout := rd.sec.Layout; layout != nil {
+		if layout.Data == nil {
+			return nil, fmt.Errorf("section has no data")
+		} else if layout.Data.Offset > math.MaxInt64 {
+			return nil, fmt.Errorf("section data offset is too large")
+		} else if layout.Data.Length > math.MaxInt64 {
+			return nil, fmt.Errorf("section data length is too large")
+		}
+
+		// Validate bounds within the range of the section.
+		var (
+			start = int64(offset)
+			end   = int64(offset) + int64(length) - 1
+		)
+		if start > int64(layout.Data.Length) || end > int64(layout.Data.Length) {
+			return nil, fmt.Errorf("section data is invalid: start=%d end=%d length=%d", start, end, layout.Data.Length)
+		}
+
+		absoluteOffset = int64(layout.Data.Offset) + offset
+	}
+
+	return rd.rr.ReadRange(ctx, absoluteOffset, length)
+}
+
+func (rd *rangeSectionReader) Metadata(ctx context.Context) (io.ReadCloser, error) {
+	metadataRegion, err := findMetadataRegion(rd.sec)
+	if err != nil {
+		return nil, err
+	} else if metadataRegion == nil {
+		return nil, fmt.Errorf("section is missing metadata")
+	}
+
+	return rd.rr.ReadRange(ctx, int64(metadataRegion.Offset), int64(metadataRegion.Length))
 }
 
 type rangeStreamsDecoder struct {
