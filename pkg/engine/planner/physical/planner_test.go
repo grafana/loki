285400d0a85e18ef4ab396b9cabc6e842b6fc148
Christian Haudum <christian.haudum@gmail.com>
2025-05-20 12:29:42 +0200

    Discard data objects that do not belong to the shard
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/planner/physical/planner_test.go
+++ pkg/engine/planner/physical/planner_test.go
@@ -1,6 +1,7 @@
 package physical
 
 import (
+	"sort"
 	"testing"
 	"time"
 
@@ -15,17 +16,116 @@ type catalog struct {
 }
 
 // ResolveDataObj implements Catalog.
-func (t *catalog) ResolveDataObj(Expression) ([]DataObjLocation, [][]int64, error) {
-	objects := make([]DataObjLocation, 0, len(t.streamsByObject))
-	streams := make([][]int64, 0, len(t.streamsByObject))
-	for o, s := range t.streamsByObject {
+func (c *catalog) ResolveDataObj(Expression) ([]DataObjLocation, [][]int64, error) {
+	objects := make([]DataObjLocation, 0, len(c.streamsByObject))
+	streams := make([][]int64, 0, len(c.streamsByObject))
+	for o, s := range c.streamsByObject {
 		objects = append(objects, DataObjLocation(o))
 		streams = append(streams, s)
 	}
+
+	// The function needs to return objects and their streamIDs in predictable order
+	sort.Slice(streams, func(i, j int) bool {
+		return objects[i] < objects[j]
+	})
+	sort.Slice(objects, func(i, j int) bool {
+		return objects[i] < objects[j]
+	})
+
 	return objects, streams, nil
 }
 
 var _ Catalog = (*catalog)(nil)
+
+func locations(t *testing.T, nodes []Node) []string {
+	res := make([]string, 0, len(nodes))
+	for _, n := range nodes {
+		obj, ok := n.(*DataObjScan)
+		if !ok {
+			t.Fatalf("failed to cast Node to DataObjScan, got %T", n)
+		}
+		res = append(res, string(obj.Location))
+	}
+	return res
+}
+
+func TestPlanner_ConvertMaketable(t *testing.T) {
+	catalog := &catalog{
+		streamsByObject: map[string][]int64{
+			"obj1": {1, 2},
+			"obj2": {3, 4},
+			"obj3": {5, 1},
+			"obj4": {2, 3},
+			"obj5": {4, 5},
+		},
+	}
+	planner := NewPlanner(catalog)
+
+	streamSelector := &logical.BinOp{
+		Left:  logical.NewColumnRef("app", types.ColumnTypeLabel),
+		Right: logical.NewLiteral("users"),
+		Op:    types.BinaryOpEq,
+	}
+
+	t.Run("0_of_1 - no shard", func(t *testing.T) {
+		relation := &logical.MakeTable{
+			Selector: streamSelector,
+			Shard:    logical.NewShardRef(0, 1),
+		}
+		planner.reset()
+		nodes, err := planner.processMakeTable(relation)
+		require.NoError(t, err)
+		require.Equal(t, len(catalog.streamsByObject), len(nodes)) // all data objects are part of the shard
+	})
+
+	t.Run("0_of_2 - shard one of two", func(t *testing.T) {
+		relation := &logical.MakeTable{
+			Selector: streamSelector,
+			Shard:    logical.NewShardRef(0, 2),
+		}
+		planner.reset()
+		nodes, err := planner.processMakeTable(relation)
+		require.NoError(t, err)
+		require.Equal(t, 3, len(nodes))
+		require.Equal(t, []string{"obj1", "obj3", "obj5"}, locations(t, nodes))
+	})
+
+	t.Run("1_of_2 - shard two of two", func(t *testing.T) {
+		relation := &logical.MakeTable{
+			Selector: streamSelector,
+			Shard:    logical.NewShardRef(1, 2),
+		}
+		planner.reset()
+		nodes, err := planner.processMakeTable(relation)
+		require.NoError(t, err)
+		require.Equal(t, 2, len(nodes))
+		require.Equal(t, []string{"obj2", "obj4"}, locations(t, nodes))
+	})
+
+	t.Run("0_of_4 - shard one of four", func(t *testing.T) {
+		relation := &logical.MakeTable{
+			Selector: streamSelector,
+			Shard:    logical.NewShardRef(0, 4),
+		}
+		planner.reset()
+		nodes, err := planner.processMakeTable(relation)
+		require.NoError(t, err)
+		require.Equal(t, 2, len(nodes))
+		require.Equal(t, []string{"obj1", "obj5"}, locations(t, nodes))
+	})
+
+	t.Run("1_of_4 - shard two of four", func(t *testing.T) {
+		relation := &logical.MakeTable{
+			Selector: streamSelector,
+			Shard:    logical.NewShardRef(1, 4),
+		}
+		planner.reset()
+		nodes, err := planner.processMakeTable(relation)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(nodes))
+		require.Equal(t, []string{"obj2"}, locations(t, nodes))
+	})
+}
 
 func TestPlanner_Convert(t *testing.T) {
 	// Build a simple query plan:
@@ -37,6 +137,7 @@ func TestPlanner_Convert(t *testing.T) {
 				Right: logical.NewLiteral("users"),
 				Op:    types.BinaryOpEq,
 			},
+			Shard: logical.NewShardRef(0, 1),
 		},
 	).Sort(
 		*logical.NewColumnRef("timestamp", types.ColumnTypeBuiltin),
