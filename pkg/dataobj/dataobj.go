d5aeb58cbbb53e2c3d00875ac1d302e341f09aa4
Robert Fratto <robertfratto@gmail.com>
2025-05-20 10:14:10 -0400

    chore(dataobj): add package comment explaining dataobjs and sections

--- pkg/dataobj/dataobj.go
+++ pkg/dataobj/dataobj.go
@@ -1,4 +1,76 @@
 // Package dataobj holds utilities for working with data objects.
+//
+// Data objects are a container format for storing data intended to be
+// retrieved from object storage. Each data object is composed of one or more
+// "sections," each of which contains a specific type of data, such as logs
+// stored in a columnar format.
+//
+// Sections are further split into two "regions": the section data and the
+// section metadata. Section metadata is intended to be a lightweight payload
+// per section (usually protobuf) which aids in reading smaller portions of the
+// data region.
+//
+// Each section has a type indicating what kind of data was encoded in that
+// section. A data object may have multiple sections of the same type.
+//
+// The dataobj package provides a low-level [Builder] interface for composing
+// sections into a dataobj, and a [SectionReader] interface for reading encoded
+// sections.
+//
+// Section implementations are stored in their own packages and provide
+// higher-level utilities for writing and reading those sections. See
+// [github.com/grafana/loki/v3/pkg/dataobj/sections/logs] for an example.
+//
+// # Creating a new section implementation
+//
+// To create a new section implementation:
+//
+//  1. Create a new package for your section.
+//
+//  2. Create a new [SectionType] for your section. Pick a combination of
+//     namespace and kind that avoids collisions with other sections that may
+//     be written to a file.
+//
+//  3. Create a "Builder" type which implmeents [SectionBuilder]. Your builder
+//     type should have methods for buffering data to be appended. Encode buffered
+//     data on a call to Flush.
+//
+//  4. Create higher-level reading APIs on top of [SectionReader], decoding the
+//     data encoded from your builder.
+//
+// Then, callers can create an instance of your Builder and store it in a data
+// object using [Builder.Append], and read it back using your higher-level
+// APIs.
+//
+// While not required, it is typical for section packages to additionally
+// implement:
+//
+//   - A package-level CheckSection function to check if a [Section] was built
+//     with your package.
+//
+//   - A package-specific Section type that wraps [Section] to use in your
+//     reading APIs.
+//
+//   - A function which returns the estimated size of something to be appended
+//     to your builder, so callers can flush the section once it gets big enough.
+//
+//   - A method on your builder to report the estimated size of the section, both
+//     before and after compression.
+//
+// ## Encoding and decoding
+//
+// There are no requirements on how to encode or decode a section, but there
+// are recommendations:
+//
+//   - Use protobuf for representing the metadata of the section.
+//
+//   - Write section data so that it can be read in smaller units. For example,
+//     columnar data is split into pages, each of which can be read
+//     independently.
+//
+// [SectionReader]s cannot access data outside of their section. Calling
+// DataRange with an offset of 0 reads data from the beginning of that
+// section's data region, not from the start of the dataobj.
 package dataobj
 
 import (
