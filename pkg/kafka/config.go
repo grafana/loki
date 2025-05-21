f05828c9c210e6083df503f090f535a406e761e6
Jackson Coelho <fcjack@gmail.com>
2025-05-21 11:34:57 +0200

    chore: parallel kafka consumer (#17262)
    Signed-off-by: Jackson Coelho <fcjack@gmail.com>
    Co-authored-by: George Robinson <george.robinson@grafana.com>

--- pkg/kafka/config.go
+++ pkg/kafka/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"runtime"
 	"time"
 
 	"github.com/grafana/dskit/flagext"
@@ -56,6 +57,7 @@ type Config struct {
 	ProducerMaxBufferedBytes   int64 `yaml:"producer_max_buffered_bytes"`
 
 	MaxConsumerLagAtStartup time.Duration `yaml:"max_consumer_lag_at_startup"`
+	MaxConsumerWorkers      int           `yaml:"max_consumer_workers"`
 
 	EnableKafkaHistograms bool `yaml:"enable_kafka_histograms"`
 }
@@ -89,6 +91,12 @@ func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
 	f.DurationVar(&cfg.MaxConsumerLagAtStartup, prefix+".max-consumer-lag-at-startup", 15*time.Second, "The guaranteed maximum lag before a consumer is considered to have caught up reading from a partition at startup, becomes ACTIVE in the hash ring and passes the readiness check. "+consumerLagUsage)
 
 	f.BoolVar(&cfg.EnableKafkaHistograms, prefix+".enable-kafka-histograms", false, "Enable collection of the following kafka latency histograms: read-wait, read-timing, write-wait, write-timing")
+	f.IntVar(&cfg.MaxConsumerWorkers, prefix+".max-consumer-workers", 1, "The maximum number of workers to use for processing records from Kafka.")
+
+	// If the number of workers is set to 0, use the number of available CPUs
+	if cfg.MaxConsumerWorkers == 0 {
+		cfg.MaxConsumerWorkers = runtime.GOMAXPROCS(0)
+	}
 }
 
 func (cfg *Config) Validate() error {
