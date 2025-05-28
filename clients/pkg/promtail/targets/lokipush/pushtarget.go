ef2cf0942e72839e82bccd6649f69e34ba327297
Edward Welch <edward.welch@grafana.com>
2025-05-28 12:50:53 +0000

    add ability to filter logging of push request streams by a list of IPs

--- clients/pkg/promtail/targets/lokipush/pushtarget.go
+++ clients/pkg/promtail/targets/lokipush/pushtarget.go
@@ -116,7 +116,7 @@ func (t *PushTarget) run() error {
 func (t *PushTarget) handleLoki(w http.ResponseWriter, r *http.Request) {
 	logger := util_log.WithContext(r.Context(), util_log.Logger)
 	userID, _ := tenant.TenantID(r.Context())
-	req, err := push.ParseRequest(logger, userID, t.config.MaxSendMsgSize, r, push.EmptyLimits{}, push.ParseLokiRequest, nil, nil, false)
+	req, err := push.ParseRequest(logger, userID, t.config.MaxSendMsgSize, r, push.EmptyLimits{}, push.ParseLokiRequest, nil, nil, false, "")
 	if err != nil {
 		level.Warn(t.logger).Log("msg", "failed to parse incoming push request", "err", err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
