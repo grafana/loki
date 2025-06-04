d27d46d373102a96c2647a98cd991a3f081df16b
Periklis Tsirakidis <periklis@redhat.com>
2025-06-04 20:12:58 +0200

    chore(limits): Add partition assignment metrics (#17975)

--- pkg/limits/frontend/ring.go
+++ pkg/limits/frontend/ring.go
@@ -41,8 +41,9 @@ type ringGatherer struct {
 	zoneCmp                 func(a, b string) int
 
 	// Metrics.
-	streams       prometheus.Counter
-	streamsFailed prometheus.Counter
+	streams                prometheus.Counter
+	streamsFailed          prometheus.Counter
+	missingPartitionsTotal *prometheus.CounterVec
 }
 
 // newRingGatherer returns a new ringGatherer.
@@ -72,6 +73,13 @@ func newRingGatherer(
 				Name: "loki_ingest_limits_frontend_streams_failed_total",
 				Help: "The total number of received streams that could not be checked.",
 			},
+		),
+		missingPartitionsTotal: promauto.With(reg).NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "loki_ingest_limits_frontend_missing_partitions_total",
+				Help: "The total number of times an instance was missing for a requested partition.",
+			},
+			[]string{"zone"},
 		),
 	}
 }
@@ -146,7 +154,7 @@ func (g *ringGatherer) doExceedsLimitsRPCs(ctx context.Context, tenant string, s
 		partition := int32(stream.StreamHash % uint64(g.numPartitions))
 		addr, ok := partitions[partition]
 		if !ok {
-			level.Warn(g.logger).Log("msg", "no instance found for partition", "partition", partition, "zone", zone)
+			g.missingPartitionsTotal.WithLabelValues(zone).Inc()
 			continue
 		}
 		instancesForStreams[addr] = append(instancesForStreams[addr], stream)
