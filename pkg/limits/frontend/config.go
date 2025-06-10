c33cd3dd4e536474788ed54198a6dfdc9fdc545c
George Robinson <george.robinson@grafana.com>
2025-06-10 18:39:24 +0100

    chore: remove unused recheck period (#17972)

--- pkg/limits/frontend/config.go
+++ pkg/limits/frontend/config.go
@@ -15,7 +15,6 @@ import (
 type Config struct {
 	ClientConfig               limits_client.Config  `yaml:"client_config"`
 	LifecyclerConfig           ring.LifecyclerConfig `yaml:"lifecycler,omitempty"`
-	RecheckPeriod              time.Duration         `yaml:"recheck_period"`
 	NumPartitions              int                   `yaml:"num_partitions"`
 	AssignedPartitionsCacheTTL time.Duration         `yaml:"assigned_partitions_cache_ttl"`
 }
@@ -23,7 +22,6 @@ type Config struct {
 func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	cfg.ClientConfig.RegisterFlagsWithPrefix("ingest-limits-frontend", f)
 	cfg.LifecyclerConfig.RegisterFlagsWithPrefix("ingest-limits-frontend.", f, util_log.Logger)
-	f.DurationVar(&cfg.RecheckPeriod, "ingest-limits-frontend.recheck-period", 10*time.Second, "The period to recheck per tenant ingestion rate limit configuration.")
 	f.IntVar(&cfg.NumPartitions, "ingest-limits-frontend.num-partitions", 64, "The number of partitions to use for the ring.")
 	f.DurationVar(&cfg.AssignedPartitionsCacheTTL, "ingest-limits-frontend.assigned-partitions-cache-ttl", 1*time.Minute, "The TTL for the assigned partitions cache. 0 disables the cache.")
 }
