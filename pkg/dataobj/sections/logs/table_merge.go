c22f351fddff7c863c95108569ad2109d35273c0
Christian Haudum <christian.haudum@gmail.com>
2025-07-02 17:40:09 +0200

    chore(dataobj): Sort logs section columns by timestamp descending
    When building data objects, rows in logs sections are sorted by
    [timestamp DESC, streamID ASC] rather than [streamID ASC, timestamp
    ASC].
    
    This would allow for reading less data in BACKWARD log queries, since it
    would not require to read all rows of a section into the sort heap any
    more, but rows can be read incrementally when the parent sort/merge
    requires more data.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/dataobj/sections/logs/table_merge.go
+++ pkg/dataobj/sections/logs/table_merge.go
@@ -209,8 +209,8 @@ func compareRows(a, b dataset.Row) int {
 		bTimestamp = b.Values[1].Int64()
 	)
 
-	if res := cmp.Compare(aStreamID, bStreamID); res != 0 {
+	if res := cmp.Compare(bTimestamp, aTimestamp); res != 0 {
 		return res
 	}
-	return cmp.Compare(aTimestamp, bTimestamp)
+	return cmp.Compare(aStreamID, bStreamID)
 }
