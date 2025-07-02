ecb8c55ddd9d9cad8949f6329b0decb124dd8376
Ben Clive <ben.clive@grafana.com>
2025-07-02 16:28:27 +0100

    WIP

--- pkg/engine/planner/physical/catalog.go
+++ pkg/engine/planner/physical/catalog.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"sort"
 	"time"
 
 	"github.com/prometheus/prometheus/model/labels"
@@ -77,12 +78,37 @@ func (c *MetastoreCatalog) ResolveDataObjWithShard(selector Expression, shard Sh
 		return nil, nil, nil, fmt.Errorf("failed to convert selector expression into matchers: %w", err)
 	}
 
+<<<<<<< HEAD:pkg/engine/planner/physical/catalog.go
 	paths, streamIDs, numSections, err := c.metastore.StreamIDs(c.ctx, from, through, matchers...)
+=======
+	sections, err := c.metastore.StreamIDsBySections(c.ctx, c.from, c.through, matchers...)
+>>>>>>> 98d7d9dc9d (WIP):pkg/engine/planner/physical/context.go
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("failed to resolve data object locations: %w", err)
 	}
+	fmt.Printf("sections=%d\n", len(sections))
 
-	return filterForShard(shard, paths, streamIDs, numSections)
+	return filterDescriptorsForShard(shard, sections)
+
+	/*
+		 	paths, streamIDs, numSections, err := c.metastore.StreamIDs(c.ctx, c.from, c.through, matchers...)
+			if err != nil {
+				return nil, nil, nil, fmt.Errorf("failed to resolve data object locations: %w", err)
+			}
+
+			locations, streams, objectSections, err := filterForShard(shard, paths, streamIDs, numSections)
+			if err != nil {
+				return nil, nil, nil, fmt.Errorf("failed to filter for shard: %w", err)
+			}
+
+			sectionsCount := 0
+			for _, section := range objectSections {
+				sectionsCount += len(section)
+			}
+			fmt.Printf("StreamIDs: sectionsCount=%d\n", sectionsCount)
+
+			return locations, streams, objectSections, nil
+	*/
 }
 
 func filterForShard(shard ShardInfo, paths []string, streamIDs [][]int64, numSections []int) ([]DataObjLocation, [][]int64, [][]int, error) {
@@ -105,12 +131,6 @@ func filterForShard(shard ShardInfo, paths []string, streamIDs [][]int64, numSec
 			locations = append(locations, DataObjLocation(paths[i]))
 			streams = append(streams, streamIDs[i])
 			sections = append(sections, sec)
-
-			// {
-			//   location: "path/abcd/object"
-			//   streamIDs: [1, 2, 3]
-			//   sections: [1, 3, 5]
-			// }
 		}
 	}
 
@@ -120,14 +140,14 @@ func filterForShard(shard ShardInfo, paths []string, streamIDs [][]int64, numSec
 // ResolveDataObj resolves DataObj locations and streams IDs based on a given
 // [Expression]. The expression is required to be a (tree of) [BinaryExpression]
 // with a [ColumnExpression] on the left and a [LiteralExpression] on the right.
-func (c *Context) ResolveSections(selector Expression, tableHint Expression) ([]metastore.DataobjSectionDescriptor, error) {
+func (c *Context) ResolveSections(selector Expression, shard ShardInfo) ([]DataObjLocation, [][]int64, [][]int, error) {
 	if c.metastore == nil {
-		return nil, errors.New("no metastore to resolve objects")
+		return nil, nil, nil, errors.New("no metastore to resolve objects")
 	}
 
 	matchers, err := expressionToMatchers(selector)
 	if err != nil {
-		return nil, fmt.Errorf("failed to convert selector expression into matchers: %w", err)
+		return nil, nil, nil, fmt.Errorf("failed to convert selector expression into matchers: %w", err)
 	}
 
 	/* 	// TODO(benclive): Handle these ambiguous column cases in a less hacky way
@@ -138,10 +158,68 @@ func (c *Context) ResolveSections(selector Expression, tableHint Expression) ([]
 
 	sectionDescriptors, err := c.metastore.StreamIDsBySections(c.ctx, c.from, c.through, matchers...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to resolve data object locations: %w", err)
+		return nil, nil, nil, fmt.Errorf("failed to resolve data object locations: %w", err)
 	}
 
-	return sectionDescriptors, err
+	return filterDescriptorsForShard(shard, sectionDescriptors)
+}
+
+// filterDescriptorsForShard filters the section descriptors for a given shard.
+// It returns the locations, streams, and sections for the shard.
+// It loses information because it doesn't map streamIDs to sections.
+func filterDescriptorsForShard(shard ShardInfo, sectionDescriptors []metastore.DataobjSectionDescriptor) ([]DataObjLocation, [][]int64, [][]int, error) {
+	index := make(map[DataObjLocation]int)
+
+	locations := make([]DataObjLocation, 0, len(sectionDescriptors))
+	streams := make([][]int64, 0, len(sectionDescriptors))
+	sections := make([][]int, 0, len(sectionDescriptors))
+
+	for _, desc := range sectionDescriptors {
+		idx, ok := index[DataObjLocation(desc.ObjectPath)]
+		if !ok {
+			idx = len(index)
+			index[DataObjLocation(desc.ObjectPath)] = idx
+			locations = append(locations, DataObjLocation(desc.ObjectPath))
+			streams = append(streams, []int64{})
+			sections = append(sections, []int{})
+		}
+
+		if int(desc.SectionIdx)%int(shard.Of) == int(shard.Shard) {
+			streams[idx] = append(streams[idx], desc.StreamIDs...)
+			sections[idx] = append(sections[idx], int(desc.SectionIdx))
+		}
+	}
+
+	for i := range streams {
+		sort.Slice(streams[i], func(j, k int) bool { return streams[i][j] < streams[i][k] })
+		sort.Slice(sections[i], func(j, k int) bool { return sections[i][j] < sections[i][k] })
+		streams[i] = uniquelong(streams[i])
+		sections[i] = uniqueint(sections[i])
+	}
+
+	return locations, streams, sections, nil
+}
+
+func uniqueint(s []int) []int {
+	next := 0
+	for i := range s {
+		if s[i] != s[next] {
+			next++
+			s[next] = s[i]
+		}
+	}
+	return s[:next+1]
+}
+
+func uniquelong(s []int64) []int64 {
+	next := 0
+	for i := range s {
+		if s[i] != s[next] {
+			next++
+			s[next] = s[i]
+		}
+	}
+	return s[:next+1]
 }
 
 func expressionToMatchers(selector Expression) ([]*labels.Matcher, error) {
