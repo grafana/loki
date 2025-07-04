d6c65967b1afd4f52b6d93d1222a00722f50a4c0
Christian Haudum <christian.haudum@gmail.com>
2025-07-04 13:30:55 +0200

    chore: Rename some identifiers
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/executor/sortmerge.go
+++ pkg/engine/executor/sortmerge.go
@@ -11,14 +11,16 @@ import (
 	"github.com/grafana/loki/v3/pkg/engine/planner/physical"
 )
 
+type compareFunc[T comparable] func(a, b T) bool
+
 // NewSortMergePipeline returns a new pipeline that merges already sorted inputs into a single output.
 func NewSortMergePipeline(inputs []Pipeline, order physical.SortOrder, column physical.ColumnExpression, evaluator expressionEvaluator) (*KWayMerge, error) {
-	var compare func(a, b int64) bool
+	var lessFunc func(a, b int64) bool
 	switch order {
 	case physical.ASC:
-		compare = func(a, b int64) bool { return a <= b }
+		lessFunc = func(a, b int64) bool { return a <= b }
 	case physical.DESC:
-		compare = func(a, b int64) bool { return a >= b }
+		lessFunc = func(a, b int64) bool { return a >= b }
 	default:
 		return nil, fmt.Errorf("invalid sort order %v", order)
 	}
@@ -26,7 +28,7 @@ func NewSortMergePipeline(inputs []Pipeline, order physical.SortOrder, column ph
 	return &KWayMerge{
 		inputs:     inputs,
 		columnEval: evaluator.newFunc(column),
-		compare:    compare,
+		compare:    lessFunc,
 	}, nil
 }
 
@@ -42,7 +44,7 @@ type KWayMerge struct {
 	exhausted   []bool
 	offsets     []int64
 	columnEval  evalFunc
-	compare     func(a, b int64) bool
+	compare     compareFunc[int64]
 }
 
 var _ Pipeline = (*KWayMerge)(nil)
@@ -107,7 +109,7 @@ func (p *KWayMerge) read() error {
 	}
 
 	timestamps := make([]int64, 0, len(p.inputs))
-	batchIndexes := make([]int, 0, len(p.inputs))
+	inputIndexes := make([]int, 0, len(p.inputs))
 
 	for i := range len(p.inputs) {
 		// Skip exhausted inputs
@@ -149,19 +151,19 @@ func (p *KWayMerge) read() error {
 		ts := tsCol.Value(int(p.offsets[i]))
 
 		// Populate slices for sorting
-		batchIndexes = append(batchIndexes, i)
+		inputIndexes = append(inputIndexes, i)
 		timestamps = append(timestamps, int64(ts))
 	}
 
 	// Pipeline is exhausted if no more input batches are available
-	if len(batchIndexes) == 0 {
+	if len(inputIndexes) == 0 {
 		p.state = Exhausted
 		return p.state.err
 	}
 
 	// If there is only a single remaining batch, return the remaining record
-	if len(batchIndexes) == 1 {
-		j := batchIndexes[0]
+	if len(inputIndexes) == 1 {
+		j := inputIndexes[0]
 		start := p.offsets[j]
 		end := p.batches[j].NumRows()
 
@@ -177,10 +179,10 @@ func (p *KWayMerge) read() error {
 		return nil
 	}
 
-	sortIndexesByTimestamps(batchIndexes, timestamps, p.compare)
+	sortIndexesByTimestamps(inputIndexes, timestamps, p.compare)
 
 	// Return the slice of the current record
-	j := batchIndexes[0]
+	j := inputIndexes[0]
 
 	// Fetch timestamp value at current offset
 	col, err := p.columnEval(p.batches[j])
@@ -215,7 +217,7 @@ func (p *KWayMerge) read() error {
 	return nil
 }
 
-func sortIndexesByTimestamps(indexes []int, timestamps []int64, lessFn func(a, b int64) bool) {
+func sortIndexesByTimestamps(indexes []int, timestamps []int64, lessFn compareFunc[int64]) {
 	if len(indexes) != len(timestamps) {
 		panic("lengths of indexes and timestamps must match")
 	}
