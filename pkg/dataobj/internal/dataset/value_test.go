26cb50e65a7d1b7b83644da947d8631dc5cd4a3f
Robert Fratto <robertfratto@gmail.com>
2025-07-08 09:40:31 -0400

    chore(dataobj): Improve performance of dataset.Value API (#18339)
    Co-authored-by: Ashwanth <iamashwanth@gmail.com>

--- pkg/dataobj/internal/dataset/value_test.go
+++ pkg/dataobj/internal/dataset/value_test.go
@@ -9,6 +9,99 @@ import (
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/metadata/datasetmd"
 )
 
+func BenchmarkValue_Type(b *testing.B) {
+	tt := []struct {
+		name  string
+		value dataset.Value
+	}{
+		{"Null", dataset.Value{}},
+		{"Int64Value", dataset.Int64Value(-1234)},
+		{"Uint64Value", dataset.Uint64Value(1234)},
+		{"ByteArrayValue", dataset.ByteArrayValue([]byte("hello, world!"))},
+	}
+
+	for _, tc := range tt {
+		b.Run(tc.name, func(b *testing.B) {
+			for b.Loop() {
+				tc.value.Type()
+			}
+		})
+	}
+}
+
+func BenchmarkValue_Create(b *testing.B) {
+	b.Run("Null", func(b *testing.B) {
+		for b.Loop() {
+			_ = dataset.Value{}
+		}
+	})
+
+	b.Run("Int64Value", func(b *testing.B) {
+		for b.Loop() {
+			_ = dataset.Int64Value(-1234)
+		}
+	})
+
+	b.Run("Uint64Value", func(b *testing.B) {
+		for b.Loop() {
+			_ = dataset.Uint64Value(1234)
+		}
+	})
+
+	b.Run("ByteArrayValue", func(b *testing.B) {
+		for b.Loop() {
+			_ = dataset.ByteArrayValue([]byte("hello, world!"))
+		}
+	})
+}
+
+func BenchmarkValue_Int64(b *testing.B) {
+	v := dataset.Int64Value(-1234)
+	for b.Loop() {
+		v.Int64()
+	}
+}
+
+func BenchmarkValue_Uint64(b *testing.B) {
+	v := dataset.Uint64Value(1234)
+	for b.Loop() {
+		v.Uint64()
+	}
+}
+
+func BenchmarkValue_ByteArray(b *testing.B) {
+	v := dataset.ByteArrayValue([]byte("hello, world!"))
+	for b.Loop() {
+		v.ByteArray()
+	}
+}
+
+func BenchmarkCompareValues(b *testing.B) {
+	tt := []struct {
+		name string
+		a, b dataset.Value
+	}{
+		{"two nulls", dataset.Value{}, dataset.Value{}},
+		{"int64 < int64", dataset.Int64Value(-1234), dataset.Int64Value(1234)},
+		{"int64 == int64", dataset.Int64Value(1234), dataset.Int64Value(1234)},
+		{"int64 > int64", dataset.Int64Value(1234), dataset.Int64Value(-1234)},
+		{"uint64 < uint64", dataset.Uint64Value(1234), dataset.Uint64Value(5678)},
+		{"uint64 == uint64", dataset.Uint64Value(1234), dataset.Uint64Value(1234)},
+		{"uint64 > uint64", dataset.Uint64Value(5678), dataset.Uint64Value(1234)},
+		{"bytearray < bytearray", dataset.ByteArrayValue([]byte("abc")), dataset.ByteArrayValue([]byte("def"))},
+		{"bytearray == bytearray", dataset.ByteArrayValue([]byte("abc")), dataset.ByteArrayValue([]byte("abc"))},
+		{"bytearray > bytearray", dataset.ByteArrayValue([]byte("def")), dataset.ByteArrayValue([]byte("abc"))},
+	}
+
+	for _, tc := range tt {
+		b.Run(tc.name, func(b *testing.B) {
+			for b.Loop() {
+				dataset.CompareValues(&tc.a, &tc.b)
+			}
+		})
+	}
+}
+
 func TestValue_MarshalBinary(t *testing.T) {
 	t.Run("Null", func(t *testing.T) {
 		var expect dataset.Value
