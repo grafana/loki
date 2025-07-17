e79a29e692d7121a3914db5af8c614216151c42f
Robert Fratto <robertfratto@gmail.com>
2025-07-17 12:06:24 -0400

    chore(logqlanalyzer): use new labels.Labels API

--- pkg/logqlanalyzer/analyzer.go
+++ pkg/logqlanalyzer/analyzer.go
@@ -75,9 +75,9 @@ func mapAllToLineResult(originLine string, analysisRecords []StageAnalysisRecord
 	return LineResult{originLine, stageRecords}
 }
 
-func mapAllToLabelsResponse(labels labels.Labels) []Label {
-	result := make([]Label, 0, len(labels))
-	for _, label := range labels {
+func mapAllToLabelsResponse(lbls []labels.Label) []Label {
+	result := make([]Label, 0, len(lbls))
+	for _, label := range lbls {
 		result = append(result, Label{Name: label.Name, Value: label.Value})
 	}
 	return result
@@ -152,8 +152,8 @@ func (s StageAnalysisRecorder) RequiredLabelNames() []string {
 type StageAnalysisRecord struct {
 	Processed    bool
 	LineBefore   string
-	LabelsBefore labels.Labels
+	LabelsBefore []labels.Label
 	LineAfter    string
-	LabelsAfter  labels.Labels
+	LabelsAfter  []labels.Label
 	FilteredOut  bool
 }
