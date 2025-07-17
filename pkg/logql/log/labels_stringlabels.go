1a7f994da012ada2eb3031c16e2ff687e853864a
Robert Fratto <robertfratto@gmail.com>
2025-07-17 10:06:29 -0400

    chore(logql): Use stringlabels by default
    Prometheus swapped the default build tags:
    
    * Using stringlabels is now the default
    * Using slicelabels must now be explicitly specified
    
    This change now reflects the go:build constraints in upstream.

--- pkg/logql/log/labels_stringlabels.go
+++ pkg/logql/log/labels_stringlabels.go
@@ -1,4 +1,4 @@
-//go:build stringlabels
+//go:build !slicelabels && !dedupelabels
 
 package log
 
@@ -18,26 +18,4 @@ func (h *hasher) Hash(lbs labels.Labels) uint64 {
 	// The results from Hash(l) and HashWithoutLabels(l, []string{}) are different with stringlabels, so using Hash
 	// here also simplifies our tests.
 	return labels.StableHash(lbs)
-}
-
-// BufferedLabelsBuilder is a simple builder that uses a label buffer passed in.
-// It is used to avoid allocations when building labels.
-type BufferedLabelsBuilder struct {
-	builder *labels.Builder
-}
-
-func NewBufferedLabelsBuilder(l labels.Labels) *BufferedLabelsBuilder {
-	return &BufferedLabelsBuilder{builder: labels.NewBuilder(l)}
-}
-
-func (b *BufferedLabelsBuilder) Reset() {
-	b.builder.Reset(labels.EmptyLabels())
-}
-
-func (b *BufferedLabelsBuilder) Add(label labels.Label) {
-	b.builder.Set(label.Name, label.Value)
-}
-
-func (b *BufferedLabelsBuilder) Labels() labels.Labels {
-	return b.builder.Labels()
 }
