fdb62df2c0f0f862bed2ec1ed0fe96058639ca71
Ben Clive <ben.clive@grafana.com>
2025-07-22 17:09:27 +0100

    Use correct TimeRange predicate for indexpointers

--- pkg/dataobj/sections/indexpointers/row_reader_test.go
+++ pkg/dataobj/sections/indexpointers/row_reader_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
+	"os"
 	"testing"
 	"time"
 
@@ -17,6 +19,37 @@ var indexPointerTestData = []IndexPointer{
 	{Path: "/path/to/index/object/1", StartTs: unixTime(10), EndTs: unixTime(20)},
 	{Path: "/path/to/index/object/2", StartTs: unixTime(12), EndTs: unixTime(17)},
 	{Path: "/path/to/index/object/3", StartTs: unixTime(13), EndTs: unixTime(18)},
+}
+
+func TestDumpRowReader(t *testing.T) {
+	path := "/Users/benclive/Downloads/2025-07-18T00_00_00Z.store"
+	fileBytes, err := os.ReadFile(path)
+	require.NoError(t, err)
+
+	obj, err := dataobj.FromReaderAt(bytes.NewReader(fileBytes), int64(len(fileBytes)))
+	require.NoError(t, err)
+
+	for _, section := range obj.Sections().Filter(CheckSection) {
+		sec, err := Open(t.Context(), section)
+		require.NoError(t, err)
+
+		r := NewRowReader(sec)
+		r.SetPredicate(TimeRangeRowPredicate{
+			Start: time.Date(2025, 7, 18, 1, 30, 0, 0, time.UTC),
+			End:   time.Date(2025, 7, 18, 1, 59, 59, 0, time.UTC),
+		})
+		buf := make([]IndexPointer, 128)
+		for {
+			n, err := r.Read(t.Context(), buf)
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			require.NoError(t, err)
+			for _, pointer := range buf[:n] {
+				fmt.Printf("Path=%s StartTs=%s EndTs=%s\n", pointer.Path, pointer.StartTs.UTC().Format(time.RFC3339), pointer.EndTs.UTC().Format(time.RFC3339))
+			}
+		}
+	}
 }
 
 func unixTime(sec int64) time.Time { return time.Unix(sec, 0) }
