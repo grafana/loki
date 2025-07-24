31d9907c6168b0aac31801e22279acb99ae1151f
Christian Haudum <christian.haudum@gmail.com>
2025-07-24 08:10:42 +0200

    chore: Address code review feedback
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/metrics.go
+++ pkg/engine/metrics.go
@@ -23,24 +23,24 @@ type metrics struct {
 func newMetrics(r prometheus.Registerer) *metrics {
 	return &metrics{
 		subqueries: promauto.With(r).NewCounterVec(prometheus.CounterOpts{
-			Name: "loki_engine_v2_subqueries",
+			Name: "loki_engine_v2_subqueries_total",
 			Help: "Total number of subqueries executed with the new engine",
 		}, []string{status}),
 		logicalPlanning: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
-			Name: "loki_engine_v2_logical_planning",
-			Help: "Duration of logical planning in seconds",
+			Name: "loki_engine_v2_logical_planning_duration_seconds",
+			Help: "Duration of logical query planning in seconds",
 		}),
 		physicalPlanning: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
-			Name: "loki_engine_v2_physical_planning",
-			Help: "Duration of physical planning in seconds",
+			Name: "loki_engine_v2_physical_planning_duration_seconds",
+			Help: "Duration of physical query planning in seconds",
 			Buckets: append(
 				prometheus.DefBuckets,                    // 0.005s -> 10s
 				prometheus.LinearBuckets(15, 5.0, 10)..., // 15s -> 60s
 			),
 		}),
 		execution: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
-			Name: "loki_engine_v2_execution",
-			Help: "Duration of execution in seconds",
+			Name: "loki_engine_v2_execution_duration_seconds",
+			Help: "Duration of query execution in seconds",
 			Buckets: append(
 				prometheus.DefBuckets,                    // 0.005s -> 10s
 				prometheus.LinearBuckets(15, 5.0, 10)..., // 15s -> 60s
