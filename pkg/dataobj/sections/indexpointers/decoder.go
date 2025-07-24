da4e59f8c6c3230d7d88f0df8e4f3513bc24f7e2
Ashwanth <iamashwanth@gmail.com>
2025-07-24 18:25:36 +0530

    chore(dataobj): store sort order information in section metadata (#18499)

--- pkg/dataobj/sections/indexpointers/decoder.go
+++ pkg/dataobj/sections/indexpointers/decoder.go
@@ -23,8 +23,8 @@ type decoder struct {
 	sr dataobj.SectionReader
 }
 
-// Columns describes the set of columns in the section.
-func (rd *decoder) Columns(ctx context.Context) ([]*indexpointersmd.ColumnDesc, error) {
+// Metadata returns the metadata for the index pointers section.
+func (rd *decoder) Metadata(ctx context.Context) (*indexpointersmd.Metadata, error) {
 	rc, err := rd.sr.Metadata(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("reading pointers section metadata: %w", err)
@@ -34,11 +34,7 @@ func (rd *decoder) Columns(ctx context.Context) ([]*indexpointersmd.ColumnDesc, 
 	br := bufpool.GetReader(rc)
 	defer bufpool.PutReader(br)
 
-	md, err := decodeIndexPointersMetadata(br)
-	if err != nil {
-		return nil, err
-	}
-	return md.Columns, nil
+	return decodeIndexPointersMetadata(br)
 }
 
 // Pages retrieves the set of pages for the provided columns. The order of page
