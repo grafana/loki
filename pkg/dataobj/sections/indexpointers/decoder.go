061d1c1d22d1e224ac70583cded6a0c9102919c7
Robert Fratto <robertfratto@gmail.com>
2025-07-24 10:55:28 -0400

    chore(dataobj): make copy of pages from downloaded window (#18566)

--- pkg/dataobj/sections/indexpointers/decoder.go
+++ pkg/dataobj/sections/indexpointers/decoder.go
@@ -135,10 +135,13 @@ func (rd *decoder) ReadPages(ctx context.Context, pages []*indexpointersmd.PageD
 			if err != nil {
 				return fmt.Errorf("reading page data: %w", err)
 			}
-			data, err := readAndClose(rc, windowSize)
-			if err != nil {
+
+			buffer := bufpool.Get(int(windowSize))
+			if err := copyAndClose(buffer, rc); err != nil {
+				bufpool.Put(buffer)
 				return fmt.Errorf("read page data: %w", err)
 			}
+			data := buffer.Bytes()
 
 			for _, wp := range window {
 				// Find the slice in the data for this page.
@@ -149,8 +152,13 @@ func (rd *decoder) ReadPages(ctx context.Context, pages []*indexpointersmd.PageD
 
 				// wp.Index is the position of the page in the original pages slice;
 				// this retains the proper order of data in results.
-				results[wp.Index] = dataset.PageData(data[dataOffset : dataOffset+wp.Data.GetInfo().DataSize])
+				//
+				// We need to make a copy here of the slice since data is pooled (and
+				// we don't want to hold on to the entire window if we don't need to).
+				results[wp.Index] = dataset.PageData(bytes.Clone(data[dataOffset : dataOffset+wp.Data.GetInfo().DataSize]))
 			}
+
+			bufpool.Put(buffer)
 		}
 
 		for _, data := range results {
@@ -161,4 +169,15 @@ func (rd *decoder) ReadPages(ctx context.Context, pages []*indexpointersmd.PageD
 
 		return nil
 	})
+}
+
+// copyAndClose copies the data from rc into the destination writer w and then
+// closes rc.
+func copyAndClose(dst io.Writer, rc io.ReadCloser) error {
+	defer rc.Close()
+
+	if _, err := io.Copy(dst, rc); err != nil {
+		return fmt.Errorf("copying data: %w", err)
+	}
+	return nil
 }
