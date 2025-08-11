40f6f471dfb4becd27577c8680115c3e7142b59d
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2025-08-11 22:30:16 +0530

    lint

--- pkg/compactor/deletion/delete_request_batch.go
+++ pkg/compactor/deletion/delete_request_batch.go
@@ -1,13 +1,13 @@
 package deletion
 
 import (
-	"github.com/grafana/loki/v3/pkg/compactor/deletion/deletionproto"
 	"time"
 
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 
+	"github.com/grafana/loki/v3/pkg/compactor/deletion/deletionproto"
 	"github.com/grafana/loki/v3/pkg/compactor/retention"
 	"github.com/grafana/loki/v3/pkg/util/filter"
 	util_log "github.com/grafana/loki/v3/pkg/util/log"
