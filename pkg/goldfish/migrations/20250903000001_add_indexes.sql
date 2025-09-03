d0f505676e1739cdbc1c2cf14974a58cc5a09b7a
Trevor Whitney <trevorjwhitney@gmail.com>
2025-09-03 17:36:02 -0600

    fix: remove duplicate index

--- pkg/goldfish/migrations/20250903000001_add_indexes.sql
+++ pkg/goldfish/migrations/20250903000001_add_indexes.sql
@@ -1,18 +1,7 @@
 -- +goose Up
-
 -- Add indexes
-CREATE INDEX idx_sampled_queries_user ON sampled_queries(user);
 
--- Composite index for the JOIN operation with comparison_outcomes
-CREATE INDEX idx_sampled_queries_correlation_composite ON sampled_queries(
-    correlation_id,
-    cell_a_status_code,
-    cell_b_status_code,
-    cell_a_response_hash,
-    cell_b_response_hash
-);
-
--- Composite index for filtering queries with WHERE clauses
+-- Composite index for filtering queries with WHERE clauses (includes user column)
 CREATE INDEX idx_sampled_queries_filter_composite ON sampled_queries(
     tenant_id,
     user,
@@ -33,19 +22,10 @@ CREATE INDEX idx_sampled_queries_cell_b_engine_filter ON sampled_queries(
     cell_b_used_new_engine
 );
 
--- Index for comparison_outcomes join performance
-CREATE INDEX idx_comparison_outcomes_correlation_status ON comparison_outcomes(
-    correlation_id, 
-    comparison_status
-);
-
 -- +goose Down
 
 -- Drop all the new indexes
-DROP INDEX idx_comparison_outcomes_correlation_status ON comparison_outcomes;
 DROP INDEX idx_sampled_queries_cell_b_engine_filter ON sampled_queries;
 DROP INDEX idx_sampled_queries_cell_a_engine_filter ON sampled_queries;
 DROP INDEX idx_sampled_queries_engine_filter_composite ON sampled_queries;
 DROP INDEX idx_sampled_queries_filter_composite ON sampled_queries;
-DROP INDEX idx_sampled_queries_correlation_composite ON sampled_queries;
-DROP INDEX idx_sampled_queries_user ON sampled_queries;
