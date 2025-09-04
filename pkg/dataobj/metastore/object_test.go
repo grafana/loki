33c7786caaeb25431108dfc44bf44e88a558b9f9
Ben Clive <ben.clive@grafana.com>
2025-09-04 10:33:55 +0100

    chore: Read multi-tenant objects in query-engine

--- pkg/dataobj/metastore/object_test.go
+++ pkg/dataobj/metastore/object_test.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/grafana/loki/v3/pkg/dataobj/consumer/logsobj"
 	"github.com/grafana/loki/v3/pkg/dataobj/index/indexobj"
-	"github.com/grafana/loki/v3/pkg/dataobj/metastore/multitenancy"
+	"github.com/grafana/loki/v3/pkg/dataobj/sections/pointers"
 	"github.com/grafana/loki/v3/pkg/dataobj/sections/streams"
 	"github.com/grafana/loki/v3/pkg/dataobj/uploader"
 	"github.com/grafana/loki/v3/pkg/logproto"
@@ -272,8 +272,22 @@ func TestSectionsForStreamMatchers(t *testing.T) {
 		require.NoError(t, err)
 	}
 
+	// Add one more stream for a different tenant to ensure it is not resolved.
+	altTenant := "tenant-alt"
+	newIdx, err := builder.AppendStream(altTenant, streams.Stream{
+		ID:               1,
+		Labels:           labels.New(labels.Label{Name: "app", Value: "foo"}, labels.Label{Name: "tenant", Value: altTenant}),
+		MinTimestamp:     now.Add(-3 * time.Hour),
+		MaxTimestamp:     now.Add(-2 * time.Hour),
+		UncompressedSize: 5,
+	})
+	require.NoError(t, err)
+	err = builder.ObserveLogLine(altTenant, "test-path", 0, newIdx, 1, now.Add(-2*time.Hour), 5)
+	require.NoError(t, err)
+
+	// Build and store the object
 	timeRanges := builder.TimeRanges()
-	require.Len(t, timeRanges, 1)
+	require.Len(t, timeRanges, 2)
 
 	obj, closer, err := builder.Flush()
 	require.NoError(t, err)
@@ -288,15 +302,14 @@ func TestSectionsForStreamMatchers(t *testing.T) {
 	require.NoError(t, err)
 
 	metastoreTocWriter := NewTableOfContentsWriter(bucket, log.NewNopLogger())
-
-	err = metastoreTocWriter.WriteEntry(context.Background(), path, []multitenancy.TimeRange{
-		{
-			Tenant:  tenantID,
-			MinTime: timeRanges[0].MinTime,
-			MaxTime: timeRanges[0].MaxTime,
-		},
-	})
+	err = metastoreTocWriter.WriteEntry(context.Background(), path, timeRanges)
 	require.NoError(t, err)
+
+	// Note the tenants by section index, for later verification
+	tenantsBySectionIdx := make(map[int]string)
+	for idx, section := range obj.Sections().Filter(pointers.CheckSection) {
+		tenantsBySectionIdx[idx] = section.Tenant
+	}
 
 	mstore := NewObjectMetastore(bucket, log.NewNopLogger(), prometheus.NewPedanticRegistry())
 
@@ -345,6 +358,9 @@ func TestSectionsForStreamMatchers(t *testing.T) {
 			sections, err := mstore.Sections(ctx, now.Add(-time.Hour), now.Add(time.Hour), tt.matchers, tt.predicates)
 			require.NoError(t, err)
 			require.Len(t, sections, tt.wantCount)
+			for _, section := range sections {
+				require.Equal(t, tenantsBySectionIdx[int(section.SectionIdx)], tenantID)
+			}
 		})
 	}
 }
