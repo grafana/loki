c266ea5feeef99cb4d9e9e282811044d9ccdb27d
Robert Fratto <robertfratto@gmail.com>
2025-09-10 10:05:28 -0400

    chore(engine): print stringified logical planning duration
    Mirrors the previous commit but for logical planning
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -170,7 +170,7 @@ func (e *QueryEngine) Execute(ctx context.Context, params logql.Params) (logqlmo
 		level.Info(logger).Log(
 			"msg", "finished logical planning",
 			"plan", logicalPlan.String(),
-			"duration", durLogicalPlanning.Seconds(),
+			"duration", durLogicalPlanning.String(),
 		)
 		span.SetStatus(codes.Ok, "")
 		return logicalPlan, nil
