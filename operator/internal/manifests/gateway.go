6ff63127118b0ec714e81f1caff9af82a2f664c1
Joao Marcal <jmarcal@redhat.com>
2025-09-11 14:43:52 +0100

    chore(operator): updated dependencies

--- operator/internal/manifests/gateway.go
+++ operator/internal/manifests/gateway.go
@@ -7,8 +7,8 @@ import (
 	"regexp"
 	"strings"
 
+	"dario.cat/mergo"
 	"github.com/ViaQ/logerr/v2/kverrors"
-	"github.com/imdario/mergo"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
