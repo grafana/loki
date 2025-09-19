35816aa16f2ac694ce01bb91a2ac047832090f41
Christian Haudum <christian.haudum@gmail.com>
2025-09-19 16:08:34 +0200

    chore(engine): Eliminate unnecessary SortMerge node from physical plan (#19236)
    This PR avoid creating the SortMerge node from the logical.Sort. This node was a no-op and only passed down the sort direction to its children.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/planner/physical/planner.go
+++ pkg/engine/planner/physical/planner.go
@@ -270,30 +270,14 @@ func (p *Planner) processSelect(lp *logical.Select, ctx *Context) ([]Node, error
 	return []Node{node}, nil
 }
 
-// Convert [logical.Sort] into one [SortMerge] node.
+// Pass sort direction from [logical.Sort] to the children.
 func (p *Planner) processSort(lp *logical.Sort, ctx *Context) ([]Node, error) {
 	order := DESC
 	if lp.Ascending {
 		order = ASC
 	}
-	node := &SortMerge{
-		Column: &ColumnExpr{Ref: lp.Column.Ref},
-		Order:  order,
-	}
 
-	p.plan.addNode(node)
-
-	children, err := p.process(lp.Table, ctx.WithDirection(order))
-	if err != nil {
-		return nil, err
-	}
-
-	for i := range children {
-		if err := p.plan.addEdge(Edge{Parent: node, Child: children[i]}); err != nil {
-			return nil, err
-		}
-	}
-	return []Node{node}, nil
+	return p.process(lp.Table, ctx.WithDirection(order))
 }
 
 // Convert [logical.Limit] into one [Limit] node.
