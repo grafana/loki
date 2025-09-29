414a94ff9cccfb71b4e1b3547ece84fbfa57a34f
Ashwanth Goli <iamashwanth@gmail.com>
2025-09-29 12:57:03 +0530

    aggregator: support empty groupBy

--- pkg/engine/executor/aggregator.go
+++ pkg/engine/executor/aggregator.go
@@ -31,6 +31,9 @@ type aggregator struct {
 }
 
 // newAggregator creates a new aggregator with the specified groupBy columns.
+// empty groupBy indicates no grouping. All values are aggregated into a single group.
+// TODO: add without argument to support `without(...)` grouping.
+// A special case of `without()` that has empty groupBy is used for Noop grouping which retains the input labels as is.
 func newAggregator(groupBy []physical.ColumnExpression, pointsSizeHint int) *aggregator {
 	a := aggregator{
 		groupBy: groupBy,
@@ -55,19 +58,28 @@ func (a *aggregator) Add(ts time.Time, value int64, labelValues []string) {
 		a.points[ts] = point
 	}
 
-	a.digest.Reset()
-	for i, val := range labelValues {
-		if i > 0 {
-			_, _ = a.digest.Write([]byte{0}) // separator
-		}
+	var key uint64
+	if len(a.groupBy) != 0 {
+		a.digest.Reset()
+		for i, val := range labelValues {
+			if i > 0 {
+				_, _ = a.digest.Write([]byte{0}) // separator
+			}
 
-		_, _ = a.digest.WriteString(val)
+			_, _ = a.digest.WriteString(val)
+		}
+		key = a.digest.Sum64()
 	}
-	key := a.digest.Sum64()
 
 	if state, ok := point[key]; ok {
 		// TODO: handle hash collisions
 		state.value += value
+	} else if len(a.groupBy) == 0 {
+		// special case: All values aggregated into a single group.
+		// This applies to queries like `sum(...)`, `sum by () (...)`, `count_over_time by () (...)`.
+		point[key] = &groupState{
+			value: value,
+		}
 	} else {
 		// create a new slice since labelValues is reused by the calling code
 		labelValuesCopy := make([]string, len(labelValues))
