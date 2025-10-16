3b66c071aecafc00ea5e70a37b9ca40fbe09ad61
Robert Fratto <robertfratto@gmail.com>
2025-10-16 10:47:49 -0400

    chore(engine): write tests for parallel pushdown pass

--- pkg/engine/internal/planner/physical/optimizer_test.go
+++ pkg/engine/internal/planner/physical/optimizer_test.go
@@ -1044,3 +1044,181 @@ func TestProjectionPushdown_PushesRequestedKeysToParseNodes(t *testing.T) {
 		})
 	}
 }
+
+func Test_parallelPushdown(t *testing.T) {
+	t.Run("canPushdown", func(t *testing.T) {
+		tt := []struct {
+			name     string
+			children []Node
+			expected bool
+		}{
+			{
+				name:     "no children",
+				children: nil,
+				expected: false,
+			},
+			{
+				name:     "one child (not Parallelize)",
+				children: []Node{&DataObjScan{}},
+				expected: false,
+			},
+			{
+				name:     "one child (Parallelize)",
+				children: []Node{&Parallelize{}},
+				expected: true,
+			},
+			{
+				name:     "multiple children (all Parallelize)",
+				children: []Node{&Parallelize{}, &Parallelize{}},
+				expected: true,
+			},
+			{
+				name:     "multiple children (not all Parallelize)",
+				children: []Node{&Parallelize{}, &DataObjScan{}},
+				expected: false,
+			},
+		}
+
+		for _, tc := range tt {
+			t.Run(tc.name, func(t *testing.T) {
+				var plan Plan
+				parent := plan.graph.Add(&Filter{})
+
+				for _, child := range tc.children {
+					plan.graph.Add(child)
+					require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: parent, Child: child}))
+				}
+
+				pass := parallelPushdown{plan: &plan}
+				require.Equal(t, tc.expected, pass.canPushdown(parent))
+			})
+		}
+	})
+
+	t.Run("Shifts Filter", func(t *testing.T) {
+		var plan Plan
+		{
+			vectorAgg := plan.graph.Add(&VectorAggregation{})
+			rangeAgg := plan.graph.Add(&RangeAggregation{})
+			filter := plan.graph.Add(&Filter{})
+			parallelize := plan.graph.Add(&Parallelize{})
+			scan := plan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: vectorAgg, Child: rangeAgg}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: rangeAgg, Child: filter}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: filter, Child: parallelize}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: scan}))
+		}
+
+		opt := newOptimizer(&plan, []*optimization{
+			newOptimization("ParallelPushdown", &plan).withRules(&parallelPushdown{plan: &plan}),
+		})
+		root, _ := plan.graph.Root()
+		opt.optimize(root)
+
+		var expectedPlan Plan
+		{
+			vectorAgg := expectedPlan.graph.Add(&VectorAggregation{})
+			rangeAgg := expectedPlan.graph.Add(&RangeAggregation{})
+			parallelize := expectedPlan.graph.Add(&Parallelize{})
+			filter := expectedPlan.graph.Add(&Filter{})
+			scan := expectedPlan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: vectorAgg, Child: rangeAgg}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: rangeAgg, Child: parallelize}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: filter}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: filter, Child: scan}))
+		}
+
+		expected := PrintAsTree(&expectedPlan)
+		require.Equal(t, expected, PrintAsTree(&plan))
+	})
+
+	t.Run("Shifts Parse", func(t *testing.T) {
+		var plan Plan
+		{
+			vectorAgg := plan.graph.Add(&VectorAggregation{})
+			rangeAgg := plan.graph.Add(&RangeAggregation{})
+			parse := plan.graph.Add(&ParseNode{})
+			parallelize := plan.graph.Add(&Parallelize{})
+			scan := plan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: vectorAgg, Child: rangeAgg}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: rangeAgg, Child: parse}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: parse, Child: parallelize}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: scan}))
+		}
+
+		opt := newOptimizer(&plan, []*optimization{
+			newOptimization("ParallelPushdown", &plan).withRules(&parallelPushdown{plan: &plan}),
+		})
+		root, _ := plan.graph.Root()
+		opt.optimize(root)
+
+		var expectedPlan Plan
+		{
+			vectorAgg := expectedPlan.graph.Add(&VectorAggregation{})
+			rangeAgg := expectedPlan.graph.Add(&RangeAggregation{})
+			parallelize := expectedPlan.graph.Add(&Parallelize{})
+			parse := expectedPlan.graph.Add(&ParseNode{})
+			scan := expectedPlan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: vectorAgg, Child: rangeAgg}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: rangeAgg, Child: parallelize}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: parse}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: parse, Child: scan}))
+		}
+
+		expected := PrintAsTree(&expectedPlan)
+		require.Equal(t, expected, PrintAsTree(&plan))
+	})
+
+	t.Run("Splits TopK", func(t *testing.T) {
+		var plan Plan
+		{
+			limit := plan.graph.Add(&Limit{})
+			topk := plan.graph.Add(&TopK{SortBy: &ColumnExpr{}})
+			parallelize := plan.graph.Add(&Parallelize{})
+			scan := plan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: limit, Child: topk}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: topk, Child: parallelize}))
+			require.NoError(t, plan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: scan}))
+		}
+
+		opt := newOptimizer(&plan, []*optimization{
+			newOptimization("ParallelPushdown", &plan).withRules(&parallelPushdown{plan: &plan}),
+		})
+		root, _ := plan.graph.Root()
+
+		// Since [optimization.optimize] does up to three passes,
+		// parallelPushdown must ignore a node after it's already been
+		// processed. Otherwise, it will cause TopK to be sharded three times,
+		// ending up with this plan:
+		//
+		//   TopK
+		//     Parallelize
+		//       TopK # Shard from first iteration
+		//         TopK # Shard from second iteration
+		//           TopK # Shard from third iteration
+		//             DataObjScan
+		opt.optimize(root)
+
+		var expectedPlan Plan
+		{
+			limit := expectedPlan.graph.Add(&Limit{})
+			globalTopK := expectedPlan.graph.Add(&TopK{SortBy: &ColumnExpr{}})
+			parallelize := expectedPlan.graph.Add(&Parallelize{})
+			localTopK := expectedPlan.graph.Add(&TopK{SortBy: &ColumnExpr{}})
+			scan := expectedPlan.graph.Add(&DataObjScan{})
+
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: limit, Child: globalTopK}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: globalTopK, Child: parallelize}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: parallelize, Child: localTopK}))
+			require.NoError(t, expectedPlan.graph.AddEdge(dag.Edge[Node]{Parent: localTopK, Child: scan}))
+		}
+
+		expected := PrintAsTree(&expectedPlan)
+		require.Equal(t, expected, PrintAsTree(&plan))
+	})
+}
