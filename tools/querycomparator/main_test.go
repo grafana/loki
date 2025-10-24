eaa1e68c2da5871d528ab6f364bfd184bf72b244
Ben Clive <ben.clive@grafana.com>
2025-10-24 17:24:08 +0100

    Linting

--- tools/querycomparator/main_test.go
+++ tools/querycomparator/main_test.go
@@ -10,21 +10,25 @@ import (
 )
 
 func TestDebugCmd(t *testing.T) {
-	t.Skip("Test for debugging purposes only")
+	t.Skip("Test for debugging only")
 
-	orgID = ""              // TODO: set org ID for querying remote instances
-	storageBucket = ""      // TODO: set bucket name for local engine support
-	indexStoragePrefix = "" // TODO: set index storage prefix for local engine v1 support
+	storageBucket = "dev-us-central-0-loki-dev-005-data"
+	orgID = "29"
+	indexStoragePrefix = "loki_dev_005_tsdb_index_"
+
+	// start := time.Date(2025, 10, 22, 0, 0, 0, 0, time.UTC)
+	// end := start.Add(24 * time.Hour)
+	// query := "{tenantId=\"9151\", service_name=\"asserts/api-server\"}    |~ \"(?i)relabel rule\" | json  | logfmt | drop __error__, __error_details__  "
 
 	start := time.Date(2025, 10, 24, 0, 0, 0, 0, time.UTC)
 	end := start.Add(45 * time.Minute)
-	query := "{namespace=\"test\"}"
+	query := "{namespace=\"microservices-demo\"} !~ \"debug|DEBUG|info|INFO\" |~ \"error|ERROR|fatal|FATAL\""
 
 	params, err := logql.NewLiteralParams(query, start, end, 0, 0, logproto.BACKWARD, 1000, nil, nil)
 	require.NoError(t, err)
 
-	// Run subcommands as necessary
 	require.NoError(t, doComparison(params, "localhost:3101", "localhost:3102"))
-	require.NoError(t, queryMetastore(params))
-	require.NoError(t, doExecuteLocallyV2(params))
+	// // require.NoError(t, queryMetastore(params))
+
+	// require.NoError(t, doExecuteLocallyV2(params))
 }
