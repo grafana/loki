4d331dec2a5b51ef5ade5f14393fc43b144ce6e3
Paul Rogers <paul.rogers@grafana.com>
2025-10-28 09:56:28 -0400

    no windows check

--- pkg/ingester/wal_windows.go
+++ pkg/ingester/wal_windows.go
@@ -4,7 +4,6 @@ package ingester
 
 import (
 	"syscall"
-	"unsafe"
 )
 
 var (
@@ -14,27 +13,6 @@ var (
 
 // checkDiskUsage returns the disk usage percentage (0.0 to 1.0) for the WAL directory.
 func (w *walWrapper) checkDiskUsage() (float64, error) {
-	dirPath, err := syscall.UTF16PtrFromString(w.cfg.Dir)
-	if err != nil {
-		return 0, err
-	}
-
-	var freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes uint64
-
-	ret, _, err := procGetDiskFreeSpaceEx.Call(
-		uintptr(unsafe.Pointer(dirPath)),
-		uintptr(unsafe.Pointer(&freeBytesAvailable)),
-		uintptr(unsafe.Pointer(&totalNumberOfBytes)),
-		uintptr(unsafe.Pointer(&totalNumberOfFreeBytes)),
-	)
-
-	if ret == 0 {
-		return 0, err
-	}
-
-	// Calculate usage percentage
-	used := totalNumberOfBytes - totalNumberOfFreeBytes
-	usagePercent := float64(used) / float64(totalNumberOfBytes)
-
-	return usagePercent, nil
+	// Disable this for Windows for now
+	return 0.0, nil
 }
