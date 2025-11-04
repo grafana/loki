d7ccb111077a081951e779afbdc6afc1c0b9ff56
Stas Spiridonov <stanislav@spiridonov.pro>
2025-11-04 11:07:55 -0500

    comment

--- pkg/engine/internal/planner/logical/planner.go
+++ pkg/engine/internal/planner/logical/planner.go
@@ -206,7 +206,6 @@ func buildPlanForLogQuery(
 
 	// Metric queries do not apply a limit.
 	if !isMetricQuery {
-		// SORT -> SortMerge
 		// We always sort DESC. ASC timestamp sorting is not supported for logs
 		// queries, and metric queries do not need sorting.
 		builder = builder.TopK(timestampColumnRef(), int(params.Limit()), false, false)
