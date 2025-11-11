eb1fe86b8de88d5744201e5665b57bcd305bce50
Robert Fratto <robertfratto@gmail.com>
2025-11-11 11:27:32 -0500

    chore(engine): use github.com/grafana/dskit/dns for scheduler discovery
    Previously, workers used github.com/grafana/dskit/grpcutil for scheduler
    discovery. This was an older package which, through how we used it,
    didn't permit SRV lookups.
    
    Other code within Loki instead uses the dns package from dskit for
    discovery. This package has better support for SRV lookups and supports
    flags like `dnssrvnoa+` for forcing a SRV lookup with no A records.
    
    This commit updates workers to use this package instead.
    
    As part of this change, the test code has been updated to use
    testing/synctest for more easy and reliably testing of the background
    discovery process.
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>

--- pkg/engine/internal/worker/scheduler_lookup_test.go
+++ pkg/engine/internal/worker/scheduler_lookup_test.go
@@ -2,90 +2,82 @@ package worker
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"net"
 	"sync"
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/go-kit/log"
-	"github.com/grafana/dskit/grpcutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
 )
 
 func Test_schedulerLookup(t *testing.T) {
-	var wg sync.WaitGroup
-	defer wg.Wait()
+	// NOTE(rfratto): synctest makes it possible to reliably test asynchronous
+	// code with time.Sleep.
+	synctest.Test(t, func(t *testing.T) {
+		var wg sync.WaitGroup
+		defer wg.Wait()
 
-	fw := &fakeWatcher{
-		ctx: t.Context(),
-		ch:  make(chan *grpcutil.Update, 1),
-	}
-
-	// Manually create a schedulerLookup so we can hook in a custom
-	// implementation of [grpcutil.Watcher].
-	disc := &schedulerLookup{
-		logger:  log.NewNopLogger(),
-		watcher: fw,
-	}
-
-	var handlers atomic.Int64
-
-	lookupContext, lookupCancel := context.WithCancel(t.Context())
-	defer lookupCancel()
-
-	wg.Add(1)
-	go func() {
-		// Decrement the wait group once Run exits. Run won't exit until all
-		// handlers have terminated, so this validates that logic.
-		defer wg.Done()
-
-		_ = disc.Run(lookupContext, func(ctx context.Context, _ net.Addr) {
-			context.AfterFunc(ctx, func() { handlers.Dec() })
-			handlers.Inc()
-		})
-	}()
-
-	// Emit 10 schedulers, then wait for there to be one handler per
-	// scheduler.
-	for i := range 10 {
-		addr := fmt.Sprintf("127.0.0.%d:8080", i+1)
-		fw.ch <- &grpcutil.Update{Op: grpcutil.Add, Addr: addr}
-	}
-
-	require.Eventually(t, func() bool {
-		return handlers.Load() == 10
-	}, time.Minute, time.Millisecond*10, "should have 10 running handlers, ended with %d", handlers.Load())
-
-	// Delete all the schedulers, then wait for all handlers to terminate (by
-	// context).
-	for i := range 10 {
-		addr := fmt.Sprintf("127.0.0.%d:8080", i+1)
-		fw.ch <- &grpcutil.Update{Op: grpcutil.Delete, Addr: addr}
-	}
-
-	require.Eventually(t, func() bool {
-		return handlers.Load() == 0
-	}, time.Minute, time.Millisecond*10, "should have no handlers running, ended with %d", handlers.Load())
-}
-
-type fakeWatcher struct {
-	ctx context.Context
-	ch  chan *grpcutil.Update
-}
-
-func (fw fakeWatcher) Next() ([]*grpcutil.Update, error) {
-	select {
-	case <-fw.ctx.Done():
-		return nil, fw.ctx.Err()
-	case update, ok := <-fw.ch:
-		if !ok {
-			return nil, errors.New("closed")
+		// Provide 10 addresses to start with.
+		addrs := []string{
+			"127.0.0.1:8080", "127.0.0.2:8080", "127.0.0.3:8080", "127.0.0.4:8080", "127.0.0.5:8080",
+			"127.0.0.6:8080", "127.0.0.7:8080", "127.0.0.8:8080", "127.0.0.9:8080", "127.0.0.10:8080",
 		}
-		return []*grpcutil.Update{update}, nil
-	}
+
+		fr := &fakeProvider{
+			resolveFunc: func(_ context.Context, _ []string) ([]string, error) { return addrs, nil },
+		}
+
+		// Manually create a schedulerLookup so we can hook in a custom
+		// implementation of [grpcutil.Watcher].
+		disc := &schedulerLookup{
+			logger:   log.NewNopLogger(),
+			watcher:  newDNSWatcher("example.com", fr),
+			interval: 1 * time.Minute,
+		}
+
+		var handlers atomic.Int64
+
+		lookupContext, lookupCancel := context.WithCancel(t.Context())
+		defer lookupCancel()
+
+		wg.Go(func() {
+			_ = disc.Run(lookupContext, func(ctx context.Context, _ net.Addr) {
+				context.AfterFunc(ctx, func() { handlers.Dec() })
+				handlers.Inc()
+			})
+		})
+
+		// There should immediately be running handlers without needing to wait
+		// for the discovery interval.
+		synctest.Wait()
+		require.Equal(t, int64(10), handlers.Load(), "should have 10 running handlers")
+
+		// Remove all the addresses from discovery; after the next interval, all
+		// handlers should be removed.
+		addrs = addrs[:0]
+		time.Sleep(disc.interval + time.Second)
+		require.Equal(t, int64(0), handlers.Load(), "should have no running handlers")
+	})
 }
 
-func (fw fakeWatcher) Close() { close(fw.ch) }
+type fakeProvider struct {
+	resolveFunc func(ctx context.Context, addrs []string) ([]string, error)
+
+	cached []string
+}
+
+func (fp *fakeProvider) Resolve(ctx context.Context, addrs []string) error {
+	resolved, err := fp.resolveFunc(ctx, addrs)
+	if err != nil {
+		return err
+	}
+	fp.cached = resolved
+	return nil
+}
+
+func (fp *fakeProvider) Addresses() []string {
+	return fp.cached
+}
