f066d378a51ed796d2239c59b313a5c52bcc1baa
Christian Haudum <christian.haudum@gmail.com>
2025-11-14 08:09:18 +0100

    chore(engine): Do not embed `types.Literal` interface in logical and physical Literal (#19858)
    This ensures that logical.Literal and physical.LiteralExpr do not implicitly implement the types.Literal interface, which caused a bug fixed with #19853
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/engine/internal/planner/logical/node_literal.go
+++ pkg/engine/internal/planner/logical/node_literal.go
@@ -9,37 +9,37 @@ import (
 //
 // The zero value of a Literal is a NULL value.
 type Literal struct {
-	types.Literal
+	inner types.Literal
 }
 
 var _ Value = (*Literal)(nil)
 
-func NewLiteral(value types.LiteralType) *Literal {
+func NewLiteral(value any) *Literal {
 	if value == nil {
-		return &Literal{Literal: types.NewNullLiteral()}
+		return &Literal{inner: types.NewNullLiteral()}
 	}
-	return &Literal{Literal: types.NewLiteral(value)}
+	return &Literal{inner: types.NewLiteral(value)}
 }
 
 // Kind returns the kind of value represented by the literal.
 func (l Literal) Kind() types.DataType {
-	return l.Type()
+	return l.inner.Type()
 }
 
 // Name returns the string form of the literal.
 func (l Literal) Name() string {
-	return l.Literal.String()
+	return l.inner.String()
 }
 
 // String returns a printable form of the literal, even if lit is not a
 // [ValueTypeString].
 func (l Literal) String() string {
-	return l.Literal.String()
+	return l.inner.String()
 }
 
 // Value returns lit's value as untyped interface{}.
 func (l Literal) Value() any {
-	return l.Any()
+	return l.inner.Any()
 }
 
 func (l *Literal) isValue() {}
