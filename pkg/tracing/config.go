86897b7bb3c83779dfcc19fa0b99cc0eac3d83e0
Ashwanth Goli <iamashwanth@gmail.com>
2025-11-27 18:56:47 +0530

    fixup! chore: allow dropping spans generated by gcs storage clients

--- pkg/tracing/config.go
+++ pkg/tracing/config.go
@@ -10,10 +10,10 @@ type Config struct {
 }
 
 func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
-	cfg.RegisterFlagsWithPrefix("", f)
+	cfg.RegisterFlagsWithPrefix("tracing.", f)
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
-	f.BoolVar(&cfg.Enabled, prefix+"tracing.enabled", true, "Set to false to disable tracing.")
-	f.BoolVar(&cfg.FilterGCSSpans, prefix+"tracing.filter-gcs-spans", false, "Set to true to drops all spans from the GCS client library. This prevents the GCS client from creating millions of spans in high-throughput production environments.")
+	f.BoolVar(&cfg.Enabled, prefix+"enabled", true, "Set to false to disable tracing.")
+	f.BoolVar(&cfg.FilterGCSSpans, prefix+"filter-gcs-spans", true, "Set to true to drops all spans from the GCS client library. This prevents the GCS client from creating millions of spans in high-throughput production environments.")
 }
