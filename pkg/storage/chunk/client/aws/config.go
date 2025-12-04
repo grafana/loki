6e93ae97c68be74d25116cc1194c2006a46420d0
Christian Haudum <christian.haudum@gmail.com>
2025-12-04 17:55:51 +0100

    fix: Fix regression in S3 client configuration (#20110)
    ### Summary
    
    This PR fixes a regression in how Loki's S3 configuration options are handled to create the S3 client which was introduced with the upgrade of the AWS SDK from v1 to v2 (#19205).
    
    With the v1 SDK the `s3` field of the `aws` configuration block was parsed to extract credentials, region, host, and schema.
    
    ```
    storage_config:
      aws:
        s3: s3://<accesskey>:<secret>@<endpoint_or_region>/<bucketnames>
    ```
    
    After the upgrade, only credentials were extraced, leaving all other parts of the URL as default, which would require explicit overrides.
    
    This was a breaking change and not documented in the upgrade notes. We want to keep backwards compatibility, even though the Thanos object client is going to supersed the custom S3/GCS/... clients.
    
    ref: #19908
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>
    (cherry picked from commit d3f9532b061c4406bbd5d95c77b5220c0bd80193)

--- pkg/storage/chunk/client/aws/config.go
+++ pkg/storage/chunk/client/aws/config.go
@@ -1,78 +1,12 @@
-// Provenance-includes-location: https://github.com/weaveworks/common/blob/main/aws/config.go
-// Provenance-includes-license: Apache-2.0
-// Provenance-includes-copyright: Weaveworks Ltd.
-
 package aws
 
 import (
-	"fmt"
-	"net"
-	"net/http"
 	"net/url"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-// DynamoConfigFromURL returns AWS config from given URL. It expects escaped
-// AWS Access key ID & Secret Access Key to be encoded in the URL. It
-// also expects region specified as a host (letting AWS generate full
-// endpoint) or fully valid endpoint with dummy region assumed (e.g
-// for URLs to emulated services).
-func DynamoConfigFromURL(awsURL *url.URL) (*dynamodb.Options, error) {
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			MaxIdleConnsPerHost:   100,
-			TLSHandshakeTimeout:   3 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		},
-	}
-	config := dynamodb.Options{HTTPClient: httpClient}
+const InvalidAWSRegion = "dummy"
 
-	// Use a custom http.Client with the golang defaults but also specifying
-	// MaxIdleConnsPerHost because of a bug in golang https://github.com/golang/go/issues/13801
-	// where MaxIdleConnsPerHost does not work as expected.
-
-	if awsURL.User != nil {
-		username := awsURL.User.Username()
-		password, _ := awsURL.User.Password()
-
-		// We request at least the username or password being set to enable the static credentials.
-		if username != "" || password != "" {
-			config.Credentials = credentials.NewStaticCredentialsProvider(username, password, "")
-		}
-	}
-
-	if strings.Contains(awsURL.Host, ".") {
-		region := os.Getenv("AWS_REGION")
-		if region == "" {
-			region = "dummy"
-		}
-		config.Region = region
-		if awsURL.Scheme == "https" {
-			config.BaseEndpoint = aws.String(fmt.Sprintf("https://%s", awsURL.Host))
-		}
-		config.BaseEndpoint = aws.String(fmt.Sprintf("http://%s", awsURL.Host))
-	} else {
-		config.Region = awsURL.Host
-	}
-	// Let AWS generate default endpoint based on region passed as a host in URL.
-	return &config, nil
-}
-
-func CredentialsFromURL(awsURL *url.URL) (key, secret string) {
+func credentialsFromURL(awsURL *url.URL) (key, secret string) {
 	if awsURL.User != nil {
 		username := awsURL.User.Username()
 		password, _ := awsURL.User.Password()
