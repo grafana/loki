e6df9af776edf43583c10e32d6aa34085ce44d5a
Salva Corts <salva.corts@grafana.com>
2025-12-12 15:28:49 +0100

    fix other test

--- pkg/util/marshal/legacy/marshal_test.go
+++ pkg/util/marshal/legacy/marshal_test.go
@@ -226,6 +226,16 @@ var queryTests = []struct {
 						"requests": 0,
 						"downloadTime": 0,
 						"queryLengthServed": 0
+					},
+					"positiveResult": {
+						"entriesFound": 0,
+						"entriesRequested": 0,
+						"entriesStored": 0,
+						"bytesReceived": 0,
+						"bytesSent": 0,
+						"requests": 0,
+						"downloadTime": 0,
+						"queryLengthServed": 0
 					}
 				},
 				"summary": {
