8e07c4a8671b093c710a8bb26c3ea961e985b8f3
Ben Clive <ben.clive@grafana.com>
2026-02-02 11:34:04 +0000

    feat(enginev2): Implement substr compute function

--- pkg/compute/utf8_test.go
+++ pkg/compute/utf8_test.go
@@ -97,7 +97,7 @@ func BenchmarkSubstrInsensitive(b *testing.B) {
 	alloc := memory.NewAllocator(nil)
 	line := strings.Repeat("A", 100) + "target" + strings.Repeat("B", 100)
 	haystack := columnar.NewUTF8([]byte(line), []int32{0, int32(len(line))}, memory.NewBitmap(alloc, 1))
-	needle := columnartest.Scalar(b, columnar.KindUTF8, "TaRgEt")
+	needle := columnartest.Scalar(b, columnar.KindUTF8, "target")
 
 	var totalSize int
 	for i := range haystack.Len() {
@@ -108,6 +108,107 @@ func BenchmarkSubstrInsensitive(b *testing.B) {
 	for b.Loop() {
 		benchAlloc.Reclaim()
 		_, _ = SubstrInsensitive(benchAlloc, haystack, needle)
+	}
+	b.SetBytes(int64(totalSize))
+}
+
+func TestSubstr(t *testing.T) {
+	alloc := memory.NewAllocator(nil)
+
+	singleValueHaystack := columnartest.Array(t, columnar.KindUTF8, alloc, "test")
+	nulllValueHaystack := columnartest.Array(t, columnar.KindUTF8, alloc, nil)
+	emptyHaystack := columnartest.Array(t, columnar.KindUTF8, alloc)
+
+	emptyNeedle := columnartest.Scalar(t, columnar.KindUTF8, "")
+	nullValueNeedle := columnartest.Scalar(t, columnar.KindUTF8, nil)
+	singleValueNeedle := columnartest.Scalar(t, columnar.KindUTF8, "test")
+	singleUnknownValueNeedle := columnartest.Scalar(t, columnar.KindUTF8, "notest")
+	singleUpperCaseValueNeedle := columnartest.Scalar(t, columnar.KindUTF8, "TEST")
+
+	singleTrue := columnartest.Array(t, columnar.KindBool, alloc, true)
+	singleFalse := columnartest.Array(t, columnar.KindBool, alloc, false)
+	emptyResult := columnartest.Array(t, columnar.KindBool, alloc)
+	nullResult := columnartest.Array(t, columnar.KindBool, alloc, nil)
+
+	cases := []struct {
+		name     string
+		haystack columnar.Datum
+		needle   columnar.Datum
+		expected columnar.Datum
+	}{
+		{
+			name:     "empty_haystack",
+			haystack: emptyHaystack,
+			needle:   singleValueNeedle,
+			expected: emptyResult,
+		},
+		{
+			name:     "empty_needle",
+			haystack: singleValueHaystack,
+			needle:   emptyNeedle,
+			expected: singleTrue,
+		},
+		{
+			name:     "match",
+			haystack: singleValueHaystack,
+			needle:   singleValueNeedle,
+			expected: singleTrue,
+		},
+		{
+			name:     "not match",
+			haystack: singleValueHaystack,
+			needle:   singleUnknownValueNeedle,
+			expected: singleFalse,
+		},
+		{
+			name:     "case insensitive match",
+			haystack: singleValueHaystack,
+			needle:   singleUpperCaseValueNeedle,
+			expected: singleFalse,
+		},
+		{
+			name:     "nil_haystack",
+			haystack: nulllValueHaystack,
+			needle:   singleValueNeedle,
+			expected: nullResult,
+		},
+		{
+			name:     "nil_needle",
+			haystack: singleValueHaystack,
+			needle:   nullValueNeedle,
+			expected: nullResult,
+		},
+		{
+			name:     "nil_haystack_and_needle",
+			haystack: nulllValueHaystack,
+			needle:   nullValueNeedle,
+			expected: nullResult,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			results, err := Substr(alloc, c.haystack, c.needle)
+			require.NoError(t, err)
+			columnartest.RequireDatumsEqual(t, c.expected, results)
+		})
+	}
+}
+
+func BenchmarkSubstr(b *testing.B) {
+	alloc := memory.NewAllocator(nil)
+	line := strings.Repeat("A", 100) + "target" + strings.Repeat("B", 100)
+	haystack := columnar.NewUTF8([]byte(line), []int32{0, int32(len(line))}, memory.NewBitmap(alloc, 1))
+	needle := columnartest.Scalar(b, columnar.KindUTF8, "TaRgEt")
+
+	var totalSize int
+	for i := range haystack.Len() {
+		totalSize += len(haystack.Get(i))
+	}
+
+	benchAlloc := memory.NewAllocator(nil)
+	for b.Loop() {
+		benchAlloc.Reclaim()
+		_, _ = Substr(benchAlloc, haystack, needle)
 	}
 	b.SetBytes(int64(totalSize))
 }
