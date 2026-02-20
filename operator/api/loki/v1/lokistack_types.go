eb543ce89fb7d2e7cb78480bb5c65d75d91ee3f7
Joao Marcal <jmarcal@redhat.com>
2026-02-20 16:56:48 +0000

    build(operator): migrate operator from opertator-sdk v1.37.0 to v1.39.2

--- operator/api/loki/v1/lokistack_types.go
+++ operator/api/loki/v1/lokistack_types.go
@@ -738,7 +738,7 @@ type BlockedQueryTypes []BlockedQueryType
 
 // BlockedQuerySpec defines the rule spec for queries to be blocked.
 //
-// +kubebuilder:validation:MinProperties:=1
+// +kubebuilder:validation:AtLeastOneOf=Hash;Pattern
 type BlockedQuerySpec struct {
 	// Hash is a 32-bit FNV-1 hash of the query string.
 	//
