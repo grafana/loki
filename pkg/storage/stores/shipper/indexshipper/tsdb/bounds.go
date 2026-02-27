147ac738a7c4e06b25005a6d11e68febe32dc4b7
Bryan Boreham <bjboreham@gmail.com>
2026-02-27 11:51:16 +0000

    chore(tsdb): remove unused function inclusiveBounds
    Not used outside of a test.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- pkg/storage/stores/shipper/indexshipper/tsdb/bounds.go
+++ pkg/storage/stores/shipper/indexshipper/tsdb/bounds.go
@@ -1,8 +1,6 @@
 package tsdb
 
 import (
-	"math"
-
 	"github.com/grafana/loki/v3/pkg/storage/stores/shipper/indexshipper/tsdb/index"
 	"github.com/prometheus/common/model"
 )
@@ -10,22 +8,6 @@ import (
 // TODO(chaudum): Replace with new v1.Interval struct
 type Bounded interface {
 	Bounds() (model.Time, model.Time)
-}
-
-// InclusiveBounds will ensure the underlying Bounded implementation
-// is turned into [lower,upper] inclusivity.
-// Generally, we consider bounds to be `[lower,upper)` inclusive
-// This helper will account for integer overflow.
-// Because model.Time is millisecond-precise, but Loki uses nanosecond precision,
-// be careful usage can handle an extra millisecond being added.
-func inclusiveBounds(b Bounded) (model.Time, model.Time) {
-	lower, upper := b.Bounds()
-
-	if int64(upper) < math.MaxInt64 {
-		upper++
-	}
-
-	return lower, upper
 }
 
 type bounds struct {
