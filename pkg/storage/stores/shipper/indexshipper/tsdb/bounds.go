3b9074d16376ef3698e1d8160187721e50a23bdd
Bryan Boreham <bjboreham@gmail.com>
2026-02-27 11:24:45 +0000

    perf(tsdb): reduce memory allocations in chunk overlap checking
    Split `Overlap` into two functions taking concrete types, to avoid a lot
    of small memory allocations because both `bounds` and `ChunkMeta` are
    passed by value and interfaces require a pointer.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- pkg/storage/stores/shipper/indexshipper/tsdb/bounds.go
+++ pkg/storage/stores/shipper/indexshipper/tsdb/bounds.go
@@ -3,6 +3,7 @@ package tsdb
 import (
 	"math"
 
+	"github.com/grafana/loki/v3/pkg/storage/stores/shipper/indexshipper/tsdb/index"
 	"github.com/prometheus/common/model"
 )
 
@@ -35,11 +36,21 @@ func newBounds(mint, maxt model.Time) bounds { return bounds{mint: mint, maxt: m
 
 func (b bounds) Bounds() (model.Time, model.Time) { return b.mint, b.maxt }
 
-// Overlap checks whether the given chunk or index bounds
+// overlapIndex checks whether the given index bounds
 // overlap with the bounds of a query range.
 // chunk/index bounds are defined as [from, through]
 // query bounds are defined as [from, through)
-func Overlap(chk, qry Bounded) bool {
+func overlapIndex(index Index, qry Bounded) bool {
+	idxFrom, idxThrough := index.Bounds()
+	qryFrom, qryThrough := qry.Bounds()
+
+	return idxFrom < qryThrough && idxThrough >= qryFrom
+}
+
+// Same as overlapIndex but for ChunkMeta. The code is repeated for performance:
+// attempting to share the implementation via the Bounded interface results in a lot of
+// small memory allocations because ChunkMeta is passed by value and interfaces require a pointer.
+func overlapChunk(chk index.ChunkMeta, qry bounds) bool {
 	chkFrom, chkThrough := chk.Bounds()
 	qryFrom, qryThrough := qry.Bounds()
 
