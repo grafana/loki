82be5b52af7d1be3f2fdff3124852a7253e30eb7
Ashwanth Goli <iamashwanth@gmail.com>
2026-03-05 19:10:31 +0530

    mmap section refs

--- pkg/storage/stores/shipper/indexshipper/tsdb/index/sectionref/table_test.go
+++ pkg/storage/stores/shipper/indexshipper/tsdb/index/sectionref/table_test.go
@@ -1,6 +1,8 @@
 package sectionref
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -58,4 +60,100 @@ func TestSectionRefTableEncodePathTooLong(t *testing.T) {
 
 	_, err := tbl.Encode()
 	require.ErrorIs(t, err, ErrSectionRefPathTooLong)
+}
+
+func TestMmapSectionRefTable_FromBytes(t *testing.T) {
+	tbl := NewSectionRefTable(nil)
+	tbl.Add(SectionRef{Path: "objects/ab/obj001", SectionID: 0, SeriesID: 1})
+	tbl.Add(SectionRef{Path: "objects/ab/obj001", SectionID: 1, SeriesID: 2})
+	tbl.Add(SectionRef{Path: "objects/cd/obj002", SectionID: 0, SeriesID: 5})
+	tbl.Add(SectionRef{Path: "objects/ab/obj001", SectionID: 0, SeriesID: 1}) // dedupe
+
+	data, err := tbl.Encode()
+	require.NoError(t, err)
+
+	mmaped, err := NewMmapSectionRefTableFromBytes(data)
+	require.NoError(t, err)
+	defer mmaped.Close()
+
+	require.Equal(t, tbl.Len(), mmaped.Len())
+
+	for i := 0; i < tbl.Len(); i++ {
+		want, ok := tbl.Lookup(uint32(i))
+		require.True(t, ok)
+		got, ok := mmaped.Lookup(uint32(i))
+		require.True(t, ok)
+		require.Equal(t, want, got, "mismatch at index %d", i)
+	}
+
+	_, ok := mmaped.Lookup(uint32(tbl.Len()))
+	require.False(t, ok, "out-of-bounds lookup should return false")
+}
+
+func TestMmapSectionRefTable_OpenMmap(t *testing.T) {
+	tbl := NewSectionRefTable(nil)
+	tbl.Add(SectionRef{Path: "s3://bucket/a", SectionID: 1, SeriesID: 4})
+	tbl.Add(SectionRef{Path: "s3://bucket/b", SectionID: 2, SeriesID: 5})
+
+	data, err := tbl.Encode()
+	require.NoError(t, err)
+
+	path := filepath.Join(t.TempDir(), "test.sections")
+	require.NoError(t, os.WriteFile(path, data, 0o644))
+
+	mmaped, err := OpenMmap(path)
+	require.NoError(t, err)
+	defer mmaped.Close()
+
+	require.Equal(t, tbl.Len(), mmaped.Len())
+
+	for i := 0; i < tbl.Len(); i++ {
+		want, ok := tbl.Lookup(uint32(i))
+		require.True(t, ok)
+		got, ok := mmaped.Lookup(uint32(i))
+		require.True(t, ok)
+		require.Equal(t, want, got)
+	}
+}
+
+func BenchmarkLookup_Decode(b *testing.B) {
+	tbl := buildBenchTable(b)
+	data, err := tbl.Encode()
+	require.NoError(b, err)
+
+	decoded, err := Decode(data)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		decoded.Lookup(uint32(i % decoded.Len()))
+	}
+}
+
+func BenchmarkLookup_Mmap(b *testing.B) {
+	tbl := buildBenchTable(b)
+	data, err := tbl.Encode()
+	require.NoError(b, err)
+
+	mmaped, err := NewMmapSectionRefTableFromBytes(data)
+	require.NoError(b, err)
+	defer mmaped.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mmaped.Lookup(uint32(i % mmaped.Len()))
+	}
+}
+
+func buildBenchTable(tb testing.TB) *SectionRefTable {
+	tb.Helper()
+	tbl := NewSectionRefTable(nil)
+	for i := 0; i < 10000; i++ {
+		tbl.Add(SectionRef{
+			Path:      "objects/ab/obj001",
+			SectionID: i % 50,
+			SeriesID:  i,
+		})
+	}
+	return tbl
 }
