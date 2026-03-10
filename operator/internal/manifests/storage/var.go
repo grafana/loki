036344d598708d7a5ecaa73415546240b34f5234
Robert Jacob <rojacob@redhat.com>
2026-03-10 18:40:07 +0100

    fix(operator): provide AWS region in environment variable for STS mode

--- operator/internal/manifests/storage/var.go
+++ operator/internal/manifests/storage/var.go
@@ -7,6 +7,8 @@ const (
 	EnvAlibabaCloudAccessKeyID = "ALIBABA_CLOUD_ACCESS_KEY_ID"
 	// EnvAlibabaCloudAccessKeySecret is the environment variable to specify the AlibabaCloud client secret to access S3.
 	EnvAlibabaCloudAccessKeySecret = "ALIBABA_CLOUD_ACCESS_KEY_SECRET"
+	// EnvAWSRegion configures the AWS region the client uses for operations
+	EnvAWSRegion = "AWS_REGION"
 	// EnvAWSAccessKeyID is the environment variable to specify the AWS client id to access S3.
 	EnvAWSAccessKeyID = "AWS_ACCESS_KEY_ID"
 	// EnvAWSAccessKeySecret is the environment variable to specify the AWS client secret to access S3.
