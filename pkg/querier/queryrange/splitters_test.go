35be63b0fe00a0cb7708fea0486859a952c1b5f4
Ben Clive <ben.clive@grafana.com>
2026-03-27 17:22:14 +0000

    fix: Preserve caching options after query split

---
+++ pkg/querier/queryrange/splitters_test.go
@@ -0,0 +1,25 @@
+package queryrange
+
+import (
+	"testing"
+	"time"
+
+	"github.com/grafana/loki/v3/pkg/storage/chunk/cache/resultscache"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_PreservesCachingOptions(t *testing.T) {
+	splitter := newDefaultSplitter(fakeLimits{}, nil)
+	req := &LokiRequest{
+		StartTs: time.Date(2026, 3, 30, 0, 0, 0, 0, time.UTC).Add(-3 * time.Hour),
+		EndTs:   time.Date(2026, 3, 30, 0, 0, 0, 0, time.UTC),
+		CachingOptions: resultscache.CachingOptions{
+			Disabled: true,
+		},
+	}
+	splitReqs := splitter.split(time.Now().UTC(), []string{"1"}, req, time.Hour)
+	require.Len(t, splitReqs, 3)
+	for _, splitReq := range splitReqs {
+		require.Equal(t, splitReq.GetCachingOptions(), req.GetCachingOptions())
+	}
+}
