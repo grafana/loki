b04458332724b87dff26797993d9eb3883399773
Robert Fratto <robertfratto@gmail.com>
2026-03-30 14:59:57 -0400

    chore(dataset): add dataset/array, dataset/buffer packages
    Introduce two new packages:
    
    * dataset/array, which encodes and decodes a hierarchical sequence of
      values
    * dataset/buffer, which hold opaque handles to encoded data.
    
    Writers accept a stream of columnar.Array and encode them into a dataset
    Array, while Readers accept a single dataset Array and decodes N values
    into the corresponding columnar.Array.
    
    Encoding and decoding is hierarchical to allow different encoding
    strategies for different parts of an Array. For example, this can permit
    choosing between plain encoding and a RLE representation for the
    validity buffer, while leaving the top-level values buffer as a plain
    bitpacked array.
    
    This initial commit introduces one encoding, Bool, which encodes a
    bitpacked sequence of boolean values. It supports nullable and
    non-nullable types. When a type is nullable, the final child Array is
    used for the validity buffer (which is also a Bool type).

---
+++ pkg/dataset/array/spec.go
@@ -0,0 +1,37 @@
+package array
+
+// Spec describes how to encode an [Array]'s data.
+//
+// [Encoding] is the counterpart to Spec which is used for reading the
+// resulting Array.
+//
+// While [EncodingKind] categorizes the encoding, the invidiual Encoding
+// implementations can hold additionally encoding-specific metadata that is
+// used for encoding and decoding.
+type Spec interface {
+	isSpec() // Sealed interface.
+
+	// Kind returns the encoding kind of this Spec.
+	Kind() EncodingKind
+}
+
+// Spec definitions.
+type (
+	// SpecBool encodes bit-packed boolean values.
+	SpecBool struct {
+		// Spec for how to encode validity data. Must only be set for nullable
+		// data types.
+		Validity Spec
+	}
+)
+
+// Kind returns [EncodingKindBool].
+func (spec *SpecBool) Kind() EncodingKind {
+	return EncodingKindBool
+}
+
+//
+// Sealed marker implementations.
+//
+
+func (spec *SpecBool) isSpec() {}
