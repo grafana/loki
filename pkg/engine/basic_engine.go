225de8466ce1323b7009125a0eec3ebc46303c22
Robert Fratto <robertfratto@gmail.com>
2026-04-21 10:40:34 -0400

    chore: change warning when query was executed with new engine

--- pkg/engine/basic_engine.go
+++ pkg/engine/basic_engine.go
@@ -270,7 +270,7 @@ func (e *Basic) Execute(ctx context.Context, params logql.Params) (logqlmodel.Re
 		"duration_full", durFull,
 	)
 
-	metadataCtx.AddWarning("Query was executed using the new experimental query engine and dataobj storage.")
+	metadataCtx.AddWarning("Query was executed using the next-generation Loki query engine.")
 	span.SetStatus(codes.Ok, "")
 	return builder.Build(stats, metadataCtx), nil
 }
