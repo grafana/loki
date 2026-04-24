fc8438c0094834001ecef2af0e695b17888f517f
Christian Haudum <christian.haudum@gmail.com>
2026-04-24 14:10:48 +0200

    chore(indexgateway): Remove dead code
    Detected using [staticcheck](https://staticcheck.dev)
    
    ```console
    $ staticcheck -checks=U1000 ./pkg/indexgateway/...
    pkg/indexgateway/client.go:405:6: func isUnimplementedCallError is unused (U1000)
    pkg/indexgateway/gateway.go:704:6: type failingIndexClient is unused (U1000)
    pkg/indexgateway/gateway.go:706:29: func failingIndexClient.QueryPages is unused (U1000)
    pkg/indexgateway/gateway.go:710:29: func failingIndexClient.Stop is unused (U1000)
    ```

--- pkg/indexgateway/gateway.go
+++ pkg/indexgateway/gateway.go
@@ -13,7 +13,6 @@ import (
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/services"
 	"github.com/grafana/dskit/tenant"
-	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
@@ -700,11 +699,3 @@ func (r refWithSizingInfo) Cmp(chk tsdb_index.ChunkMeta) iter.Ord {
 
 	return iter.Eq
 }
-
-type failingIndexClient struct{}
-
-func (f failingIndexClient) QueryPages(_ context.Context, _ []seriesindex.Query, _ seriesindex.QueryPagesCallback) error {
-	return errors.New("index client is not initialized likely due to boltdb-shipper not being used")
-}
-
-func (f failingIndexClient) Stop() {}
