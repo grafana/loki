1b2fb040959634e53bf3856117a71984603ed308
George Robinson <george.robinson@grafana.com>
2026-04-25 00:52:39 +0100

    feat: add NoCancelProducer to kafkav2
    This commit adds a new type, NoCancelProducer, which fixes some
    undesirable behavior in franz-go where the cancelation of a single
    user-request (context) can cancel all other buffered records in the
    same partition(s).

--- pkg/kafkav2/util_test.go
+++ pkg/kafkav2/util_test.go
@@ -1,6 +1,8 @@
 package kafkav2
 
 import (
+	"context"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -21,4 +23,34 @@ func mustKafkaClient(t *testing.T, seed string, opts ...kgo.Opt) *kgo.Client {
 	require.NoError(t, err)
 	t.Cleanup(client.Close)
 	return client
+}
+
+type produceNoPromise struct{}
+
+// A recordingProducer records the args of all calls.
+type recordingProducer struct {
+	calls []any
+	mtx   sync.Mutex
+}
+
+// While Produce is supposed to be asynchronous, here the promise is called
+// synchronously. An optional value produceNoPromise can be passed via ctx
+// which tells it to not call the promise.
+func (p *recordingProducer) Produce(ctx context.Context, r *kgo.Record, promise func(r *kgo.Record, err error)) {
+	p.mtx.Lock()
+	p.calls = append(p.calls, []any{"Produce", ctx, r, promise})
+	p.mtx.Unlock()
+	if promise != nil && ctx.Value(produceNoPromise{}) == nil {
+		promise(r, nil)
+	}
+}
+
+// TryProduce has the same behavior as Produce here.
+func (p *recordingProducer) TryProduce(ctx context.Context, r *kgo.Record, promise func(r *kgo.Record, err error)) {
+	p.mtx.Lock()
+	p.calls = append(p.calls, []any{"TryProduce", ctx, r, promise})
+	p.mtx.Unlock()
+	if promise != nil && ctx.Value(produceNoPromise{}) == nil {
+		promise(r, nil)
+	}
 }
