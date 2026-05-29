f0d2dea559b3737a54369eab3eb1070956d9d5e4
Christian Haudum <christian.haudum@gmail.com>
2026-05-29 17:59:06 +0200

    fixup! chore: Remove references to index_queries_cache_config

--- integration/loki_micro_services_test.go
+++ integration/loki_micro_services_test.go
@@ -905,7 +905,6 @@ func TestCategorizedLabels(t *testing.T) {
 		tIndexGateway = clu.AddComponent(
 			"index-gateway",
 			"-target=index-gateway",
-			"-store.index-cache-read.embedded-cache.enabled=true",
 		)
 	)
 	require.NoError(t, clu.Run())
