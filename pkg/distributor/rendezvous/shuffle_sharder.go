bc75e15e8d9f8ccc21073300d78c0c9599ae2b5b
Dan Hopper <dan.hopper@grafana.com>
2026-06-03 10:45:05 +0100

    Defensive programming - create a copy of the partitions slice in NewShuffleSharder

--- pkg/distributor/rendezvous/shuffle_sharder.go
+++ pkg/distributor/rendezvous/shuffle_sharder.go
@@ -21,11 +21,13 @@ type ShuffleSharder struct {
 }
 
 func NewShuffleSharder(partitions []int32) ShuffleSharder {
+	partitionsCopy := make([]int32, len(partitions))
 	hashes := make([]uint64, len(partitions))
 	for i, partition := range partitions {
+		partitionsCopy[i] = partition
 		hashes[i] = xxhash.Sum64String("loki-distributor-" + strconv.Itoa(int(partition)))
 	}
-	return ShuffleSharder{partitions, hashes}
+	return ShuffleSharder{partitionsCopy, hashes}
 }
 
 func (r *ShuffleSharder) Shard(key uint32) (int32, error) {
