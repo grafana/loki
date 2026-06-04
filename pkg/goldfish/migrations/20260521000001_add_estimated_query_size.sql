efbc6b67571de656b91f37ba0c77956278362d7e
Ivan Kalita <ivan.kalita@grafana.com>
2026-06-04 16:33:20 +0200

    fix(goldfish): Fix migration typo (#22272)
    (cherry picked from commit bd4eab223cbe3a7d4d4eb467c43e5c687b006780)

--- pkg/goldfish/migrations/20260521000001_add_estimated_query_size.sql
+++ pkg/goldfish/migrations/20260521000001_add_estimated_query_size.sql
@@ -3,7 +3,7 @@
 -- This is the estimated size of the query as returned by query stats. Defaults to 0 if unknown.
 
 ALTER TABLE sampled_queries
-ADD COLUMN cell_a_estimated_query_size BIGINT NOT NULL DEFAULT 0
+ADD COLUMN cell_a_estimated_query_size BIGINT NOT NULL DEFAULT 0,
 ADD COLUMN cell_b_estimated_query_size BIGINT NOT NULL DEFAULT 0;
 
 -- +goose Down
