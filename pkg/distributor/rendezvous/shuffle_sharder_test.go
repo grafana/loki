6ca8cd1277d3839c9d10fbf3a45a7059d01e7dad
Dan Hopper <dan.hopper@grafana.com>
2026-06-04 11:05:41 +0100

    Add benchmarks and stability unit test
    Benchmarks:
    ```
    BenchmarkShuffleShard
    BenchmarkShuffleShard/n=200,k1=200,k2=200
    BenchmarkShuffleShard/n=200,k1=200,k2=200-12         	   70806	     15539 ns/op
    BenchmarkShuffleShard/n=200,k1=190,k2=180
    BenchmarkShuffleShard/n=200,k1=190,k2=180-12         	   78914	     15409 ns/op
    BenchmarkShuffleShard/n=200,k1=20,k2=10
    BenchmarkShuffleShard/n=200,k1=20,k2=10-12           	  137419	      8435 ns/op
    BenchmarkShuffleShard/n=200,k1=100,k2=50
    BenchmarkShuffleShard/n=200,k1=100,k2=50-12          	  105848	     11688 ns/op
    BenchmarkShuffleShard/n=1000,k1=990,k2=980
    BenchmarkShuffleShard/n=1000,k1=990,k2=980-12        	   10000	    115228 ns/op
    BenchmarkShuffleShard/n=1000,k1=500,k2=250
    BenchmarkShuffleShard/n=1000,k1=500,k2=250-12        	   16069	     73546 ns/op
    BenchmarkShuffleShard/n=1000,k1=20,k2=10
    BenchmarkShuffleShard/n=1000,k1=20,k2=10-12          	   24585	     48835 ns/op
    PASS
    ```

--- pkg/distributor/rendezvous/shuffle_sharder_test.go
+++ pkg/distributor/rendezvous/shuffle_sharder_test.go
@@ -82,3 +82,47 @@ func TestShuffleShard_MorePartitionsThanRequested(t *testing.T) {
 		assert.InDelta(t, expected, counts[p], tolerance, "partition %d: appeared %d times, expected ~%d", p, counts[p], expected)
 	}
 }
+
+func TestShuffleShard_Stability(t *testing.T) {
+	partitions := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	s := NewShuffleSharder(partitions)
+	previousSubPartitions := make([]int32, 0)
+	for i := 1; i <= 10; i++ {
+		newSubPartitions := s.ShuffleShard("foo", i)
+		// Verify the new shard contains all partitions from the previous one
+		for _, p := range previousSubPartitions {
+			assert.Contains(t, newSubPartitions.partitions, p, "shard of size %d should contain all partitions from shard of size %d", i, i-1)
+		}
+		previousSubPartitions = newSubPartitions.partitions
+	}
+}
+
+func benchmarkShuffleShard(b *testing.B, partitionCount int, tenantShuffleShardSize int, segmentationKeyShuffleShardSize int) {
+	b.Run(fmt.Sprintf("n=%d,k1=%d,k2=%d", partitionCount, tenantShuffleShardSize, segmentationKeyShuffleShardSize), func(b *testing.B) {
+		require.GreaterOrEqual(b, partitionCount, tenantShuffleShardSize)
+		require.GreaterOrEqual(b, tenantShuffleShardSize, segmentationKeyShuffleShardSize)
+		partitions := make([]int32, partitionCount)
+		for i := range partitionCount {
+			partitions[i] = int32(i)
+		}
+		shuffleSharder := NewShuffleSharder(partitions)
+		b.ResetTimer()
+		for range b.N {
+			shuffleSharderLayer1 := shuffleSharder.ShuffleShard("foo", tenantShuffleShardSize)
+			shuffleSharderLayer2 := shuffleSharderLayer1.ShuffleShard("bar", segmentationKeyShuffleShardSize)
+			_, err := shuffleSharderLayer2.Shard(123)
+			require.NoError(b, err)
+		}
+	})
+}
+
+func BenchmarkShuffleShard(b *testing.B) {
+	benchmarkShuffleShard(b, 200, 200, 200)
+	benchmarkShuffleShard(b, 200, 190, 180)
+	benchmarkShuffleShard(b, 200, 20, 10)
+	benchmarkShuffleShard(b, 200, 100, 50)
+
+	benchmarkShuffleShard(b, 1000, 990, 980)
+	benchmarkShuffleShard(b, 1000, 500, 250)
+	benchmarkShuffleShard(b, 1000, 20, 10)
+}
