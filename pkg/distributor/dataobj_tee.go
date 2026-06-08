092bf984b7fed7b07ce49dafb268f62179c18a82
Dan Hopper <dan.hopper@grafana.com>
2026-06-08 10:35:16 +0100

    Correct handling of tenantRateBytes, and rename it to tenantRateLimitBytes

--- pkg/distributor/dataobj_tee.go
+++ pkg/distributor/dataobj_tee.go
@@ -213,10 +213,10 @@ func (t *DataObjTee) Duplicate(ctx context.Context, tenant string, streams []Key
 	}
 }
 
-func (t *DataObjTee) duplicate(ctx context.Context, tenant string, stream segmentedStream, rateBytes, tenantRateBytes uint64, pushTracker *PushTracker) {
+func (t *DataObjTee) duplicate(ctx context.Context, tenant string, stream segmentedStream, rateBytes, tenantRateBytesLimit uint64, pushTracker *PushTracker) {
 	t.streams.Inc()
 
-	partition, err := t.resolver.Resolve(tenant, stream.SegmentationKey, stream.HashKey, rateBytes, tenantRateBytes)
+	partition, err := t.resolver.Resolve(tenant, stream.SegmentationKey, stream.HashKey, rateBytes, tenantRateBytesLimit)
 	if err != nil {
 		level.Error(t.logger).Log("msg", "failed to resolve partition", "err", err)
 		t.streamFailures.Inc()
