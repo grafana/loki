grafana/loki

commit 351222259cf154ba863609591aaaf2a3ed643afe

chore: Use local version info to reduce dependencies (#22281)
By declaring one small VersionInfo struct locally we can lose a lot of dependencies from Prometheus code.

pkg/util/build/build.go
@@ -2,15 +2,9 @@ package build
 
 import (
 	"runtime"
-
-	"github.com/prometheus/common/version"
-	prom "github.com/prometheus/prometheus/web/api/v1"
 )
 
-// Version information passed to Prometheus version package.
-// Package path as used by linker changes based on vendoring being used or not,
-// so it's easier just to use stable Loki path, and pass it to
-// Prometheus in the code.
+// These variables are set from the Makefile.
 var (
 	Version   string
 	Revision  string
@@ -21,21 +15,25 @@ var (
 )
 
 func init() {
-	version.Version = Version
-	version.Revision = Revision
-	version.Branch = Branch
-	version.BuildUser = BuildUser
-	version.BuildDate = BuildDate
-	version.GoVersion = runtime.Version()
+	GoVersion = runtime.Version()
 }
 
-func GetVersion() prom.PrometheusVersion {
-	return prom.PrometheusVersion{
-		Version:   version.Version,
-		Revision:  version.Revision,
-		Branch:    version.Branch,
-		BuildUser: version.BuildUser,
-		BuildDate: version.BuildDate,
-		GoVersion: version.GoVersion,
+func GetVersion() VersionInfo {
+	return VersionInfo{
+		Version:   Version,
+		Revision:  Revision,
+		Branch:    Branch,
+		BuildUser: BuildUser,
+		BuildDate: BuildDate,
+		GoVersion: GoVersion,
 	}
+}
+
+type VersionInfo struct {
+	Version   string `json:"version"`
+	Revision  string `json:"revision"`
+	Branch    string `json:"branch"`
+	BuildUser string `json:"buildUser"`
+	BuildDate string `json:"buildDate"`
+	GoVersion string `json:"goVersion"`
 }

commit c8e99110b44d101e08b20912a55a0484844b279a

bugfix: Copy build info to Prometheus common library
The values there are read by Prometheus client_golang to populate the
`loki_build_info` metric. This got broken by #22281.

pkg/util/build/build.go
@@ -2,6 +2,8 @@ package build
 
 import (
 	"runtime"
+
+	"github.com/prometheus/common/version"
 )
 
 // These variables are set from the Makefile.
@@ -15,6 +17,12 @@ var (
 )
 
 func init() {
+	// Copy settings for this build into Prometheus common package, where they are fetched by Prometheus client_golang.
+	version.Version = Version
+	version.Revision = Revision
+	version.Branch = Branch
+	version.BuildUser = BuildUser
+	version.BuildDate = BuildDate
 	GoVersion = runtime.Version()
 }
 