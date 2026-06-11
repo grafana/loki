bb658c0adb1ae71510d20a07286f05237fbf1280
shantanualshi <shantanu.alshi@grafana.com>
2026-06-11 17:47:53 +0530

    Rename config to read_postings_sections

--- pkg/dataobj/metastore/config.go
+++ pkg/dataobj/metastore/config.go
@@ -7,9 +7,9 @@ import (
 
 // Config is the configuration block for the metastore settings.
 type Config struct {
-	IndexStoragePrefix  string `yaml:"index_storage_prefix" experimental:"true"`
-	PartitionRatio      int    `yaml:"partition_ratio" experimental:"true"`
-	UsePostingsSections bool   `yaml:"use_postings_sections" experimental:"true"`
+	IndexStoragePrefix   string `yaml:"index_storage_prefix" experimental:"true"`
+	PartitionRatio       int    `yaml:"partition_ratio" experimental:"true"`
+	ReadPostingsSections bool   `yaml:"read_postings_sections" experimental:"true"`
 }
 
 // RegisterFlags registers the flags for the metastore settings.
@@ -17,7 +17,7 @@ func (c *Config) RegisterFlags(f *flag.FlagSet) {
 	prefix := "dataobj-metastore."
 	f.StringVar(&c.IndexStoragePrefix, prefix+"index-storage-prefix", "index/v0", "Experimental: A prefix to use for storing indexes in object storage. Used for testing only.")
 	f.IntVar(&c.PartitionRatio, prefix+"partition-ratio", 10, "Experimental: The ratio of log partitions to metastore partitions. For example, a value of 10 means there is 1 metastore partition for every 10 log partitions.")
-	f.BoolVar(&c.UsePostingsSections, prefix+"use-postings-sections", false, "Experimental: When enabled, reads from new-format postings sections in index objects instead of the streams sections. Defaults to false.")
+	f.BoolVar(&c.ReadPostingsSections, prefix+"read-postings-sections", false, "Experimental: When enabled, reads from new-format postings sections in index objects instead of the streams sections. Defaults to false.")
 }
 
 // Validate validates the metastore settings.
