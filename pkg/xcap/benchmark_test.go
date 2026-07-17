f7c26f6de37e8ba6e3a3d28cd85035b88647a579
ashwanthgoli <iamashwanth@gmail.com>
2026-07-17 15:35:26 +0530

    fixup! intern statistics

--- pkg/xcap/benchmark_test.go
+++ pkg/xcap/benchmark_test.go
@@ -25,49 +25,21 @@ func BenchmarkCaptureUnmarshalBinary(b *testing.B) {
 	}
 }
 
-func BenchmarkCaptureMerge(b *testing.B) {
-	src := benchmarkCapture(b)
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for b.Loop() {
-		_, dst := NewCapture(context.Background(), nil)
-		dst.Merge(nil, src)
-		benchmarkRegions = len(dst.Regions())
-	}
-}
-
-func BenchmarkCaptureValue(b *testing.B) {
-	capture := benchmarkCapture(b)
-	stat := NewStatisticInt64("benchmark.sum", AggregationTypeSum)
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for b.Loop() {
-		benchmarkValue = Value[int64](capture, stat)
-	}
-}
-
 var (
 	benchmarkRegions int
-	benchmarkValue   int64
 )
 
 func benchmarkCapture(b *testing.B) *Capture {
 	b.Helper()
 
 	ctx, capture := NewCapture(context.Background(), nil)
-	sum := NewStatisticInt64("benchmark.sum", AggregationTypeSum)
-	min := NewStatisticFloat64("benchmark.min", AggregationTypeMin)
-	flag := NewStatisticFlag("benchmark.flag")
 
-	for i := range 12 {
+	for r := range 4 {
 		var region *Region
-		ctx, region = StartRegion(ctx, fmt.Sprintf("region-%d", i))
-		for j := range 32 {
-			region.Record(sum.Observe(int64(i + j)))
-			region.Record(min.Observe(float64(i + j)))
-			region.Record(flag.Observe(j%2 == 0))
+		ctx, region = StartRegion(ctx, fmt.Sprintf("region-%d", r))
+		for s := range 32 {
+			stat := NewStatisticInt64(fmt.Sprintf("stat.%d", s), AggregationTypeSum)
+			region.Record(stat.Observe(int64(s+1) << 20))
 		}
 		region.End()
 	}
