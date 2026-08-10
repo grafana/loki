5f01edaa7202dac7915fb566f072fb76a7fd3ada
George Robinson <george.robinson@grafana.com>
2026-08-10 11:10:42 +0100

    chore: Move logging to its own method logPushRequestStreams

--- pkg/distributor/http.go
+++ pkg/distributor/http.go
@@ -1,6 +1,7 @@
 package distributor
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/dustin/go-humanize"
+	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/httpgrpc"
 
@@ -19,6 +21,7 @@ import (
 
 	"github.com/grafana/dskit/tenant"
 
+	push2 "github.com/grafana/loki/pkg/push"
 	"github.com/grafana/loki/v3/pkg/loghttp/push"
 	util_log "github.com/grafana/loki/v3/pkg/util/log"
 	"github.com/grafana/loki/v3/pkg/validation"
@@ -129,34 +132,7 @@ func (d *Distributor) pushHandler(w http.ResponseWriter, r *http.Request, pushRe
 	// Only reported for tenants that have enabled log_otlp_attribute_expansion in their runtime config.
 	d.otlpAttrReporter.Report(logger, tenantID, pushStats.OTLPAttributes)
 
-	if d.shouldLogPushRequestStreams(tenantID, presumedAgentIP) {
-		for _, s := range req.Streams {
-			lbs, err := syntax.ParseLabels(s.Labels)
-			if err != nil {
-				// We just log the error and continue, we need the parsed labels to log the policy.
-				// In this case, the lbs will be empty and the policy will be empty.
-				level.Error(logger).Log("msg", "error parsing labels before logging push request", "err", err)
-			}
-
-			logValues := []interface{}{
-				"msg", "push request streams",
-				"stream", s.Labels,
-				"streamLabelsHash", util.HashedQuery(s.Labels), // this is to make it easier to do searching and grouping
-				"streamSizeBytes", humanize.Bytes(uint64(pushStats.StreamSizeBytes[s.Labels])),
-				"policy", streamResolver.PolicyFor(r.Context(), lbs),
-			}
-			if timestamp, ok := pushStats.MostRecentEntryTimestampPerStream[s.Labels]; ok {
-				logValues = append(logValues, "mostRecentLagMs", time.Since(timestamp).Milliseconds())
-			}
-			if presumedAgentIP != "" {
-				logValues = append(logValues, "presumedAgentIp", presumedAgentIP)
-			}
-			if pushStats.HashOfAllStreams != 0 {
-				logValues = append(logValues, "hashOfAllStreams", pushStats.HashOfAllStreams)
-			}
-			level.Debug(logger).Log(logValues...)
-		}
-	}
+	d.logPushRequestStreams(r.Context(), logger, tenantID, req.Streams, streamResolver, pushStats, presumedAgentIP)
 
 	_, err = d.PushWithResolver(r.Context(), req, streamResolver, format)
 	if err == nil {
@@ -208,6 +184,48 @@ func (d *Distributor) shouldLogPushRequestStreams(tenantID, presumedAgentIP stri
 		}
 	}
 	return true
+}
+
+// logPushRequestStreams logs all streams in the push request. This must be enabled
+// on a per-tenant basis.
+func (d *Distributor) logPushRequestStreams(
+	ctx context.Context,
+	logger log.Logger,
+	tenantID string,
+	streams []push2.Stream,
+	streamResolver *requestScopedStreamResolver,
+	pushStats *push.Stats,
+	presumedAgentIP string,
+) {
+	if !d.shouldLogPushRequestStreams(tenantID, presumedAgentIP) {
+		return
+	}
+	for _, s := range streams {
+		lbs, err := syntax.ParseLabels(s.Labels)
+		if err != nil {
+			// We just log the error and continue, we need the parsed labels to log the policy.
+			// In this case, the lbs will be empty and the policy will be empty.
+			level.Error(logger).Log("msg", "error parsing labels before logging push request", "err", err)
+		}
+
+		logValues := []interface{}{
+			"msg", "push request streams",
+			"stream", s.Labels,
+			"streamLabelsHash", util.HashedQuery(s.Labels), // this is to make it easier to do searching and grouping
+			"streamSizeBytes", humanize.Bytes(uint64(pushStats.StreamSizeBytes[s.Labels])),
+			"policy", streamResolver.PolicyFor(ctx, lbs),
+		}
+		if timestamp, ok := pushStats.MostRecentEntryTimestampPerStream[s.Labels]; ok {
+			logValues = append(logValues, "mostRecentLagMs", time.Since(timestamp).Milliseconds())
+		}
+		if presumedAgentIP != "" {
+			logValues = append(logValues, "presumedAgentIp", presumedAgentIP)
+		}
+		if pushStats.HashOfAllStreams != 0 {
+			logValues = append(logValues, "hashOfAllStreams", pushStats.HashOfAllStreams)
+		}
+		level.Debug(logger).Log(logValues...)
+	}
 }
 
 // ServeHTTP implements the distributor ring status page.
