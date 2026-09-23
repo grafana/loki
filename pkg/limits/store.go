d24a174e868c69030015ef067614e7052e325184
George Robinson <george.robinson@grafana.com>
2026-09-23 14:07:56 +0100

    chore: Remove UpdateRates RPC from the limits service

--- pkg/limits/store.go
+++ pkg/limits/store.go
@@ -269,40 +269,6 @@ func (s *usageStore) UpdateCond(tenant string, metadata []*proto.StreamMetadata,
 	return toProduce, accepted, rejected, nil
 }
 
-func (s *usageStore) UpdateRates(tenant string, metadata []*proto.StreamMetadata, seenAt time.Time) ([]streamUsage, error) {
-	if !s.withinActiveWindow(seenAt.UnixNano()) {
-		return nil, errOutsideActiveWindow
-	}
-	var (
-		updated = make([]streamUsage, 0, len(metadata))
-		cutoff  = seenAt.Add(-s.activeWindow).UnixNano()
-	)
-	s.withLock(tenant, func(i int) {
-		for _, m := range metadata {
-			partition := s.getPartitionForHash(m.StreamHash)
-			s.checkInitMap(i, tenant, partition, noPolicy)
-			streams := s.stripes[i][tenant][partition][noPolicy]
-			stream, ok := streams[m.StreamHash]
-
-			// If the stream does not exist, or exists but has expired,
-			// we need to check if accepting it would exceed the maximum
-			// stream limit.
-			if !ok || stream.lastSeenAt < cutoff {
-				if ok {
-					// The stream has expired, delete it so it doesn't count
-					// towards the active streams.
-					delete(streams, m.StreamHash)
-				}
-			}
-			s.updateWithBuckets(i, tenant, partition, noPolicy, m, seenAt)
-			got, _ := s.get(i, tenant, partition, m.StreamHash)
-			got.rateBuckets = getActiveRateBuckets(got.rateBuckets, s.withinRateWindow)
-			updated = append(updated, got)
-		}
-	})
-	return updated, nil
-}
-
 // Evict evicts all streams that have not been seen within the window.
 func (s *usageStore) Evict() map[string]int {
 	cutoff := s.clock.Now().Add(-s.activeWindow).UnixNano()
